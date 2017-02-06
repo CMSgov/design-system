8ece980c229de704609a0bba19835d3d97959b60
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-02-06 13:56:48 -0500

    Use mixture of 18f/USWDS/Primer stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,10 @@
-// Stylelint config based on github.com/primer/stylelint-config-primer
+// Selected rules based on @18F/stylelint-rules and stylelint-config-primer
 module.exports = {
   "rules": {
+    "at-rule-empty-line-before": [ "always", {
+      except: [ "blockless-after-blockless", "first-nested" ],
+      ignore: ["after-comment"],
+    } ],
     "at-rule-name-case": "lower",
     "at-rule-name-space-after": "always-single-line",
     "at-rule-semicolon-newline-after": "always",
@@ -38,13 +42,14 @@ module.exports = {
       }
     ],
     "declaration-block-no-shorthand-property-overrides": true,
-    "declaration-block-semicolon-newline-after": "always",
+    "declaration-block-semicolon-newline-after": "always-multi-line",
+    "declaration-block-semicolon-space-after": "always-single-line",
     "declaration-block-semicolon-space-before": "never",
     "declaration-block-single-line-max-declarations": 1,
     "declaration-block-trailing-semicolon": "always",
-    "declaration-colon-newline-after": "always-multi-line",
     "declaration-colon-space-after": "always-single-line",
     "declaration-colon-space-before": "never",
+    "declaration-no-important": true,
     "declaration-property-value-blacklist": {
       "/^transition/": [
         "/all/"
@@ -60,44 +65,47 @@ module.exports = {
         "initial"
       ]
     },
+    "declaration-property-value-whitelist": {
+      "/color/": ["/(\@|\#)/"]
+    },
     "font-family-name-quotes": "always-where-recommended",
     "function-calc-no-unspaced-operator": true,
-    "function-comma-newline-after": "always-multi-line",
     "function-comma-space-after": "always-single-line",
     "function-comma-space-before": "never",
     "function-linear-gradient-no-nonstandard-direction": true,
     "function-max-empty-lines": 0,
     "function-name-case": "lower",
-    "function-parentheses-newline-inside": "always-multi-line",
     "function-parentheses-space-inside": "never-single-line",
     "function-url-quotes": "always",
     "function-whitespace-after": "always",
     "indentation": 2,
     "length-zero-no-unit": true,
     "max-empty-lines": 1,
-    "max-nesting-depth": 3,
+    "max-nesting-depth": 4,
     "media-feature-colon-space-after": "always",
     "media-feature-colon-space-before": "never",
-    "media-feature-parentheses-space-inside": "never",
+    "media-feature-parentheses-space-inside": "always",
     "media-feature-range-operator-space-after": "always",
     "media-feature-range-operator-space-before": "always",
-    "media-query-list-comma-newline-after": "always-multi-line",
     "media-query-list-comma-space-after": "always-single-line",
     "media-query-list-comma-space-before": "never",
     "no-duplicate-selectors": true,
     "no-eol-whitespace": true,
     "no-extra-semicolons": true,
+    "no-invalid-double-slash-comments": true,
     "no-missing-end-of-source-newline": true,
     "number-leading-zero": "always",
     "number-no-trailing-zeros": true,
     "property-case": "lower",
     "property-no-vendor-prefix": true,
+    "rule-empty-line-before": [ "always-multi-line", {
+      ignore: ["after-comment"],
+    } ],
     "selector-attribute-brackets-space-inside": "never",
     "selector-attribute-operator-space-after": "never",
     "selector-attribute-operator-space-before": "never",
     "selector-combinator-space-after": "always",
     "selector-combinator-space-before": "always",
-    "selector-list-comma-newline-after": "always",
     "selector-list-comma-space-before": "never",
     "selector-max-compound-selectors": 3,
     "selector-max-empty-lines": 0,
@@ -112,7 +120,7 @@ module.exports = {
     "selector-type-case": "lower",
     "shorthand-property-no-redundant-values": true,
     "string-no-newline": true,
-    "string-quotes": "double",
+    "string-quotes": "single",
     "unit-case": "lower",
     "unit-no-unknown": true,
     "value-list-comma-newline-after": "always-multi-line",
