5f225f5fc1341bbb92462bb551b228eefd2fa8aa
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-02-14 10:54:32 -0500

    Add KSS documentation workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,25 @@ module.exports = function(grunt) {
     .forEach(grunt.loadNpmTasks);
 
   grunt.initConfig({
+    browserSync: { // Documentation server
+      dev: {
+        bsFiles: {
+          src : ['docs/**/*.css', 'docs/**/*.html']
+        },
+        options: {
+          server: {
+            baseDir: "./docs"
+          },
+          notify: false,
+          open: false,
+          watchTask: true
+        }
+      }
+    },
     copy: {
       fonts: {
+        // TODO(sawyer) Figure out a better way of managing these font files,
+        // since it seems unecessary to have the same files in src/ and dist/
         files: [
           {
             expand: true,
@@ -16,9 +33,22 @@ module.exports = function(grunt) {
             filter: 'isFile'
           }
         ]
+      },
+      docs: {
+        // Copy the compiled files into the docs site so they can be
+        // referenced using relative URLs
+        files: [
+          {
+            expand: true,
+            cwd: 'dist',
+            src: ['fonts/*', 'styles/all.css'],
+            dest: 'docs/dist/',
+            filter: 'isFile'
+          }
+        ]
       }
     },
-    eslint: {
+    eslint: { // Lint JavaScript
       src: {
         files: [{
           expand: true,
@@ -27,7 +57,16 @@ module.exports = function(grunt) {
         }]
       }
     },
-    less: {
+    kss: { // Documentation generation
+      options: {
+        css: '/dist/styles/all.css'
+      },
+      dist: {
+        src: ['src/styles/'],
+        dest: 'docs'
+      }
+    },
+    less: { // Preprocess our CSS
       src: {
         options: {
           paths: ['src/styles']
@@ -41,11 +80,11 @@ module.exports = function(grunt) {
         }]
       }
     },
-    postcss: {
+    postcss: { // Transfrom the preprocessed CSS
       options: {
         processors: [
           require('postcss-import')(), // inline imports
-          require('autoprefixer')(),
+          require('autoprefixer')(), // add any necessary vender prefixes
           require('cssnano')() // minify the result
         ]
       },
@@ -53,24 +92,35 @@ module.exports = function(grunt) {
         src: 'dist/styles/**/*.css'
       }
     },
-    stylelint: {
+    stylelint: { // Lint CSS
       src: ['src/styles/**/*.less'],
       options: {
         reportNeedlessDisables: true,
         syntax: 'less'
       }
     },
-    watch: {
+    watch: { // Run corresponding tasks when certain files change
+      docs: {
+        files: ['dist/styles/**/*.css'],
+        tasks: ['build:docs'],
+      },
       scripts: {
         files: ['src/scripts/**/*.js', 'src/scripts/**/*.jsx'],
         tasks: ['eslint'],
       },
       styles: {
         files: ['src/styles/**/*.less'],
-        tasks: ['stylelint', 'less', 'postcss'],
+        tasks: ['stylelint', 'build:css'],
       }
     },
   });
 
-  grunt.registerTask('build', ['eslint', 'stylelint', 'less', 'postcss', 'copy']);
+  // Grouped tasks
+  grunt.registerTask('lint', ['eslint', 'stylelint']);
+  grunt.registerTask('build:css', ['less', 'postcss']);
+  grunt.registerTask('build:docs', ['copy:docs', 'kss']);
+
+  // Executable tasks
+  grunt.registerTask('default', ['browserSync', 'watch']);
+  grunt.registerTask('build', ['lint', 'copy:fonts', 'build:css', 'build:docs']);
 };
