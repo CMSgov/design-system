078b10f918deb7f82f8aeb4dc0a9bfa8ccbad3c0
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-03-06 18:21:17 -0500

    Added initial set of of utility classes
    Typography, background colors, and layout

--- config/stylelint.js
+++ config/stylelint.js
@@ -119,7 +119,7 @@ module.exports = {
     'no-extra-semicolons': true,
     'no-invalid-double-slash-comments': true,
     'no-missing-end-of-source-newline': true,
-    'number-leading-zero': 'never',
+    'number-leading-zero': 'always',
     'number-no-trailing-zeros': true,
     'property-case': 'lower',
     'property-no-vendor-prefix': true,
