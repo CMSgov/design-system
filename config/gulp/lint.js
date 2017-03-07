185068c3e861ba75a349272b21a0c6611fa6d893
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-03-07 17:19:19 -0500

    Add stylelint-order; Move config to root
    Moved to root to work better with SublimeLinter

--- config/gulp/lint.js
+++ config/gulp/lint.js
@@ -2,7 +2,17 @@ const argv = require('yargs').argv;
 const count = require('gulp-count');
 const eslint = require('gulp-eslint');
 const stylelint = require('gulp-stylelint');
-const stylelintConfig = require('../stylelint');
+const stylelintConfig = require('../../stylelint.config');
+
+/**
+ * Class naming pattern
+ * ~~~~~~~~~~~~~~~~~~~
+ * Namespaced with: ds-
+ * Followed by a prefix: l- c- u- is- has-
+ * Followed by BEM syntax: [BLOCK]__[ELEMENT]--[MODIFIER]
+ * Examples: .ds-c-button--primary, .ds-c-card__title, .ds-u-text-underlined
+ */
+const systemNamePattern = /^(ds-)(l|c|u|is|has|)(-[a-z0-9]+)((--?|__)[a-z0-9]+)*$/;
 
 module.exports = (gulp) => {
   // Lint Sass files using stylelint. Further configuration for CSS linting
@@ -10,9 +20,9 @@ module.exports = (gulp) => {
   function lintSass(cwd, enforceNamePattern = true) {
     let config = stylelintConfig;
 
-    if (!enforceNamePattern) {
+    if (enforceNamePattern) {
       config = Object.assign({}, config);
-      config.rules['selector-class-pattern'] = null;
+      config.rules['selector-class-pattern'] = systemNamePattern;
     }
 
     return gulp
