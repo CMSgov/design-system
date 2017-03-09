c1c4fa2a53dce0d5b68a90405a243b1d56031015
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-03-09 18:07:42 -0500

    Catch react-docgen error caused by generated component

--- config/gulp/common/react-docgen.js
+++ config/gulp/common/react-docgen.js
@@ -2,6 +2,7 @@
  * Creates a Gulp-friendly implementation of react-docgen. Takes a stream
  * of React component files and returns a stream of JSON documentation files.
  */
+const dutil = require('../doc-util');
 const gUtil = require('gulp-util');
 const reactDocgen = require('react-docgen');
 const path = require('path');
@@ -15,18 +16,24 @@ function getPropertyName(nameAfter, filePath) {
 
 module.exports = function(options) {
   options = options || {};
+  let response = {};
 
   return through.obj((file, encoding, cb) => {
-    if (file.isNull()) return cb(null, file);
+    try {
+      if (file.isNull()) return cb(null, file);
 
-    let doc = reactDocgen.parse(file.contents);
-    // Reduce filesize by removing properties we don't need
-    delete doc.methods;
+      let doc = reactDocgen.parse(file.contents);
+      // Reduce filesize by removing properties we don't need
+      delete doc.methods;
 
-    // Assign the doc object to a unique property so we can
-    // merge the JSON files in another stream
-    let response = {};
-    response[getPropertyName(options.nameAfter, file.path)] = doc;
+      // Assign the doc object to a unique property so we can
+      // merge the JSON files in another stream
+      response[getPropertyName(options.nameAfter, file.path)] = doc;
+    } catch(e) {
+      dutil.logError('react-docgen', e);
+      dutil.logData('react-docgen', file.path);
+      response = {};
+    }
 
     file.contents = new Buffer(JSON.stringify(response));
     file.path = gUtil.replaceExtension(file.path, '.json');
