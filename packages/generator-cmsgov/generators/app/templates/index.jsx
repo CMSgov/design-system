b715af30c5091215b98885f3fa5a834776a905a6
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-03-09 12:39:48 -0500

    Add Yeoman generator for creating a core component

---
+++ packages/generator-cmsgov/generators/app/templates/index.jsx
@@ -0,0 +1,15 @@
+/*import React from 'react';
+
+class <%= name %> extends React.Component {
+  render() {
+    return (
+      // TODO: Enter JSX here
+    );
+  }
+}
+
+<%= name %>.displayName = '<%= name %>';
+<%= name %>.propTypes = {
+};
+
+export default <%= name %>;*/
