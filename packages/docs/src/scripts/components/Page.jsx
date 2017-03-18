1b4b4dc4a8e79dc871bba1b76aefd85646a6a251
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-03-18 16:56:37 -0400

    Check for sections before testing length

--- packages/docs/src/scripts/components/Page.jsx
+++ packages/docs/src/scripts/components/Page.jsx
@@ -7,7 +7,7 @@ import PageBlock from './PageBlock';
 
 class Page extends React.Component {
   childSections() {
-    if (this.props.sections.length && this.props.depth >= 2) {
+    if (this.props.sections && this.props.sections.length && this.props.depth >= 2) {
       return this.props.sections.map(section => (
         <PageBlock key={section.referenceURI} {...section} />
       ));
