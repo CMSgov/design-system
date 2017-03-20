08452ce22c921759f9db109f95908d9870b74980
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-03-19 21:49:45 -0400

    Fix pages array not being reset on each call

--- config/gulp/docs/convertMarkdownPages.js
+++ config/gulp/docs/convertMarkdownPages.js
@@ -7,11 +7,9 @@
 const fs = require('mz/fs');
 const path = require('path');
 const processMarkdownPage = require('./processMarkdownPage');
-
 const pagesDir = path.resolve(__dirname, '../../../packages/docs/src/pages');
-let pages = [];
 
-function convertPage(filename, rootPath) {
+function convertPage(pages, filename, rootPath) {
   return fs.readFile(path.join(pagesDir, filename), 'utf8')
     .then(data => {
       pages.push(processMarkdownPage(filename, data, rootPath));
@@ -19,12 +17,14 @@ function convertPage(filename, rootPath) {
 }
 
 function convertMarkdownPages(rootPath) {
+  let pages = [];
+
   // Loop through files in the pages directory. If we wanted to support nested
   // pages in the future, this will need recursive support added.
   return fs.readdir(pagesDir)
     .then(filenames => {
       return Promise.all(
-        filenames.map(page => convertPage(page, rootPath))
+        filenames.map(page => convertPage(pages, page, rootPath))
       );
     })
     .then(() => pages);
