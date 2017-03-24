abf14800fcabe8734086b024eca41dd83b6c1b1a
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-03-24 17:12:12 -0400

    Add master check to publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,6 +8,11 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 NC='\033[0m' # No color
 
+echo "${GREEN}Making sure we have the latest from master...${NC}"
+git stash
+git checkout master
+git pull
+
 # Publish public packages to NPM
 echo "${GREEN}Publishing core package to NPM...${NC}"
 cd packages/core
@@ -16,3 +21,4 @@ npm publish
 echo "${ORANGE}[TODO] Publish docs${NC}"
 
 echo "${GREEN}Publishing Succeeded!${NC}"
+git stash apply
