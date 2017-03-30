a307125ae41cfbf0b90d8365ded3c51b1d3ad405
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-03-30 12:14:40 -0400

    Update button React tests

--- packages/core/src/components/Button/Button.test.jsx
+++ packages/core/src/components/Button/Button.test.jsx
@@ -1,89 +1,111 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-addons-test-utils';
+import {shallow} from 'enzyme';
+
 import Button from './Button.jsx';
 
-describe('Button', function() {
-  var labelText = 'required label';
+describe('Button', () => {
+  const buttonText = 'required label';
 
   function testDisabledState(disabled) {
-    var onClickMock = jest.fn();
-    var props = {
-      label: labelText,
+    const onClickMock = jest.fn();
+    const expectedCallCount = disabled ? 0 : 1;
+    const props = {
       onClick: onClickMock,
       disabled: disabled
     };
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
 
-    var button = ReactTestUtils.renderIntoDocument(
-      React.createElement(Button, props));
+    wrapper.simulate('click');
 
-    var buttonElement = ReactDOM.findDOMNode(
-      ReactTestUtils.findRenderedDOMComponentWithTag(button, 'button'));
-
-    expect(buttonElement.textContent).toBe(labelText);
-
-    // We expect the button to reflect its disabled state
-    expect(buttonElement.disabled).toBe(disabled);
-
-    // We also expect the button to have the 'disabled' classname as
-    // appropriate for styling.
-    var index = buttonElement.className.indexOf('disabled');
-    var hasDisabledClassName = index !== -1;
-    expect(hasDisabledClassName).toBe(disabled);
-
-    // We finally expect clicks to appropriate trigger the click handler.
-    ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(button));
-
-    var expectedCallCount = disabled ? 0 : 1;
+    expect(wrapper.text()).toBe(buttonText);
+    expect(wrapper.prop('disabled')).toBe(disabled);
+    expect(wrapper.hasClass('ds-c-button--disabled')).toBe(disabled);
     expect(onClickMock.mock.calls.length).toBe(expectedCallCount);
   }
 
-  it('appears disabled', function() {
+  it('appears disabled', () => {
     testDisabledState(true);
   });
 
-  it('appears enabled', function() {
+  it('appears enabled', () => {
     testDisabledState(false);
   });
 
-  it('renders as a green primary button', function() {
-    var props = {
-      label: labelText
-    };
-
-    var button = ReactTestUtils.renderIntoDocument(
-      React.createElement(Button, props));
-
-    var buttonElement = ReactDOM.findDOMNode(
-      ReactTestUtils.findRenderedDOMComponentWithTag(button, 'button'));
-
-    // Expect the button to be green
-    var isGreen = buttonElement.className.indexOf('ds-c-button-success') !== -1;
-    expect(isGreen).toBe(true);
-
-    // Expect the button to not be blue
-    var isBlue = buttonElement.className.indexOf('ds-c-button-blue') !== -1;
-    expect(isBlue).toBe(false);
+  it('renders as button', () => {
+    const wrapper = shallow(<Button>{buttonText}</Button>);
+    expect(wrapper.prop('type')).toBe('button');
   });
 
-  it('renders as a blue secondary button', function() {
-    var props = {
-      label: labelText,
-      use: 'secondary'
+  it('renders as submit button', () => {
+    const props = {type: 'submit'};
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
+    expect(wrapper.prop('type')).toBe('submit');
+  });
+
+  it('appends additional class names', () => {
+    const props = {className: 'foobar'};
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
+    expect(wrapper.hasClass('foobar')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button')).toBe(true);
+  });
+
+  it('renders as a success button', () => {
+    const props = {modifier: 'success'};
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
+
+    expect(wrapper.hasClass('ds-c-button')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button--success')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button--primary')).toBe(false);
+  });
+
+  it('renders as a small button', () => {
+    const props = {size: 'small'};
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
+
+    expect(wrapper.hasClass('ds-c-button')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button--small')).toBe(true);
+  });
+
+  it('overrides modifier class when disabled', () => {
+    const props = {
+      disabled: true,
+      modifier: 'primary'
     };
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
 
-    var button = ReactTestUtils.renderIntoDocument(
-      React.createElement(Button, props));
+    expect(wrapper.hasClass('ds-c-button--disabled')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button--primary')).toBe(false);
+  });
 
-    var buttonElement = ReactDOM.findDOMNode(
-      ReactTestUtils.findRenderedDOMComponentWithTag(button, 'button'));
+  it('applies inverse suffix to modifier class', () => {
+    const props = {
+      inverse: true,
+      modifier: 'transparent'
+    };
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
 
-    // Expect the button to be blue
-    var isBlue = buttonElement.className.indexOf('ds-c-button-blue') !== -1;
-    expect(isBlue).toBe(true);
+    expect(wrapper.hasClass('ds-c-button--transparent-inverse')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button--transparent')).toBe(false);
+    expect(wrapper.hasClass('ds-c-button')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button--inverse')).toBe(false);
+  });
 
-    // Expect the button to not be green
-    var isGreen = buttonElement.className.indexOf('ds-c-button-success') !== -1;
-    expect(isGreen).toBe(false);
+  it('applies inverse suffix to disabled class', () => {
+    const props = {
+      inverse: true,
+      disabled: true
+    };
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
+
+    expect(wrapper.hasClass('ds-c-button--disabled-inverse')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button')).toBe(true);
+  });
+
+  it('applies inverse suffix to default button class', () => {
+    const props = {inverse: true};
+    const wrapper = shallow(<Button {...props}>{buttonText}</Button>);
+
+    expect(wrapper.hasClass('ds-c-button--inverse')).toBe(true);
+    expect(wrapper.hasClass('ds-c-button')).toBe(true);
   });
 });
