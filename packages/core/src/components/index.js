7ed03d2082836399cc449defa7169280c4d84a62
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-04-14 12:03:24 -0400

    Add test to ensure components are exported in index.js

--- packages/core/src/components/index.js
+++ packages/core/src/components/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 export * from './Alert/Alert';
+export * from './Badge/Badge';
 export * from './Button/Button';
 export * from './ChoiceList/Choice';
 export * from './Form/FormLabel';
