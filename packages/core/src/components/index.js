ef9ec5de0efcedf712002d4d726badc1b3e56c88
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-04-14 13:00:47 -0400

    Export ChoiceList and FormField

--- packages/core/src/components/index.js
+++ packages/core/src/components/index.js
@@ -4,5 +4,7 @@ export * from './Alert/Alert';
 export * from './Badge/Badge';
 export * from './Button/Button';
 export * from './ChoiceList/Choice';
+export * from './ChoiceList/ChoiceList';
+export * from './Form/FormField';
 export * from './Form/FormLabel';
 export * from './Select/Select';
