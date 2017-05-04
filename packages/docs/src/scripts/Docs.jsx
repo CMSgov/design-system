ca25274171b7c8fa569fef4f0c26e871dcf74829
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-05-04 15:34:50 -0400

    Add header

--- packages/docs/src/scripts/Docs.jsx
+++ packages/docs/src/scripts/Docs.jsx
@@ -1,11 +1,16 @@
-import Nav from './Nav';
-import Page from './Page';
+/**
+ * This is main template file for the documentation site.
+ */
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Page from './components/Page';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 const Docs = (props) => {
   return (
     <div>
+      <Header />
       <Nav
         currentPageURI={props.page.referenceURI}
         routes={props.routes}
---
+++ packages/docs/src/scripts/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='ds-u-border-bottom--1'>
+      <div className='ds-u-fill--warn-lightest ds-u-padding-x--3 ds-u-padding-y--1 ds-u-font-size--small'>
+        <strong>Alpha status</strong>:
+        The design system is under active development and working towards a 1.0 release.
+      </div>
+      <div className='ds-u-padding--3'>
+        <h1 className='ds-h3'>CMS.gov Design System</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
