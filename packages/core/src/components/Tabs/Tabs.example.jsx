1ac99aae67a6472f7a3b4fb5b341ed4ccab068b9
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-05-15 17:26:08 -0400

    Add responsive tab styles and guidance

--- packages/core/src/components/Tabs/Tabs.example.jsx
+++ packages/core/src/components/Tabs/Tabs.example.jsx
@@ -6,11 +6,14 @@ import Tabs from './Tabs';
 export default function() {
   return (
     <Tabs defaultSelectedId='panel-2'>
-      <TabPanel id='panel-1' tab='Tab 1'>
+      <TabPanel id='panel-1' tab='Profile'>
         Panel content for Tab 1
       </TabPanel>
-      <TabPanel id='panel-2' tab='Tab 2'>
+      <TabPanel id='panel-2' tab='Preferences'>
         Panel content for Tab 2
+      </TabPanel>
+      <TabPanel id='panel-3' tab='Advanced'>
+        Panel content for Tab 3
       </TabPanel>
     </Tabs>
   );
