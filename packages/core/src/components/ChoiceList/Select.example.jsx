d23cc95000c4fca6a1bfc2562c88131677cb70e6
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-06-29 10:52:54 -0400

    Update Select guidance

--- packages/core/src/components/ChoiceList/Select.example.jsx
+++ packages/core/src/components/ChoiceList/Select.example.jsx
@@ -8,6 +8,10 @@ export default () => {
       <option value='1'>Option 1</option>
       <option value='2'>Option 2</option>
       <option value='3'>Option 3</option>
+      <option value='4'>Option 4</option>
+      <option value='5'>Option 5</option>
+      <option value='6'>Option 6</option>
+      <option value='7'>Option 7</option>
     </Select>
   );
 };
