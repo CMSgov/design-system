2aeddd25b61705ff878eadf8c02d1cb20ac403ac
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-07-06 22:49:51 -0400

    Remove scrollbars from iframes

--- tools/gulp/docs/generatePage.js
+++ tools/gulp/docs/generatePage.js
@@ -103,7 +103,7 @@ function generateMarkupPage(page, modifier, rootPath) {
   <title>Example: ${page.reference}</title>
   <link rel="stylesheet" href="/${rootPath}public/styles/example.css" />
 </head>
-<body class="ds-base ds-u-padding--1">
+<body class="ds-base">
   ${markup}
   <script type="text/javascript">
     var id = '${id}';
