e7144206d2e8e991f97c584406c301f1c5a4abf1
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-07-19 14:42:27 -0400

    Fix lerna bin path

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,4 +12,4 @@ echo "${GREEN}Pulling latest from GitHub...${NC}"
 git pull --rebase
 
 echo "${GREEN}Publishing packages...${NC}"
-../node_modules/.bin/lerna publish --skip-git --skip-npm
+./node_modules/.bin/lerna publish --skip-git --skip-npm
