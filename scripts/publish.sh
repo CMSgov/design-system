9cacb2198e96fefe4daf85e1450d139f0c3f42bc
Sawyer Hollenshead <sawyerh@users.noreply.github.com>
2017-07-19 16:02:14 -0400

    Add prepublish script (#92)
    * Add prepublish script; Use "npm publish"
    
    * Change permission of prepulish script
    
    * Add master checkout to publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,14 +2,10 @@
 
 set -e
 
-# Set colors for the outputted messages.
-# http://stackoverflow.com/a/5947802
 GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
 NC='\033[0m' # No color
 
-echo "${GREEN}Pulling latest from GitHub...${NC}"
-git pull --rebase
+git checkout master
 
 echo "${GREEN}Publishing packages...${NC}"
-./node_modules/.bin/lerna publish
+./node_modules/.bin/lerna exec npm publish --ignore "{@cmsgov/design-system-docs,generator-cmsgov}"
