e5e7f85bca13610bdd06eea77a89509ec77b9af4
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-07-19 15:51:58 -0400

    Add master checkout to publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,5 +5,7 @@ set -e
 GREEN='\033[0;32m'
 NC='\033[0m' # No color
 
+git checkout master
+
 echo "${GREEN}Publishing packages...${NC}"
 ./node_modules/.bin/lerna exec npm publish --ignore "{@cmsgov/design-system-docs,generator-cmsgov}"
