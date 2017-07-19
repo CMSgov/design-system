eb717821ad916543b6f23cc1d6ee4b8f836f2f60
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-07-19 15:42:57 -0400

    Add prepublish script; Use "npm publish"

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-# Set colors for the outputted messages.
-# http://stackoverflow.com/a/5947802
 GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
 NC='\033[0m' # No color
 
-echo "${GREEN}Pulling latest from GitHub...${NC}"
-git pull --rebase
-
 echo "${GREEN}Publishing packages...${NC}"
-./node_modules/.bin/lerna publish
+./node_modules/.bin/lerna exec npm publish --ignore "{@cmsgov/design-system-docs,generator-cmsgov}"
