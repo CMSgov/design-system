c1789beab45f10bcd9cc55ee316ac3a6cf8510bf
Patrick - Wolfert <patrick.wolfert@adhocteam.us>
2017-07-27 10:13:37 -0700

    Moved the nav wrapper to the `Docs` component because its layout sibling is the main content, and the `Nav` component is mostly about the list anyway. If we need to we can make another wrapper component for it in the future.

--- packages/docs/src/scripts/Docs.jsx
+++ packages/docs/src/scripts/Docs.jsx
@@ -11,12 +11,11 @@ const Docs = (props) => {
   return (
     <div>
       <Header />
-      <div className='l-grid'>
-        <Nav
-          items={props.routes}
-          selectedId={props.page.referenceURI}
-        />
-        <main className='l-col-9 l-col-grow'>
+      <div className='ds-l-row ds-u-flex-wrap--nowrap ds-u-margin--0'>
+        <nav className='ds-l-md-col--3 ds-u-border-right--1 ds-u-padding--2 l-sidebar'>
+          <Nav items={props.routes} selectedId={props.page.referenceURI} />
+        </nav>
+        <main className='ds-l-md-col ds-u-padding--0'>
           <Page {...props.page} />
         </main>
       </div>
