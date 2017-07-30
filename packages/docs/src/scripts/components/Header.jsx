18598403c185ffa64668ea1973be70f03f4a442e
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-07-29 20:31:33 -0400

    Add link to homepage in header

--- packages/docs/src/scripts/components/Header.jsx
+++ packages/docs/src/scripts/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Header = () => {
+  const rootPath = process.env.root ? `/${process.env.root}` : '/';
+
   return (
     <header className='ds-base--inverse ds-u-padding--3'>
-      <h1 className='ds-h3'>CMS.gov Design System</h1>
+      <h1 className='ds-h3'>
+        <a href={rootPath} className='c-header__title' title='Home'>
+          CMSGov Design System
+        </a>
+      </h1>
     </header>
   );
 };
