5d95a42eb5a2564e00acee19087945f19e7a630b
Patrick - Wolfert <patrick.wolfert@adhocteam.us>
2017-08-02 07:46:03 -0700

    Added the same thing to the other page template

--- tools/gulp/docs/generatePage.js
+++ tools/gulp/docs/generatePage.js
@@ -103,6 +103,7 @@ function generateMarkupPage(page, modifier, rootPath) {
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Example: ${page.reference}</title>
   <link rel="stylesheet" href="/${rootPath}public/styles/example.css" />
 </head>
