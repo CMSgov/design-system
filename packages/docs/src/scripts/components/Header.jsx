08670005cc4fd604f96b0a4ac14ba9cfdcbf98b2
Patrick Wolfert <patrickrwolfert@gmail.com>
2017-08-07 12:19:18 -0700

    Docs: Helpful links in header (#108)
    * Added links to current release and github repository landing page in the header
    
    * Addressed feedback
    
    * Fix lint errors

--- packages/docs/src/scripts/components/Header.jsx
+++ packages/docs/src/scripts/components/Header.jsx
@@ -1,9 +1,11 @@
+import GitHubLinks from './GitHubLinks';
 import React from 'react';
 
 const Header = () => {
   return (
-    <header className='ds-base--inverse ds-u-padding--3'>
-      <h1 className='ds-h3'>CMS.gov Design System</h1>
+    <header className='ds-base--inverse ds-u-padding--3 ds-u-display--flex ds-u-justify-content--between ds-u-align-items--center'>
+      <h1 className='ds-h3 ds-u-margin-bottom--0'>CMS.gov Design System</h1>
+      <GitHubLinks className='ds-u-display--none ds-u-md-display--block' inverse />
     </header>
   );
 };
