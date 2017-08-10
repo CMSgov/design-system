e3ba6ebff27bb91f33b4510e2f904ff7348109e5
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-08-10 10:58:32 -0400

    Add SkipNav component

---
+++ packages/core/src/components/SkipNav/SkipNav.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export function SkipNav(props) {
+  return <a className='ds-c-skip-nav' href={props.href}>{props.children}</a>;
+}
+
+SkipNav.defaultProps = {
+  children: 'Skip to main content'
+};
+
+SkipNav.propTypes = {
+  children: PropTypes.node.isRequired,
+  /**
+   * The anchor or target for the link (where the link will jump the user to)
+   */
+  href: PropTypes.string.isRequired
+};
+
+export default SkipNav;
