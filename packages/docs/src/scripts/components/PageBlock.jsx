3983575a0f72c1cfea45839ecf4a17ebe33444cc
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-08-10 15:28:12 -0400

    Add label to react components

--- packages/docs/src/scripts/components/PageBlock.jsx
+++ packages/docs/src/scripts/components/PageBlock.jsx
@@ -74,8 +74,12 @@ class PageBlock extends React.PureComponent {
 
     return (
       <div>
+        {
+          this.props.reactComponent &&
+          <span className='ds-h6'>React</span>
+        }
         <h2
-          className='ds-h2'
+          className='ds-h2 ds-u-margin-top--0'
           // Headers can contain HTML markup, therefore dangerously set...
           dangerouslySetInnerHTML={{ __html: this.props.header }}
           id={this.props.reference}
