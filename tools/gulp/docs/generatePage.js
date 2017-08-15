dbd3f8ec7353cfa9d7d00ca0e7d94cee71d49a87
Sawyer Hollenshead <sawyerh@users.noreply.github.com>
2017-08-15 17:06:47 -0400

    Change favicon (#119)

--- tools/gulp/docs/generatePage.js
+++ tools/gulp/docs/generatePage.js
@@ -65,7 +65,7 @@ function generateDocPage(routes, page, rootPath) {
   <title>${page.header} - CMSGov Design System</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="icon" type="image/png" href="/${rootPath}public/images/favicon.png" />
+  <link rel="shortcut icon" type="image/x-icon" href="/${rootPath}public/images/favicon.ico" />
   <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,700" rel="stylesheet" />
   <link rel="stylesheet" href="/${rootPath}public/styles/docs.css" />
 
