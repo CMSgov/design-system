60a37402fba01cad4cd047516fa47af1acaa7bee
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-08-17 17:13:35 -0400

    Polyfill Object.assign
    Also removed babel-polyfill, since it didn't seem to be used anywhere.

--- packages/core/dist/components/VerticalNav/VerticalNavItemLabel.js
+++ packages/core/dist/components/VerticalNav/VerticalNavItemLabel.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.VerticalNavItemLabel = undefined;
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _propTypes = require('prop-types');
@@ -91,9 +93,9 @@ var VerticalNavItemLabel = exports.VerticalNavItemLabel = function (_React$PureC
       };
 
       if (this.LabelComponent === 'a') {
-        props = Object.assign(props, this.anchorProps());
+        props = _extends(props, this.anchorProps());
       } else if (this.LabelComponent === 'button') {
-        props = Object.assign(props, this.buttonProps());
+        props = _extends(props, this.buttonProps());
       }
 
       return _react2.default.createElement(
