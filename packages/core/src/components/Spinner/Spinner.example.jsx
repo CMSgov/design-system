97279bc969dc80b6f7148a643c1240776c73283c
Patrick - Wolfert <patrick.wolfert@adhocteam.us>
2017-08-23 15:53:05 -0700

    Minor delinting

--- packages/core/src/components/Spinner/Spinner.example.jsx
+++ packages/core/src/components/Spinner/Spinner.example.jsx
@@ -5,14 +5,14 @@ import React from 'react';
 export default function() {
   return (
     <div>
-      <div className="ds-u-display--inline-block ds-u-padding--3">
-        <Spinner className="ds-u-valign--middle" />
+      <div className='ds-u-display--inline-block ds-u-padding--3'>
+        <Spinner className='ds-u-valign--middle' />
       </div>
-      <div className="ds-u-display--inline-block ds-u-padding--2 ds-u-fill--background-inverse">
-        <Spinner background className="ds-u-valign--middle" />
+      <div className='ds-u-display--inline-block ds-u-padding--2 ds-u-fill--background-inverse'>
+        <Spinner background className='ds-u-valign--middle' />
       </div>
-      <div className="ds-u-display--inline-block ds-u-padding--2">
-        <Spinner background inversed className="ds-u-valign--middle" />
+      <div className='ds-u-display--inline-block ds-u-padding--2'>
+        <Spinner background inversed className='ds-u-valign--middle' />
       </div>
     </div>
   );
