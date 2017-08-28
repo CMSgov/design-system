192c0eb600c7714c08673d639976d1e1be4c04ec
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-08-28 16:33:37 -0400

    Span entire width

--- packages/docs/src/scripts/components/Footer.jsx
+++ packages/docs/src/scripts/components/Footer.jsx
@@ -39,17 +39,17 @@ function renderLinks(links) {
 const Footer = () => {
   return (
     <footer className='ds-u-fill--primary-alt-lightest ds-u-padding-y--6 ds-u-md-padding-bottom--3'>
-      <div className='ds-l-container ds-u-padding-x--3 ds-u-margin-x--0'>
+      <div className='ds-u-padding-x--3 ds-u-margin-x--0'>
         <div className='ds-l-row'>
           <article className='ds-l-col--12 ds-l-lg-col--4 ds-u-margin-bottom--4'>
             <h2 className='ds-h4'>Find an issue or have a feature request?</h2>
             <a className='ds-c-button ds-c-button--primary' href={githubUrl('issues')} target='_blank'>Ask questions on GitHub</a>
-            <p className='ds-text ds-u-font-size--small ds-u-margin-top--2 ds-u-measure--wide'>
+            <p className='ds-text ds-u-color--primary-darkest ds-u-font-size--small ds-u-margin-top--3 ds-u-measure--base'>
               A federal government website managed by the Centers for Medicare & Medicaid Services 7500 Security Boulevard, Baltimore, MD 21124
             </p>
           </article>
           <article className='ds-l-col--12 ds-l-sm-col--6 ds-l-lg-col--4 ds-u-margin-bottom--4'>
-            <h2 className='ds-h4'>CMS & HHS Websites</h2>
+            <h2 className='ds-h4'>CMS &amp; HHS Websites</h2>
             <ul className='ds-c-list ds-c-list--bare ds-u-font-size--small'>{renderLinks(cmsLinks)}</ul>
           </article>
           <article className='ds-l-col--12 ds-l-sm-col--6 ds-l-lg-col--4 ds-u-margin-bottom--4'>
