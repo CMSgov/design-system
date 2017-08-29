f6565fd3da4a1343b8adae06197e7cb23b92c13b
Sawyer Hollenshead <sawyerh@users.noreply.github.com>
2017-08-29 19:31:53 -0400

    Add Spinner component (#133)
    * The guidance and other documentation is incomplete, but here is all the code and some examples.
    
    * Moved the color changing notes into their own section
    
    * `inverse` -> `inversed`
    
    * Minor delinting
    
    * Style delinting
    
    * `filled` it is!
    
    * Increased the animation speed slightly
    
    * Added a Button component example in the react component section
    
    * Added a single link for now
    
    * Added another link that I was previously hesitant about adding.
    
    * Should fix lint issues
    
    * Added another link that Sawyer suggested
    
    * Responding to feedback. Putting it in alpha

---
+++ packages/core/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+
+export const Spinner = (props) => {
+  const className = classNames(
+    'ds-c-spinner',
+    props.size && `ds-c-spinner--${props.size}`,
+    props.inversed && 'ds-u-fill--background-inverse ds-u-color--base-inverse',
+    props.filled && 'ds-c-spinner--filled',
+    props.className
+  );
+
+  return <span className={className} />;
+};
+
+Spinner.propTypes = {
+  /**
+   * Additional classes to be added to the spinner element.
+   * Useful for adding utility classes.
+   */
+  className: PropTypes.string,
+  /** Applies the inverse theme styling */
+  inversed: PropTypes.bool,
+  /** Adds a background behind the spinner for extra contrast */
+  filled: PropTypes.bool,
+  /** Smaller or larger variant */
+  size: PropTypes.oneOf(['small', 'big'])
+};
+
+export default Spinner;
