b051dc377e87362672ad2c5a77ffacf3d382824f
Patrick - Wolfert <patrick.wolfert@adhocteam.us>
2017-09-25 11:09:59 -0700

    Excluded .example.jsx files from the KSS traversal and responded to other feedback

--- tools/gulp/watch.js
+++ tools/gulp/watch.js
@@ -30,7 +30,7 @@ module.exports = (gulp, shared) => {
       `packages/${packages}/src/**/*.{js,jsx}`,
       `!packages/${packages}/src/**/*.example.{js,jsx}`,
       `!packages/${packages}/src/**/*.test.{js,jsx}`
-    ], ['lint:packages-scripts', 'sass:process:docs', 'docs:generate-pages', 'docs:react']);
+    ], ['lint:packages-scripts', 'docs:react', 'docs:generate-pages']);
   });
 
   gulp.task('watch:docs', () => {
