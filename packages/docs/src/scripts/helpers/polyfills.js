41461350e09bd659921368c9b72f58145d0bb5f1
sawyerh <s.hollenshead@gmail.com>
2017-10-08 20:25:20 -0400

    Update docs to React 16

--- packages/docs/src/scripts/helpers/polyfills.js
+++ packages/docs/src/scripts/helpers/polyfills.js
@@ -3,8 +3,13 @@
 // code in the bundle to avoid causing issues with React in production mode.
 // - github.com/algolia/react-element-to-jsx-string/issues/147
 // - github.com/facebook/react/issues/8379#issuecomment-264867090
-require('core-js/fn/array/includes');
-require('core-js/fn/array/fill');
-require('core-js/fn/function/name');
-require('core-js/fn/object/get-own-property-symbols');
-require('core-js/fn/string/includes');
+import 'core-js/fn/array/includes';
+import 'core-js/fn/array/fill';
+import 'core-js/fn/function/name';
+import 'core-js/fn/object/get-own-property-symbols';
+import 'core-js/fn/string/includes';
+
+// Polyfills for React 16 compatibility
+// https://reactjs.org/docs/javascript-environment-requirements.html
+import 'core-js/es6/map';
+import 'core-js/es6/set';
