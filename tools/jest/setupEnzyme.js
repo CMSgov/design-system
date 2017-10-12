2283f5b291b8e02d4ee22106caa771a5671acb77
sawyerh <s.hollenshead@gmail.com>
2017-10-12 10:37:28 -0400

    Set lifecycleExperimental to true
    This is supposedly the default, but doesn't seem to be the case. See for more info: https://github.com/airbnb/enzyme/issues/1247

--- tools/jest/setupEnzyme.js
+++ tools/jest/setupEnzyme.js
@@ -1,4 +1,7 @@
 import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
 
-configure({ adapter: new Adapter() });
+configure({
+  adapter: new Adapter(),
+  lifecycleExperimental: true // https://github.com/airbnb/enzyme/issues/1247
+});
