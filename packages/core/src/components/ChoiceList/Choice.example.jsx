b6a3b73baf85435eca917e51e0e13a4cbb4a1464
sawyerh <s.hollenshead@gmail.com>
2017-10-25 16:31:26 -0400

    Add uncheckedChildren example

--- packages/core/src/components/ChoiceList/Choice.example.jsx
+++ packages/core/src/components/ChoiceList/Choice.example.jsx
@@ -11,16 +11,26 @@ export default function() {
       <Choice name="checkbox_choice" value="b">
         Checkbox B
       </Choice>
-      <Choice disabled name="checkbox_choice" value="c">
-        Checkbox B
+      <Choice
+        name="checkbox_choice"
+        uncheckedChildren={
+          <span className="ds-c-field__hint">
+            Hint text only visible when Checkbox C is not checked
+          </span>
+        }
+        value="c"
+      >
+        Checkbox C
       </Choice>
 
-      <Choice defaultChecked name="radio_choice" type="radio" value="a">
-        Radio A
-      </Choice>
-      <Choice name="radio_choice" type="radio" value="b">
-        Radio B
-      </Choice>
+      <div className="ds-u-margin-top--4">
+        <Choice defaultChecked name="radio_choice" type="radio" value="a">
+          Radio A
+        </Choice>
+        <Choice name="radio_choice" type="radio" value="b">
+          Radio B
+        </Choice>
+      </div>
     </div>
   );
 }
