4940c1fa60f6287dfe8a086ca5d05e9ca44f57a6
sawyerh <s.hollenshead@gmail.com>
2017-10-27 11:51:32 -0400

    Add stylelint-config-prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: ['./packages/stylelint-config-design-system'],
+  extends: [
+    './packages/stylelint-config-design-system',
+    'stylelint-config-prettier'
+  ],
   ignoreFiles: [
     'packages/generator-cmsgov/*',
     'packages/support/src/vendor/**/*.scss'
