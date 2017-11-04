8c2a878aa1af91fbfa1892bac50ec28d58cf0dd0
Sawyer Hollenshead <sawyerh@users.noreply.github.com>
2017-11-04 15:51:45 -0400

    Add checkedChildren and uncheckedChildren props to Choice (#173)
    * Add checkedChildren and uncheckedChildren props
    
    * DRY up Choice unit tests
    
    * Add state to Choice
    
    * Reduce ChoiceList example length
    
    * Add uncheckedChildren example
    
    * Remove checkedChildren example
    
    * Remove exposed within pattern page
    
    * Remove ExposedWithin example
    
    * Fix for onChange not being called on unchecked radios
    
    * const wrapper
    
    * Recompile file

--- packages/core/src/components/ChoiceList/Choice.jsx
+++ packages/core/src/components/ChoiceList/Choice.jsx
@@ -1,8 +1,12 @@
+import EvEmitter from 'ev-emitter';
 import FormLabel from '../FormLabel/FormLabel';
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 import uniqueId from 'lodash.uniqueid';
+
+/** Used to emit events to all Choice components */
+const dsChoiceEmitter = new EvEmitter();
 
 /**
  * A `Choice` component can be used to render a checkbox or radio button.
@@ -11,41 +15,112 @@ import uniqueId from 'lodash.uniqueid';
  * to the `input` element, so you can use this to set additional attributes if
  * necessary.
  */
-export const Choice = function(props) {
-  /* eslint-disable prefer-const */
-  let {
-    // Using let rather than const since we sometimes rewrite id
-    children,
-    className,
-    id,
-    inversed,
-    inputPlacement,
-    inputClassName,
-    size,
-    requirementLabel,
-    ...inputProps
-  } = props;
-  /* eslint-enable prefer-const */
+export class Choice extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-  const inputClasses = classNames(inputClassName, 'ds-c-choice', {
-    'ds-c-choice--inverse': inversed,
-    'ds-c-choice--right': inputPlacement === 'right',
-    'ds-c-choice--small': size === 'small'
-  });
+    this.handleChange = this.handleChange.bind(this);
+    this.id =
+      this.props.id || uniqueId(`${this.props.type}_${this.props.name}_`);
 
-  if (!id) {
-    id = uniqueId(`${inputProps.type}_${inputProps.name}_`);
+    if (typeof this.props.checked === 'undefined') {
+      this.isControlled = false;
+      // Since this isn't a controlled component, we need a way
+      // to track when the value has changed. This can then be used
+      // to identify when to toggle the visibility of (un)checkedChildren
+      this.state = { checked: this.props.defaultChecked };
+
+      // Event emitters are only relevant for uncontrolled radio buttons
+      if (this.props.type === 'radio') {
+        this.uncheckEventName = `${this.props.name}-uncheck`;
+        dsChoiceEmitter.on(
+          this.uncheckEventName,
+          this.handleUncheck.bind(this)
+        );
+      }
+    } else {
+      this.isControlled = true;
+    }
   }
 
-  return (
-    <div className={className}>
-      <input className={inputClasses} id={id} {...inputProps} />
-      <FormLabel fieldId={id} requirementLabel={requirementLabel}>
-        {children}
-      </FormLabel>
-    </div>
-  );
-};
+  checked() {
+    if (this.isControlled) {
+      return this.props.checked;
+    }
+
+    return this.state.checked;
+  }
+
+  /**
+   * A radio button doesn't receive an onChange event when it is unchecked,
+   * so we fire an "uncheck" event when any radio option is selected. This
+   * allows us to check each radio options' checked state.
+   * @param {String} checkedId - ID of the checked radio option
+   */
+  handleUncheck(checkedId) {
+    if (checkedId !== this.id && this.input.checked !== this.state.checked) {
+      this.setState({ checked: this.input.checked });
+    }
+  }
+
+  handleChange(evt) {
+    if (this.props.onChange) {
+      this.props.onChange(evt);
+    }
+
+    if (!this.isControlled) {
+      this.setState({ checked: evt.target.checked });
+    }
+
+    if (this.uncheckEventName && evt.target.checked) {
+      // Emit the uncheck event so other radio options update their state
+      dsChoiceEmitter.emitEvent(this.uncheckEventName, [this.id]);
+    }
+  }
+
+  render() {
+    const {
+      checkedChildren,
+      children,
+      className,
+      inversed,
+      inputPlacement,
+      inputClassName,
+      requirementLabel,
+      size,
+      uncheckedChildren,
+      ...inputProps
+    } = this.props;
+
+    const inputClasses = classNames(inputClassName, 'ds-c-choice', {
+      'ds-c-choice--inverse': inversed,
+      'ds-c-choice--right': inputPlacement === 'right',
+      'ds-c-choice--small': size === 'small'
+    });
+
+    // Remove props we have our own implementations for
+    if (inputProps.id) delete inputProps.id;
+    if (inputProps.onChange) delete inputProps.onChange;
+
+    return (
+      <div className={className}>
+        <input
+          className={inputClasses}
+          id={this.id}
+          onChange={this.handleChange}
+          ref={input => {
+            this.input = input;
+          }}
+          {...inputProps}
+        />
+        <FormLabel fieldId={this.id} requirementLabel={requirementLabel}>
+          {children}
+        </FormLabel>
+        {this.checked() ? checkedChildren : uncheckedChildren}
+      </div>
+    );
+  }
+}
 
 Choice.defaultProps = {
   type: 'checkbox',
@@ -62,6 +137,14 @@ Choice.propTypes = {
    * mutable, use `defaultChecked`. Otherwise, set either `onChange` or `readOnly`
    */
   checked: PropTypes.bool,
+  /**
+   * Content to be shown when the choice is checked
+   */
+  checkedChildren: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /**
+   * Content to be shown when the choice is not checked
+   */
+  uncheckedChildren: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   /**
    * Additional classes to be added to the root `div` element.
    */
--- packages/core/src/components/ChoiceList/Choice.test.jsx
+++ packages/core/src/components/ChoiceList/Choice.test.jsx
@@ -3,21 +3,31 @@ import FormLabel from '../FormLabel/FormLabel';
 import React from 'react';
 import { shallow } from 'enzyme';
 
-describe('Choice', () => {
-  const label = 'George Washington';
+const label = 'George Washington';
 
+function shallowRender(customProps = {}, children = label) {
+  const props = Object.assign(
+    {
+      name: 'foo',
+      value: 'boo'
+    },
+    customProps
+  );
+
+  return {
+    props: props,
+    wrapper: shallow(<Choice {...props}>{children}</Choice>)
+  };
+}
+
+describe('Choice', () => {
   it('accepts a node as innerHTML', () => {
-    const props = {
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(
-      <Choice {...props}>
-        <p>
-          <strong>Hello</strong> World
-        </p>
-      </Choice>
-    );
+    const wrapper = shallowRender(
+      {},
+      <p>
+        <strong>Hello</strong> World
+      </p>
+    ).wrapper;
     const labelNode = wrapper.find(FormLabel).dive();
 
     expect(
@@ -31,11 +41,7 @@ describe('Choice', () => {
   });
 
   it('is not checked', () => {
-    const props = {
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender().wrapper;
     const input = wrapper.find('input');
 
     expect(input.prop('checked')).toBeUndefined();
@@ -43,12 +49,7 @@ describe('Choice', () => {
   });
 
   it('is checked', () => {
-    const props = {
-      checked: true,
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender({ checked: true }).wrapper;
     const input = wrapper.find('input');
 
     expect(input.prop('checked')).toBe(true);
@@ -56,12 +57,7 @@ describe('Choice', () => {
   });
 
   it('is defaultChecked', () => {
-    const props = {
-      defaultChecked: true,
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender({ defaultChecked: true }).wrapper;
     const input = wrapper.find('input');
 
     expect(input.prop('checked')).toBeUndefined();
@@ -69,23 +65,14 @@ describe('Choice', () => {
   });
 
   it('is required', () => {
-    const props = {
-      name: 'presidents',
-      required: true,
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender({ required: true }).wrapper;
     const input = wrapper.find('input');
 
     expect(input.prop('required')).toBe(true);
   });
 
   it('is a checkbox by default', () => {
-    const props = {
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender().wrapper;
     const input = wrapper.find('input');
 
     expect(input.prop('type')).toBe('checkbox');
@@ -93,23 +80,14 @@ describe('Choice', () => {
   });
 
   it('is a radio button', () => {
-    const props = {
-      name: 'presidents',
-      type: 'radio',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender({ type: 'radio' }).wrapper;
     const input = wrapper.find('input');
 
     expect(input.prop('type')).toBe('radio');
   });
 
   it('applies className to input', () => {
-    const props = {
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender().wrapper;
     const input = wrapper.find('input');
 
     expect(input.hasClass('ds-c-choice')).toBe(true);
@@ -117,12 +95,7 @@ describe('Choice', () => {
   });
 
   it('applies inverse className to input', () => {
-    const props = {
-      inversed: true,
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender({ inversed: true }).wrapper;
     const input = wrapper.find('input');
 
     expect(input.hasClass('ds-c-choice')).toBe(true);
@@ -130,11 +103,7 @@ describe('Choice', () => {
   });
 
   it('places input on left by default', () => {
-    const props = {
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender().wrapper;
     const input = wrapper.find('input');
 
     expect(input.hasClass('ds-c-choice')).toBe(true);
@@ -142,12 +111,7 @@ describe('Choice', () => {
   });
 
   it('places input on right', () => {
-    const props = {
-      inputPlacement: 'right',
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender({ inputPlacement: 'right' }).wrapper;
     const input = wrapper.find('input');
 
     expect(input.hasClass('ds-c-choice')).toBe(true);
@@ -155,13 +119,7 @@ describe('Choice', () => {
   });
 
   it('applies small className to input', () => {
-    const props = {
-      size: 'small',
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
-
+    const wrapper = shallowRender({ size: 'small' }).wrapper;
     const input = wrapper.find('input');
 
     expect(input.hasClass('ds-c-choice')).toBe(true);
@@ -169,62 +127,39 @@ describe('Choice', () => {
   });
 
   it('applies additional classNames to root element', () => {
-    const props = {
-      className: 'foo',
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender({ className: 'foo' }).wrapper;
 
     expect(wrapper.hasClass('foo')).toBe(true);
   });
 
   it('applies additional classNames to input element', () => {
-    const props = {
-      inputClassName: 'foo',
-      name: 'presidents',
-      value: label
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
+    const wrapper = shallowRender({ inputClassName: 'foo' }).wrapper;
     const input = wrapper.find('input');
 
     expect(input.hasClass('foo')).toBe(true);
   });
 
   it('accepts a string value', () => {
-    const props = {
-      name: 'foo',
-      value: 'bar'
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
-    const input = wrapper.find('input');
+    const data = shallowRender({ value: 'bar' });
+    const input = data.wrapper.find('input');
 
-    expect(input.prop('value')).toBe(props.value);
+    expect(input.prop('value')).toBe(data.props.value);
   });
 
   it('accepts a number value', () => {
-    const props = {
-      name: 'foo',
-      value: 100
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
-    const input = wrapper.find('input');
+    const data = shallowRender({ value: 100 });
+    const input = data.wrapper.find('input');
 
-    expect(input.prop('value')).toBe(props.value);
+    expect(input.prop('value')).toBe(data.props.value);
   });
 
   it('accepts a custom id', () => {
-    const props = {
-      id: 'custom_id',
-      name: 'foo',
-      value: 100
-    };
-    const wrapper = shallow(<Choice {...props}>{label}</Choice>);
-    const input = wrapper.find('input');
-    const labelNode = wrapper.find(FormLabel).dive();
+    const data = shallowRender({ id: 'custom_id' });
+    const input = data.wrapper.find('input');
+    const labelNode = data.wrapper.find(FormLabel).dive();
 
-    expect(input.prop('id')).toBe(props.id);
-    expect(labelNode.prop('htmlFor')).toBe(props.id);
+    expect(input.prop('id')).toBe(data.props.id);
+    expect(labelNode.prop('htmlFor')).toBe(data.props.id);
   });
 
   it('generates a unique id', () => {
@@ -242,6 +177,7 @@ describe('Choice', () => {
       </div>
     );
     const $wrapper = wrapper.render();
+
     const idRegex = new RegExp(`checkbox_${sharedProps.name}_[0-9]+`);
     const inputAId = $wrapper
       .find('input')
@@ -274,35 +210,182 @@ describe('Choice', () => {
     ).toBe(inputBId);
   });
 
-  describe('event handlers', () => {
-    let wrapper;
-    let onBlurMock;
-    let onChangeMock;
+  describe('state', () => {
+    it('sets state for uncontrolled component', () => {
+      const data = shallowRender({ defaultChecked: true });
 
-    beforeEach(() => {
-      onBlurMock = jest.fn();
-      onChangeMock = jest.fn();
-
-      const sharedProps = {
-        name: 'presidents',
-        onBlur: onBlurMock,
-        onChange: onChangeMock,
-        value: 'b'
-      };
-
-      wrapper = shallow(<Choice {...sharedProps}>{label}</Choice>);
+      expect(data.wrapper.instance().isControlled).toBe(false);
+      expect(data.wrapper.state('checked')).toBe(data.props.defaultChecked);
     });
 
-    it('calls the onChange handler', () => {
-      wrapper.find('input').simulate('change');
-      expect(onBlurMock.mock.calls.length).toBe(0);
-      expect(onChangeMock.mock.calls.length).toBe(1);
+    it('does not set state for controlled component', () => {
+      const data = shallowRender({ checked: true });
+
+      expect(data.wrapper.instance().isControlled).toBe(true);
+      expect(data.wrapper.state('checked')).toBeUndefined();
+    });
+  });
+
+  describe('event handlers and emitters', () => {
+    let props;
+
+    beforeEach(() => {
+      props = {
+        onBlur: jest.fn(),
+        onChange: jest.fn()
+      };
+    });
+
+    describe('onChange', () => {
+      it('calls the onChange handler', () => {
+        const data = shallowRender(props);
+        const input = data.wrapper.find('input');
+
+        input.simulate('change', {
+          target: { checked: true }
+        });
+
+        expect(data.props.onBlur.mock.calls.length).toBe(0);
+        expect(data.props.onChange.mock.calls.length).toBe(1);
+      });
+
+      it('updates state when uncontrolled component', () => {
+        props.defaultChecked = true;
+        const data = shallowRender(props);
+        const input = data.wrapper.find('input');
+
+        input.simulate('change', {
+          target: { checked: false }
+        });
+
+        expect(data.wrapper.state('checked')).toBe(false);
+      });
+
+      it('skips updating state when controlled component', () => {
+        props.checked = true;
+        const data = shallowRender(props);
+        const input = data.wrapper.find('input');
+
+        input.simulate('change', {
+          target: { checked: false }
+        });
+
+        expect(data.wrapper.state('checked')).toBeUndefined();
+      });
     });
 
     it('calls the onBlur handler', () => {
-      wrapper.find('input').simulate('blur');
-      expect(onBlurMock.mock.calls.length).toBe(1);
-      expect(onChangeMock.mock.calls.length).toBe(0);
+      const data = shallowRender(props);
+      const input = data.wrapper.find('input');
+
+      input.simulate('blur');
+
+      expect(data.props.onBlur.mock.calls.length).toBe(1);
+      expect(data.props.onChange.mock.calls.length).toBe(0);
+    });
+
+    describe('uncheck event emitter', () => {
+      it('sets uncheckEventName for uncontrolled radio buttons', () => {
+        const data = shallowRender({ type: 'radio', defaultChecked: false });
+
+        expect(data.wrapper.instance().uncheckEventName).toBe(
+          `${data.props.name}-uncheck`
+        );
+      });
+
+      it('does not set uncheckEventName for controlled radio buttons', () => {
+        const data = shallowRender({ type: 'radio', checked: false });
+
+        expect(data.wrapper.instance().uncheckEventName).toBeUndefined();
+      });
+
+      it('does not set uncheckEventName for uncontrolled checkbox', () => {
+        const data = shallowRender({ defaultChecked: true });
+
+        expect(data.wrapper.instance().uncheckEventName).toBeUndefined();
+      });
+    });
+  });
+
+  describe('nested content', () => {
+    let props;
+
+    function expectCheckedChildren(wrapper) {
+      expect(wrapper.find('.unchecked-child').length).toBe(0);
+      expect(wrapper.find('.checked-child').length).toBe(1);
+    }
+
+    function expectUncheckedChildren(wrapper) {
+      expect(wrapper.find('.unchecked-child').length).toBe(1);
+      expect(wrapper.find('.checked-child').length).toBe(0);
+    }
+
+    beforeEach(() => {
+      props = {
+        checkedChildren: (
+          <strong className="checked-child">I am checked</strong>
+        ),
+        uncheckedChildren: (
+          <strong className="unchecked-child">I am unchecked</strong>
+        ),
+        name: 'foo',
+        value: 'bar'
+      };
+    });
+
+    describe('controlled component', () => {
+      it('renders uncheckedChildren when not checked', () => {
+        const wrapper = shallow(<Choice {...props}>Foo</Choice>);
+
+        expectUncheckedChildren(wrapper);
+      });
+
+      it('renders uncheckedChildren when checked is changed to false', () => {
+        props.checked = true;
+        const wrapper = shallow(<Choice {...props}>Foo</Choice>);
+        wrapper.setProps({ checked: false });
+
+        expectUncheckedChildren(wrapper);
+      });
+
+      it('renders checkedChildren when checked', () => {
+        props.checked = true;
+        const wrapper = shallow(<Choice {...props}>Foo</Choice>);
+
+        expectCheckedChildren(wrapper);
+      });
+
+      it('renders checkedChildren when checked is changed to true', () => {
+        props.checked = false;
+        const wrapper = shallow(<Choice {...props}>Foo</Choice>);
+        wrapper.setProps({ checked: true });
+
+        expectCheckedChildren(wrapper);
+      });
+    });
+
+    describe('uncontrolled component', () => {
+      it('renders uncheckedChildren when not defaultChecked', () => {
+        props.defaultChecked = false;
+        const wrapper = shallow(<Choice {...props}>Foo</Choice>);
+
+        expectUncheckedChildren(wrapper);
+      });
+
+      it('renders uncheckedChildren when changed to unchecked', () => {
+        props.defaultChecked = true;
+        const wrapper = shallow(<Choice {...props}>Foo</Choice>);
+
+        wrapper.setState({ checked: false });
+        expectUncheckedChildren(wrapper);
+      });
+
+      it('renders checkedChildren when defaultChecked', () => {
+        props.defaultChecked = true;
+        const wrapper = shallow(<Choice {...props}>Foo</Choice>);
+
+        expectCheckedChildren(wrapper);
+      });
     });
   });
 });
