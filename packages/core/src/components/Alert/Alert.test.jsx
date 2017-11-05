17eb43ac4b057c340501101e24a0c4466e586d8e
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-11-05 13:16:33 -0500

    Refactor Alert.test

--- packages/core/src/components/Alert/Alert.test.jsx
+++ packages/core/src/components/Alert/Alert.test.jsx
@@ -2,11 +2,18 @@ import Alert from './Alert';
 import React from 'react';
 import { shallow } from 'enzyme';
 
-describe('Alert', function() {
-  const text = 'Ruhroh';
+const text = 'Ruhroh';
 
-  it('should render alert', () => {
-    const wrapper = shallow(<Alert>{text}</Alert>);
+function render(props = {}, children = text) {
+  return {
+    props: props,
+    wrapper: shallow(<Alert {...props}>{children}</Alert>)
+  };
+}
+
+describe('Alert', function() {
+  it('renders alert', () => {
+    const { wrapper } = render();
     const $body = wrapper.render().find('.ds-c-alert__body');
 
     expect(wrapper.hasClass('ds-c-alert')).toBe(true);
@@ -15,39 +22,32 @@ describe('Alert', function() {
     expect($body.text()).toBe(text);
   });
 
-  it('should render a heading', () => {
-    const props = { heading: 'Error' };
-    const wrapper = shallow(<Alert {...props}>{text}</Alert>);
+  it('renders a heading', () => {
+    const { props, wrapper } = render({ heading: 'Error' });
     const $heading = wrapper.render().find('.ds-c-alert__heading');
 
     expect($heading.length).toBe(1);
     expect($heading.text()).toBe(props.heading);
   });
 
-  it('should appear as an error', () => {
-    const props = { variation: 'error' };
-    const wrapper = shallow(<Alert {...props}>{text}</Alert>);
+  it('appears as an error', () => {
+    const { wrapper } = render({ variation: 'error' });
 
     expect(wrapper.hasClass('ds-c-alert--error')).toBe(true);
   });
 
-  it('should render additional className and role prop', () => {
-    const props = {
+  it('renders additional className and role prop', () => {
+    const { props, wrapper } = render({
       className: 'ds-u-test',
       role: 'alert'
-    };
-    const wrapper = shallow(<Alert {...props}>{text}</Alert>);
+    });
 
-    expect(wrapper.hasClass('ds-u-test')).toBe(true);
-    expect(wrapper.prop('role')).toBe('alert');
+    expect(wrapper.hasClass(props.className)).toBe(true);
+    expect(wrapper.prop('role')).toBe(props.role);
   });
 
-  it('should render HTML children', () => {
-    const wrapper = shallow(
-      <Alert>
-        <p className="ds-text">{text}</p>
-      </Alert>
-    );
+  it('renders HTML children', () => {
+    const { wrapper } = render({}, <p className="ds-text">{text}</p>);
     const $p = wrapper.render().find('.ds-text');
 
     expect($p.length).toBe(1);
