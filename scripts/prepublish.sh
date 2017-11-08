66287b8b536186191b077c0661bc51232aaafd48
sawyerh <s.hollenshead@gmail.com>
2017-11-08 18:22:42 -0500

    Replace references to staging branch with master

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -6,11 +6,11 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No color
 
 echo "${GREEN}Pulling latest from GitHub...${NC}"
-git checkout staging
+git checkout master
 git pull
 
 echo "${GREEN}Bumping version...${NC}"
-./node_modules/.bin/lerna publish --skip-git --skip-npm --allow-branch staging --scope "@cmsgov/design-system-*"
+./node_modules/.bin/lerna publish --skip-git --skip-npm --allow-branch master --scope "@cmsgov/design-system-*"
 
 echo "${GREEN}Building...${NC}"
 npm run build
