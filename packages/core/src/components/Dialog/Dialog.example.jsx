04829cf7103f08e196eab40c43fde72edccbfebe
sawyerh <s.hollenshead@gmail.com>
2017-11-30 18:14:10 -0500

    Remove boilerplate JSX files

--- packages/core/src/components/Dialog/Dialog.example.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/display-name */
-/* import React from 'react';
-import Modal from './Modal';
-
-export default function() {
-  return <Modal />;
-} */
--- packages/core/src/components/Dialog/Dialog.test.jsx
+++
@@ -1,7 +0,0 @@
-/* import React from 'react';
-import {shallow} from 'enzyme';
-import Modal from './Modal';
-
-describe('Modal', function() {
-
-}); */
