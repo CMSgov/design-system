ecde05a8768ddf2610ebc4707760786da28f164f
sawyerh <s.hollenshead@gmail.com>
2017-12-14 15:44:56 -0500

    Update React example; Remove react data file

--- packages/core/src/components/AutocompleteField/AutocompleteField.example.jsx
+++ packages/core/src/components/AutocompleteField/AutocompleteField.example.jsx
@@ -1,110 +1,109 @@
-/* eslint-disable react/display-name */
 import AutocompleteField from './AutocompleteField';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import TextField from '../TextField/TextField';
 
-export default function() {
-  return (
-    <div>
-      <AutocompleteField
-        constrainedList
-        itemToString={i => {
-          return i ? i.name : '';
-        }}
-        items={[
-          {
-            id: 'kRf6c2fY',
-            name: 'Cook County, IL'
-          },
-          {
-            id: 'lYf5cGfM',
-            name: 'Cook County, MD'
-          },
-          {
-            id: 'mZfKcGf9',
-            name: 'Cook County, TN'
-          }
-        ]}
-        onChange={selectedItem => console.log(selectedItem)}
-        onStateChange={changes => {
-          if (changes.type === '__autocomplete_keydown_enter__') {
-            console.log('Yep, the user pressed Enter!');
-          }
+ReactDOM.render(
+  <div>
+    <AutocompleteField
+      constrainedList
+      itemToString={i => {
+        return i ? i.name : '';
+      }}
+      items={[
+        {
+          id: 'kRf6c2fY',
+          name: 'Cook County, IL'
+        },
+        {
+          id: 'lYf5cGfM',
+          name: 'Cook County, MD'
+        },
+        {
+          id: 'mZfKcGf9',
+          name: 'Cook County, TN'
+        }
+      ]}
+      onChange={selectedItem => console.log(selectedItem)}
+      onStateChange={changes => {
+        if (changes.type === '__autocomplete_keydown_enter__') {
+          console.log('Yep, the user pressed Enter!');
+        }
 
-          if (changes.type === '__autocomplete_change_input__') {
-            console.log(changes.inputValue);
-          }
-        }}
-      >
-        <TextField
-          hint="This is an autocomplete field. Begin typing to search for relevant information. The number of results will be updated as you type."
-          label="Constrained list"
-          name="Downshift_autocomplete"
-        />
-      </AutocompleteField>
+        if (changes.type === '__autocomplete_change_input__') {
+          console.log(changes.inputValue);
+        }
+      }}
+    >
+      <TextField
+        hint="This is an autocomplete field. Begin typing to search for relevant information. The number of results will be updated as you type."
+        label="Constrained list"
+        name="Downshift_autocomplete"
+      />
+    </AutocompleteField>
 
-      <AutocompleteField
-        itemToString={i => {
-          return i ? i.name : '';
-        }}
-        items={[
-          {
-            id: 'kRf6c2fY',
-            name: 'Cook County, IL'
-          },
-          {
-            id: 'lYf5cGfM',
-            name: 'Cook County, MD'
-          },
-          {
-            id: 'mZfKcGf9',
-            name: 'Cook County, TN'
-          }
-        ]}
-        onChange={selectedItem => console.log(selectedItem)}
-        onStateChange={changes => {
-          if (changes.type === '__autocomplete_keydown_enter__') {
-            console.log('Yep, the user pressed Enter!');
-          }
+    <AutocompleteField
+      itemToString={i => {
+        return i ? i.name : '';
+      }}
+      items={[
+        {
+          id: 'kRf6c2fY',
+          name: 'Cook County, IL'
+        },
+        {
+          id: 'lYf5cGfM',
+          name: 'Cook County, MD'
+        },
+        {
+          id: 'mZfKcGf9',
+          name: 'Cook County, TN'
+        }
+      ]}
+      onChange={selectedItem => console.log(selectedItem)}
+      onStateChange={changes => {
+        if (changes.type === '__autocomplete_keydown_enter__') {
+          console.log('Yep, the user pressed Enter!');
+        }
 
-          if (changes.type === '__autocomplete_change_input__') {
-            console.log(changes.inputValue);
-          }
-        }}
-      >
-        <TextField
-          hint="This is an autocomplete field. Begin typing to search for relevant information. The number of results will be updated as you type."
-          label="Unconstrained list"
-          name="Downshift_autocomplete"
-        />
-      </AutocompleteField>
+        if (changes.type === '__autocomplete_change_input__') {
+          console.log(changes.inputValue);
+        }
+      }}
+    >
+      <TextField
+        hint="This is an autocomplete field. Begin typing to search for relevant information. The number of results will be updated as you type."
+        label="Unconstrained list"
+        name="Downshift_autocomplete"
+      />
+    </AutocompleteField>
 
-      <AutocompleteField
-        isDisabled
-        itemToString={i => {
-          return i ? i.name : '';
-        }}
-        items={[
-          {
-            id: 'kRf6c2fY',
-            name: 'Cook County, IL'
-          },
-          {
-            id: 'lYf5cGfM',
-            name: 'Cook County, MD'
-          },
-          {
-            id: 'mZfKcGf9',
-            name: 'Cook County, TN'
-          }
-        ]}
-      >
-        <TextField
-          hint="This is an autocomplete field. Begin typing to search for relevant information. The number of results will be updated as you type."
-          label="Disabled autocomplete"
-          name="Downshift_autocomplete_disabled"
-        />
-      </AutocompleteField>
-    </div>
-  );
-}
+    <AutocompleteField
+      isDisabled
+      itemToString={i => {
+        return i ? i.name : '';
+      }}
+      items={[
+        {
+          id: 'kRf6c2fY',
+          name: 'Cook County, IL'
+        },
+        {
+          id: 'lYf5cGfM',
+          name: 'Cook County, MD'
+        },
+        {
+          id: 'mZfKcGf9',
+          name: 'Cook County, TN'
+        }
+      ]}
+    >
+      <TextField
+        hint="This is an autocomplete field. Begin typing to search for relevant information. The number of results will be updated as you type."
+        label="Disabled autocomplete"
+        name="Downshift_autocomplete_disabled"
+      />
+    </AutocompleteField>
+  </div>,
+  document.getElementById('js-example')
+);
