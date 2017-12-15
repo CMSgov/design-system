80127a1c69eaaa0225de983c0b60fab7e676fe51
Sawyer Hollenshead <s.hollenshead@gmail.com>
2017-12-14 21:06:54 -0500

    Revert example app changes

--- examples/react-app/src/scripts/index.js
+++ examples/react-app/src/scripts/index.js
@@ -1,69 +1,22 @@
 // Import all components and reference one by name. You can optimize this using
 // your build process (ie. Tree shaking in Webpack)
-import { Alert, Dialog } from '@cmsgov/design-system-core';
+import { Alert } from '@cmsgov/design-system-core';
 // Import individual component. No special optimizations needed.
 import Button from '@cmsgov/design-system-core/dist/components/Button/Button';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Example extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showingDialog: false
-    };
-
-    this.handleModalToggle = this.handleModalToggle.bind(this);
-  }
-
-  handleModalToggle() {
-    this.setState({ showingDialog: !this.state.showingDialog });
-  }
-
-  dialog() {
-    if (!this.state.showingDialog) return null;
-
-    const actions = [
-      <Button variation="primary" key="primary">
-        Dialog action
-      </Button>,
-      <Button
-        variation="transparent"
-        key="cancel"
-        onClick={this.handleModalToggle}
-      >
-        Cancel
-      </Button>
-    ];
-
-    return (
-      <Dialog
-        onExit={this.handleModalToggle}
-        getApplicationNode={() => document.querySelector('#jsx-root')}
-        title="Dialog title"
-        actions={actions}
-      >
-        Hello world
-      </Dialog>
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <Alert heading="Hello world">
-          <p className="ds-c-alert__text">
-            You did it! You&rsquo;ve ran the example.
-          </p>
-          <Button className="ds-c-button" onClick={this.handleModalToggle}>
-            Show a dialog
-          </Button>
-          {this.dialog()}
-        </Alert>
-      </div>
-    );
-  }
-}
+const Example = function() {
+  return (
+    <div>
+      <Alert heading="Hello world">
+        <p className="ds-c-alert__text">
+          You did it! You&rsquo;ve ran the example.
+        </p>
+        <Button className="ds-u-margin-top--2">Learn more</Button>
+      </Alert>
+    </div>
+  );
+};
 
 ReactDOM.render(<Example />, document.querySelector('#jsx-root'));
