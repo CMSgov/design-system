07f2e20d309e8d091da93a3508a0c2e122cef3e2
Patrick - Wolfert <patrick.wolfert@adhocteam.us>
2017-12-22 14:55:35 -0800

    Reverted the complication of the date field example

--- packages/core/src/components/DateField/DateField.example.jsx
+++ packages/core/src/components/DateField/DateField.example.jsx
@@ -2,36 +2,22 @@ import DateField from './DateField';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class DateFieldExample extends React.PureComponent {
-  constructor(props) {
-    super(props);
+ReactDOM.render(
+  <React.Fragment>
+    <DateField
+      monthDefaultValue={10}
+      dayDefaultValue="31"
+      yearDefaultValue="2020"
+      onComponentBlur={() => console.log('[DateField]: Component lost focus')}
+    />
 
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <DateField
-          monthDefaultValue={10}
-          dayDefaultValue="31"
-          yearDefaultValue="2020"
-          hint={this.state.hint}
-          onComponentBlur={() =>
-            this.setState({ hint: '[DateField]: Component lost focus' })
-          }
-        />
-
-        <DateField
-          errorMessage="Please enter a year in the past"
-          monthDefaultValue={10}
-          dayDefaultValue="31"
-          yearDefaultValue="2020"
-          yearInvalid
-        />
-      </React.Fragment>
-    );
-  }
-}
-
-ReactDOM.render(<DateFieldExample />, document.getElementById('js-example'));
+    <DateField
+      errorMessage="Please enter a year in the past"
+      monthDefaultValue={10}
+      dayDefaultValue="31"
+      yearDefaultValue="2020"
+      yearInvalid
+    />
+  </React.Fragment>,
+  document.getElementById('js-example')
+);
