3787ba6a61799237d77e2d5f9a47c173cad9dae3
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2018-03-22 10:42:48 -0700

    Added more Button tests and confirmed that the tests catch differences

--- e2e/Button.test.js
+++ e2e/Button.test.js
@@ -15,8 +15,38 @@ describe('<Button>', () => {
     await page.close();
   });
 
-  it('default button should match snapshot', async() => {
+  it('default button example should match snapshot', async() => {
     await gotoExample(page, 'components.button');
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it('primary button example should match snapshot', async() => {
+    await gotoExample(page, 'components.button..ds-c-button--primary');
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it('transparent button example should match snapshot', async() => {
+    await gotoExample(page, 'components.button..ds-c-button--transparent');
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it('danger button example should match snapshot', async() => {
+    await gotoExample(page, 'components.button..ds-c-button--danger');
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it('success button example should match snapshot', async() => {
+    await gotoExample(page, 'components.button..ds-c-button--success');
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it('disabled button example should match snapshot', async() => {
+    await gotoExample(page, 'components.button.disabled');
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it('inverse button example should match snapshot', async() => {
+    await gotoExample(page, 'components.button.inverse');
     expect(await page.screenshot()).toMatchImageSnapshot();
   });
 });
