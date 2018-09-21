ec360e5aaa59763db62e3da066d7c87333bf0e34
Bernard Wang <bernard@adhocteam.us>
2018-09-21 15:26:29 -0500

    Add editContent prop to Review, update tests and example

--- packages/core/src/components/Review/Review.example.jsx
+++ packages/core/src/components/Review/Review.example.jsx
@@ -2,25 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Review from './Review';
 
+const editButtonHref = 'javascript:void(0);';
 ReactDOM.render(
   <div>
-    <Review heading="A single Review component" editHref="javascript:void(0);">
+    <Review heading="A single Review component" editHref={editButtonHref}>
       This is an example of a single React Review component.
     </Review>
     <Review
       className="ds-u-padding-top--6"
       heading="Multiple Review components"
-      editHref="javascript:void(0);"
+      editHref={editButtonHref}
     >
       Multiple Review components can be combined together one after another.
     </Review>
-    <Review heading="Full Name" editHref="javascript:void(0);">
+    <Review
+      heading="A Review component with edit content"
+      editContent={
+        <div>
+          <a href={editButtonHref}>Edit</a>
+          <span>|</span>
+          <a href={editButtonHref}>Remove</a>
+        </div>
+      }
+    >
+      The edit link can be replaced to customize the component.
+    </Review>
+    <Review heading="Full Name" editHref={editButtonHref}>
       John Doe
     </Review>
-    <Review heading="Date of Birth" editHref="javascript:void(0);">
+    <Review heading="Date of Birth" editHref={editButtonHref}>
       October 11, 1980
     </Review>
-    <Review heading="Shopping List" editHref="javascript:void(0);">
+    <Review heading="Shopping List" editHref={editButtonHref}>
       <ul>
         <li>Milk</li>
         <li>Eggs</li>
@@ -28,7 +41,7 @@ ReactDOM.render(
         <li>Sugar</li>
       </ul>
     </Review>
-    <Review editHref="javascript:void(0);">The heading is not required.</Review>
+    <Review editHref={editButtonHref}>The heading is not required.</Review>
   </div>,
   document.getElementById('js-example')
 );
