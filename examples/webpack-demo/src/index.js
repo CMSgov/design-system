e9469e3d0ba6558ad34e2939fd09de83f30d6e02
Sophia Dengo <sdengo@gmail.com>
2018-10-31 08:44:01 -0400

    remove tutorial content

--- examples/webpack-demo/src/index.js
+++ examples/webpack-demo/src/index.js
@@ -1,20 +1,3 @@
 import './style.scss';
 import Icon from './test.jpg';
 import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
