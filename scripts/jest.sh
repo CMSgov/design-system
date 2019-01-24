7d259b26bbefe4471115f13373470f1d1a1cb543
Trevor Pierce <trevor@adhocteam.us>
2019-01-24 07:36:43 -0600

    WIP. Breaking changes in package.json, but working with direct Jest commands.

---
+++ scripts/jest.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# Reorganized Jest controller for unit tests, e2e, and snapshots
+
+# Initialize variables
+WATCH = false
+SKIP_BUILD=${SKIP_BUILD:-false}
+E2E=false
+EXTRA_OPTS=()
+
+# Populate variables from parameters
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -w|--watch)
+      WATCH=true
+      shift # past argument
+      ;;
+    --skip-build)
+      SKIP_BUILD=true
+      shift # past argument
+      ;;
+    --e2e)
+      E2E=true
+      shift # past argument
+      ;;
+    --slow-mo)
+      SLOW_MO="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    *) # unknown option
+      EXTRA_OPTS+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+
+if [ "$E2E" = true ]; then
+  # e2e config here
+else
+  OPTS="--config=tools/jest/unit.config.js"
+fi
+
+if [ "$SKIP_BUILD" = true ]; then
+  export SKIP_BUILD
+fi
+
+if [ "$SLOW_MO" = true ]; then
+  export SLOW_MO
+fi
+
+OPTS="${OPTS} ${EXTRA_OPTS[@]}"
+
+if [ "$WATCH" = true ]; then
+  # By default if we're watching, build it once up front and tell
+  # Jest to skip the build. If --skip-build is passed explicitly,
+  # skip the first build also.
+  if [ "$SKIP_BUILD" = false ]; then
+    set +x
+    yarn build
+    set -x
+  fi
+  export SKIP_BUILD=true
+  yarn jest $OPTS --watch
+else
+  yarn jest $OPTS
+fi
