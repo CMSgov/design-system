c4e9e4adc9dde314492cf1f4b1c9888a13eaf9ce
Scott Weber <scottweber@scottwebersmbp.home>
2019-02-11 16:58:07 -0500

    Update choice example - add fieldsets and make naming consistent.

--- packages/core/src/components/ChoiceList/Choice.example.jsx
+++ packages/core/src/components/ChoiceList/Choice.example.jsx
@@ -18,67 +18,77 @@ const childSelect = (
   <ChoiceList
     choices={selectChoices}
     label="Select example"
+    className=""
     name="select_choices_field"
   />
 );
 
 ReactDOM.render(
   <div>
-    <Choice
-      defaultChecked
-      hint="Checkbox A hint"
-      name="checkbox_choice"
-      value="a"
-    >
-      Checkbox A
-    </Choice>
-    <Choice name="checkbox_choice" value="b">
-      Checkbox B
-    </Choice>
-    <Choice name="checkbox_choice" value="c">
-      Checkbox C
-    </Choice>
+    <fieldset className="ds-c-fieldset">
+      <legend className="ds-c-label">Checkbox example</legend>
+      <Choice
+        defaultChecked
+        hint="Checkbox A hint"
+        name="checkbox_choice"
+        value="a"
+      >
+        Checkbox A
+      </Choice>
+      <Choice name="checkbox_choice" value="b">
+        Checkbox B
+      </Choice>
+      <Choice name="checkbox_choice" value="c">
+        Checkbox C
+      </Choice>
+    </fieldset>
 
-    <div className="ds-u-margin-top--4">
-      <fieldset className="ds-c-fieldset">
-        <legend className="ds-c-label">Checkboxes with children</legend>
-        <Choice defaultChecked name="radio_choice" value="a">
-          Checkbox A
-        </Choice>
-        <Choice name="radio_choice" value="b" checkedChildren={childSelect}>
-          Checkbox B - with children
-        </Choice>
-        <Choice name="checkbox_choice" value="c">
-          Checkbox C
-        </Choice>
-      </fieldset>
-    </div>
+    <fieldset className="ds-c-fieldset">
+      <legend className="ds-c-label">Checkbox example with children</legend>
+      <Choice
+        hint="Checkbox A hint"
+        defaultChecked
+        name="checkbox_choice_children"
+        value="a"
+      >
+        Checkbox A
+      </Choice>
+      <Choice
+        name="checkbox_choice_children"
+        value="b"
+        checkedChildren={childSelect}
+      >
+        Checkbox B - with children
+      </Choice>
+      <Choice name="checkbox_choice_children" value="c">
+        Checkbox C
+      </Choice>
+    </fieldset>
 
-    <div className="ds-u-margin-top--4">
+    <fieldset className="ds-c-fieldset">
+      <legend className="ds-c-label">Radio example</legend>
       <Choice name="radio_choice" type="radio" value="a">
         Radio A
       </Choice>
       <Choice name="radio_choice" type="radio" value="b">
         Radio B
       </Choice>
-    </div>
+    </fieldset>
 
-    <div className="ds-u-margin-top--4">
-      <fieldset className="ds-c-fieldset">
-        <legend className="ds-c-label">Radios with children</legend>
-        <Choice name="radio_choice_children" type="radio" value="c">
-          Radio C
-        </Choice>
-        <Choice
-          name="radio_choice_children"
-          type="radio"
-          value="d"
-          checkedChildren={childSelect}
-        >
-          Radio D - with children
-        </Choice>
-      </fieldset>
-    </div>
+    <fieldset className="ds-c-fieldset">
+      <legend className="ds-c-label">Radio example with children</legend>
+      <Choice name="radio_choice_children" type="radio" value="c">
+        Radio A
+      </Choice>
+      <Choice
+        name="radio_choice_children"
+        type="radio"
+        value="d"
+        checkedChildren={childSelect}
+      >
+        Radio B - with children
+      </Choice>
+    </fieldset>
   </div>,
   document.getElementById('js-example')
 );
