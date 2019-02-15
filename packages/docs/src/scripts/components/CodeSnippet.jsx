d33b512b239cc47c5e48b520f73f1755c6caba70
Scott Weber <scott.weber@adhocteam.us>
2019-02-15 11:04:23 -0500

    Add underline to transparent buttons (#322)
    * Add underline to transparent buttons
    
    * Remove underline from arrow to expand/collapse code examples
    
    * Remove !important style linter didn't like it. Add util class.

--- packages/docs/src/scripts/components/CodeSnippet.jsx
+++ packages/docs/src/scripts/components/CodeSnippet.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const CodeSnippet = function(props) {
   return (
     <details open={props.open}>
-      <summary className="ds-u-margin-y--1 ds-c-button ds-c-button--small ds-c-button--transparent ds-u-padding--1">
+      <summary className="ds-u-margin-y--1 ds-c-button ds-c-button--small ds-c-button--transparent ds-u-padding--1 ds-u-text-decoration--none">
         Code snippet
       </summary>
       <pre className="ds-u-margin-bottom--4 ds-u-overflow--auto">
