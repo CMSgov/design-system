7539f2af1301623d5745afa505f53161cce527c8
Trevor Pierce <1Copenut@users.noreply.github.com>
2019-04-11 21:49:09 -0500

    End-to-end test suite with accessibility scans (#308)
    * WIP. First e2e test commit.
    
    * Adding basic constant refactoring, headless Chrome as default.
    
    * Adding more refactors to constants ahead of splitting unit and e2e tests.
    
    * WIP. Breaking changes in package.json, but working with direct Jest commands.
    
    * WIP. Adding remainer of files before moving e2e tests to permanent location.
    
    * WIP. Plumbed e2e tests to operate by bash script.
    
    * WIP. Moving e2e tests, adding self-contained build and serve functions.
    
    * Adding Travis.yml file for CI/CD builds.
    
    * Refactoring for Chrome options, better setup and teardown.
    
    * Adding Autocomplete, Badge tests.
    
    * WIP. Adding all tests before bumping Selenium Webdriver.
    
    * Bumped Webdriver to handle async errors.
    
    * Fixing e2e smoke test file.
    
    * Adding a way to run e2e tests without building again
    
    * Adding updated test scripts to CONTRIBUTING#scripts documentation.
    
    * Tweaking test:unit bullet points.
    
    * Final tweaks for unit testing, restoring --watch command.
    
    * Adding a space for final CI/CD test run.
    
    * Removing entry from Gitignore.
    
    * Moved the e2e tests into the component directories and updated the jest configs
    
    * Moved all the webdriver setup into a jest "test environment" so we don't have to worry about setting up in all the .e2e.test.js files. Had to make a bunch of other little edits because of eslint.
    
    * Moved the Axe tests into a helper function
    
    * Remove dist files
    
    * Fixed unit test config

---
+++ tools/jest/e2e.environment.js
@@ -0,0 +1,41 @@
+/* eslint-disable  filenames/match-exported */
+const NodeEnvironment = require('jest-environment-node');
+const { Builder, By, Capabilities, until } = require('selenium-webdriver');
+
+class WebDriverEnvironment extends NodeEnvironment {
+  constructor(config) {
+    super(config);
+    const options = config.testEnvironmentOptions || {};
+    this.chromeOptions = options.chromeOptions;
+    this.browserName = options.browser || 'chrome';
+  }
+
+  async buildDriver() {
+    let driver = new Builder().forBrowser(this.browserName);
+
+    if (this.chromeOptions) {
+      const chromeCapabilities = Capabilities.chrome();
+      chromeCapabilities.set('chromeOptions', this.chromeOptions);
+      driver = driver.withCapabilities(chromeCapabilities);
+    }
+
+    return driver.build();
+  }
+
+  async setup() {
+    await super.setup();
+    this.driver = await this.buildDriver();
+    this.global.driver = this.driver;
+    this.global.by = By;
+    this.global.element = locator => this.driver.findElement(locator);
+    this.global.element.all = locator => this.driver.findElements(locator);
+    this.global.until = until;
+  }
+
+  async teardown() {
+    await this.driver.quit();
+    await super.teardown();
+  }
+}
+
+module.exports = WebDriverEnvironment;
