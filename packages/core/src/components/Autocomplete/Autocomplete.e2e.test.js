a4b723f484a1f3751d73267ae2f1a9ae25fe71e2
Trevor Pierce <trevor@adhocteam.us>
2019-05-08 13:55:43 -0500

    [HDSG-214] WIP. Adding a few e2e tests for Autocomplete.

--- packages/core/src/components/Autocomplete/Autocomplete.e2e.test.js
+++ packages/core/src/components/Autocomplete/Autocomplete.e2e.test.js
@@ -1,8 +1,8 @@
 /* global driver */
+import { getElementByClassName, getElementByXPath } from '../../helpers/e2e';
 import { ROOT_URL } from '../../helpers/e2e/constants';
 
 import assertNoAxeViolations from '../../helpers/e2e/assertNoAxeViolations';
-import { getElementByClassName } from '../../helpers/e2e';
 
 const rootURL = `${ROOT_URL}/example/components.autocomplete.react/`;
 
@@ -12,6 +12,19 @@ describe('Alert component', () => {
 
     const el = await getElementByClassName('ds-u-clearfix ds-c-autocomplete');
     expect(el).toBeTruthy();
+  });
+
+  it('Should expand listbox on input value', async() => {
+    await driver.get(rootURL);
+
+    const input = await getElementByXPath('//*[@id="autocomplete_1"]');
+
+    driver.findElement(input).sendKeys('c');
+
+    const listbox = await getElementByXPath(
+      '//*[@id="autocomplete_owned_container_4"]'
+    );
+    expect(listbox).toBeTruthy();
   });
 
   it('Should have no accessibility violations', async() => {
