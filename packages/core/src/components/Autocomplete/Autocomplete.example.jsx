6157c2a16c899910a305500cd6354bcea299d916
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2019-06-28 22:17:15 -0700

    Autocompletes that were being rendered in any kind of stateful tree were unable to track their own state between renders because of this `MyDiv` component that was being defined in the render function. I've changed the example to be stateful for illustration purposes. If you were to move the MyDiv (WrapperDiv) back to being defined in the render function and then you were to run the example, you'd notice very bizarre behavior when typing into the Autocomplete.

--- packages/core/src/components/Autocomplete/Autocomplete.example.jsx
+++ packages/core/src/components/Autocomplete/Autocomplete.example.jsx
@@ -3,107 +3,121 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TextField from '../TextField/TextField';
 
-ReactDOM.render(
-  <div>
-    <Autocomplete
-      items={[
-        {
-          id: 'kRf6c2fY',
-          name: 'Cook County, IL'
-        },
-        {
-          id: 'lYf5cGfM',
-          name: 'Cook County, MD'
-        },
-        {
-          id: 'mZfKcGf9',
-          name: 'Cook County, TN'
-        },
-        {
-          id: 'xFz6dLba',
-          name: 'Cook County, AK'
-        },
-        {
-          id: 'vTr5c99',
-          name: 'Cook County, FL'
-        },
-        {
-          id: 'ntY8Lha',
-          name: 'Cook County, AL'
-        },
-        {
-          id: 'uRe0Wqo',
-          name: 'Cook County, WA'
-        },
-        {
-          id: 'yUR7MWl',
-          name: 'Cook County, OR'
-        }
-      ]}
-      focusTrigger
-      label="Select from the options below:"
-      onChange={selectedItem => console.log(selectedItem)}
-      onInputValueChange={inputVal =>
-        console.log('[Autocomplete]: ' + inputVal)
-      }
-    >
-      <TextField
-        hint="Type c then use ARROW keys to change options, ENTER key to make a selection, ESC to dismiss."
-        label="Labeled list"
-        name="Downshift_autocomplete"
-      />
-    </Autocomplete>
+class AutocompleteExample extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-    <Autocomplete
-      items={[
-        {
-          id: 'kRf6c2fY',
-          name: 'Cook County, IL'
-        },
-        {
-          id: 'lYf5cGfM',
-          name: 'Cook County, MD'
-        },
-        {
-          id: 'mZfKcGf9',
-          name: 'Cook County, TN'
-        }
-      ]}
-      onChange={selectedItem => console.log(selectedItem)}
-      onInputValueChange={inputVal =>
-        console.log('[Autocomplete]: ' + inputVal)
-      }
-    >
-      <TextField
-        hint="Type c then use ARROW keys to change options, ENTER key to make a selection, ESC to dismiss."
-        label="Simple list"
-        name="Downshift_autocomplete"
-      />
-    </Autocomplete>
+    this.state = {
+      inputValue: ''
+    };
+  }
 
-    <Autocomplete items={[]} loading clearSearchButton={false}>
-      <TextField
-        hint="List should return string Loading to simulate async data call."
-        label="Loading message"
-        name="Downshift_autocomplete"
-      />
-    </Autocomplete>
+  render() {
+    return (
+      <div>
+        <Autocomplete
+          items={[
+            {
+              id: 'kRf6c2fY',
+              name: 'Cook County, IL'
+            },
+            {
+              id: 'lYf5cGfM',
+              name: 'Cook County, MD'
+            },
+            {
+              id: 'mZfKcGf9',
+              name: 'Cook County, TN'
+            },
+            {
+              id: 'xFz6dLba',
+              name: 'Cook County, AK'
+            },
+            {
+              id: 'vTr5c99',
+              name: 'Cook County, FL'
+            },
+            {
+              id: 'ntY8Lha',
+              name: 'Cook County, AL'
+            },
+            {
+              id: 'uRe0Wqo',
+              name: 'Cook County, WA'
+            },
+            {
+              id: 'yUR7MWl',
+              name: 'Cook County, OR'
+            }
+          ]}
+          focusTrigger
+          label="Select from the options below:"
+          onChange={selectedItem => console.log(selectedItem)}
+          onInputValueChange={inputValue => {
+            console.log('[Autocomplete]: ' + inputValue);
+            this.setState({ inputValue });
+          }}
+        >
+          <TextField
+            hint="Type c then use ARROW keys to change options, ENTER key to make a selection, ESC to dismiss."
+            label="Labeled list"
+            name="Downshift_autocomplete"
+          />
+        </Autocomplete>
 
-    <Autocomplete items={[]} clearSearchButton={false}>
-      <TextField
-        hint="List should return string No results found."
-        label="No results message"
-        name="Downshift_autocomplete"
-      />
-    </Autocomplete>
+        <Autocomplete
+          items={[
+            {
+              id: 'kRf6c2fY',
+              name: 'Cook County, IL'
+            },
+            {
+              id: 'lYf5cGfM',
+              name: 'Cook County, MD'
+            },
+            {
+              id: 'mZfKcGf9',
+              name: 'Cook County, TN'
+            }
+          ]}
+          onChange={selectedItem => console.log(selectedItem)}
+          onInputValueChange={inputVal =>
+            console.log('[Autocomplete]: ' + inputVal)
+          }
+        >
+          <TextField
+            hint="Type c then use ARROW keys to change options, ENTER key to make a selection, ESC to dismiss."
+            label="Simple list"
+            name="Downshift_autocomplete"
+          />
+        </Autocomplete>
 
-    <Autocomplete clearSearchButton={false}>
-      <TextField
-        hint="No list should be shown if no item array is provided and it's not loading. This could be the case if a user has not yet entered the minimum number of characters required for a search."
-        label="Nothing shown"
-        name="Downshift_autocomplete"
-      />
-    </Autocomplete>
-  </div>,
-  document.getElementById('js-example')
-);
+        <Autocomplete items={[]} loading clearSearchButton={false}>
+          <TextField
+            hint="List should return string Loading to simulate async data call."
+            label="Loading message"
+            name="Downshift_autocomplete"
+          />
+        </Autocomplete>
+
+        <Autocomplete items={[]} clearSearchButton={false}>
+          <TextField
+            hint="List should return string No results found."
+            label="No results message"
+            name="Downshift_autocomplete"
+          />
+        </Autocomplete>
+
+        <Autocomplete clearSearchButton={false}>
+          <TextField
+            hint="No list should be shown if no item array is provided and it's not loading. This could be the case if a user has not yet entered the minimum number of characters required for a search."
+            label="Nothing shown"
+            name="Downshift_autocomplete"
+          />
+        </Autocomplete>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<AutocompleteExample />, document.getElementById('js-example'));
---
+++ packages/core/src/components/Autocomplete/WrapperDiv.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+// See https://github.com/downshift-js/downshift#getrootprops
+// Custom container returns a plain div, without the ARIA markup
+// required for a WAI-ARIA 1.1 combobox. See the comments at the
+// top of the component file for an explanation of this decision.
+const WrapperDiv = ({ innerRef, ...rest }) => <div ref={innerRef} {...rest} />;
+
+export default WrapperDiv;
