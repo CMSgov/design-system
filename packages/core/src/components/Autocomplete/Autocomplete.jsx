dab99ac9a76a585031acae74d39e6b49ecc36ffa
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2019-06-28 22:25:12 -0700

    Revert "This commit is broken on purpose. The example should be broken to show what happens when the component was placed in a stateful tree before the fix."
    This reverts commit 61e5fe8f1f2e5e762f131810b2cae52ed3cbe45d.

--- packages/core/src/components/Autocomplete/Autocomplete.jsx
+++ packages/core/src/components/Autocomplete/Autocomplete.jsx
@@ -20,6 +20,7 @@ import Downshift from 'downshift';
 import PropTypes from 'prop-types';
 import React from 'react';
 import TextField from '../TextField/TextField';
+import WrapperDiv from './WrapperDiv';
 import classNames from 'classnames';
 import uniqueId from 'lodash.uniqueid';
 
@@ -144,10 +145,6 @@ export class Autocomplete extends React.PureComponent {
       'ds-u-clearfix',
       'ds-c-autocomplete',
       className
-    );
-
-    const WrapperDiv = ({ innerRef, ...rest }) => (
-      <div ref={innerRef} {...rest} />
     );
 
     return (
