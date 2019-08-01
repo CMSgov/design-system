ebb1a24df1ba7e59d04169d8c08f216da6774735
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2019-08-01 12:41:21 -0700

    Fix CI tests by updating chromedriver (#471)
    * Update chromedriver so travis ci tests will work (hopefully)
    
    * Travis' chrome is broken, so pin chromedriver to the random version they have lying around on their system (#473)
    
    
    * specify exact version
    
    * Remove the version printing

--- tools/jest/e2e.config.js
+++ tools/jest/e2e.config.js
@@ -1,7 +1,12 @@
 const headless = process.env.HEADLESS ? JSON.parse(process.env.HEADLESS) : true;
 const browser = headless ? 'chrome' : process.env.BROWSER || 'chrome';
 const chromeOptions = headless && {
-  args: ['--headless', '--window-size=1024,768']
+  args: [
+    '--headless',
+    '--window-size=1024,768',
+    '--no-sandbox',
+    '--disable-dev-shm-usage'
+  ]
 };
 
 const validBrowsers = ['chrome', 'firefox', 'safari', 'ie', 'edge'];
