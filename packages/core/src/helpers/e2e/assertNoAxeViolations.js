863b267cc7178da26a1546562730c00bbb52c294
Bernard Wang <bernard@bernard.wang>
2019-09-20 15:28:51 -0500

    Add disabledRule param to axe-webdriverjs tests, and temporarily disable rule for Modal

--- packages/core/src/helpers/e2e/assertNoAxeViolations.js
+++ packages/core/src/helpers/e2e/assertNoAxeViolations.js
@@ -2,14 +2,15 @@
 import AxeBuilder from 'axe-webdriverjs';
 import { RULESET_ALL } from '../../helpers/e2e/constants';
 
-export default async function assertNoAxeViolations(url) {
+export default async function assertNoAxeViolations(url, disabledRules = []) {
   if (url) {
     await driver.get(url);
   }
+  const defaultDisabledRules = ['bypass'];
 
   await AxeBuilder(driver)
     .withTags(RULESET_ALL)
-    .disableRules('bypass')
+    .disableRules(defaultDisabledRules.concat(disabledRules))
     .analyze((err, results) => {
       if (err) {
         console.error(err);
