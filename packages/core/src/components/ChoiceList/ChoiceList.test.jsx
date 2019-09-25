b26c6e8eb45a75d2497c35047e39766d1d161e8c
Bernard Wang <bernard@bernard.wang>
2019-09-25 15:48:43 -0500

    Add tests

--- packages/core/src/components/ChoiceList/ChoiceList.test.jsx
+++ packages/core/src/components/ChoiceList/ChoiceList.test.jsx
@@ -166,18 +166,37 @@ describe('ChoiceList', () => {
         .first()
         .simulate('change');
 
-      expect(onChange.mock.calls.length).toBe(1);
+      expect(onChange).toHaveBeenCalled();
     });
 
     it('calls onBlur', () => {
       const onBlur = jest.fn();
-      const data = shallowRender({ onBlur });
+      const onComponentBlur = jest.fn();
+      const data = shallowRender({ onBlur, onComponentBlur });
       data.wrapper
         .find('Choice')
         .first()
         .simulate('blur');
 
-      expect(onBlur.mock.calls.length).toBe(1);
+      expect(onBlur).toHaveBeenCalled();
+      setTimeout(() => {
+        expect(onComponentBlur).not.toHaveBeenCalled();
+      }, 20);
+    });
+
+    it('calls onComponentBlur', () => {
+      const onBlur = jest.fn();
+      const onComponentBlur = jest.fn();
+      const data = shallowRender({ onBlur, onComponentBlur });
+      data.wrapper
+        .find('Choice')
+        .last()
+        .simulate('blur');
+
+      expect(onBlur).toHaveBeenCalled();
+      setTimeout(() => {
+        expect(onComponentBlur).toHaveBeenCalled();
+      }, 20);
     });
   });
 
@@ -286,7 +305,7 @@ describe('ChoiceList', () => {
         .first()
         .simulate('change');
 
-      expect(onChange.mock.calls.length).toBe(1);
+      expect(onChange).toHaveBeenCalled();
     });
 
     it('calls onBlur', () => {
@@ -298,7 +317,7 @@ describe('ChoiceList', () => {
         .first()
         .simulate('blur');
 
-      expect(onBlur.mock.calls.length).toBe(1);
+      expect(onBlur).toHaveBeenCalled();
     });
   });
 
