e7be6893d802113024b84cb4d7944c575067aaed
Bernard Wang <bernard@bernard.wang>
2019-10-29 10:42:27 -0500

    Move example wrapper class to docs scss

--- packages/core/src/components/Alert/Alert.example.jsx
+++ packages/core/src/components/Alert/Alert.example.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <div className="alert-wrapper">
+  <div className="example--wrapper">
     <Alert heading="Status heading">
       <p className="ds-c-alert__text">This is a React Alert component.</p>
     </Alert>
