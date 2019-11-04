d3b166010ef9472fd019251635f9495d83f10887
Bernard Wang <bernard@bernard.wang>
2019-11-04 15:23:52 -0600

    Update FormLabel example

--- packages/core/src/components/FormLabel/FormLabel.example.jsx
+++ packages/core/src/components/FormLabel/FormLabel.example.jsx
@@ -1,15 +1,34 @@
+import React, { Fragment } from 'react';
 import FormLabel from './FormLabel';
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <FormLabel
-    errorMessage="Please enter a valid phone number."
-    hint="We'll use this number as a backup if we need to contact you about your application."
-    fieldId="number-field-id"
-    requirementLabel="Optional"
-  >
-    Phone number
-  </FormLabel>,
+  <Fragment>
+    <div>
+      <FormLabel
+        component="label"
+        errorMessage="Please enter a valid phone number."
+        hint="We'll use this number as a backup if we need to contact you about your application."
+        fieldId="number-field-id"
+        requirementLabel="Optional"
+      >
+        Phone number
+      </FormLabel>
+    </div>
+    <fieldset className="ds-c-fieldset">
+      <FormLabel
+        component="legend"
+        hint={
+          <span>
+            Select all that apply.{' '}
+            <a href="void:javascript(0)">Learn more about these documents.</a>
+          </span>
+        }
+        fieldId="document-field-id"
+      >
+        Select a document type
+      </FormLabel>
+    </fieldset>
+  </Fragment>,
   document.getElementById('js-example')
 );
