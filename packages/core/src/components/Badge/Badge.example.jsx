616e749fe21b6088c712225a3f10fa2c5aba8a78
Bernard <bernardwang@users.noreply.github.com>
2019-12-13 12:21:03 -0600

    [WNMGDS-326] Fix react button styling (#572)
    * Add combined ds-c-button--transparent ds-c-button--inverse styles as equivalent to --transparent-inverse
    
    * Update Button.jsx, example and tests
    
    * Update Badge example to not use <code>

--- packages/core/src/components/Badge/Badge.example.jsx
+++ packages/core/src/components/Badge/Badge.example.jsx
@@ -5,12 +5,8 @@ import ReactDOM from 'react-dom';
 ReactDOM.render(
   <Fragment>
     <Badge>Default badge</Badge>
-    <Badge variation="info">
-      Badge with <code>variation</code> prop
-    </Badge>
-    <Badge size="big">
-      Badge with <code>size</code> prop
-    </Badge>
+    <Badge variation="info">Badge with `variation` prop</Badge>
+    <Badge size="big">Badge with `size` prop</Badge>
   </Fragment>,
   document.getElementById('js-example')
 );
