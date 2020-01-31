47dc52f7697c7096aeb7043a8a80a0fca1ca30cb
line47 <scott.weber@adhocteam.us>
2020-01-31 11:50:38 -0500

    move footer outside body. Add close button class

--- packages/core/src/components/HelpDrawer/HelpDrawer.jsx
+++ packages/core/src/components/HelpDrawer/HelpDrawer.jsx
@@ -40,7 +40,7 @@ export class HelpDrawer extends React.PureComponent {
             </h3>
             <Button
               aria-label={ariaLabel}
-              className="ds-u-margin-left--auto"
+              className="ds-u-margin-left--auto ds-c-help-drawer__close-button"
               size="small"
               onClick={onCloseClick}
             >
@@ -53,11 +53,11 @@ export class HelpDrawer extends React.PureComponent {
             <div className="ds-c-help-drawer__content ds-u-md-font-size--small ds-u-lg-font-size--base ds-u-padding--2">
               {children}
             </div>
-            <div className="ds-c-help-drawer__footer ds-u-fill--primary-alt-lightest ds-u-md-font-size--small ds-u-lg-font-size--base ds-u-padding--2">
-              <h4 className="ds-text ds-u-margin--0">{footerTitle}</h4>
-              <div className="ds-text ds-u-margin--0">{footerBody}</div>
-            </div>
           </div>
+        </div>
+        <div className="ds-c-help-drawer__footer ds-u-fill--primary-alt-lightest ds-u-md-font-size--small ds-u-lg-font-size--base ds-u-padding--2">
+          <h4 className="ds-text ds-u-margin--0">{footerTitle}</h4>
+          <div className="ds-text ds-u-margin--0">{footerBody}</div>
         </div>
       </div>
     );
