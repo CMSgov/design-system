c8958b1119a57296ae059db383c8dbc82cfb76e2
bernardwang <bw.vbnm@gmail.com>
2020-02-18 10:13:36 -0600

    Move files to pattern folder in core package

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,3 +1,4 @@
 'use strict';
 
 export * from './components'; // eslint-disable-line import/export
+export * from './patterns'; // eslint-disable-line import/export
---
+++ packages/core/src/patterns/index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+export * from './Review/Review';
+export * from './StepList/StepList';
