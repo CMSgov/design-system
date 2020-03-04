bb4330186d9258343fa41e2424462b5b9dac04c3
bernardwang <bw.vbnm@gmail.com>
2020-03-04 15:15:16 -0600

    Add keypress event for links styled as buttons

--- packages/core/src/components/Button/Button.jsx
+++ packages/core/src/components/Button/Button.jsx
@@ -23,6 +23,9 @@ export class Button extends React.PureComponent {
         );
       }
     }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   // Get an object of props to pass to the rendered <Button> component
@@ -52,16 +55,32 @@ export class Button extends React.PureComponent {
     };
 
     if (this.props.onClick) {
-      attrs.onClick = this.handleClick.bind(this);
+      attrs.onClick = this.handleClick;
+    }
+
+    if (component !== 'button' || this.props.href) {
+      // Assume `component` is not a <button> and remove <button> specific attributes
+      attrs.role = 'button';
+      delete attrs.disabled;
+      delete attrs.type;
     }
 
     return attrs;
   }
 
+  componentType() {
+    let component = this.props.component;
+    if (component === 'button' && this.props.href) {
+      // If `href` is provided and a custom component is not, we render `<a>` instead
+      component = 'a';
+    }
+    return component;
+  }
+
   classNames() {
     const variationClass = this.props.variation && `ds-c-button--${this.props.variation}`;
     const disabledClass =
-      this.props.disabled && (this.props.href || this.props.component) && 'ds-c-button--disabled';
+      this.props.disabled && this.componentType() !== 'button' && 'ds-c-button--disabled';
     const sizeClass = this.props.size && `ds-c-button--${this.props.size}`;
     const inverseClass = this.props.inverse && 'ds-c-button--inverse';
 
@@ -75,37 +94,39 @@ export class Button extends React.PureComponent {
     );
   }
 
+  handleKeyPress(e) {
+    // Trigger onClick on space key event for `<a>` elements
+    if (e.key === ' ') {
+      this.handleClick(e);
+    }
+  }
+
   handleClick(e) {
-    if (!this.props.disabled) {
+    if (!this.props.disabled && this.props.onClick) {
       this.props.onClick(e);
     }
   }
 
   render() {
     const attrs = this.attrs();
-    let ComponentType = 'button';
-
-    if (this.props.component) {
-      ComponentType = this.props.component;
-      // Assume `component` is not a <button>
-      delete attrs.disabled;
-      delete attrs.type;
-    } else if (this.props.href) {
-      ComponentType = 'a';
-      // Remove <button> specific attributes
-      delete attrs.disabled;
-      delete attrs.type;
-    }
+    const ComponentType = this.componentType();
 
     return (
-      <ComponentType ref={this.props.inputRef || this.props.buttonRef} {...attrs}>
+      <ComponentType
+        ref={this.props.inputRef || this.props.buttonRef}
+        onKeyPress={this.componentType() === 'a' ? this.handleKeyPress : undefined}
+        {...attrs}
+      >
         {this.props.children}
       </ComponentType>
     );
   }
 }
 
-Button.defaultProps = { type: 'button' };
+Button.defaultProps = {
+  type: 'button',
+  component: 'button'
+};
 Button.propTypes = {
   /**
    * Label text or HTML
