f6c8f91acc8cd0d8c94cfce35b2d8cc2c352bd66
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2020-03-18 10:42:53 -0700

    For the child design system where I'm executing the build script in the package directory, `./` won't work for gulp. For some reason `''` won't work either. Now I'm just resolving the paths up front so there's no ambiguity. The other changes are to make eslint happy.

--- packages/design-system-scripts/cli.js
+++ packages/design-system-scripts/cli.js
@@ -3,6 +3,9 @@
 const path = require('path');
 const yargs = require('yargs');
 
+// The yargs library actually made it so you have to access `.argv` at the end
+// or else it won't do anything. Not sure what the reasoning there was.
+// eslint-disable-next-line no-unused-expressions
 yargs
   .usage('Usage: $0 <command> [options]')
   .describe(
@@ -25,14 +28,16 @@ yargs
 function describeSourcePackageDir(yargs) {
   yargs.positional('sourcePackageDir', {
     desc: 'The relative path to your main design-system package (that contains a src directory)',
-    type: 'string'
+    type: 'string',
+    coerce: dir => path.resolve(dir)
   });
 }
 
-function describeDocsPackageDirs(yargs) {
-  yargs.positional('docsPackageDirs..', {
-    desc:
-      'The relative paths to one or more docs-package directories. The first directory will be the default set of docs, and every docs directory specified after it will override files in the previous one, where the rightmost directory path takes the most precedence.',
-    type: 'string'
-  });
-}
+// function describeDocsPackageDirs(yargs) {
+//   yargs.positional('docsPackageDirs..', {
+//     desc:
+//       'The relative paths to one or more docs-package directories. The first directory will be the default set of docs, and every docs directory specified after it will override files in the previous one, where the rightmost directory path takes the most precedence.',
+//     type: 'string',
+//     coerce: dirs => dirs.map(dir => path.resolve(dir))
+//   });
+// }
