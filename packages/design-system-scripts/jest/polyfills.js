f004ff20c39754aff463a22c396f1980acc26e4c
bernardwang <bw.vbnm@gmail.com>
2020-05-01 16:03:29 -0500

    Update jest polyfills

--- packages/design-system-scripts/jest/polyfills.js
+++ packages/design-system-scripts/jest/polyfills.js
@@ -1,3 +1,4 @@
 // Polyfills for e2e test files and React 16 compatibility
 // https://reactjs.org/docs/javascript-environment-requirements.html
 import 'core-js/stable';
+import 'regenerator-runtime/runtime';
