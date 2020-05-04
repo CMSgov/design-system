b816c1af1480371dbfe9c491085cd54c2ed9fabe
bernardwang <bw.vbnm@gmail.com>
2020-05-04 15:00:46 -0500

    Replace polyfill.js with react-app-polyfill/stable, move setupTest.js out of design-system-scripts package

--- packages/design-system-scripts/jest/unit.config.js
+++ packages/design-system-scripts/jest/unit.config.js
@@ -3,14 +3,10 @@ const path = require('path');
 module.exports = (rootDir) => ({
   rootDir,
   testURL: 'http://localhost',
-  setupFiles: [`<rootDir>/${path.relative(rootDir, __dirname)}/polyfills.js`],
-  setupFilesAfterEnv: [`<rootDir>/${path.relative(rootDir, __dirname)}/setupEnzyme.js`],
+  setupFiles: [require.resolve('react-app-polyfill/stable')],
+  setupFilesAfterEnv: [`<rootDir>/${path.relative(rootDir, 'setupTests.js')}`],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  testPathIgnorePatterns: [
-    '<rootDir>/*/dist/',
-    '<rootDir>/*/node_modules/',
-    '.+\\.e2e\\.test\\.js$',
-  ],
+  testPathIgnorePatterns: ['dist/', 'node_modules/', '.+\\.e2e\\.test\\.js$'],
   moduleNameMapper: {
     //  '^@cmsgov/design-system/(.*)$': '<rootDir>/packages/design-system/src/$1',
     //  '^@cmsgov/design-system$': '<rootDir>/packages/design-system/src/index.js'
