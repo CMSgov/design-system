babaa85c179d19097dc66cf736da5d4cd3fb0f81
bernardwang <bw.vbnm@gmail.com>
2020-05-04 14:38:44 -0500

    Add regenerator-runtime

--- packages/design-system-scripts/jest/polyfills.js
+++ packages/design-system-scripts/jest/polyfills.js
@@ -1,4 +1,6 @@
 // Polyfills for e2e test files and React 16 compatibility
 // https://reactjs.org/docs/javascript-environment-requirements.html
+// Identical to react-app-polyfill/stable
+// https://github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/stable.js
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
