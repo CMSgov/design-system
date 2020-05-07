76690c9cf73a05bd85ca330afe331ee9b84c1d6d
bernardwang <bw.vbnm@gmail.com>
2020-05-07 12:46:20 -0500

    Skip stats when building for e2e

--- packages/design-system-scripts/jest/e2e.global-setup.js
+++ packages/design-system-scripts/jest/e2e.global-setup.js
@@ -8,7 +8,7 @@ const APP_PORT = 3001;
 
 function buildApp() {
   log(chalk.green('\nBuilding docs site in production mode...\n'));
-  childProcess.execSync('yarn build', {
+  childProcess.execSync('yarn build --skipLatest', {
     stdio: ['ignore', 'ignore', process.stderr],
   });
   log(chalk.green('done ✓'));
