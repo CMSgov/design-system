011be1e832b0edf26288d9307db62e89abd7076d
bernardwang <bw.vbnm@gmail.com>
2020-05-11 16:29:50 -0500

    Update prepublish script and add release npm script

--- prepublish.sh
+++ prepublish.sh
@@ -2,14 +2,13 @@
 
 set -e
 
+RED="\033[0;31m"
 GREEN='\033[0;32m'
 NC='\033[0m' # No color
 
 echo "${GREEN}Pulling latest from GitHub...${NC}"
-git stash
-git fetch origin
 git checkout master
-git pull origin master
+git pull
 
 echo "${GREEN}Cleaning directory and fresh installing..${NC}"
 lerna clean --yes
@@ -19,10 +18,12 @@ echo "${GREEN}Building files and running tests..${NC}"
 yarn build
 yarn test
 yarn test:e2e --skipBuild
-backstop test
+#backstop test
 
-echo "${GREEN}Bumping version...${NC}"
-yarn lerna version --no-git-tag-version --force-publish
+echo "${GREEN}Bumping version and pushing tagged commit to Github...${NC}"
+yarn lerna version --force-publish
 
-echo "${GREEN}Creating release...${NC}"
-# Use github cli
+echo "${GREEN}Creating release zip...${NC}"
+npm pack ./packages/design-system/
+
+echo "${GREEN}Prepublish complete...${NC}"
