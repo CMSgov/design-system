cc85910599bba6681b9e0bc79f32dd1b136524a6
bernardwang <bw.vbnm@gmail.com>
2020-05-12 16:30:04 -0500

    Manually create and push release branch because we don't allow pushing to master

--- prepublish.sh
+++ prepublish.sh
@@ -11,19 +11,27 @@ git checkout master
 git pull
 
 echo "${GREEN}Cleaning directory and fresh installing..${NC}"
-lerna clean --yes
+rm -rf **/node_modules
+# We need to delete the package-lock.json created by npm link in design-system-scripts
+# to prevent an error later on in `lerna version`
+rm -rf **/package-lock.json
 yarn install
 
 echo "${GREEN}Building files and running tests..${NC}"
 yarn build
 yarn test
 yarn test:e2e --skipBuild
-#backstop test
 
-echo "${GREEN}Bumping version and pushing tagged commit to Github...${NC}"
-yarn lerna version --force-publish
+echo "${GREEN}Bumping version and creating tagged release commit...${NC}"
+yarn lerna version --no-push --force-publish
+
+echo "${GREEN}Pushing release commit to Github...${NC}"
+PACKAGE_VERSION=$(node -pe "require('./lerna.json').version")
+TAG_PREFIX=$(node -pe "require('./lerna.json').tagVersionPrefix")
+git checkout -b $TAG_PREFIX$PACKAGE_VERSION
+git push --set-upstream origin $TAG_PREFIX$PACKAGE_VERSION
 
 echo "${GREEN}Creating release zip...${NC}"
 npm pack ./packages/design-system/
 
-echo "${GREEN}Prepublish complete...${NC}"
+echo "${GREEN}Prepublish complete, make sure to merge the release branch $TAG_PREFIX$PACKAGE_VERSION into master...${NC}"
