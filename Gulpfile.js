fc1c3805c36c230c196ad1488ac4d7776e0fd8ff
Bernard <bernardwang@users.noreply.github.com>
2020-05-18 09:52:30 -0500

    [WNMGDS-422] Create release scripts (#695)
    * Move publishing scripts
    
    * Use lerna version instead of publish
    
    * Lint files
    
    * Update prepublish script and add release npm script
    
    * Fix e2e tests
    
    * Misc dev file clean up
    
    * Manually create and push release branch because we don't allow pushing to master
    
    * Update release notes
    
    * Fix directory cleaning and update yarn.lock
    
    * Upgrade to lerna 3
    
    * Update release notes
    
    * Prevent package-lock.json lerna error
    
    * Fix release branch naming and tag
    
    * PR feedback
    
    Co-authored-by: Scott Weber <scott.weber@adhocteam.us>

--- Gulpfile.js
+++
@@ -1,8 +0,0 @@
-/**
- * Don't touch this file! Gulp tasks should be configured in the tools/gulp
- * directory. Shared configuration options can be added to tools/gulp/index.js
- */
-'use strict';
-
-const gulp = require('gulp');
-require('./tools/gulp')(gulp);
