d220c94e48603daf647f18dd0e9960b5c820946c
Ni Chia <ni.chia@adhocteam.us>
2020-06-10 07:46:42 -0400

    Fix reactPropDoc testing

--- packages/design-system-docs/src/scripts/components/__tests__/ReactPropDoc.test.js
+++ packages/design-system-docs/src/scripts/components/__tests__/ReactPropDoc.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactPropDoc from '../ReactPropDoc';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 /**
  * Helper method for shallow rendering the <ReactPropDoc> component. The only props
@@ -8,7 +8,7 @@ import { shallow } from 'enzyme';
  * @param {object} customProps - Additional props
  * @return {object}
  */
-function shallowRender(customProps = {}) {
+function render(customProps = {}) {
   const props = Object.assign(
     {
       name: 'foo',
@@ -20,43 +20,43 @@ function shallowRender(customProps = {}) {
 
   return {
     props: props,
-    wrapper: shallow(<ReactPropDoc {...props} />),
+    wrapper: mount(<ReactPropDoc {...props} />),
   };
 }
 
 describe('ReactPropDoc', () => {
   it('should render name', () => {
-    const data = shallowRender();
+    const data = render();
 
     expect(data.wrapper.find('td').first().text()).toMatch(data.props.name);
   });
 
   it('should render "required"', () => {
-    const data = shallowRender({ required: true });
+    const data = render({ required: true });
 
     expect(data.wrapper.find('td .ds-c-badge').first().text()).toBe('Required');
   });
 
   it('should render type', () => {
-    const data = shallowRender();
+    const data = render();
 
     expect(data.wrapper.find('td').at(1).text()).toMatch(data.props.type.name);
   });
 
   it('should render default value', () => {
-    const data = shallowRender({ defaultValue: { value: 'bar' } });
+    const data = render({ defaultValue: { value: 'bar' } });
 
     expect(data.wrapper.find('td').at(2).text()).toMatch(data.props.defaultValue.value);
   });
 
   it('should render description', () => {
-    const data = shallowRender({ description: 'bar' });
+    const data = render({ description: 'bar' });
 
     expect(data.wrapper.find('td').at(3).html()).toMatch(/bar/);
   });
 
   it('should render shape for PropType.arrayOf(PropType.shape)', () => {
-    const data = shallowRender({
+    const data = render({
       type: {
         name: 'arrayOf',
         value: {
@@ -77,7 +77,7 @@ describe('ReactPropDoc', () => {
   });
 
   it('should render computed shape value', () => {
-    const data = shallowRender({
+    const data = render({
       type: {
         name: 'arrayOf',
         value: {
@@ -92,7 +92,7 @@ describe('ReactPropDoc', () => {
   });
 
   it('should render valid values of PropType.oneOf', () => {
-    const data = shallowRender({
+    const data = render({
       type: {
         name: 'union',
         value: [{ name: 'string' }, { name: 'bool' }],
