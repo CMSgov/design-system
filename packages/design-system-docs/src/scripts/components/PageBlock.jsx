cbf3b1b3c61c0dd1c79d7e07161dcda78044c8c5
line47 <scott.weber@adhocteam.us>
2020-06-15 09:14:27 -0400

    Add ability to put status badge on page section

--- packages/design-system-docs/src/scripts/components/PageBlock.jsx
+++ packages/design-system-docs/src/scripts/components/PageBlock.jsx
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import ReactContent from './ReactContent';
 import Source from './Source';
+import classNames from 'classnames';
 
 class PageBlock extends React.PureComponent {
   markupExamples() {
@@ -42,6 +43,24 @@ class PageBlock extends React.PureComponent {
     );
   }
 
+  statusBadge() {
+    if (this.props.status) {
+      const classes = classNames(
+        'ds-c-badge ds-u-margin-right--1 ds-u-text-transform--capitalize',
+        {
+          'ds-c-badge--success': this.props.status === 'Ready',
+          'ds-c-badge--alert': this.props.status === 'Draft',
+        }
+      );
+
+      return (
+        <div>
+          <span className={classes}>{this.props.status}</span>
+        </div>
+      );
+    }
+  }
+
   description() {
     if (this.props.description) {
       return (
@@ -55,6 +74,15 @@ class PageBlock extends React.PureComponent {
     }
   }
 
+  GitHubSource() {
+    if (this.props.hideHeader || this.props.header.match(/---/)) return;
+
+    const source = this.props.reactComponentPath && (
+      <Source key="reactSource" reactComponentPath={this.props.reactComponentPath} />
+    );
+    return [source];
+  }
+
   header() {
     // The regex conditional allows us to create KSS comments that provide additional
     // descriptive text below a section that already has a title. For example,
@@ -63,33 +91,31 @@ class PageBlock extends React.PureComponent {
     // text below the title + code snippet block. It's hacky, but works.
     if (this.props.hideHeader || this.props.header.match(/---/)) return;
 
-    const source = this.props.reactComponentPath && (
-      <Source key="reactSource" reactComponentPath={this.props.reactComponentPath} />
-    );
+    const subheader = this.props.reactComponentPath && <span key="subheader">React&nbsp;</span>;
 
-    const subheader = this.props.reactComponentPath && (
-      <span className="ds-h6" key="subheader">
-        React
-      </span>
+    const subheaderComponent = this.props.reactComponentPath && (
+      <span key="subheader">&nbsp;Component</span>
     );
 
     return [
       subheader,
-      <h2
+      <span
         className="ds-h2 ds-u-margin-top--0"
         // Headers can contain HTML markup, therefore dangerously set...
         dangerouslySetInnerHTML={{ __html: this.props.header }}
         id={this.props.reference}
         key="header"
       />,
-      source,
+      subheaderComponent,
     ];
   }
 
   render() {
     return (
       <article className="ds-u-margin-y--3 ds-u-sm-margin-y--6 l-content">
-        {this.header()}
+        <h2>{this.header()}</h2>
+        {this.statusBadge()}
+        {this.GitHubSource()}
         {this.description()}
         {this.markupExamples()}
         <ReactContent
@@ -115,6 +141,7 @@ PageBlock.propTypes = {
   reactExampleSource: ReactContent.propTypes.reactExampleSource,
   reference: PropTypes.string,
   responsive: PropTypes.bool,
+  status: PropTypes.string,
 };
 
 export default PageBlock;
