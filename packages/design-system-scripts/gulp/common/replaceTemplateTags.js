f6e8e0579726d458db1522b0bc9d86a91abe9110
bernardwang <bw.vbnm@gmail.com>
2020-07-09 11:48:05 -0500

    Add npm package option and add templates

--- packages/design-system-scripts/gulp/common/replaceTemplateTags.js
+++ packages/design-system-scripts/gulp/common/replaceTemplateTags.js
@@ -8,6 +8,27 @@ function replaceTemplateTags(str, options) {
     str = str.replace(/{{root}}/g, '');
   }
 
+  if (options.npmPackage) {
+    str = str.replace(/{{npm}}/g, `${options.npmPackage}`);
+  } else {
+    str = str.replace(/{{npm}}/g, '');
+    console.log(str);
+  }
+
+  if (options.githubUrl) {
+    str = str.replace(/{{github}}/g, `${options.githubUrl}`);
+  } else {
+    str = str.replace(/{{github}}/g, '');
+    console.log(str);
+  }
+
+  if (options.name) {
+    str = str.replace(/{{name}}/g, `${options.name}`);
+  } else {
+    str = str.replace(/{{name}}/g, '');
+    console.log(str);
+  }
+
   return str;
 }
 
