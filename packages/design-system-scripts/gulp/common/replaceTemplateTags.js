31f0072485c75d903376a11882d7835296ebfe8b
bernardwang <bw.vbnm@gmail.com>
2020-07-17 12:57:14 -0500

    Fix template tags in KSS files

--- packages/design-system-scripts/gulp/common/replaceTemplateTags.js
+++ packages/design-system-scripts/gulp/common/replaceTemplateTags.js
@@ -2,28 +2,31 @@
  * Replace template tags with string values
  */
 function replaceTemplateTags(str, options) {
+  // KSS processes the markdown in documentation content before we can replace the tags
+  // So unfortunately we need to match for the encoded version of the template tags
+  // When KSS is removed we can update the regex here
   if (options.rootPath) {
-    str = str.replace(/{{root}}/g, `/${options.rootPath}`);
+    str = str.replace(/{{root}}|%7B%7Broot%7D%7D/g, `/${options.rootPath}`);
   } else {
-    str = str.replace(/{{root}}/g, '');
+    str = str.replace(/{{root}}|%7B%7Broot%7D%7D/g, '');
   }
 
   if (options.npmPackage) {
-    str = str.replace(/{{npm}}/g, `${options.npmPackage}`);
+    str = str.replace(/{{npm}}|%7B%7Bnpm%7D%7D/g, `${options.npmPackage}`);
   } else {
-    str = str.replace(/{{npm}}/g, '');
+    str = str.replace(/{{npm}}|%7B%7Bnpm%7D%7D/g, '');
   }
 
   if (options.githubUrl) {
-    str = str.replace(/{{github}}/g, `${options.githubUrl}`);
+    str = str.replace(/{{github}}|%7B%7Bgithub%7D%7D/g, `${options.githubUrl}`);
   } else {
-    str = str.replace(/{{github}}/g, '');
+    str = str.replace(/{{github}}|%7B%7Bgithub%7D%7D/g, '');
   }
 
   if (options.name) {
-    str = str.replace(/{{name}}/g, `${options.name}`);
+    str = str.replace(/{{name}}|%7B%7Bname%7D%7D/g, `${options.name}`);
   } else {
-    str = str.replace(/{{name}}/g, '');
+    str = str.replace(/{{name}}|%7B%7Bname%7D%7D/g, '');
   }
 
   return str;
