0511f192fb683d5f0921062b75660625c0912b38
bernardwang <bw.vbnm@gmail.com>
2020-07-20 11:25:14 -0500

    Update replace template tag function

--- packages/design-system-scripts/gulp/common/replaceTemplateTags.js
+++ packages/design-system-scripts/gulp/common/replaceTemplateTags.js
@@ -5,31 +5,12 @@ function replaceTemplateTags(str, options) {
   // KSS processes the markdown in documentation content before we can replace the tags
   // So unfortunately we need to match for the encoded version of the template tags
   // When KSS is removed we can update the regex here
-  if (options.rootPath) {
-    str = str.replace(/{{root}}|%7B%7Broot%7D%7D/g, `/${options.rootPath}`);
-  } else {
-    str = str.replace(/{{root}}|%7B%7Broot%7D%7D/g, '');
-  }
-
-  if (options.npmPackage) {
-    str = str.replace(/{{npm}}|%7B%7Bnpm%7D%7D/g, `${options.npmPackage}`);
-  } else {
-    str = str.replace(/{{npm}}|%7B%7Bnpm%7D%7D/g, '');
-  }
-
-  if (options.githubUrl) {
-    str = str.replace(/{{github}}|%7B%7Bgithub%7D%7D/g, `${options.githubUrl}`);
-  } else {
-    str = str.replace(/{{github}}|%7B%7Bgithub%7D%7D/g, '');
-  }
-
-  if (options.name) {
-    str = str.replace(/{{name}}|%7B%7Bname%7D%7D/g, `${options.name}`);
-  } else {
-    str = str.replace(/{{name}}|%7B%7Bname%7D%7D/g, '');
-  }
-
-  return str;
+  const tagRegex = tag => new RegExp(`${tag}|${encodeURI(tag)}`, 'g');
+  return str
+    .replace(tagRegex('{{root}}'), options.rootPath ? `/${options.rootPath}` : '')
+    .replace(tagRegex('{{npm}}'), options.npmPackage ? `${options.npmPackage}` : '')
+    .replace(tagRegex('{{github}}'), options.githubUrl ? `${options.githubUrl}` : '')
+    .replace(tagRegex('{{name}}'), options.name ? `${options.name}` : '')
 }
 
 module.exports = replaceTemplateTags;
