2cd2eedeec768eec4df68d3780f3a8ce5d3e6526
bernardwang <bw.vbnm@gmail.com>
2020-07-28 12:08:20 -0500

    Add comments on the alias import shorthand

--- examples/child-design-system/docs/src/pages/components/Card/Card.example.jsx
+++ examples/child-design-system/docs/src/pages/components/Card/Card.example.jsx
@@ -1,4 +1,9 @@
-// Example files in the docs directory can use the @src alias to import from the design system source directory
+// CMSDS scripts uses webpack's `alias` resolve option to allow for this shorthand import
+// https://webpack.js.org/configuration/resolve/#resolvealias
+//
+// Because the react example files are located separately from the design system source directory (`sourceDir` in `cmsds.config.js`),
+// We provide the `@src` alias to allow for easy imports from `sourceDir`
+// This is also possible in typescript child design systems via the `paths` compiler option in the `tsconfig.json`
 import Card from '@src/components/Card/Card';
 import React from 'react';
 import ReactDOM from 'react-dom';
