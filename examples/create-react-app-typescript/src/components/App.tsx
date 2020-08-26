987199cb77fdb36159610c252edd0deb3b2ca7e2
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2020-08-26 14:54:01 -0700

    Use the result of the getMonthNames call so we don't get linter errors

--- examples/create-react-app-typescript/src/components/App.tsx
+++ examples/create-react-app-typescript/src/components/App.tsx
@@ -1,4 +1,4 @@
-import { Badge, MonthPicker, getMonthNames } from '@cmsgov/design-system';
+import { Alert, Badge, MonthPicker, getMonthNames } from '@cmsgov/design-system';
 import React, { SyntheticEvent, useState } from 'react';
 import usflag from '../images/us_flag_small.png';
 
@@ -6,6 +6,7 @@ const LOCALE = "en";
 const NUM_MONTHS = 12;
 const monthNumbers: number[] = Array.from(new Array(NUM_MONTHS), (_, i) => i + 1);
 const monthNames = getMonthNames(LOCALE);
+console.log(monthNames)
 
 function App() {
   const [selectedMonths, setSelectedMonths] = useState<number[]>([]);
@@ -18,7 +19,7 @@ function App() {
     } else {
       months.push(month);
     }
-    setSelectedMonths(months);
+    setSelectedMonths(months.sort((a, b) => a - b));
   };
 
   const handleSelectAll = () => {
@@ -53,6 +54,18 @@ function App() {
           onClearAll={handleClearAll}
           locale={LOCALE}
         />
+
+        {selectedMonths.length > 0 && (
+          <Alert heading="Your favorite months" hideIcon>
+            <ul>
+              {selectedMonths.map(monthNumber => (
+                <li key={monthNumber}>
+                  {monthNames[monthNumber - 1]}
+                </li>
+              ))}
+            </ul>
+          </Alert>
+        )}
       </div>
     </div>
   );
