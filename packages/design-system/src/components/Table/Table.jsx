4ae262564c1814bbfe9ee03182e9d350b266935e
Ni Chia <ni.chia@adhocteam.us>
2020-09-01 10:50:06 -0400

    Update packages/design-system/src/components/Table/Table.jsx
    Co-authored-by: Bernard <bernardwang@users.noreply.github.com>

--- packages/design-system/src/components/Table/Table.jsx
+++ packages/design-system/src/components/Table/Table.jsx
@@ -177,7 +177,7 @@ Table.propTypes = {
    */
   stackable: PropTypes.bool,
   /**
-   * Applies responsive styles to vertically stacked rows at different viewpoint sizes.
+   * The viewpoint size at which responsive vertically stacked row styles are applied. Only used when the `stackable` prop is set to `true`.
    */
   stackableBreakpoint: PropTypes.oneOf(['sm', 'md', 'lg']),
   /**
