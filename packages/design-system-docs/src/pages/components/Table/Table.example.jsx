cbf9061a4400ff9f57b9f58778be1ec14b16c114
Ni Chia <ni.chia@adhocteam.us>
2020-09-01 14:08:44 -0400

    Remove unnecessary attributes from the scrollable example

--- packages/design-system-docs/src/pages/components/Table/Table.example.jsx
+++ packages/design-system-docs/src/pages/components/Table/Table.example.jsx
@@ -11,57 +11,44 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <>
-    <Table scrollable>
+    <Table scrollable striped>
       <TableCaption>Responsive scrollable table</TableCaption>
       <TableHead>
         <TableRow>
-          <TableCell id="column_1">Document title</TableCell>
-          <TableCell id="column_2">Description</TableCell>
-          <TableCell id="column_3">Links</TableCell>
-          <TableCell id="column_4">Year</TableCell>
+          <TableCell>Document title</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell>Links</TableCell>
+          <TableCell>Year</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
         <TableRow>
-          <TableCell stackedTitle="Document title" headers="column_1">
-            Declaration of Independence
-          </TableCell>
-          <TableCell stackedTitle="Description" headers="column_2">
+          <TableCell>Declaration of Independence</TableCell>
+          <TableCell>
             Statement adopted by the Continental Congress declaring independence from the British
             Empire.
           </TableCell>
-          <TableCell stackedTitle="Links" headers="column_3">
-            <em>
-              <a href="# ">
-                https://billofrightsinstitute.org/founding-documents/declaration-of-independence/
-              </a>
-            </em>
+          <TableCell>
+            <a href="# ">
+              https://billofrightsinstitute.org/founding-documents/declaration-of-independence/
+            </a>
           </TableCell>
-          <TableCell stackedTitle="Year" headers="column_4">
-            1776
-          </TableCell>
+          <TableCell>1776</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell stackedTitle="Document title" headers="column_1">
-            Bill of Rights
-          </TableCell>
-          <TableCell stackedTitle="Description" headers="column_2">
+          <TableCell>Bill of Rights</TableCell>
+          <TableCell>
             The first ten amendments of the U.S. Constitution guaranteeing rights and freedoms.
           </TableCell>
-          <TableCell stackedTitle="Links" headers="column_3">
-            <em>
-              <a href="# ">https://billofrightsinstitute.org/founding-documents/bill-of-rights/</a>
-            </em>
+          <TableCell>
+            <a href="# ">https://billofrightsinstitute.org/founding-documents/bill-of-rights/</a>
           </TableCell>
-          <TableCell stackedTitle="Year" headers="column_4">
-            1791
-          </TableCell>
+          <TableCell>1791</TableCell>
         </TableRow>
       </TableBody>
     </Table>
 
-    <hr className="ds-u-margin-top--2" />
-    <Table stackable stackableBreakpoint="md">
+    <Table className="ds-u-margin-top--2" stackable stackableBreakpoint="md">
       <TableCaption>Stackable table</TableCaption>
       <TableHead>
         <TableRow>
