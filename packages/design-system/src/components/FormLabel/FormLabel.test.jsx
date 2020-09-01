d99f4880960d1a62f143a096f81c838213285025
Scott Weber <scott.weber@adhocteam.us>
2020-09-01 11:01:46 -0400

    [WNMGDS-487] Add aria-label to FormLabel label and ChoiceList legend (#795)
    * add aria-label to FormLabel component
    
    * Add aria-label to ChoiceList component legend
    
    * update form label to add ...labelProps
    
    * Revert "Add aria-label to ChoiceList component legend"
    
    This reverts commit cd0a382c6550171e533f983ac1fe25def9bf8e05.
    
    * Add props to FormLabel
    
    * Update snapshots
    
    * Update FormLabel Typescript
    
    * Update failing datefield snapshot
    
    * add hint and component props
    
    * update snapshots
    
    * update test and snapshot
    
    * update to extend with HTML label and legend
    
    * Update form label test

--- packages/design-system/src/components/FormLabel/FormLabel.test.jsx
+++ packages/design-system/src/components/FormLabel/FormLabel.test.jsx
@@ -101,10 +101,11 @@ describe('FormLabel', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('supports additional classNames', () => {
-    const props = { className: 'ds-u-foo', labelClassName: 'ds-u-bar' };
+  it('supports additional attributes', () => {
+    const props = { className: 'ds-u-foo', ariaLabel: 'testing aria' };
     const wrapper = shallow(<FormLabel {...props}>{labelText}</FormLabel>);
 
+    expect(wrapper.prop('ariaLabel')).toBe('testing aria');
     expect(wrapper).toMatchSnapshot();
   });
 });
