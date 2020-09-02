c5ec90f3b34ff2b88181bf5482627a06293ce3c1
bernardwang <bw.vbnm@gmail.com>
2020-09-02 14:29:24 -0500

    PR feedback

--- packages/design-system-docs/src/pages/components/ChoiceList/Choice.example.jsx
+++ packages/design-system-docs/src/pages/components/ChoiceList/Choice.example.jsx
@@ -7,56 +7,58 @@ const childField = (
 );
 
 ReactDOM.render(
-  <>
-    <Choice name="checkbox_choice" type="checkbox" label="Checkbox A" value="a" />
-    <Choice
-      defaultChecked
-      name="checkbox_choice_checked"
-      type="checkbox"
-      label="Checkbox B with checked children"
-      value="b"
-      checkedChildren={<div className="ds-c-choice__checkedChild">{childField}</div>}
-    />
-    <Choice
-      defaultChecked
-      name="checkbox_choice_unchecked"
-      type="checkbox"
-      label="Checkbox C with unchecked children"
-      value="c"
-      uncheckedChildren={<div className="ds-c-choice__checkedChild">{childField}</div>}
-    />
-    <Choice
-      name="checkbox_choice_disabled"
-      type="checkbox"
-      label="Disabled Checkbox d"
-      value="d"
-      disabled
-    />
-    <Choice
-      name="checkbox_choice_small"
-      size="small"
-      type="checkbox"
-      label="Small Checkbox E"
-      value="e"
-    />
-    <Choice name="radio_choice" type="radio" label="Radio A" value="a" />
-    <Choice
-      name="radio_choice_checked"
-      type="radio"
-      label="Radio B with checked children"
-      value="b"
-      checkedChildren={<div className="ds-c-choice__checkedChild">{childField}</div>}
-    />
-    <Choice
-      defaultChecked
-      name="radio_choice_unchecked"
-      type="radio"
-      label="Radio C with unchecked children"
-      value="c"
-      uncheckedChildren={<div className="ds-c-choice__checkedChild">{childField}</div>}
-    />
-    <Choice name="radio_choice_disabled" type="radio" label="Disabled Radio D" value="d" disabled />
-    <Choice name="radio_choice_small" size="small" type="radio" label="Small Radio E" value="e" />
-  </>,
+  <div className="example--wrapper">
+    <fieldset className="ds-c-fieldset">
+      <legend className="ds-c-label">
+        <span>Custom checkbox inputs</span>
+      </legend>
+      <Choice name="checkbox_choice" type="checkbox" label="Checkbox A" value="a" />
+      <Choice
+        defaultChecked
+        name="checkbox_choice"
+        type="checkbox"
+        label="Checkbox B with checked children"
+        value="b"
+        checkedChildren={<div className="ds-c-choice__checkedChild">{childField}</div>}
+      />
+      <Choice
+        defaultChecked
+        name="checkbox_choice"
+        type="checkbox"
+        label="Checkbox C with unchecked children"
+        value="c"
+        uncheckedChildren={<div className="ds-c-choice__checkedChild">{childField}</div>}
+      />
+      <Choice
+        name="checkbox_choice"
+        type="checkbox"
+        label="Disabled Checkbox d"
+        value="d"
+        disabled
+      />
+    </fieldset>
+    <fieldset className="ds-c-fieldset">
+      <legend className="ds-c-label">
+        <span>Custom radio inputs</span>
+      </legend>
+      <Choice name="radio_choice" type="radio" label="Radio A" value="a" />
+      <Choice
+        name="radio_choice"
+        type="radio"
+        label="Radio B with checked children"
+        value="b"
+        checkedChildren={<div className="ds-c-choice__checkedChild">{childField}</div>}
+      />
+      <Choice
+        defaultChecked
+        name="radio_choice"
+        type="radio"
+        label="Radio C with unchecked children"
+        value="c"
+        uncheckedChildren={<div className="ds-c-choice__checkedChild">{childField}</div>}
+      />
+      <Choice name="radio_choice" type="radio" label="Disabled Radio D" value="d" disabled />
+    </fieldset>
+  </div>,
   document.getElementById('js-example')
 );
