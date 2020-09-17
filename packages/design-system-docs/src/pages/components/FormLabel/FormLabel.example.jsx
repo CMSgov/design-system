33d9a59d412d447bc1842da7d00953f7666f715f
Bernard <bernardwang@users.noreply.github.com>
2020-09-17 10:27:01 -0500

    [WNMGDS-622] Update inverse examples (#813)
    * Add inverse examples to all form examples, make html and react examples consistent
    
    * Example tweaks
    
    * backstop update
    
    * Update packages/design-system-docs/src/pages/components/FormLabel/form-label.example.html
    
    Co-authored-by: Scott Weber <scott.weber@adhocteam.us>
    
    * Update packages/design-system-docs/src/pages/components/DateField/datefield.example.html
    
    Co-authored-by: Ni Chia <ni.chia@adhocteam.us>
    
    * Update dropdown labels
    
    * Fix choice e2e test
    
    Co-authored-by: Scott Weber <scott.weber@adhocteam.us>
    Co-authored-by: Ni Chia <ni.chia@adhocteam.us>

--- packages/design-system-docs/src/pages/components/FormLabel/FormLabel.example.jsx
+++ packages/design-system-docs/src/pages/components/FormLabel/FormLabel.example.jsx
@@ -3,13 +3,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <FormLabel
-    errorMessage="Please enter a valid phone number."
-    hint="We'll use this number as a backup if we need to contact you about your application."
-    fieldId="number-field-id"
-    requirementLabel="Optional"
-  >
-    Phone number
-  </FormLabel>,
+  <div className="example--wrapper">
+    <FormLabel
+      errorMessage="Please enter a valid phone number."
+      hint="We'll use this number as a backup if we need to contact you about your application."
+      fieldId="number-field-id"
+      requirementLabel="Optional"
+    >
+      Phone number
+    </FormLabel>
+    <div className="example--wrapper example--inverse">
+      <FormLabel
+        errorMessage="Example error message"
+        hint="Helpful hint text"
+        fieldId="inverse-field-id"
+        inversed
+      >
+        Inverse example
+      </FormLabel>
+    </div>
+  </div>,
   document.getElementById('js-example')
 );
--- packages/design-system-docs/src/pages/components/TextField/TextField.example.jsx
+++ packages/design-system-docs/src/pages/components/TextField/TextField.example.jsx
@@ -6,28 +6,38 @@ ReactDOM.render(
   <div>
     <TextField
       defaultValue="Example value"
-      label="Single line"
+      label="Single line field"
       labelClassName="ds-u-margin-top--0"
       name="single_example"
-      requirementLabel="Optional"
     />
     <TextField defaultValue="123" label="Number field" name="number_example" numeric />
-    <TextField label="Small size modifier" name="small_example" size="small" />
-    <TextField label="Medium size modifier" name="medium_example" size="medium" />
-    <TextField
-      errorMessage="Error message example"
-      hint="Example of a multiline field with an error"
-      label="Multiline"
-      multiline
-      name="multiline_example"
-    />
-    <TextField label="Disabled field" disabled name="disabled_example" value="Example value" />
     <TextField
       label="Password field"
-      name="disabled_example"
+      name="password_example"
       type="password"
       value="Example value"
     />
+    <TextField label="Small size field" name="small_example" size="small" />
+    <TextField label="Medium size field" name="medium_example" size="medium" />
+    <TextField label="Multiline field" multiline name="multiline_example" />
+    <TextField
+      errorMessage="Example error message"
+      hint="Helpful hint text"
+      label="Error field"
+      name="error_example"
+    />
+    <TextField label="Success field" fieldClassName="ds-c-field--success" name="success_example" />
+    <TextField label="Disabled field" disabled name="disabled_example" />
+    <div className="example--wrapper example--inverse">
+      <TextField
+        labelClassName="ds-u-margin-top--0"
+        errorMessage="Example error message"
+        hint="Helpful hint text"
+        label="Inverse example"
+        name="inverse_example"
+        inversed
+      />
+    </div>
   </div>,
   document.getElementById('js-example')
 );
