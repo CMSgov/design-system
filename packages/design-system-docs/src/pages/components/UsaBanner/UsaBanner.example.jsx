f791ced5528ba45b5bb0402ac3d84a04cb12bec3
line47 <scott.weber@adhocteam.us>
2020-11-06 14:30:26 -0500

    Update example

--- packages/design-system-docs/src/pages/components/UsaBanner/UsaBanner.example.jsx
+++ packages/design-system-docs/src/pages/components/UsaBanner/UsaBanner.example.jsx
@@ -2,22 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { UsaBanner } from '@cmsgov/design-system';
 
-class UsaBannerExample extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { isBannerOpen: false };
-    this.handleToggleBanner = this.handleToggleBanner.bind(this);
-  }
-
-  handleToggleBanner() {
-    this.setState({ isBannerOpen: !this.state.isBannerOpen });
-  }
-
-  render() {
-    return (
-      <UsaBanner isBannerOpen={this.state.isBannerOpen} onToggleBanner={this.handleToggleBanner} />
-    );
-  }
-}
-
-ReactDOM.render(<UsaBannerExample />, document.getElementById('js-example'));
+ReactDOM.render(<UsaBanner />, document.getElementById('js-example'));
