3691a4e9bce3c4cd5e629d8020bd3b63f03d6a32
line47 <scott.weber@adhocteam.us>
2020-11-09 15:17:03 -0500

    Test updates

--- packages/design-system/src/components/UsaBanner/UsaBanner.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import UsaBanner from './UsaBanner';
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<UsaBanner />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
