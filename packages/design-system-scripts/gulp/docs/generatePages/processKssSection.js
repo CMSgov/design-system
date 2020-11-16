f471d9bcabfff8e550a3ee2effbaab3e4353fb04
Ni Chia <ni.chia@adhocteam.us>
2020-11-16 14:29:57 -0500

    Update utilities weight to ensure Layout Grid appears first

--- packages/design-system-scripts/gulp/docs/generatePages/processKssSection.js
+++ packages/design-system-scripts/gulp/docs/generatePages/processKssSection.js
@@ -39,6 +39,7 @@ function processKssSection(kssSection, options) {
   section = processFlags(section);
   section.description = replaceTemplateTags(section.description, options);
   section.referenceURI = section.reference.replace(/\./g, '/');
+  section.weight = updateUtilitiesWeight(section);
 
   // We only need to support Markdown's code syntax in headers, so we manually
   // parse those rather than running it through the marked library.
@@ -96,6 +97,22 @@ function processFlags(section) {
       .trim();
   }
   return section;
+}
+
+/**
+ * For sections with parent URI of `utilities`, ensure Layout Grid appears first by
+ * increamenting the `weight` of sections which is not a `grid`
+ * @param {Object} section
+ * @return {Object.property} section.weight
+ */
+function updateUtilitiesWeight(section) {
+  if (section.referenceURI.indexOf('utilities/') === 0) {
+    if (section.referenceURI.indexOf('utilities/grid') !== 0) {
+      section.weight++;
+    }
+  }
+
+  return section.weight;
 }
 
 function hrefUrl(str) {
