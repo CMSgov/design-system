65ce7157a9d65e1823b1452805210be4b3461cbc
Ni Chia <ni.chia@adhocteam.us>
2020-11-25 09:29:09 -0500

    Add @babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -14,7 +14,11 @@ module.exports = function (api) {
     '@babel/preset-react',
   ];
 
-  const plugins = ['@babel/plugin-transform-object-assign', 'inline-react-svg'];
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-object-assign',
+    'inline-react-svg',
+  ];
 
   return {
     presets,
