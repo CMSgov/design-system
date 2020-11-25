60c3792dab60e0207a8b89131a2380bf130dd267
Ni Chia <ni.chia@adhocteam.us>
2020-11-25 09:58:21 -0500

    Add babel-eslint for parsing of js ES6~7 syntax like 'static'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: '@cmsgov/eslint-config-design-system',
+  parser: 'babel-eslint',
   rules: {
     // Avoid exploits. If you need dangerouslySetInnerHTML, then temporarily
     // disable this rule in the script rather than removing it from here.
