b3bf4fc2c6605dce5e5ff266f0cb6420e58574f4
Ni Chia <ni.chia@adhocteam.us>
2020-11-27 13:47:28 -0500

    Add prop for rendering USA banner in Spanish

--- packages/design-system/src/components/UsaBanner/UsaBanner.jsx
+++ packages/design-system/src/components/UsaBanner/UsaBanner.jsx
@@ -1,10 +1,11 @@
 import DotGovIcon from '../../images/usa-banner-dot-gov.svg';
 import HttpsIcon from '../../images/usa-banner-https.svg';
 import LockIcon from '../../images/usa-banner-lock.svg';
+import PropTypes from 'prop-types';
 import React from 'react';
 import UsaFlagIcon from '../../images/usa-banner-flag.svg';
 
-class UsaBanner extends React.PureComponent {
+export class UsaBanner extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = { isBannerOpen: false };
@@ -16,6 +17,55 @@ class UsaBanner extends React.PureComponent {
   }
 
   render() {
+    const tld = `.gov`;
+
+    const lockImg = (
+      <span>
+        <LockIcon className="ds-c-usa-banner__lock-image" />
+      </span>
+    );
+
+    const bannerText = this.props.localeSpanish
+      ? `Un sitio oficial del Gobierno de Estados Unidos`
+      : `An official website of the United States government`;
+
+    const bannerActionText = this.props.localeSpanish
+      ? `Así es como usted puede verificarlo`
+      : `Here’s how you know`;
+
+    const domainHeadingText = this.props.localeSpanish
+      ? `Los sitios web oficiales usan ${tld}`
+      : `Official websites use ${tld}`;
+
+    const domainText = this.props.localeSpanish ? (
+      <span>
+        Un sitio web <strong>{tld}</strong> pertenece a una organización oficial del Gobierno de
+        Estados Unidos.
+      </span>
+    ) : (
+      <span>
+        A <strong>{tld}</strong> website belongs to an official government organization in the
+        United States.
+      </span>
+    );
+
+    const httpsHeadingText = this.props.localeSpanish
+      ? `Los sitios web seguros ${tld} usan HTTPS`
+      : `Secure ${tld} websites use HTTPS`;
+
+    const httpsText = this.props.localeSpanish ? (
+      <span>
+        Un <strong>candado</strong> {lockImg} o <strong>https://</strong> significa que usted se
+        conectó de forma segura a un sitio web {tld}. Comparta información sensible sólo en sitios
+        web oficiales y seguros.
+      </span>
+    ) : (
+      <span>
+        A <strong>lock</strong> {lockImg} or <strong>https://</strong> means you’ve safely connected
+        to the {tld} website. Share sensitive information only on official, secure websites.
+      </span>
+    );
+
     return (
       <div className="ds-c-usa-banner">
         <header
@@ -25,14 +75,14 @@ class UsaBanner extends React.PureComponent {
         >
           <UsaFlagIcon className="ds-c-usa-banner__header-flag" />
           <p className="ds-c-usa-banner__header-text">
-            <span>An official website of the United States government</span>
+            <span>{bannerText}</span>
             <button
               onClick={this.handleToggleBanner}
               className="ds-c-usa-banner__button"
               aria-expanded={this.state.isBannerOpen}
               aria-controls="gov-banner"
             >
-              Here’s how you know
+              {bannerActionText}
             </button>
           </p>
         </header>
@@ -41,21 +91,17 @@ class UsaBanner extends React.PureComponent {
             <div className="ds-c-usa-banner__guidance">
               <DotGovIcon className="ds-c-usa-banner__icon" />
               <p className="ds-c-usa-banner__media-body">
-                <strong>Official websites use .gov</strong>
-                <br />A <strong>.gov</strong> website belongs to an official government organization
-                in the United States.
+                <strong>{domainHeadingText}</strong>
+                <br />
+                {domainText}
               </p>
             </div>
             <div className="ds-c-usa-banner__guidance">
               <HttpsIcon className="ds-c-usa-banner__icon" />
               <p className="ds-c-usa-banner__media-body">
-                <strong>Secure .gov websites use HTTPS</strong>
-                <br />A <strong>lock</strong> ({' '}
-                <span>
-                  <LockIcon className="ds-c-usa-banner__lock-image" />
-                </span>{' '}
-                ) or <strong>https://</strong> means you’ve safely connected to the .gov website.
-                Share sensitive information only on official, secure websites.
+                <strong>{httpsHeadingText}</strong>
+                <br />
+                {httpsText}
               </p>
             </div>
           </div>
@@ -64,5 +110,12 @@ class UsaBanner extends React.PureComponent {
     );
   }
 }
+
+UsaBanner.propTypes = {
+  /**
+   * Render USA Banner in Spanish.
+   */
+  localeSpanish: PropTypes.bool,
+};
 
 export default UsaBanner;
