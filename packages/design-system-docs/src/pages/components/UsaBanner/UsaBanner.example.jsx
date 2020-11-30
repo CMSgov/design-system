b49272a3dfc55ed6bbf515ae14cc03f7a8107051
Ni Chia <ni.chia@adhocteam.us>
2020-11-30 11:25:05 -0500

    Add Spanish version of usa banner to js examples

--- packages/design-system-docs/src/pages/components/UsaBanner/UsaBanner.example.jsx
+++ packages/design-system-docs/src/pages/components/UsaBanner/UsaBanner.example.jsx
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { UsaBanner } from '@cmsgov/design-system';
 
-ReactDOM.render(<UsaBanner />, document.getElementById('js-example'));
+ReactDOM.render(
+  <div>
+    <h6 className="preview__label">.gov Domains</h6>
+    <UsaBanner />
+    <h6 className="preview__label">.gov Domains (Spanish)</h6>
+    <UsaBanner locale="es-US" />
+  </div>,
+  document.getElementById('js-example')
+);
