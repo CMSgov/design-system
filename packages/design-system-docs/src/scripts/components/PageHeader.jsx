91b77148b1447854fea06c1e5559589641b3c58c
line47 <scott.weber@adhocteam.us>
2020-12-02 10:12:26 -0500

    Update doc site header to add cmsds link flag

--- packages/design-system-docs/src/scripts/components/PageHeader.jsx
+++ packages/design-system-docs/src/scripts/components/PageHeader.jsx
@@ -5,19 +5,30 @@ import classNames from 'classnames';
 class PageHeader extends React.PureComponent {
   uswdsLink() {
     if (this.props.uswds) {
-      return <a href={this.props.uswds}>View related U.S. Web Design Standard</a>;
+      return (
+        <a href={this.props.uswds}>
+          View related <abbr title="U.S. Web Design System">USWDS</abbr> component
+        </a>
+      );
+    }
+  }
+
+  cmsdsLink() {
+    if (this.props.cmsds) {
+      return (
+        <a href={this.props.cmsds} className="ds-u-margin-right--2">
+          Inherits from <abbr title="CMS Design System">CMSDS</abbr>
+        </a>
+      );
     }
   }
 
   statusBadge() {
     if (this.props.status) {
-      const classes = classNames(
-        'ds-c-badge ds-u-margin-right--1 ds-u-text-transform--capitalize',
-        {
-          'ds-c-badge--success': this.props.status === 'Ready',
-          'ds-c-badge--alert': this.props.status === 'Draft',
-        }
-      );
+      const classes = classNames('ds-c-badge ds-u-margin-left--1 ds-u-text-transform--capitalize', {
+        'ds-c-badge--success': this.props.status === 'Ready',
+        'ds-c-badge--alert': this.props.status === 'Draft',
+      });
 
       return <span className={classes}>{this.props.status}</span>;
     }
@@ -36,18 +47,18 @@ class PageHeader extends React.PureComponent {
   render() {
     return (
       <header className="ds-u-padding--3 ds-u-sm-padding--6 ds-u-display--block ds-u-fill--gray-lightest">
-        <h1
-          className="ds-display"
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: this.props.header }}
-          id={this.props.reference}
-        />
-        <div className="ds-u-clearfix" />
-        <div className="ds-u-font-size--small">
+        <div className="ds-u-display--flex ds-u-align-items--center">
+          <h1
+            className="ds-display ds-u-display--inline-block"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: this.props.header }}
+            id={this.props.reference}
+          />
           {this.statusBadge()}
-
+        </div>
+        <div className="ds-u-font-size--small">
           {this.guidanceLink()}
-
+          {this.cmsdsLink()}
           {this.uswdsLink()}
         </div>
       </header>
@@ -56,6 +67,7 @@ class PageHeader extends React.PureComponent {
 }
 
 PageHeader.propTypes = {
+  cmsds: PropTypes.string,
   header: PropTypes.string.isRequired,
   reference: PropTypes.string,
   status: PropTypes.string,
