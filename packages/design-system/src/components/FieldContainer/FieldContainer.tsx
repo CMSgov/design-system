1f8a7cb41b55ca7d93266dd0cedbfe1a8abb70a8
bernardwang <bw.vbnm@gmail.com>
2020-12-21 16:16:39 -0600

    Convert FieldContainer to typescript

--- packages/design-system/src/components/FieldContainer/FieldContainer.tsx
+++ packages/design-system/src/components/FieldContainer/FieldContainer.tsx
@@ -4,8 +4,68 @@ import React from 'react';
 import classNames from 'classnames';
 import { uniqueId } from 'lodash';
 
-export class FieldContainer extends React.Component {
-  constructor(props) {
+interface FieldContainerProps {
+  /**
+   * Additional classes to be added to the field container.
+   */
+  className?: string;
+  /**
+   * The HTML element used to render the container
+   */
+  component: 'div' | 'fieldset';
+  errorMessage?: React.ReactNode;
+  /**
+   * Used to focus the field input on `componentDidMount()`
+   */
+  focusTrigger?: boolean;
+  /**
+   * Additional hint text to display
+   */
+  hint?: React.ReactNode;
+  /**
+   * A unique ID to be used for the field input. If one isn't provided, a unique ID will be generated.
+   */
+  id?: string;
+  /**
+   * Access a reference to the field input
+   */
+  inputRef?: (...args: any[]) => any;
+  /**
+   * Applies the "inverse" UI theme
+   */
+  inversed?: boolean;
+  /**
+   * Label for the field.
+   */
+  label: React.ReactNode;
+  /**
+   * Additional classes to be added to the field label
+   */
+  labelClassName?: string;
+  /**
+   * The root HTML element used to render the field label
+   */
+  labelComponent: 'label' | 'legend';
+  /**
+   * A unique `id` to be used on the field label. If one isn't provided, a unique ID will be generated.
+   */
+  labelId?: string;
+  /**
+   * Text showing the requirement ("Required", "Optional", etc.). See [Required and Optional Fields]({{root}}/guidelines/forms/#required-and-optional-fields).
+   */
+  requirementLabel?: React.ReactNode;
+  /**
+   * A function that returns a field input element to accept render props
+   */
+  render: (...args: any[]) => any;
+};
+
+export class FieldContainer extends React.Component<FieldContainerProps> {
+  id: string;
+  labelId: string;
+  focusRef: ((HTMLDivElement) => void) | React.RefObject<HTMLDivElement>;
+
+  constructor(props: FieldContainerProps) {
     super(props);
 
     this.id = props.id || uniqueId('field_');
@@ -13,13 +73,13 @@ export class FieldContainer extends React.Component {
     this.setFieldRef = this.setFieldRef.bind(this);
   }
 
-  componentDidMount() {
+  componentDidMount(): void {
     if (this.props.focusTrigger) {
       this.focusRef && this.focusRef.focus();
     }
   }
 
-  setFieldRef(elem) {
+  setFieldRef(elem: HTMLDivElement): void {
     // Use React.forwardRef when upgraded to React 16.3
     if (this.props.focusTrigger) {
       this.focusRef = elem;
