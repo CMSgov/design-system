eec9d0f2de3cc529df8b0a35ad552190913d2ca7
bernardwang <bw.vbnm@gmail.com>
2020-12-22 11:42:36 -0600

    Update eslint to only use typescript rules on ts and tsx files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
-  extends: ['@cmsgov/eslint-config-design-system', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: ['@cmsgov/eslint-config-design-system'],
   rules: {
     // Avoid exploits. If you need dangerouslySetInnerHTML, then temporarily
     // disable this rule in the script rather than removing it from here.
     'react/no-danger': 'error',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {},
+    },
+  ],
 };
