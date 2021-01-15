8ad1fcc5d132caea77e156690228e72bf03a4a6a
Ni Chia <ni.chia@adhocteam.us>
2021-01-15 16:50:03 -0500

    Update docs site examples

--- packages/design-system-docs/src/pages/components/UsaBanner/UsaBanner.example.jsx
+++ packages/design-system-docs/src/pages/components/UsaBanner/UsaBanner.example.jsx
@@ -6,6 +6,8 @@ ReactDOM.render(
   <div>
     <h6 className="preview__label">English banner</h6>
     <UsaBanner />
+    <h6 className="preview__label">Spanish banner</h6>
+    <UsaBanner locale="es" />
   </div>,
   document.getElementById('js-example')
 );
