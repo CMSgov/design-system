1ead7f622ec86d076ed7dba88a59d3f0a813974b
Ni Chia <ni.chia@adhocteam.us>
2021-01-15 10:48:12 -0500

    Update banner examples and test

--- packages/design-system-docs/src/pages/components/UsaBanner/UsaBanner-es.example.jsx
+++ packages/design-system-docs/src/pages/components/UsaBanner/UsaBanner-es.example.jsx
@@ -4,8 +4,8 @@ import { UsaBanner } from '@cmsgov/design-system';
 
 ReactDOM.render(
   <div>
-    <h6 className="preview__label">English banner</h6>
-    <UsaBanner />
+    <h6 className="preview__label">Spanish banner</h6>
+    <UsaBanner locale="es" />
   </div>,
   document.getElementById('js-example')
 );
