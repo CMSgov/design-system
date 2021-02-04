d42cb7630667f208de283335cf4063a45133587c
Ni Chia <ni.chia@adhocteam.us>
2021-02-04 11:10:35 -0500

    Replace passing column header via span instead of data-title

--- packages/design-system/src/components/Table/TableCell.jsx
+++ packages/design-system/src/components/Table/TableCell.jsx
@@ -69,9 +69,12 @@ export const TableCell = ({
       scope={defaultScope}
       headers={headers}
       id={id}
-      data-title={stackedTitle}
       {...tableCellProps}
     >
+      {/* Add column heading for stacked table */}
+      <span aria-hidden="true" className="ds-c-table--stacked__col-header">
+        {stackedTitle}
+      </span>
       {children}
     </Component>
   );
