44672565206be532732bbc8829a9a2fb9d633321
Ni Chia <ni.chia@adhocteam.us>
2021-02-10 14:12:20 -0500

    Revert "Update jsx table examples"
    This reverts commit 3ac1965f62af1f65bbd4b0d1371bf67bfcd791a0.

--- packages/design-system-docs/src/pages/components/Table/Table.example.jsx
+++ packages/design-system-docs/src/pages/components/Table/Table.example.jsx
@@ -11,47 +11,39 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <>
-    <Table scrollable stackable stackableBreakpoint="sm">
+    <Table scrollable>
       <TableCaption>Responsive scrollable table</TableCaption>
       <TableHead>
         <TableRow>
-          <TableCell id="col_1">Document title</TableCell>
-          <TableCell id="col_2">Description</TableCell>
-          <TableCell id="col_3">Links</TableCell>
-          <TableCell id="col_4">Year</TableCell>
+          <TableCell>Document title</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell>Links</TableCell>
+          <TableCell>Year</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
         <TableRow>
-          <TableCell stackedTitle="Document title" headers="col_1">
-            Declaration of Independence
-          </TableCell>
-          <TableCell stackedTitle="Description" headers="col_2">
+          <TableCell>Declaration of Independence</TableCell>
+          <TableCell>
             Statement adopted by the Continental Congress declaring independence from the British
             Empire.
           </TableCell>
-          <TableCell stackedTitle="Link" headers="col_3">
+          <TableCell>
             <a href="# ">
               https://billofrightsinstitute.org/founding-documents/declaration-of-independence/
             </a>
           </TableCell>
-          <TableCell stackedTitle="Year" headers="col_4">
-            1776
-          </TableCell>
+          <TableCell>1776</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell stackedTitle="Document title" headers="col_1">
-            Bill of Rights
-          </TableCell>
-          <TableCell stackedTitle="Description" headers="col_2">
+          <TableCell>Bill of Rights</TableCell>
+          <TableCell>
             The first ten amendments of the U.S. Constitution guaranteeing rights and freedoms.
           </TableCell>
-          <TableCell stackedTitle="Link" headers="col_3">
+          <TableCell>
             <a href="# ">https://billofrightsinstitute.org/founding-documents/bill-of-rights/</a>
           </TableCell>
-          <TableCell stackedTitle="Year" headers="col_4">
-            1791
-          </TableCell>
+          <TableCell>1791</TableCell>
         </TableRow>
       </TableBody>
     </Table>
@@ -66,29 +58,6 @@ ReactDOM.render(
         </TableRow>
       </TableHead>
       <TableBody>
-        <TableRow>
-          <TableCell stackedTitle="Document title" headers="column_1">
-            Declaration of Independence
-          </TableCell>
-          <TableCell stackedTitle="Description" headers="column_2">
-            Statement adopted by the Continental Congress declaring independence from the British
-            Empire.
-          </TableCell>
-          <TableCell stackedTitle="Year" headers="column_3">
-            1776
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell stackedTitle="Document title" headers="column_1">
-            Bill of Rights
-          </TableCell>
-          <TableCell stackedTitle="Description" headers="column_2">
-            The first ten amendments of the U.S. Constitution guaranteeing rights and freedoms.
-          </TableCell>
-          <TableCell stackedTitle="Year" headers="column_3">
-            1791
-          </TableCell>
-        </TableRow>
         <TableRow>
           <TableCell stackedTitle="Document title" headers="column_1">
             Declaration of Sentiments
@@ -106,7 +75,7 @@ ReactDOM.render(
             Emancipation Proclamation
           </TableCell>
           <TableCell stackedTitle="Description" headers="column_2">
-            An executive order granting freedom to slaves in designated southern states.{' '}
+            An executive order granting freedom to slaves in designated southern states.
           </TableCell>
           <TableCell stackedTitle="Year" headers="column_3">
             1863
