4d6cfb5f85a0739d36fb13775abf89808af82387
Ni Chia <ni.chia@adhocteam.us>
2021-02-24 09:52:04 -0500

    Disable eslint-disable camelcase for anylytics object

--- packages/design-system/src/components/Utilities/Analytics.ts
+++ packages/design-system/src/components/Utilities/Analytics.ts
@@ -4,47 +4,42 @@
 // screener/src/assets/js/utils/analytics-util.js
 // coverage-tools-frontend/src/helpers/objectUtilities.ts
 
-import { AnalyticsPayload } from ".";
+import { AnalyticsPayload } from '.';
 
 export const EVENT_CATEGORY = {
-  contentTools: "content tools",
+  contentTools: 'content tools',
 };
 
 export const EVENT_ACTION = {
-  helpDrawerOpen: "opened help drawer",
-  helpDrawerClose: "closed help drawer",
+  helpDrawerOpen: 'opened help drawer',
+  helpDrawerClose: 'closed help drawer',
 };
 
+/* eslint-disable camelcase */
 interface AnalyticsEventProps {
-  event_category: string,
-  event_action: string,
-  event_label: string,
-  event_value: string,
-  additional_props?: Record<string, unknown>,
+  event_category: string;
+  event_action: string;
+  event_label: string;
+  event_value: string;
+  additional_props?: Record<string, unknown>;
 }
 
-export async function sendTealiumEvent(
-  props: AnalyticsEventProps
-): Promise<string> {
-  const {    
-    event_category,
-    event_action,
-    event_label,
-    event_value="",
-    additional_props} = props;
+export async function sendTealiumEvent(props: AnalyticsEventProps): Promise<string> {
+  const { event_category, event_action, event_label, event_value = '', additional_props } = props;
 
-  if (!window.utag) return "";
+  if (!window.utag) return '';
 
   const payload: AnalyticsPayload = {
-    event_type: "cmsds",
+    event_type: 'cmsds',
     event_category,
     event_action,
     event_label,
     event_value,
     additional_props,
-  }
+  };
 
   window.utag.link(payload);
 
   return `Tealium event sent: ${event_category} - ${event_action} - ${event_label}`;
 }
+/* eslint-enable camelcase */
--- packages/design-system/src/components/Utilities/index.ts
+++ packages/design-system/src/components/Utilities/index.ts
@@ -1,16 +1,18 @@
 declare global {
-    interface Window {
-        utag?: {
-        link: (params: AnalyticsPayload) => void;
-        }
-    }
+  interface Window {
+    utag?: {
+      link: (params: AnalyticsPayload) => void;
+    };
+  }
 }
 
+/* eslint-disable camelcase */
 export interface AnalyticsPayload {
-    event_type: "cmsds";
-    event_category: string;
-    event_action: string;
-    event_label: string;
-    event_value: string,
-    additional_props?: Record<string, unknown>;
-  }
+  event_type: 'cmsds';
+  event_category: string;
+  event_action: string;
+  event_label: string;
+  event_value: string;
+  additional_props?: Record<string, unknown>;
+}
+/* eslint-enable camelcase */
