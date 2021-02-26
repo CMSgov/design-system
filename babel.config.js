db88d2472dbed3c1d5651e72c0f2c819d2b06597
Ni Chia <ni.chia@adhocteam.us>
2021-02-26 15:36:51 -0500

    Revert "Add @babel/plugin-transform-runtime package for async function to work"
    This reverts commit 9de78a4184979fd805c84309de37501fb8790254.

--- babel.config.js
+++ babel.config.js
@@ -18,7 +18,6 @@ module.exports = function (api) {
   const plugins = [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-object-assign',
-    '@babel/plugin-transform-runtime',
     'inline-react-svg',
   ];
 
