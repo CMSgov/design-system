510d9af1ba06e7713d84099c8ba4fc0683517485
line47 <scott.weber@adhocteam.us>
2021-03-09 16:52:28 -0500

    Updating component code

--- packages/design-system/src/components/Accordion/Accordion.tsx
+++ packages/design-system/src/components/Accordion/Accordion.tsx
@@ -1,34 +1,24 @@
-import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import AccordionItem, { AccordionItemData } from './AccordionItem';
 
 interface AccordionProps {
   items: AccordionItemData[];
+  className?: string;
+  bordered: boolean;
 }
 
-const classes = classNames(
-  'ds-c-accordion',
-  this.props.variation && `ds-c-accordion--${this.props.variation}`,
-  this.props.className
-);
-
-const Accordion: React.FC<AccordionProps> = ({ items }) => {
+const Accordion: FunctionComponent<{
+  items: AccordionItemData[];
+  className?: string;
+  bordered?: boolean;
+}> = ({ items, className, bordered = false }) => {
   return (
-    <div className={classes}>
+    <div className={`ds-c-accordion ${className} ${bordered ? 'ds-c-accordion--bordered' : ''}`}>
       {items.map((i) => (
         <AccordionItem key={i.id} content={i.content} expanded={i.expanded} title={i.title} />
       ))}
     </div>
   );
-};
-
-Accordion.propTypes = {
-  className: PropTypes.string,
-  /**
-   * A string corresponding to the `Accordion` variation classes (`bordered`)
-   */
-  variation: PropTypes.oneOf(['bordered']),
 };
 
 export default Accordion;
