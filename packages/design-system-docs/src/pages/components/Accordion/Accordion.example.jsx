12ed2d9166fc05b0a3e6ba5265882de0785b015e
line47 <scott.weber@adhocteam.us>
2021-03-11 13:23:47 -0500

    Update accordion example

--- packages/design-system-docs/src/pages/components/Accordion/Accordion.example.jsx
+++ packages/design-system-docs/src/pages/components/Accordion/Accordion.example.jsx
@@ -1,36 +1,23 @@
 // import AccordionItem from '@cmsgov/design-system/src/components/Accordion/AccordionItem';
-import { Accordion } from '@design-system';
+import { Accordion, AccordionItem } from '@design-system';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <div className="example--wrapper">
-    <Accordion
-      items={[
-        {
-          id: '1',
-          title: 'Control and supplemental guidance',
-          content: (
-            <>
-              <p>
-                We the People of the United States, in Order to form a more perfect Union, establish
-                Justice, insure domestic Tranquility, provide for the common defence, promote the
-                general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity,
-                do ordain and establish this Constitution for the United States of America.
-              </p>
-              <p>
-                We the People of the United States, in Order to form a more perfect Union, establish
-                Justice, insure domestic Tranquility, provide for the common defence, promote the
-                general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity,
-                do ordain and establish this Constitution for the United States of America.
-              </p>
-            </>
-          ),
-          expanded: true,
-        },
-        { id: '2', title: 'Assessment guidance', content: <p>Hello world!</p> },
-      ]}
-    />
+    <Accordion bordered>
+      <AccordionItem heading="Control and supplemental guidance">
+        <p>
+          We the People of the United States, in Order to form a more perfect Union, establish
+          Justice, insure domestic Tranquility, provide for the common defence, promote the general
+          Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and
+          establish this Constitution for the United States of America.
+        </p>
+      </AccordionItem>
+      <AccordionItem heading="Assessment guidance">
+        <p>Hello world!</p>
+      </AccordionItem>
+    </Accordion>
   </div>,
   document.getElementById('js-example')
 );
