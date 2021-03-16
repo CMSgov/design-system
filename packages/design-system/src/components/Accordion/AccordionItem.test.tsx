10ab9155129965cd92e98000fea9885f76e0c207
Ni Chia <ni.chia@adhocteam.us>
2021-03-16 10:10:38 -0400

    Update accordionItem test

--- packages/design-system/src/components/Accordion/AccordionItem.test.tsx
+++ packages/design-system/src/components/Accordion/AccordionItem.test.tsx
@@ -1,23 +1,42 @@
+import Accordion from './Accordion';
 import AccordionItem from './AccordionItem';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
-const defaultChildren = '<p> Content for accotdion item</p>';
+const defaultChildren = '<p>Content for accordion item</p>';
+const defaultProps = {
+  heading: "Heading for accordion item",
+}
 
 function render(customProps = {}) {
-  const props = { ...customProps };
+  const props = { ...defaultProps, ...customProps };
+  const children = <AccordionItem {...props}>{defaultChildren}</AccordionItem>;
+
   return {
     props: props,
-    wrapper: shallow(<AccordionItem heading {...props}></AccordionItem>),
+    wrapper: mount(<Accordion>{children}</Accordion>),
   };
 }
 
 describe('AccordionItem', function () {
   it('renders an accordion item', () => {
     const { wrapper } = render();
-    wrapper.render().find('.ds-c-accordion__content');
-    wrapper.render().find('.ds-c-accordion__header');
-    wrapper.render().find('.ds-text');
+    const accordionItem = wrapper.find('AccordionItem');
+
+    expect(accordionItem).toHaveLength(1);
+  });
+  it('renders an accordion heading', () => {
+    const { wrapper } = render();
+    const headerWrapper = wrapper.find('h2');
+
+    expect(headerWrapper).toHaveLength(1);
+    expect(headerWrapper.hasClass('ds-c-accordion__heading')).toBe(true);
+  });
+
+  it('renders an accordion content', () => {
+    const { wrapper } = render();
+    const contentWrapper = wrapper.find('div');
+    expect(contentWrapper.hasClass('ds-c-accordion__content')).toBe(true);
   });
 
   it('renders additional className for header', () => {
@@ -35,7 +54,7 @@ describe('AccordionItem', function () {
     });
 
     const content_class = wrapper.render().find('div.ds-c-accordion__content');
-    expect(content_class.hasClass('ds-u-test')).toBe(true);
+    expect(content_class.hasClass('.ds-c-accordion__header')).toBe(true);
   });
 
   it('renders header text', () => {
