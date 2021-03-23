018e4d8ffa575f505f8d75c956d334f18213943c
line47 <scott.weber@adhocteam.us>
2021-03-23 15:12:00 -0400

    getting the focus working initial

--- packages/design-system/src/components/Accordion/Accordion.tsx
+++ packages/design-system/src/components/Accordion/Accordion.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import classNames from 'classnames';
+import AccordionItem from './AccordionItem';
 
 export interface AccordionProps {
   /**
@@ -13,10 +14,31 @@ export interface AccordionProps {
   className?: string;
 }
 
+const handleKeyDown = (e) => {
+  const target = e.target;
+  // To Do - Figure out different way to get triggers
+  var triggers = Array.prototype.slice.call(document.querySelectorAll('.ds-c-accordion__button'));
+  console.log(triggers);
+  if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+    const direction = e.key == 'ArrowDown' ? 1 : -1;
+    var index = triggers.indexOf(target);
+    var length = triggers.length;
+    var newIndex = (index + length + direction) % length;
+
+    triggers[newIndex].focus();
+
+    e.preventDefault();
+  }
+};
+
 const Accordion: FunctionComponent<AccordionProps> = ({ bordered, children, className }) => {
   const classes = classNames('ds-c-accordion', bordered && 'ds-c-accordion--bordered', className);
 
-  return <div className={classes}>{children}</div>;
+  return (
+    <div onKeyDown={handleKeyDown} className={classes}>
+      {children}
+    </div>
+  );
 };
 
 export default Accordion;
