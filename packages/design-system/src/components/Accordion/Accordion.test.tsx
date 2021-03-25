47aa11ac4d50e938e85545609f39117aa00dc35c
line47 <scott.weber@adhocteam.us>
2021-03-25 10:00:50 -0400

    Update test for arrow down

--- packages/design-system/src/components/Accordion/Accordion.test.tsx
+++ packages/design-system/src/components/Accordion/Accordion.test.tsx
@@ -1,35 +1,74 @@
 import Accordion from './Accordion';
+import AccordionItem from './AccordionItem';
 import React from 'react';
 import { shallow } from 'enzyme';
 
-function render(customProps = {}) {
+const defaultAccordionItemChildren = 'Foo';
+const defaultAccordionItemProps = {
+  id: 'accordionitem-1',
+  heading: 'Accordion item header',
+};
+
+function render(customProps = {}, children) {
   const props = { ...customProps };
+
+  if (!children) {
+    children = (
+      <AccordionItem {...defaultAccordionItemProps}>{defaultAccordionItemChildren}</AccordionItem>
+    );
+  }
+
   return {
     props,
-    wrapper: shallow(<Accordion {...props}></Accordion>),
+    wrapper: shallow(<Accordion {...props}>{children}</Accordion>),
   };
 }
 
 describe('Accordion', function () {
   it('renders accordion', () => {
-    const { wrapper } = render();
+    const children = [];
+    const { wrapper } = render(undefined, children);
 
     expect(wrapper.hasClass('ds-c-accordion')).toBe(true);
   });
 
   it('renders additional className', () => {
-    const { wrapper } = render({
-      className: 'ds-u-test',
-    });
+    const children = [];
+    const { wrapper } = render(
+      {
+        className: 'ds-u-test',
+      },
+      children
+    );
 
     expect(wrapper.hasClass('ds-u-test')).toBe(true);
   });
 
   it('renders ds-c-accordion--bordered class when a bordered prop is set', () => {
-    const { wrapper } = render({
-      bordered: true,
-    });
+    const children = [];
+    const { wrapper } = render(
+      {
+        bordered: true,
+      },
+      children
+    );
 
     expect(wrapper.hasClass('ds-c-accordion--bordered')).toBe(true);
+  });
+
+  it('selects the second accordion item on down arrow keyDown', () => {
+    const children = [
+      <AccordionItem heading="First amendment" id="1">
+        <p>Hello world!</p>
+      </AccordionItem>,
+      <AccordionItem heading="Second amendment" id="2">
+        <p>Hello world!</p>
+      </AccordionItem>,
+    ];
+    const data = render({}, children);
+    const accordionitem = data.wrapper.find('AccordionItem');
+    accordionitem.at(1).simulate('keyDown', { key: 'ArrowDown' });
+
+    expect(accordionitem.at(1).focus()).toBe(true);
   });
 });
