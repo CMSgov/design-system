3dac66928e1a05843eeb6c4b9db9ef06f7b6e600
Ni Chia <ni.chia@adhocteam.us>
2021-03-30 12:15:00 -0400

    Add TooltipIcon typedef file

---
+++ packages/design-system/src/types/Tooltip/TooltipIcon.d.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface TooltipIconProps {
+  inversed?: boolean;
+}
+
+declare const TooltipIcon: React.FC<TooltipIconProps>;
+
+export default TooltipIcon;
