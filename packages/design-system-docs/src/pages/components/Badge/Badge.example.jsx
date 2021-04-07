c0422e81cb0d3ce7305a3a7bfc08165fe487aa80
Kimberly Dibble <kimberly.childs@gmail.com>
2021-04-07 12:29:47 -0400

    First draft of adding dismissible badge

--- packages/design-system-docs/src/pages/components/Badge/Badge.example.jsx
+++ packages/design-system-docs/src/pages/components/Badge/Badge.example.jsx
@@ -2,11 +2,49 @@ import { Badge } from '@design-system';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const clearIcon = (
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    viewBox="0 0 512 512"
+    className="ds-c-clear-icon"
+  >
+    <path
+      className="ds-c-clear-icon__x"
+      d="M256,0C114.6,0,0,114.6,0,256s114.6,256,256,256s256-114.6,256-256S397.4,0,256,0z M160,113.9l96,96l96-96l46.1,46.1
+		l-96,96l96,96L352,398.1l-96-96l-96,96L113.9,352l96-96l-96-96L160,113.9z"
+    />
+  </svg>
+);
+
 ReactDOM.render(
   <>
     <Badge>Default badge</Badge>
     <Badge variation="info">Badge with `variation` prop</Badge>
     <Badge size="big">Badge with `size` prop</Badge>
+    <div className="ds-u-margin-top--3">
+      <Badge dismissible>Badge with `dismissible` prop</Badge>
+      <Badge dismissible variation="warn">
+        Badge with `dismissible` prop and `warn` variation
+      </Badge>
+      <Badge dismissible variation="alert">
+        Badge with `dismissible` prop and `alert` variation
+      </Badge>
+    </div>
+    <div className="ds-u-margin-top--3">
+      <Badge dismissible clearIcon={clearIcon}>
+        Dismissible badge with custom icon
+      </Badge>
+      <Badge dismissible variation="warn" clearIcon={clearIcon}>
+        Dismissible warning badge with custom icon
+      </Badge>
+      <Badge dismissible variation="alert" clearIcon={clearIcon}>
+        Dismissible alert badge with custom icon
+      </Badge>
+    </div>
   </>,
   document.getElementById('js-example')
 );
--- packages/design-system/src/components/Badge/Badge.jsx
+++ packages/design-system/src/components/Badge/Badge.jsx
@@ -1,20 +1,61 @@
+import ClearIcon from './ClearIcon';
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 
-export const Badge = (props) => {
-  const { className, children, size, variation, ...others } = props;
-  const sizeClasses = { big: 'ds-u-font-size--base' };
+export class Badge extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      badgeDismissed: false,
+    };
+  }
 
-  const variationClass = variation && `ds-c-badge--${variation}`;
-  const classes = classNames('ds-c-badge', variationClass, sizeClasses[size], className);
+  handleClick(evt) {
+    if (this.props.onClick) {
+      this.props.onClick(evt);
+    }
 
-  return (
-    <span className={classes} {...others}>
-      {children}
-    </span>
-  );
-};
+    if (this.props.dismissible) {
+      this.setState({ badgeDismissed: true });
+    }
+  }
+
+  render() {
+    const { className, children, size, variation, dismissible, clearIcon, ...others } = this.props;
+    const sizeClasses = { big: 'ds-u-font-size--base' };
+
+    const variationClass = variation && `ds-c-badge--${variation}`;
+    const dismissibleClass = dismissible && 'ds-c-badge--dismissible';
+    const classes = classNames(
+      'ds-c-badge',
+      variationClass,
+      sizeClasses[size],
+      dismissibleClass,
+      className
+    );
+
+    const BadgeComponent = dismissible ? 'button' : 'span';
+
+    return (
+      <>
+        {!this.state.badgeDismissed && (
+          <>
+            <BadgeComponent className={classes} onClick={this.handleClick} {...others}>
+              {children}
+              {dismissible && (
+                <span className="ds-c-badge-icon__container ">
+                  {clearIcon ? <>{clearIcon}</> : <ClearIcon className="ds-c-badge-icon" />}
+                </span>
+              )}
+            </BadgeComponent>
+          </>
+        )}
+      </>
+    );
+  }
+}
 
 Badge.propTypes = {
   /**
@@ -23,9 +64,21 @@ Badge.propTypes = {
    */
   className: PropTypes.string,
   /**
+   * When provided with the dismissible prop, will use a custom icon instead of default icon
+   */
+  clearIcon: PropTypes.node,
+  /**
    * Label text or HTML.
    */
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  /**
+   * Whether badge should be dismissible
+   */
+  dismissible: PropTypes.bool,
+  /**
+   * Function called onClick
+   */
+  onClick: PropTypes.func,
   /**
    * Sets the font size of the Badge
    */
