b41a26f9a5227826b00d27c96989368d65d137f4
Kimberly Dibble <kimberly.childs@gmail.com>
2021-04-08 16:03:59 -0400

    Add test

--- packages/design-system/src/components/Badge/Badge.test.jsx
+++ packages/design-system/src/components/Badge/Badge.test.jsx
@@ -6,4 +6,32 @@ describe('Badge', () => {
   it('should include children as label', () => {
     expect(shallow(<Badge>Foo</Badge>).text()).toEqual('Foo');
   });
+
+  it('renders span and should not disappear when clicked if dismissible prop is not included', () => {
+    const wrapper = shallow(<Badge>Foo</Badge>);
+    expect(wrapper.text()).toEqual('Foo');
+
+    const span = wrapper.find('span');
+    const button = wrapper.find('button');
+    expect(span.length).toEqual(1);
+    expect(button.length).toEqual(0);
+
+    span.simulate('click');
+    wrapper.update();
+    expect(wrapper.text()).toEqual('Foo');
+  });
+
+  it('renders button and should disappear when clicked if dismissible prop is included', () => {
+    const wrapper = shallow(<Badge dismissible>Foo</Badge>);
+    expect(wrapper.text()).toEqual('Foo<ClearIcon />');
+
+    const span = wrapper.find('span');
+    const button = wrapper.find('button');
+    expect(span.length).toEqual(1);
+    expect(button.length).toEqual(1);
+
+    button.simulate('click');
+    wrapper.update();
+    expect(wrapper.text()).toEqual('');
+  });
 });
