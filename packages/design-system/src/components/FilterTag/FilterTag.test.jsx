23e338c10e5d12168000966dd93fc52158fc8e8b
Kimberly Dibble <kimberly.childs@gmail.com>
2021-04-12 10:48:02 -0400

    Add test

---
+++ packages/design-system/src/components/FilterTag/FilterTag.test.jsx
@@ -0,0 +1,21 @@
+import FilterTag from './FilterTag';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('FilterTag', () => {
+  it('should include children as label', () => {
+    expect(shallow(<FilterTag label="Foo" />).text()).toEqual(' Foo<ClearIcon />');
+  });
+
+  it('renders button and should disappear when clicked', () => {
+    const wrapper = shallow(<FilterTag label="Foo" />);
+    expect(wrapper.text()).toEqual(' Foo<ClearIcon />');
+
+    const button = wrapper.find('button');
+    expect(button.length).toEqual(1);
+
+    button.simulate('click');
+    wrapper.update();
+    expect(wrapper.text()).toEqual('');
+  });
+});
