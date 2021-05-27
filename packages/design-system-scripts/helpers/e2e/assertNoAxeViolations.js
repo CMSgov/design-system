7a8e8e2bbe39508d23e64a790466e5156796543a
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2021-05-27 15:59:16 -0700

    [NO-TICKET] Output useful information for a11y violations (#1052)
    Format the Axe a11y violations so they're useful
    
    Before we console.log'd the top-level violations array, which doesn't expand objects deep enough to print useful information. I've added code to print each violation in a way that is legible and drills down enough to show the actual issues Axe found.

--- packages/design-system-scripts/helpers/e2e/assertNoAxeViolations.js
+++ packages/design-system-scripts/helpers/e2e/assertNoAxeViolations.js
@@ -1,5 +1,45 @@
 /* global driver, axeBuilder */
 const { RULESET_ALL } = require('./constants');
+const chalk = require('chalk');
+
+function impactColor(impact) {
+  switch (impact) {
+    case 'critical':
+    case 'serious':
+      return chalk.redBright;
+    case 'moderate':
+    default:
+      return chalk.yellowBright;
+  }
+}
+
+// Can't use console.log because Jest will output a bunch of metadata
+function println(line = '', indentation = 4) {
+  process.stdout.write(' '.repeat(indentation) + line + '\n');
+}
+
+function printViolation({ id, impact, nodes, ...rest }) {
+  const color = impactColor(impact);
+  println(color(`● ${id} (${impact})`));
+  Object.keys(rest).forEach((key) => {
+    println(`${key}: ${rest[key]}`, 6);
+  });
+  println('nodes:', 6);
+  println();
+  nodes.forEach(({ failureSummary, ...rest }) => {
+    println(`● ${failureSummary}`, 6);
+    Object.keys(rest).forEach((key) => {
+      println(`${key}: ${rest[key]}`, 8);
+    });
+    println();
+  });
+}
+
+function printViolations(violations) {
+  println(chalk.bgRed.bold('  Accessibility violations:  '), 0);
+  println();
+  violations.forEach(printViolation);
+}
 
 module.exports = async function assertNoAxeViolations(url, disabledRules = []) {
   if (url) {
@@ -11,11 +51,12 @@ module.exports = async function assertNoAxeViolations(url, disabledRules = []) {
     .withTags(RULESET_ALL)
     .disableRules(defaultDisabledRules.concat(disabledRules))
     .analyze((err, results) => {
-      if (err) {
-        console.error(err);
-      }
       if (results.violations.length >= 1) {
-        console.log(results.violations);
+        printViolations(results.violations);
+      }
+      if (err) {
+        // ESLint wants us to handle the error directly, but that's what
+        // our assertion does below.
       }
       expect(results.violations.length).toBe(0);
     });
