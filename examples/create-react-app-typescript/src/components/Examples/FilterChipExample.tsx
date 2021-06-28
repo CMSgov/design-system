ac458e09f4e83288b6ac76d39ff595080f712190
kimieann <kimberly.childs@gmail.com>
2021-06-28 10:05:29 -0400

    [WNMGDS-914] Add component examples to CRA and CRA-typescript (#1057)
    * Add example for every component for create-react-app and create-react-app-typescript
    
    * Change package.json back
    
    * Lint changes
    
    * PR feedback
    
    * Remove comma from tabs example
    
    Co-authored-by: Kimberly Dibble <kimberlydibble@kimberlys-mbp.lan>
    Co-authored-by: Scott Weber <scott.weber@adhocteam.us>
    Co-authored-by: Ni Chia <ni.chia@adhocteam.us>

---
+++ examples/create-react-app-typescript/src/components/Examples/FilterChipExample.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { FilterChip } from '@cmsgov/design-system';
+
+function FilterChipExample(): React.ReactElement {
+  const [showFilterChip, setShowFilterChip] = useState(true);
+
+  return (
+    <div>
+      <h2>FilterChip Example</h2>
+      {showFilterChip && (
+        <FilterChip
+          label="Dismiss me"
+          ariaClearLabel="Clear"
+          onDelete={() => {
+            setShowFilterChip(false);
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default FilterChipExample;
