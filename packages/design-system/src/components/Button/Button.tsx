9b55d340d12facf74fe4dedbf2c6a260dd6bfdbc
Scott Weber <scott.weber@adhocteam.us>
2021-08-13 14:10:59 -0400

    [WNMGDS-916] Feature/secondary tertiary buttons (#1106)
    * Adding SCSS variables
    
    * Update example
    
    * add variations for secondary and tertiary
    
    * Add button line height and improve descriptions
    
    * add specific button line height variable
    
    * Add button font-weight variable
    
    * move order of styles to fix build
    
    * Remove focus if statements and update SCSS
    
    * organizing SCSS better
    
    * update SCSS button mapping
    
    * set bg on transparent buttons to be transparent
    
    * Update text decoration for disabled buttons
    
    * add inverse primary styles and clean up scss
    
    * add type to button

--- packages/design-system/src/components/Button/Button.tsx
+++ packages/design-system/src/components/Button/Button.tsx
@@ -8,7 +8,13 @@ export type ButtonType = 'button' | 'submit';
  * A string corresponding to the button-component variation classes.
  * The danger variation is deprecated and will be removed in a future release.
  */
-export type ButtonVariation = 'primary' | 'danger' | 'success' | 'transparent';
+export type ButtonVariation =
+  | 'primary'
+  | 'secondary'
+  | 'tertiary'
+  | 'danger'
+  | 'success'
+  | 'transparent';
 
 export interface ButtonProps {
   /**
