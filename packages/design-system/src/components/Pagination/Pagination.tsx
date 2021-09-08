312686793305540d14ee846d030e89325ad13e21
zarahzachz <sarah.sachs@gmail.com>
2021-09-08 14:56:59 -0400

    Address style layout issues on prev/next buttons.

--- packages/design-system/src/components/Pagination/Pagination.tsx
+++ packages/design-system/src/components/Pagination/Pagination.tsx
@@ -236,33 +236,33 @@ function Pagination({
 
   return (
     <nav className={classes} aria-label={ariaLabel} {...rest}>
-      {currentPage !== 1 && (
-        <Button
-          variation="tertiary"
-          href={customUrl ? `${customUrl}/${currentPage - 1}` : `#${currentPage - 1}`}
-          onClick={pageChange(currentPage - 1)}
-          aria-label={startAriaLabel}
-        >
-          <span className="ds-c-pagination__nav--previous-img-container">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="chevron-left"
-              className="ds-c-pagination__nav--image"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-            >
-              <path
-                fill="currentColor"
-                d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
-              />
-            </svg>
-          </span>
-          {startLabelText}
-        </Button>
-      )}
+      <Button
+        variation="tertiary"
+        href={customUrl ? `${customUrl}/${currentPage - 1}` : `#${currentPage - 1}`}
+        onClick={pageChange(currentPage - 1)}
+        aria-label={startAriaLabel}
+        style={{ visibility: currentPage === 1 ? 'hidden' : 'visible' }}
+        aria-hidden={currentPage === 1}
+      >
+        <span className="ds-c-pagination__nav--previous-img-container">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="chevron-left"
+            className="ds-c-pagination__nav--image"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path
+              fill="currentColor"
+              d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
+            />
+          </svg>
+        </span>
+        {startLabelText}
+      </Button>
 
       {isMobile || compact ? (
         <span
@@ -275,33 +275,33 @@ function Pagination({
         <ol>{pages}</ol>
       )}
 
-      {currentPage !== totalPages && (
-        <Button
-          variation="tertiary"
-          href={customUrl ? `${customUrl}/${currentPage + 1}` : `#${currentPage + 1}`}
-          onClick={pageChange(currentPage + 1)}
-          aria-label={endAriaLabel}
-        >
-          {endLabelText}
-          <span className="ds-c-pagination__nav--next-img-container">
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="chevron-right"
-              className="ds-c-pagination__nav--image"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-            >
-              <path
-                fill="currentColor"
-                d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
-              />
-            </svg>
-          </span>
-        </Button>
-      )}
+      <Button
+        variation="tertiary"
+        href={customUrl ? `${customUrl}/${currentPage + 1}` : `#${currentPage + 1}`}
+        onClick={pageChange(currentPage + 1)}
+        aria-label={endAriaLabel}
+        style={{ visibility: currentPage === totalPages ? 'hidden' : 'visible' }}
+        aria-hidden={currentPage === totalPages}
+      >
+        {endLabelText}
+        <span className="ds-c-pagination__nav--next-img-container">
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="chevron-right"
+            className="ds-c-pagination__nav--image"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 320 512"
+          >
+            <path
+              fill="currentColor"
+              d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+            />
+          </svg>
+        </span>
+      </Button>
     </nav>
   );
 }
