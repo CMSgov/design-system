19dddfd5996b194823da28d94982d602f24c0a8a
Anna Kammeyer <scheul93@gmail.com>
2021-10-06 13:32:31 -0600

    exporting icons, updaing example page, some styling updates

--- packages/design-system-docs/src/pages/components/Icon/IconComponent.example.jsx
+++ packages/design-system-docs/src/pages/components/Icon/IconComponent.example.jsx
@@ -1,16 +1,54 @@
-import { AddIcon, ArrowsStacked, ArrowIcon, CheckIcon } from '@design-system';
+import {
+  AddIcon,
+  AlertCircleIcon,
+  ArrowsStackedIcon,
+  ArrowIcon,
+  BuildingCircleIcon,
+  CheckCircleIcon,
+  CheckIcon,
+  CloseIcon,
+  CloseIconThin,
+  DownloadIcon,
+  ImageIcon,
+  InfoCircleIcon,
+  InfoCircleIconThin,
+  LockIcon,
+  NextIcon,
+  PdfIcon,
+  RemoveIcon,
+  StarIcon,
+  UsaFlagIcon,
+  WarningIcon,
+} from '@design-system';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <>
     <AddIcon />
-    <ArrowsStacked />
+    <AlertCircleIcon />
+    <ArrowsStackedIcon />
     <ArrowIcon />
     <ArrowIcon direction="down" />
     <ArrowIcon direction="left" />
     <ArrowIcon direction="right" />
+    <BuildingCircleIcon />
+    <CheckCircleIcon />
     <CheckIcon />
+    <CloseIcon />
+    <CloseIconThin />
+    <DownloadIcon />
+    <ImageIcon />
+    <InfoCircleIcon />
+    <InfoCircleIconThin />
+    <LockIcon />
+    <NextIcon />
+    <PdfIcon />
+    <RemoveIcon />
+    <StarIcon />
+    <StarIcon isFilled />
+    <UsaFlagIcon />
+    <WarningIcon />
   </>,
   document.getElementById('js-example')
 );
