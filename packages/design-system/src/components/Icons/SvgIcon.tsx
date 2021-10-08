2d0e83519e232283e981146fb21d28bbe3a99887
Anna Kammeyer <scheul93@gmail.com>
2021-10-08 14:35:42 -0600

    icon accessibility features

--- packages/design-system/src/components/Icons/SvgIcon.tsx
+++ packages/design-system/src/components/Icons/SvgIcon.tsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
+import uniqueid from 'lodash/uniqueid';
 
 export interface SvgIconProps {
+  /**
+   * Describes the value of the `aria-hidden` attribute on the SVG. Use if the icon content is described elsewhere.
+   * For example, if the icon is used in a button or link along with text, the text may describe the icon content more specifically.
+   */
+  ariaHidden?: boolean;
   /**
    * Additional CSS classes to be added to the svg element
    */
@@ -12,57 +18,68 @@ export interface SvgIconProps {
    */
   children: React.ReactNode;
   /**
+   * Long-text description of any SVG. Use for complex icons, otherwise `title` prop will suffice.
+   */
+  description?: string;
+  /**
+   * A custom `id` attribute for the SVG
+   */
+  id?: string;
+  /**
    * If `true` sets inverse fill color.
    */
   inversed?: boolean;
   /**
-   * Describes if the SVG icon is a decorative element. If `true`, an `aria-hidden="true"` will be added to the svg along with a `role="img"`
-   */
-  isDecorative?: boolean;
-  /**
    * The descriptive nave for the SVG icon
    */
-  title?: string;
+  title: string;
   /**
    * A string describing the viewbox of the SVG
    */
   viewBox?: string;
 }
 
-type OmitProps = 'className' | 'children' | 'title' | 'viewBox';
+type OmitProps = 'className' | 'children' | 'id' | 'title' | 'viewBox';
 
 // TODO: Should this extend SVG props?
 function SvgIcon({
+  ariaHidden,
   className,
   children,
-  isDecorative,
+  description,
+  id,
+  inversed,
   title,
   viewBox,
-}: Omit<React.SVGProps<SVGSVGElement>, OmitProps> & SvgIconProps): React.ReactElement {
-  const svgClasses = classNames(
-    'ds-c-icon',
+}: Omit<React.SVGProps<SVGElement>, OmitProps> & SvgIconProps): React.ReactElement {
+  const svgClasses = classNames('ds-c-icon', { 'ds-c-icon--inverse': inversed }, className);
 
-    className
-  );
+  const [iconId] = useState(id || uniqueid('icon-'));
+  const titleId = `${iconId}__title`;
+  const descriptionId = `${iconId}__desc`;
+  const ariaLabelledBy = description ? `${titleId} ${descriptionId}` : titleId;
 
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden={isDecorative ? true : undefined}
+      aria-labelledby={ariaLabelledBy}
+      aria-hidden={ariaHidden}
       className={svgClasses}
       focusable={false}
+      id={iconId}
+      role="img"
       viewBox={viewBox}
-      role={isDecorative ? 'img' : undefined}
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <title>{title}</title>
+      <title id={titleId}>{title}</title>
+      {description && <desc id={descriptionId}>{description}</desc>}
       {children}
     </svg>
   );
 }
 
 SvgIcon.defaultProps = {
+  ariaHidden: false,
   inversed: false,
-  isDecorative: false,
 };
 
 export default SvgIcon;
