7242d857d8ed3d23ed380dba253bbd408c52ea2b
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2021-10-13 12:23:50 -0700

    Purposely break something to see CI tests catch it

--- packages/design-system/src/components/Alert/Alert.tsx
+++ packages/design-system/src/components/Alert/Alert.tsx
@@ -168,30 +168,7 @@ export class Alert extends React.PureComponent<
       className
     );
 
-    return (
-      <div
-        className={classes}
-        /* eslint-disable no-return-assign */
-        ref={(ref) => {
-          this.alertTextRef = ref;
-          if (autoFocus) {
-            this.focusRef = ref;
-          } else if (alertRef) {
-            alertRef(ref);
-          }
-        }}
-        /* eslint-enable no-return-assign */
-        tabIndex={alertRef || autoFocus ? -1 : null}
-        role={role}
-        aria-labelledby={heading ? this.headingId : undefined}
-        {...alertProps}
-      >
-        <div className="ds-c-alert__body">
-          {this.heading()}
-          {children}
-        </div>
-      </div>
-    );
+    return <div>Boom!</div>;
   }
 }
 
