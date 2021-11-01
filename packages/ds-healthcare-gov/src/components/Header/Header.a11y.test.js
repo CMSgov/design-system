a3f5da1d32f690fe0c4b8e6b7edc6b4cc0938c6b
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2021-11-01 15:08:14 -0700

    Convert hc.gov e2e tests to a11y tests

--- packages/ds-healthcare-gov/src/components/Header/Header.a11y.test.js
+++ packages/ds-healthcare-gov/src/components/Header/Header.a11y.test.js
@@ -1,19 +1,9 @@
-/* global driver */
-import { ROOT_URL } from '@cmsgov/design-system-scripts/helpers/e2e/constants';
-
-import assertNoAxeViolations from '@cmsgov/design-system-scripts/helpers/e2e/assertNoAxeViolations';
-import { getElementByClassName } from '@cmsgov/design-system-scripts/helpers/e2e';
+import { ROOT_URL } from '@cmsgov/design-system-scripts/helpers/a11y/constants';
+import assertNoAxeViolations from '@cmsgov/design-system-scripts/helpers/a11y/assertNoAxeViolations';
 
 const rootURL = `${ROOT_URL}/example/patterns.header.react/`;
 
 describe('Header component', () => {
-  it('Should render', async () => {
-    await driver.get(rootURL);
-
-    const el = await getElementByClassName('hc-c-header');
-    expect(el).toBeTruthy();
-  });
-
   // TODO: Skipping this because the example now has multiple headers rendered on the same
   // page. This causes an accessibility violation in the form of duplicate ids. Because the
   // change to the example is a good one that will improve developer experience, I'd like
