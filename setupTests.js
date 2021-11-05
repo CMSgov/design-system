4e59daa55d7c8c00271e0bcd86bb6a86827990d5
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2021-11-04 17:18:24 -0700

    Update config for unit tests and change e2e -> a11y

--- setupTests.js
+++ setupTests.js
@@ -7,4 +7,6 @@
 // Enzyme for React 16
 import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
+
+import '@testing-library/jest-dom/extend-expect';
 configure({ adapter: new Adapter() });
