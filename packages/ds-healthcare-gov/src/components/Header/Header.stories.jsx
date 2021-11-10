0adec8b8fd1ad404c6ae926019ca10499e0dade9
Anna Kammeyer <scheul93@gmail.com>
2021-11-10 16:06:19 -0700

    setting up storybook for healthcare

---
+++ packages/ds-healthcare-gov/src/components/Header/Header.stories.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Header from './I18nHeader';
+
+// Because the DS exports the wrapped Header component, Header props are not shown in the args table
+// TODO: look into later if storybook is used more publicly
+export default {
+  title: 'Components/Header',
+  component: Header,
+  argTypes: {
+    className: {
+      control: { type: 'text' },
+      defaultValue: '',
+      type: { name: 'string', required: false },
+    },
+    firstName: {
+      control: { type: 'text' },
+      defaultValue: 'Margaret FirstName',
+    },
+  },
+};
+
+const Template = ({ data, ...args }) => <Header {...args} />;
+
+export const MinimalHeader = Template.bind({});
+MinimalHeader.args = {
+  hideLanguageSwitch: true,
+  hideLoginLink: true,
+};
+export const LoggedInHeader = Template.bind({});
+LoggedInHeader.args = {
+  loggedIn: true,
+};
+export const LoggedOutHeader = Template.bind({});
