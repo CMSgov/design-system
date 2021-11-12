b687d576ce605179f5677939232b018b80ed4646
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2021-11-12 13:26:47 -0800

    Set error-placement flag in ds-healthcare-gov
    We tried to do this a long time ago, but when apps would build in production mode, the flag setting wouldn't work. We discovered this was because our package declared having no side effects, so their build tools were optimizing under the assumption that our modules had no side effects. However, setting that state within our index.js file is indeed a side effect. After testing different "sideEffects" values, we determined that setting it to true had no negative effects on the tested application's overall JavaScript bundle size.

--- packages/ds-healthcare-gov/src/components/index.js
+++ packages/ds-healthcare-gov/src/components/index.js
@@ -11,9 +11,13 @@
 
 import './Button';
 
+import { setErrorPlacementDefault } from '@design-system';
+
 export * from '@cmsgov/design-system';
 export * from './Footer';
 export * from './Header';
 export * from './Logo';
 
 export * from './flags';
+
+setErrorPlacementDefault('bottom');
