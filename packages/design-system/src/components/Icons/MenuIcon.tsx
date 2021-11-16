b0cd3311023bdc4ef2cf83f960eea3f0b206d9f8
Forrest Baer <forrest@forrestbaer.com>
2021-11-16 11:41:03 -0600

    Moving MenuItem component to core and updating dependencies and documentation

--- packages/design-system/src/components/Icons/MenuIcon.tsx
+++ packages/design-system/src/components/Icons/MenuIcon.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
+import SvgIcon, { IconCommonProps } from './SvgIcon';
 
-/**
- * MenuIcon component which returns the menu SVG as a React component
- */
+const defaultProps = {
+  className: 'svg-inline--fa fa-bars fa-w-14',
+  title: 'Menu Icon',
+  viewBox: '0 0 448 512',
+};
 
-export function MenuIcon({ className }) {
+function MenuIcon(props: IconCommonProps): React.ReactElement {
+  const iconCssClasses = `ds-c-icon--menu ${props.className || ''}`;
+
   return (
-    <svg
-      aria-hidden="true"
-      data-prefix="fas"
-      data-icon="bars"
-      className={`svg-inline--fa fa-bars fa-w-14 ${className}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 448 512"
-    >
+    <SvgIcon {...defaultProps} {...props} className={iconCssClasses}>
       <path
         fill="currentColor"
         d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
       />
-    </svg>
+    </SvgIcon>
   );
 }
 
--- packages/design-system/src/components/Icons/index.ts
+++ packages/design-system/src/components/Icons/index.ts
@@ -15,6 +15,7 @@ export { default as InfoCircleIcon } from './InfoCircleIcon';
 export { default as InfoCircleIconThin } from './InfoCircleIconThin';
 export { default as LockCircleIcon } from './LockCircleIcon';
 export { default as LockIcon } from './LockIcon';
+export { default as MenuIcon } from './MenuIcon';
 export { default as NextIcon } from './NextIcon';
 export { default as PdfIcon } from './PdfIcon';
 export { default as RemoveIcon } from './RemoveIcon';
