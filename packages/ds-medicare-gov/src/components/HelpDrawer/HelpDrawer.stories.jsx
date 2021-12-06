48150af3cb98792bd2ef34e3037b3f461e1e52fc
Anna Kammeyer <scheul93@gmail.com>
2021-12-06 08:58:31 -0700

    help drawer toggle icon replacement wip

---
+++ packages/ds-medicare-gov/src/components/HelpDrawer/HelpDrawer.stories.jsx
@@ -0,0 +1,91 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import { Title, Subtitle, Description, ArgsTable, PRIMARY_STORY } from '@storybook/addon-docs';
+import { useArgs } from '@storybook/client-api';
+
+import { HelpDrawer, HelpDrawerToggle } from '@cmsgov/design-system';
+import InfoCircleOutlineIcon from '../Icons/InfoCircleOutlineIcon';
+// import HelpDrawerToggle from './index';
+
+export default {
+  title: 'Components/Help Drawer',
+  component: HelpDrawer,
+  argTypes: {
+    closeButtonText: {
+      control: { type: 'text' },
+    },
+  },
+  args: {
+    footerTitle: 'Footer Title',
+    footerBody: <p className="ds-text ds-u-margin--0">Footer content</p>,
+    heading: 'Drawer Heading',
+  },
+  subcomponents: { HelpDrawerToggle },
+  // The Drawer was overlapping the docs page, so customizing the docs page to remove the examples
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <ArgsTable story={PRIMARY_STORY} />
+        </>
+      ),
+    },
+  },
+};
+
+const drawerContent = (
+  <>
+    <strong>An Explanation</strong>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+      qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+      officia deserunt mollit anim id est laborum.
+    </p>
+  </>
+);
+
+export const HelpDrawerToggleWithDrawer = () => {
+  const [{ isDrawerVisible }, setIsDrawerVisible] = useArgs();
+
+  return (
+    <>
+      {isDrawerVisible && (
+        <HelpDrawer
+          onCloseClick={() => setIsDrawerVisible({ isDrawerVisible: false })}
+          footerTitle="Footer Title"
+          footerBody={<p className="ds-text ds-u-margin--0">Footer content</p>}
+          heading="Drawer Heading"
+        >
+          {drawerContent}
+        </HelpDrawer>
+      )}
+      <HelpDrawerToggle
+        showDrawer={() => setIsDrawerVisible({ isDrawerVisible: true })}
+        helpDrawerOpen={isDrawerVisible || false}
+        icon={<InfoCircleOutlineIcon />}
+      >
+        Drawer Toggle
+      </HelpDrawerToggle>
+    </>
+  );
+};
