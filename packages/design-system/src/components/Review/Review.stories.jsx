1b8d7ea9fe13507c48367d0e0cbb3df9536c1d89
zarahzachz <sarah.sachs@gmail.com>
2021-12-07 15:42:32 -0500

    Init Review story.

---
+++ packages/design-system/src/components/Review/Review.stories.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+import Review from './Review';
+
+export default {
+  title: 'Patterns/Review',
+  component: Review,
+  argTypes: {
+    headingLevel: {
+      control: {
+        type: 'select',
+      },
+      options: [1, 2, 3, 4, 5, 6],
+      defaultValue: 3,
+    },
+  },
+};
+
+const Template = ({ data, ...args }) => <Review {...args} />;
+
+export const DefaultReview = Template.bind({});
+DefaultReview.args = {
+  children: 'This is an example of a default Review component.',
+};
+
+export const SingleReview = Template.bind({});
+SingleReview.args = {
+  heading: 'A single Review component',
+  children: 'This is an example of a single Review component.',
+  editHref: 'javascript:void(0)',
+};
+
+export const MultipleReviews = Template.bind({});
+MultipleReviews.args = {
+  heading: 'Multiple Review components',
+  children: 'Multiple Review components can be combined together one after another.',
+  editHref: 'javascript:void(0)',
+};
+MultipleReviews.decorators = [
+  (Story) => (
+    <div>
+      {Story()}
+      {Story()}
+    </div>
+  ),
+];
+
+export const CustomActions = Template.bind({});
+CustomActions.args = {
+  heading: 'A Review component with custom actions',
+  children: 'You can add custom action links as needed.',
+  editContent: (
+    <div>
+      <a href="javascript:void(0)">Edit</a>
+      <span>|</span>
+      <a href="javascript:void(0)">Remove</a>
+    </div>
+  ),
+};
