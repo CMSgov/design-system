048e74bf55a687f2c0de167b0af55f1e75dea45f
niralkunadia12 <nirlkunadia@gmail.com>
2021-12-10 13:38:07 -0500

    PR Changes for adding Props

--- packages/ds-healthcare-gov/src/types/Footer/index.d.ts
+++ packages/ds-healthcare-gov/src/types/Footer/index.d.ts
@@ -1,9 +1,13 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable filenames/match-exported */
+
 import { Language } from '../index';
 
 export interface FooterProps {
   className?: string;
   initialLanguage?: Language;
   primaryDomain?: string;
+  footerTop?: React.ReactNode;
 }
 
 export class Footer extends React.Component<FooterProps, any> {
