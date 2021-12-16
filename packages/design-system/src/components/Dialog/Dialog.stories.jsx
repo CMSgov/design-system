2b5c274378ba75f30308c292f24f3c967b0db8b5
Forrest <forrest@forrestbaer.com>
2021-12-16 09:00:40 -0600

    added dialog component

---
+++ packages/design-system/src/components/Dialog/Dialog.stories.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useArgs } from '@storybook/client-api';
+import { Dialog as DialogComponent } from './Dialog';
+import Button from '../Button/Button';
+
+export default {
+  title: 'Components/Modal Dialog',
+  component: DialogComponent,
+  argTypes: {
+    children: { control: false },
+    shown: { table: { disable: true } },
+  },
+  args: {
+    alert: false,
+    analytics: false,
+    escapeExits: true,
+    underlayClickExits: false,
+    children: (
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan diam vitae metus
+        lacinia, eget tempor purus placerat.
+      </div>
+    ),
+    heading: 'Dialog Heading',
+    shown: true,
+  },
+};
+
+export const ModalDialog = ({ ...args }) => {
+  const [{ shown }, setShown] = useArgs();
+  const toggleShown = () => {
+    setShown({ shown: !shown });
+  };
+
+  return (
+    <>
+      <Button onClick={toggleShown} size="big" variation="primary">
+        Click to show modal
+      </Button>
+
+      {shown && (
+        <DialogComponent
+          {...args}
+          getApplicationNode={() => document.getElementById('root')}
+          actions={[
+            <button className="ds-c-button ds-c-button--primary ds-u-margin-right--1" key="primary">
+              Dialog action
+            </button>,
+            <button
+              className="ds-c-button ds-c-button--transparent"
+              key="cancel"
+              onClick={toggleShown}
+            >
+              Cancel
+            </button>,
+          ]}
+          onExit={toggleShown}
+        />
+      )}
+    </>
+  );
+};
