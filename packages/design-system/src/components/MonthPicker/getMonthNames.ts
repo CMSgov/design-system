d5d15c40575f1094aa8827905734eac76bad3ca6
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2021-12-17 10:53:39 -0800

    [WNMGDS-972] Convert MonthPicker to TypeScript (#1392)
    * A WIP first pass of migrating MonthPicker to TypeScript
    
    * Finished converting MonthPicker to TypeScript
    
    * Update tests so they don't print errors about missing handler functions
    
    * Actually, we don't need to require these. We treat them as optional in the code
    
    * Update file reference
    
    * Remove old type definition file

--- packages/design-system/src/components/MonthPicker/getMonthNames.ts
+++ packages/design-system/src/components/MonthPicker/getMonthNames.ts
@@ -7,13 +7,12 @@ export const NUM_MONTHS = 12;
  * @param  {boolean} [short] whether to return short month names
  * @return {string[]}        array of month names
  */
-export function getMonthNames(locale, short = true) {
-  const options = { month: short ? 'short' : 'long' };
+export function getMonthNames(locale: string, short = true) {
   const months = [];
   for (let i = 0; i < NUM_MONTHS; i++) {
     const date = new Date();
     date.setMonth(i, 1);
-    months.push(date.toLocaleString(locale, options));
+    months.push(date.toLocaleString(locale, { month: short ? 'short' : 'long' }));
   }
   return months;
 }
