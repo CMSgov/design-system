f88af0d486cc1381426a0eec53075381d6ba12ba
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2021-12-28 14:11:16 -0800

    (WIP) Fix last error with ts-ignore, but ts-ignore not allowed by linter

--- packages/design-system/src/components/TextField/TextInput.tsx
+++ packages/design-system/src/components/TextField/TextInput.tsx
@@ -9,7 +9,7 @@ export type TextInputSize = 'small' | 'medium';
 export type TextInputValue = string | number;
 export type TextInputErrorPlacement = 'top' | 'bottom';
 
-export interface TextInputProps {
+export interface CommonTextInputProps {
   /**
    * Apply an `aria-label` to the text field to provide additional
    * context to assistive devices.
@@ -84,18 +84,24 @@ export interface TextInputProps {
   value?: TextInputValue;
 }
 
-type OmitProps = 'size' | 'ref';
+export type OmitProps = 'size' | 'ref' | 'onCopy';
+
+export type MultilineTextInputProps = CommonTextInputProps & Omit<React.ComponentPropsWithoutRef<'textarea'>, OmitProps> & {
+  multiline: true;
+}
+
+export type SingleLineTextInputProps = CommonTextInputProps & Omit<React.ComponentPropsWithoutRef<'input'>, OmitProps> & {
+  multiline?: false;
+}
+
+export type TextInputProps = MultilineTextInputProps | SingleLineTextInputProps
 
 /**
  * <TextInput> is an internal component used by <TextField>, which wraps it and handles shared form UI like labels, error messages, etc
  * <TextInput> is also exported for advanced design system use cases, where the internal component can be leveraged to build custom form components
  * As an internal component, it's subject to more breaking changes. Exercise caution using <TextInput> outside of those special cases
  */
-const TextInput: FunctionComponent<
-  Omit<React.ComponentPropsWithoutRef<'textarea'>, OmitProps> &
-    Omit<React.ComponentPropsWithoutRef<'input'>, OmitProps> &
-    TextInputProps
-> = (props: TextInputProps) => {
+const TextInput: FunctionComponent<TextInputProps> = (props: TextInputProps) => {
   const {
     ariaLabel,
     errorId,
@@ -111,6 +117,7 @@ const TextInput: FunctionComponent<
     setRef,
     type,
     pattern,
+    onCopyCapture,
     ...inputProps
   } = props;
 
@@ -157,6 +164,9 @@ const TextInput: FunctionComponent<
       inputMode={numeric ? 'numeric' : undefined}
       pattern={numeric && !pattern ? '[0-9]*' : pattern}
       type={inputType}
+      // @ts-ignore: The ClipboardEventHandler for textareas and inputs are incompatible, and TS
+      // is failing to infer which one is being used here based on ComponentType.
+      onCopyCapture={onCopyCapture}
       {...inputProps}
     />
   );
