a123d1f7a3aeb230d18dff5c7f41a0e1b2c62d94
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2021-12-29 13:33:20 -0800

    Convert StepLink to TypeScript

---
+++ packages/design-system/src/components/StepList/StepLink.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+interface StepLinkProps {
+  /**
+   * Label text or HTML.
+   */
+  children: React.ReactNode;
+  href?: string;
+  stepId?: string;
+  screenReaderText?: string;
+  className?: string;
+  onClick: (href?: string, stepId?: string) => any;
+  component?: React.ComponentType | keyof JSX.IntrinsicElements;
+}
+
+export const StepLink = (props: StepLinkProps) => {
+  function handleClick(event: React.SyntheticEvent) {
+    if (props.onClick) {
+      event.preventDefault();
+      props.onClick(props.href, props.stepId);
+    }
+  }
+
+  const ComponentType = props.component ?? 'a';
+
+  return (
+    <ComponentType href={props.href} onClick={handleClick} className={props.className}>
+      {props.children}
+      {props.screenReaderText && (
+        <span className="ds-u-visibility--screen-reader"> {props.screenReaderText}</span>
+      )}
+    </ComponentType>
+  );
+};
+
+export default StepLink;
