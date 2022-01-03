fa58477b03f0de9baa42625e631f38aefc366b4e
Forrest Baer <forrest.baer@adhocteam.us>
2022-01-03 10:09:59 -0600

    [WNMGDS-979] - Tooltip/TooltipIcon Typescript conversion (#1451)
    * Initial cleanup
    
    * got popper working
    
    * settimeout makes it work somehow, saving for pair analysis
    
    * using refs and useLayoutEffect works
    
    * updated hooks for options
    
    * moved listeners to useeffect
    
    * cleanup, snapshot update
    
    * very small cleanup
    
    * oops this shouldnt be in here
    
    * syntax updates
    
    * dont need these either
    
    * fixing snapshots with ref change

--- packages/design-system/src/components/Tooltip/TooltipIcon.tsx
+++ packages/design-system/src/components/Tooltip/TooltipIcon.tsx
@@ -1,9 +1,15 @@
 import { InfoCircleIconThin } from '../Icons';
-import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 
-export const TooltipIcon = (props) => {
+export interface TooltipIconProps {
+  /*
+   * Renders inversed version of icon
+   */
+  inversed?: boolean;
+}
+
+export const TooltipIcon = (props: TooltipIconProps): React.ReactElement => {
   return (
     <span className="ds-c-tooltip-icon__container">
       <InfoCircleIconThin
@@ -13,10 +19,6 @@ export const TooltipIcon = (props) => {
       />
     </span>
   );
-};
-
-TooltipIcon.propTypes = {
-  inversed: PropTypes.bool,
 };
 
 export default TooltipIcon;
