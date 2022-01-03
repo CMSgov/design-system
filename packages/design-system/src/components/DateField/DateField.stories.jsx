7493aba41ced3413c750558a734ab3952202c1d2
Forrest Baer <forrest.baer@adhocteam.us>
2022-01-03 16:45:09 -0600

    [WNMGDS-1280] Clean up stories using background parameters (#1446)
    * moved params to global preview.js for storybook
    
    * removing params from choicelist

--- packages/design-system/src/components/DateField/DateField.stories.jsx
+++ packages/design-system/src/components/DateField/DateField.stories.jsx
@@ -12,25 +12,6 @@ export default {
       type: { name: 'string', required: true },
     },
   },
-  parameters: {
-    backgrounds: {
-      default: 'light',
-      values: [
-        {
-          name: 'light',
-          value: '#fff',
-        },
-        {
-          name: 'Hcgov dark',
-          value: '#112e51',
-        },
-        {
-          name: 'Mgov dark',
-          value: '#146a5d',
-        },
-      ],
-    },
-  },
   subcomponents: { DateInput },
 };
 
