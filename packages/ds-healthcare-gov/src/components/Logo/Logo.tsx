c9a32818fa92c73b79eecf032a668ee048e0ddb6
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-01-04 15:47:53 -0800

    Convert Logo component to TypeScript

--- packages/ds-healthcare-gov/src/components/Logo/Logo.tsx
+++ packages/ds-healthcare-gov/src/components/Logo/Logo.tsx
@@ -1,18 +1,19 @@
 import LogoEnSvg from './LogoEnSvg';
 import LogoEsSvg from './LogoEsSvg';
-import PropTypes from 'prop-types';
 import React from 'react';
 
-export function Logo(props) {
+type Language = 'en' | 'es';
+
+export interface LogoProps {
+  locale: Language;
+}
+
+export function Logo(props: LogoProps) {
   return <span className="hc-c-logo">{props.locale === 'es' ? <LogoEsSvg /> : <LogoEnSvg />}</span>;
 }
 
 Logo.defaultProps = {
   locale: 'en',
-};
-
-Logo.propTypes = {
-  locale: PropTypes.oneOf(['en', 'es']),
 };
 
 export default Logo;
