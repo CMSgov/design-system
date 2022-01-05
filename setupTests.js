f15a9dccde6ed405436436cd7425b3b5680e2b85
Anna Kammeyer <scheul93@gmail.com>
2022-01-05 10:46:06 -0700

    applying refactor changes to USA banner

--- setupTests.js
+++ setupTests.js
@@ -10,3 +10,15 @@ import { configure } from 'enzyme';
 
 import '@testing-library/jest-dom/extend-expect';
 configure({ adapter: new Adapter() });
+
+// following Jest's recommendation: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(), // Deprecated
+  removeListener: jest.fn(), // Deprecated
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+});
