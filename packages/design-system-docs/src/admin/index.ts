b6d9aa0c765c452da3c7ccba15fdfd79bdf51554
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-01-11 14:43:13 -0800

    Add the "Getting started" pages, since they're very similar to the guidelines pages

--- packages/design-system-docs/src/admin/index.ts
+++ packages/design-system-docs/src/admin/index.ts
@@ -4,3 +4,4 @@ import BasicPagePreview from './preview-templates/BasicPagePreview';
 CMS.init();
 CMS.registerPreviewStyle(`${process.env.rootPath}/index.css`);
 CMS.registerPreviewTemplate('guidelines', BasicPagePreview);
+CMS.registerPreviewTemplate('startup', BasicPagePreview);
