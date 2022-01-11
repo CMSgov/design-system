fcbf13c8262a8ecd5a941a153bb9e75e87852c41
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-01-11 12:21:15 -0800

    Loads preview stylesheet but no fonts

--- packages/design-system-docs/src/admin/index.ts
+++ packages/design-system-docs/src/admin/index.ts
@@ -3,3 +3,4 @@ import GuidancePagePreview from './preview-templates/GuidancePagePreview';
 
 CMS.init();
 CMS.registerPreviewTemplate('guidelines', GuidancePagePreview);
+CMS.registerPreviewStyle(`${process.env.rootPath}/index.css`);
