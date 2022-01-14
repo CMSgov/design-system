26e91bed53b3110b5889fa9fd409c02e4b5bf3f9
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-01-13 17:24:59 -0800

    Some commented-out code that doesn't work but illustrates what I'm trying to do

--- packages/design-system-docs/src/admin/preview-templates/BasicPagePreview.tsx
+++ packages/design-system-docs/src/admin/preview-templates/BasicPagePreview.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import Page from '../../scripts/components/Page';
 import { PreviewTemplateComponentProps } from 'netlify-cms-core';
 
-const BasicPagePreview = ({ entry, widgetFor }: PreviewTemplateComponentProps) => {
-  const title = entry.getIn(['data', 'title']);
-  const body = widgetFor('body');
+function replaceTemplateTags(content: string) {
+  const { rootPath, npmPackage, githubUrl, name } = process.env;
+  return content
+    .replace('{{root}}', rootPath ? `/${rootPath}` : '')
+    .replace('{{npm}}', npmPackage ? `${npmPackage}` : '')
+    .replace('{{github}}', githubUrl ? `${githubUrl}` : '')
+    .replace('{{name}}', name ? `${name}` : '');
+}
+
+const BasicPagePreview = (props: PreviewTemplateComponentProps) => {
+  const title = props.entry.getIn(['data', 'title']);
+  // const entryWithBodyUpdates = props.entry.updateIn(['data', 'body'],replaceTemplateTags)
+  // const body = props.widgetFor('body', undefined, entryWithBodyUpdates);
+  const body = props.widgetFor('body');
   return (
     <div className="ds-base">
       <Page header={title} description={body} />
