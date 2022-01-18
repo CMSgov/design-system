9faa44b6fcef06963ed5a5fc87f0b5e6f2846aad
Forrest Baer <forrest@forrestbaer.com>
2022-01-18 15:02:49 -0600

    scss export working

--- packages/design-system-tokens/index.ts
+++ packages/design-system-tokens/index.ts
@@ -1,47 +1,59 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import * as Types from './tokens/types'
+import * as fs from 'fs';
+import * as path from 'path';
 
-const OUTPUT_DIR = './dist'
-
-function main(): number {
-  // first two args are not needed
-  let args = process.argv.slice(2)
-  if (args.length  <= 0) {
-    console.warn('__ token tool arguments required __')
-    console.log('usage: index.ts infile output_type')
-    console.log('output_type can be one of: sketch, scss')
-    return
-  }
-
-  const input = args[0]
-  const inputExt = path.parse(input).ext
-  const inputName = path.parse(input).name
-
-  if (inputExt === '.ts' ) {
-    tsToScss(input).then((fd) => {
-      fs.writeFile(`${OUTPUT_DIR}/${inputName}`, fd[0], (err): void => {
-        if (err) console.error(`There was an issue writing to ${OUTPUT_DIR}/${inputName}: ${err}`)
-      })
-    })
-  }
-
-  return 1
-}
+const OUTPUT_DIR = './dist';
 
 async function tsToScss(file: string): Promise<string[]> {
-  let c,s
-  if (!file) return
+  let c = '';
+  let s = '';
+  if (!file) return ['error'];
 
-  const tsData: Types.Theme = await import(file) 
-  for (const k in tsData.color) {
-    c =+ `${k}: ${tsData.color[k]}\n`
+  const tsData = await import(file);
+
+  for (const [k, v] of Object.entries(tsData.default.color)) {
+    c += `$${k}: ${v}\n`;
   }
-  for (const k in tsData.spacing) {
-    c =+ `${k}: ${tsData.spacing[k]}\n`
+  for (const [k, v] of Object.entries(tsData.default.spacing)) {
+    s += `$${k}: ${v}\n`;
   }
 
-  return [c, s]
+  return [c, s];
 }
 
-main()
+function main() {
+  // first two args are not needed
+  const args = process.argv.slice(2);
+  if (args.length <= 0) {
+    console.warn('__ token tool arguments required __');
+    console.log('usage: index.ts infile output_type');
+    console.log('output_type can be one of: sketch, scss');
+    return 0;
+  }
+
+  const input = args[0];
+  const inputExt = path.parse(input).ext;
+  const inputName = path.parse(input).name;
+  const inputPath = path.parse(input).dir;
+  const inputAsImport = './' + inputPath + '/' + inputName;
+
+  if (inputExt === '.ts') {
+    return tsToScss(inputAsImport).then((fd) => {
+      fs.writeFile(`${OUTPUT_DIR}/${inputName}-color.scss`, fd[0], (err): void => {
+        if (err)
+          console.error(
+            `There was an issue writing to ${OUTPUT_DIR}/${inputName}-color.scss: ${err}`
+          );
+      });
+      fs.writeFile(`${OUTPUT_DIR}/${inputName}-spacing.scss`, fd[1], (err): void => {
+        if (err)
+          console.error(
+            `There was an issue writing to ${OUTPUT_DIR}/${inputName}-spacing.scss: ${err}`
+          );
+      });
+    });
+  }
+
+  return 1;
+}
+
+main();
--- packages/design-system-tokens/tests/test.ts
+++ packages/design-system-tokens/tests/test.ts
@@ -1,6 +1,6 @@
-import * as Tokens from '../tokens'
-import coreTheme from '../tokens/coreTheme'
+import * as Tokens from '../tokens';
+import coreTheme from '../brands/core/default-theme';
 
-console.log(Tokens.Color['gray-300'])
-console.log(Tokens.Spacing['spacer-5'])
-console.log(coreTheme.spacing['spacer-6'])
+console.log(Tokens.Color['color-blue-teal-900']);
+console.log(Tokens.Spacing['spacer-5']);
+console.log(coreTheme.spacing['spacer-6']);
