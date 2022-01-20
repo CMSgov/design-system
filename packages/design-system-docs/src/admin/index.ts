73dbb1b0378d2fec71405d1997c460be539d6daf
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-01-19 16:46:59 -0800

    Revert "Hooking up the identity widget to a dummy netlify.com works but..."
    This reverts commit e152d0b341799895e4dd22160572c941b2457105.

--- packages/design-system-docs/src/admin/index.ts
+++ packages/design-system-docs/src/admin/index.ts
@@ -1,12 +1,5 @@
 import CMS from 'netlify-cms-app';
-import netlifyIdentity from 'netlify-identity-widget';
 import BasicPagePreview from './preview-templates/BasicPagePreview';
-
-netlifyIdentity.init({ APIUrl: 'https://hungry-mccarthy-99ee22.netlify.app/.netlify/identity' });
-netlifyIdentity.open();
-netlifyIdentity.on('login', (user) => {
-  netlifyIdentity.close();
-});
 
 CMS.init();
 CMS.registerPreviewStyle(`${process.env.rootPath}/index.css`);
