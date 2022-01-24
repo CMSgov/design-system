79e3e9d3c192c50c1de7e76f7aa184f3ef316e61
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-01-24 15:31:14 -0800

    Add the `type` default back in
    Need to do it the functional-component way, and also need to move the destructuring assignment into the function's call signature in order for the prop-table generator to pick up on that default value

--- packages/design-system/src/components/Button/Button.tsx
+++ packages/design-system/src/components/Button/Button.tsx
@@ -46,6 +46,10 @@ type CommonButtonProps<T extends ButtonComponentType> = {
   onClick?: (...args: any[]) => any;
   size?: ButtonSize;
   /**
+   * Button [`type`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#attr-type) attribute
+   */
+  type?: React.ComponentProps<'button'>['type'];
+  /**
    * A string corresponding to the button-component variation classes.
    * The `'danger'` variation is deprecated and will be removed in a future release.
    */
@@ -72,14 +76,28 @@ type OtherProps<T extends ButtonComponentType> = Omit<
 
 export type ButtonProps<T extends ButtonComponentType> = CommonButtonProps<T> & OtherProps<T>;
 
-export const Button = <T extends ButtonComponentType>(props: ButtonProps<T>) => {
+export const Button = <T extends ButtonComponentType>({
+  children,
+  className,
+  component,
+  disabled,
+  href,
+  inputRef,
+  inversed,
+  inverse,
+  onClick,
+  size,
+  variation,
+  type = 'button',
+  ...otherProps
+}: ButtonProps<T>) => {
   if (process.env.NODE_ENV !== 'production') {
-    if (props.inverse) {
+    if (inverse) {
       console.warn(
         `[Deprecated]: Please remove the 'inverse' prop in <Button>, use 'inversed' instead. This prop has been renamed and will be removed in a future release.`
       );
     }
-    if (props.variation === 'danger') {
+    if (variation === 'danger') {
       console.warn(
         `[Deprecated]: Please remove the 'danger' variation prop in <Button>. This prop has will be removed in a future release.`
       );
@@ -87,8 +105,8 @@ export const Button = <T extends ButtonComponentType>(props: ButtonProps<T>) => 
   }
 
   function handleClick(e: React.MouseEvent | React.KeyboardEvent): void {
-    if (!props.disabled && props.onClick) {
-      props.onClick(e);
+    if (!disabled && onClick) {
+      onClick(e);
     }
   }
 
@@ -98,30 +116,10 @@ export const Button = <T extends ButtonComponentType>(props: ButtonProps<T>) => 
       handleClick(e);
     }
   }
-
-  /**
-   * Since any number of arbitrary props can be passed into this component, we
-   * use a destructuring assignment to get only the props we want to pass to the
-   * rendered HTML element. For example, the "variation" prop is used to generate
-   * the classNames, but doesn't need to be passed to the rendered component, so
-   * we omit it here so that it's not included in the props object.
-   */
-  const {
-    className,
-    component,
-    inputRef,
-    inversed,
-    inverse,
-    onClick,
-    size,
-    variation,
-    ...otherProps
-  } = props;
-
-  const ComponentType = component ?? (props.href ? 'a' : 'button');
+  const ComponentType = component ?? (href ? 'a' : 'button');
 
   const variationClass = variation && `ds-c-button--${variation}`;
-  const disabledClass = props.disabled && ComponentType !== 'button' && 'ds-c-button--disabled';
+  const disabledClass = disabled && ComponentType !== 'button' && 'ds-c-button--disabled';
   const sizeClass = size && `ds-c-button--${size}`;
   const inverseClass = (inversed || inverse) && 'ds-c-button--inverse';
   const allClassNames = classNames(
@@ -135,15 +133,19 @@ export const Button = <T extends ButtonComponentType>(props: ButtonProps<T>) => 
 
   const attrs: any = {
     className: allClassNames,
+    disabled,
+    href,
+    type,
     ...otherProps,
   };
 
-  if (props.onClick) {
+  if (onClick) {
     attrs.onClick = handleClick;
   }
 
   if (ComponentType !== 'button') {
-    // Assume `component` is not a <button> and remove <button> specific attributes
+    // Assume `component` is not a custom component rendering a <button>
+    // and remove <button> specific attributes
     attrs.role = 'button';
     delete attrs.disabled;
     delete attrs.type;
@@ -155,7 +157,7 @@ export const Button = <T extends ButtonComponentType>(props: ButtonProps<T>) => 
       onKeyPress={ComponentType === 'a' ? handleKeyPress : undefined}
       {...attrs}
     >
-      {props.children}
+      {children}
     </ComponentType>
   );
 };
