cbb7d7e23ce7d23f2d956b9d62a649e5ade824ae
Anna Kammeyer <scheul93@gmail.com>
2022-01-25 10:34:21 -0700

    drawer toggle test updates

--- packages/design-system/src/components/Drawer/DrawerToggle.test.tsx
+++ packages/design-system/src/components/Drawer/DrawerToggle.test.tsx
@@ -1,6 +1,7 @@
 import DrawerToggle, { DrawerToggleProps } from './DrawerToggle';
 import React from 'react';
-import { shallow } from 'enzyme';
+// import { shallow } from 'enzyme';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 const defaultProps = {
   drawerOpen: false,
@@ -9,12 +10,11 @@ const defaultProps = {
 };
 
 function renderDrawerToggle(props: Partial<DrawerToggleProps> = {}) {
-  const wrapper = shallow(
+  return render(
     <DrawerToggle {...defaultProps} {...props}>
       <p>content</p>
     </DrawerToggle>
   );
-  return { wrapper };
 }
 
 describe('DrawerToggle', () => {
@@ -23,34 +23,44 @@ describe('DrawerToggle', () => {
   });
 
   it('renders a button', () => {
-    const { wrapper } = renderDrawerToggle();
-    expect(wrapper).toMatchSnapshot();
+    const { asFragment } = renderDrawerToggle();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   it('calls showDrawer() on toggle click', () => {
-    const { wrapper } = renderDrawerToggle();
-    const toggle = wrapper.find('Button');
-    toggle.simulate('click');
+    renderDrawerToggle();
+    fireEvent.click(screen.getByRole('button'));
     expect(defaultProps.showDrawer).toHaveBeenCalled();
   });
 
   it('applies inline display style via inline prop', () => {
-    const { wrapper } = renderDrawerToggle({ inline: true });
-    const toggle = wrapper.find('Button');
-    expect(toggle.hasClass('ds-c-drawer__toggle--inline')).toBe(true);
+    renderDrawerToggle({ inline: true });
+    const toggle = screen.getByRole('button');
+    expect(toggle.classList.contains('ds-c-drawer__toggle--inline')).toBe(true);
   });
 
   it('applies custom class via className prop', () => {
     const className = 'test-class';
-    const { wrapper } = renderDrawerToggle({ className });
-    const toggle = wrapper.find('Button');
-    expect(toggle.props().className).toContain(className);
+    renderDrawerToggle({ className });
+    const toggle = screen.getByRole('button');
+    expect(toggle.classList).toContain(className);
   });
 
   it('passes through extra props', () => {
     const ariaLabel = 'test';
-    const { wrapper } = renderDrawerToggle({ 'aria-label': ariaLabel });
-    const toggle = wrapper.find('Button');
-    expect(toggle.props()['aria-label']).toBe(ariaLabel);
+    renderDrawerToggle({ 'aria-label': ariaLabel });
+    const toggle = screen.getByRole('button');
+    expect(toggle.getAttribute('aria-label')).toBe(ariaLabel);
+  });
+
+  it('focuses button when drawer is closed', () => {
+    const { rerender } = renderDrawerToggle({ drawerOpen: true });
+    rerender(
+      <DrawerToggle {...defaultProps} drawerOpen={false}>
+        <p>content</p>
+      </DrawerToggle>
+    );
+    const toggle = screen.getByRole('button');
+    expect(toggle).toEqual(document.activeElement);
   });
 });
