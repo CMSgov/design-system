53e9afe546f35a8a682f7826a71612b895bb078e
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-01-25 16:50:39 -0800

    Update version search to fetch versions from package files

---
+++ scripts/findDesignSystemVersions/index.ts
@@ -0,0 +1,96 @@
+import { request } from '@octokit/request';
+import Table from 'cli-table3';
+import chalk from 'chalk';
+
+const accessToken = process.argv[3];
+
+// PR is a Github pull request.
+interface Dependent {
+  repo: string;
+  packageName: string;
+  version: string;
+}
+
+async function findPackageVersions(packageName: string) {
+  // connect to github
+  const baseUrl = 'https://github.cms.gov/api/v3';
+  const apiRequest = request.defaults({
+    baseUrl,
+    headers: {
+      authorization: 'token ' + accessToken,
+      accept: 'application/vnd.github.v3.text-match+json',
+    },
+  });
+
+  const requestFile = request.defaults({
+    baseUrl,
+    headers: {
+      authorization: 'token ' + accessToken,
+      accept: 'application/vnd.github.v3.raw',
+    },
+  });
+
+  // Make request to Github search API
+  const searchString = packageName.replace('@cmsgov/', '') + '"';
+  const qualifiers = 'filename:package extension:json';
+  const q = `${searchString} ${qualifiers}`;
+  const searchResponse = await apiRequest('GET /search/code', { q });
+
+  // Pick through the results and keep the valid ones
+  const dependents: Dependent[] = [];
+  for (const item of searchResponse.data.items) {
+    const path = item.git_url.replace(baseUrl, '');
+    const fileResponse = await requestFile(`GET ${path}`);
+    const packageData = JSON.parse(fileResponse.data);
+    const version =
+      packageData.dependencies?.[packageName] ?? packageData.devDependencies?.[packageName];
+    if (version) {
+      // Only if we find the package key in the dependencies is this a valid result. Examples
+      // A common false positive is repos that use other design systems with the string
+      // "design-system" in the package name. Unfortunately the GitHub code-search API doesn't
+      // allow us to search strings the include characters like `@` and `/`
+      dependents.push({ repo: item.repository.full_name, packageName, version });
+    }
+  }
+
+  return dependents;
+}
+
+async function findDesignSystemVersions(packages: string[]) {
+  return Promise.all(packages.map(findPackageVersions));
+}
+
+// function printCsv(dependents: Dependent[]) {}
+
+function printTables(packages: string[], packageDependents: Dependent[][]) {
+  for (let i = 0; i < packages.length; i++) {
+    const packageName = packages[i];
+    const deps = packageDependents[i];
+    const table = new Table({ head: ['Repository', 'Version'] });
+    deps.forEach(({ repo, version }) => table.push([repo, version]));
+    console.log('');
+    console.log(chalk.cyan(packageName));
+    console.log(table.toString());
+  }
+}
+
+async function main() {
+  const packages = [
+    '@cmsgov/design-system',
+    '@cmsgov/ds-healthcare-gov',
+    '@cmsgov/ds-medicare-gov',
+    '@cmsgov/design-system-core',
+  ];
+  let packageDependents: Dependent[][];
+
+  try {
+    packageDependents = await findDesignSystemVersions(packages);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+  printTables(packages, packageDependents);
+}
+
+main();
