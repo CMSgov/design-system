45901e898d9466216a735ce74fd2e346446185a5
Anna Kammeyer <scheul93@gmail.com>
2022-02-02 11:08:40 -0700

    story for idle timeout dialog

--- packages/design-system/src/components/IdleTimeout/IdleTimeout.stories.tsx
+++ packages/design-system/src/components/IdleTimeout/IdleTimeout.stories.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
 import IdleTimeout from './IdleTimeout';
-import { useArgs } from '@storybook/client-api';
+import IdleTimeoutDialog from './IdleTimeoutDialog';
 
 export default {
   title: 'Components/Idle Timeout',
@@ -27,3 +29,15 @@ const Template = ({ ...args }) => (
 );
 
 export const Default = Template.bind({});
+export const ViewDialog = () => (
+  <IdleTimeoutDialog
+    continueSessionText="Continue Session"
+    heading="Are you still there?"
+    endSessionButtonText="Logout"
+    endSessionRedirectUrl="#"
+    message='You’ve been inactive for a while. <br/>Your session will end in <strong>2 minutes</strong>. <br/><br/>Select "Continue session" below if you want more time.'
+    onSessionContinue={() => {}}
+    onSessionForcedEnd={() => {}}
+    showSessionEndButton
+  />
+);
