c4ac42b57e4ff08ceabec72398d3d35c14e52211
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-03-25 15:27:53 -0700

    Working on updating UsaBanner unit tests but going to revert
    I wanted to use the prettyDOM function to filter out the bulk of the svg nodes for the sake of smaller snapshots. Unfortunately the output of that function is colorized. So I'll need to do something else

--- packages/design-system/src/components/UsaBanner/UsaBanner.test.jsx
+++ packages/design-system/src/components/UsaBanner/UsaBanner.test.jsx
@@ -1,53 +1,65 @@
 import React from 'react';
 import UsaBanner from './UsaBanner';
-import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { fireEvent, render, screen, prettyDOM } from '@testing-library/react';
 
-function render(customProps = {}) {
+function renderBanner(customProps = {}) {
   const props = Object.assign({}, customProps);
+  return render(<UsaBanner {...props} />);
+}
 
-  return {
-    props: props,
-    wrapper: shallow(<UsaBanner {...props} />),
-  };
+/**
+ * https://github.com/testing-library/dom-testing-library/blob/460115a471bcbd7e963f2751f7544bfe6f10f828/src/pretty-dom.js#L14-L25
+ */
+function filterOutElements(tagNames, container) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType#node_type_constants
+  const ELEMENT_NODE = 1;
+
+  const matchString = tagNames.join(', ');
+  const filteredHtml = prettyDOM(container, undefined, {
+    filterNode: (node) => node.nodeType !== ELEMENT_NODE || !node.matches(matchString),
+  });
+  return filteredHtml;
+}
+
+function expectMatchesSnapshot(container) {
+  expect(filterOutElements(['path', 'circle'], container)).toMatchSnapshot();
 }
 
 describe('UsaBanner', function () {
   it('renders correctly', () => {
-    const tree = renderer.create(<UsaBanner />).toJSON();
-
-    expect(tree).toMatchSnapshot();
+    const { container } = renderBanner();
+    // expect(asFragment()).toMatchSnapshot();
+    expectMatchesSnapshot(container);
   });
 
   it('applies Spanish translation', () => {
-    const tree = renderer.create(<UsaBanner locale="es" />).toJSON();
-
-    expect(tree).toMatchSnapshot();
+    const { asFragment } = renderBanner({ locale: 'es' });
+    expect(asFragment()).toMatchSnapshot();
   });
 
   it('applies additional class names to expanded banner', () => {
-    const { wrapper } = render();
-    const openButton = wrapper.find('.ds-c-usa-banner__button');
-    openButton.simulate('click');
-    const banner = wrapper.find('header');
-    expect(banner.hasClass('ds-c-usa-banner__header--expanded')).toBe(true);
-    expect(wrapper).toMatchSnapshot();
+    const { asFragment } = renderBanner();
+    const openButton = screen.getByRole('button');
+    fireEvent.click(openButton);
+    const header = screen.getByLabelText('bannerLabel').querySelector('header');
+    expect(header.className).toContain('ds-c-usa-banner__header--expanded');
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  it('adds className to root element', () => {
-    const data = render({ className: 'bar' });
+  // it('adds className to root element', () => {
+  //   const data = renderBanner({ className: 'bar' });
 
-    expect(data.wrapper.hasClass('bar')).toBe(true);
-  });
+  //   expect(data.wrapper.hasClass('bar')).toBe(true);
+  // });
 
-  it('has a unique id', () => {
-    const banner1 = render({ id: 'banner_unique' });
-    const banner2 = render();
-    const button1 = banner1.wrapper.find('.ds-c-usa-banner__button').first();
-    const content1 = banner1.wrapper.find('.ds-c-usa-banner__content').first();
-    const content2 = banner2.wrapper.find('.ds-c-usa-banner__content').first();
+  // it('has a unique id', () => {
+  //   const banner1 = renderBanner({ id: 'banner_unique' });
+  //   const banner2 = renderBanner();
+  //   const button1 = banner1.wrapper.find('.ds-c-usa-banner__button').first();
+  //   const content1 = banner1.wrapper.find('.ds-c-usa-banner__content').first();
+  //   const content2 = banner2.wrapper.find('.ds-c-usa-banner__content').first();
 
-    expect(button1.prop('aria-controls')).toBe(content1.prop('id'));
-    expect(content1.prop('id')).not.toBe(content2.prop('id'));
-  });
+  //   expect(button1.prop('aria-controls')).toBe(content1.prop('id'));
+  //   expect(content1.prop('id')).not.toBe(content2.prop('id'));
+  // });
 });
