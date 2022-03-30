b8642b9d0e9a4d5e52bcc3f845e88cff6bc71cf2
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-03-30 12:00:14 -0700

    This is not a bug yet, but...
    potentially an undefined value in the languageMatches call could fall back to the value of an undefined variable (`language`)

--- packages/design-system/src/components/i18n.ts
+++ packages/design-system/src/components/i18n.ts
@@ -5,7 +5,7 @@ import get from 'lodash/get';
 export type Language = 'en' | 'es';
 
 function detectDocumentLanguage(): Language | undefined {
-  const detectedLang = document?.querySelector('html')?.lang;
+  const detectedLang = document?.querySelector('html')?.lang ?? '';
   if (['en', 'es'].some((lang) => languageMatches(lang, detectedLang))) {
     return detectedLang as Language;
   } else {
