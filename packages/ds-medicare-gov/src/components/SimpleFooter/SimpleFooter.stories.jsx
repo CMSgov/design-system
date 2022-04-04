5d374e02647a971fe8715307e669a0f575bad6f6
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-04-04 14:27:46 -0700

    Add stories and new VRT screenshots for some medicare components

---
+++ packages/ds-medicare-gov/src/components/SimpleFooter/SimpleFooter.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import SimpleFooter from './SimpleFooter';
+
+export default {
+  title: 'Components/SimpleFooter',
+  component: SimpleFooter,
+  argTypes: {
+    language: {
+      control: 'radio',
+      options: ['en', 'es'],
+    },
+  },
+};
+
+const Template = ({ data, ...args }) => <SimpleFooter {...args} />;
+
+export const BasicSimpleFooter = Template.bind({});
