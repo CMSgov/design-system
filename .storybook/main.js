78440134dc402230e6c0b32740bd82d0a0d406bd
Anna Kammeyer <scheul93@gmail.com>
2022-04-04 18:20:40 -0600

    building storybook with mgov fonts wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,12 +16,44 @@ const getSrcDirectories = () => {
   return projectDirs;
 };
 
+const getStaticDirectoryPaths = () => {
+  let projectDirs;
+  switch (process.env.STORYBOOK_DS) {
+    case 'mgov':
+      projectDirs = [
+        {
+          from: '../packages/design-system/src/fonts',
+          to: '/core/fonts',
+        },
+        {
+          from: '../packages/design-system/src/images',
+          to: '/core/images',
+        },
+        {
+          from: '../packages/ds-medicare-gov/src/fonts',
+          to: 'mgov/fonts',
+        },
+        {
+          from: '../packages/ds-medicare-gov/src/images',
+          to: 'mgov/images',
+        },
+      ];
+      break;
+    case 'hcgov':
+      projectDirs = ['../packages/design-system/src', '../packages/ds-healthcare-gov/src'];
+      break;
+    default:
+      projectDirs = ['../packages/design-system/src'];
+  }
+  return projectDirs;
+};
+
 const getPathsForStories = () =>
   getSrcDirectories().map((dir) => `${dir}/**/*.stories.@(js|jsx|ts|tsx|mdx)`);
 
 module.exports = {
   stories: getPathsForStories(),
-  staticDirs: getSrcDirectories(),
+  staticDirs: getStaticDirectoryPaths(),
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
