bb01f8d8799849586668b6e685096af8312dbc2f
Anna Scheuler Kammeyer <33579665+scheul93@users.noreply.github.com>
2022-04-06 09:44:06 -0600

    [WNMGDS-1466] icon title updates (#1678)
    * Updating SVGicon and tooltip icon for optional title support
    
    * Updated tests
    
    * usa banner icon updates
    
    * updated accessibility for SVGIcon and tests
    
    * building storybook with mgov fonts wip
    
    * Icon updates especially for Tooltip
    
    * updated TooltipIcon guidance
    
    * updated tests
    
    * reverting out accidental wip code
    
    * Tooltip Icon docs clarification
    
    Co-authored-by: Patrick Wolfert <patrick.wolfert@adhocteam.us>
    
    Co-authored-by: Patrick Wolfert <patrick.wolfert@adhocteam.us>

--- packages/design-system/src/components/Tooltip/TooltipIcon.tsx
+++ packages/design-system/src/components/Tooltip/TooltipIcon.tsx
@@ -1,21 +1,23 @@
 import { InfoCircleIconThin } from '../Icons';
+import { IconCommonProps } from '../Icons/SvgIcon';
 import React from 'react';
 import classNames from 'classnames';
 
-export interface TooltipIconProps {
+export interface TooltipIconProps extends IconCommonProps {
   /*
    * Renders inversed version of icon
    */
   inversed?: boolean;
 }
 
-export const TooltipIcon = (props: TooltipIconProps): React.ReactElement => {
+export const TooltipIcon = ({ inversed, ...iconProps }: TooltipIconProps): React.ReactElement => {
   return (
     <span className="ds-c-tooltip-icon__container">
       <InfoCircleIconThin
         className={classNames('ds-c-tooltip-icon', {
-          'ds-c-tooltip-icon--inverse': props.inversed,
+          'ds-c-tooltip-icon--inverse': inversed,
         })}
+        {...iconProps}
       />
     </span>
   );
