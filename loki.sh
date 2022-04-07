39f40ef76c4afd6802940f4794744beb6b2c9608
Forrest Baer <forrest@forrestbaer.com>
2022-04-07 09:31:40 -0500

    adding params, new args, removed colors, set up trap handlers

--- loki.sh
+++ loki.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
+set -Eeo pipefail
+trap cleanup SIGINT SIGTERM ERR
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-t target] args
+
+Assists with running the Storybook Loki visual regression tests. Items in brackets
+are optional. Must have docker running and able to start up instances.
+
+Available options:
+
+args            One of: update/test/approve
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-t, --target    Optional, core is default, options are: healthcare/medicare
+
+EOF
+  exit
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR
+  pkill -f storybook
+}
 
 die() {
   local msg=$1
@@ -9,63 +35,72 @@ die() {
   exit "$code"
 }
 
-setup_colors() {
-  if [ -t 2 ] && [ "${TERM-}" != "dumb" ]; then
-    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-  else
-    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-  fi
-}
-setup_colors
-
 msg() {
   echo >&2 -e "${1-}"
 }
 
 run_storybook() {
+  targ=""
+
+  while :; do
+    case "${1-}" in
+      -h | --help) usage ;;
+      -v | --verbose) set -x ;;
+      -t | --target) 
+        targ=":${2-}"
+        export LOKI_FILE_PREFIX="${2-}"
+        shift
+        ;;
+      *) break ;;
+    esac
+    shift
+  done
+
   args=("$@")
 
-  if [ ${#args[@]} -eq 0 ]; then
-    args=''
-  else
-    args=":$@"
-  fi
+  [ ${#args[@]} -eq 0 ] && usage
 
-  yarn storybook$args --no-open > /dev/null 2>&1 &
+  yarn storybook$targ --no-open > /dev/null 2>&1 &
   SB_PID=$!
-  msg "${NOFORMAT}--- Starting ${CYAN}yarn storybook${args}${NOFORMAT} with process id ${GREEN}${SB_PID}${NOFORMAT}"
+  msg "\n-+- Starting: yarn storybook${targ} PID: ${SB_PID}"
+
+  return 0
 }
 
 run_storybook "$@"
 
 # wait for storybook to be ready
+msg "-i- Waiting for storybook instance to be ready, timeout in 20s ..\n"
+
 attempts=0
 until $(curl --output /dev/null --silent --head --fail http://localhost:6006); do
   if [ $attempts -eq 10 ]; then
     kill $SB_PID
-    die "[x] something went wrong starting storybook .."
+    die "[x] Something went wrong starting storybook ..\n"
   fi
   attempts=$(($attempts+1))
   sleep 2
 done
 
 # storybook is up, start the appropriate loki process
-msg "[${GREEN}✓${NOFORMAT}] Storybook is up!"
-msg "${NOFORMAT}--- Starting ${CYAN}yarn loki:update${args}${NOFORMAT}"
+msg "[✓] Storybook instance found at http://localhost:6006!"
+msg "-+- Starting: yarn loki ${args} ${LOKI_FILE_PREFIX}\n"
 
-if yarn loki:update$args ; then
-  msg "[${GREEN}✓${NOFORMAT}] Loki updated successfully"
+if yarn loki $args ; then
+  msg "\n[✓] Loki ${args} ran successfully"
 else
-  msg "[${RED}X${NOFORMAT}] There was a problem with Loki"
+  msg "\n[X] There was a problem with Loki ${args}"
 fi
 
 if pkill -f storybook ; then
-  msg "[${GREEN}✓${NOFORMAT}] Successfully killed storybook instance"
+  msg "[✓] Successfully killed storybook instance"
 else
-  msg "[${RED}X${NOFORMAT}] There was a problem killing storybook, trying harder"
+  msg "[X] There was a problem killing storybook, trying harder"
   if kill -9 $SB_PID ; then
-    msg "[${GREEN}✓${NOFORMAT}] That did it, storybook closed"
+    msg "[✓] That did it, storybook closed"
   else
-    msg "[${RED}X${NOFORMAT}] Storybook can't be killed, oh no!"
+    msg "[X] Storybook can't be killed, oh no, run!"
   fi
 fi
+
+echo ""
