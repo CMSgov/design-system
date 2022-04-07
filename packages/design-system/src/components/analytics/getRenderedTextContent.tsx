3c6ba84fc8b34aba5c0e34337ce7645262b3fe66
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-04-07 14:34:45 -0700

    Don't need to bother with testing for non-string types

--- packages/design-system/src/components/analytics/getRenderedTextContent.tsx
+++ packages/design-system/src/components/analytics/getRenderedTextContent.tsx
@@ -3,9 +3,8 @@ import ReactDOMServer from 'react-dom/server';
 
 export function getRenderedTextContent(reactNode: string | React.ReactNode): string {
   if (typeof reactNode === 'string') {
+    // Short circuit if we already have a string
     return reactNode;
-  } else if (typeof reactNode === 'number' || typeof reactNode === 'boolean') {
-    return reactNode.toString();
   }
 
   const staticHtml = ReactDOMServer.renderToStaticMarkup(<>{reactNode}</>);
