295709af41fc1d4b4afeb48759ba683d59928a2a
Anna Scheuler Kammeyer <33579665+scheul93@users.noreply.github.com>
2022-04-22 12:16:07 -0600

    [WNMGDS-1589] gatsby mobile nav (#1739)
    * gitignore for gatsby setup
    
    * initial add of gatsby doc site
    
    * updated gatsby build commands
    
    * updating header and layout for gatsby doc site
    
    * saving latest release version as environment variable for download
    
    * updated gatsby dependency classification to help resolve package version mis-matches
    
    * adding to lerna ignore
    
    * dynamic doc site nav
    
    * previous naming convention content files removal
    
    * adding classnames to doc site
    
    * moving github links into reusable component
    
    * mobile nav for gatsby doc site
    
    * adding spacing in footer for mobile nav button
    
    * cleanup
    
    * styling update
    
    * styling updates

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -1,15 +1,20 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import '../styles/index.scss';
 
 import Header from '../components/DocSiteHeader';
 import Footer from '../components/DocSiteFooter';
-import Sidebar from '../components/DocSiteNav';
+import Sidebar from '../components/DocSiteSidebar';
 import { SkipNav } from '@cmsgov/design-system';
 
 // Main landing page for site
 const IndexPage = () => {
   const env = 'prod';
+  const [isMobileNavOpen, setMobileNavOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setMobileNavOpen(!isMobileNavOpen);
+  };
 
   return (
     <div className="ds-base">
@@ -21,11 +26,17 @@ const IndexPage = () => {
       <SkipNav href="#main" />
       <Header />
       <div className="ds-l-row ds-u-margin--0">
-        <Sidebar />
+        <Sidebar isMobileNavOpen={isMobileNavOpen} />
         <main id="main" className="ds-u-text-transform--uppercase">
           Placeholder for main content
         </main>
       </div>
+      <button
+        className="ds-c-button ds-c-button--primary ds-u-md-display--none c-mobile-nav-button"
+        onClick={toggleMenu}
+      >
+        {isMobileNavOpen ? 'Close' : 'Menu'}
+      </button>
       <Footer />
     </div>
   );
