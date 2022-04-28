e2736bee7872f71a22be71ebdb8ab4935e251bf6
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-04-28 13:58:28 -0700

    Or we can just have a side-effect in the render function
    This way is simpler and theoretically more performant

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,7 @@
 import './storybookStyles.scss';
-import React, { useLayoutEffect } from 'react';
+import React from 'react';
 import { setLanguage } from '../packages/design-system/src/components/i18n';
 import { setLanguage as setLanguageFromPackage } from '@cmsgov/design-system';
-import { addons } from '@storybook/addons';
-import { FORCE_RE_RENDER } from '@storybook/core-events';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -42,14 +40,13 @@ export const globalTypes = {
 const languageSettingDecorator = (Story, context) => {
   const { language } = context.globals;
 
-  useLayoutEffect(() => {
-    setLanguage(language);
-    // In order for it to work in child design systems, which import i18n things from
-    // node_modules, we need to also call this version of the function which comes from
-    // our node_modules
-    setLanguageFromPackage(language);
-    addons.getChannel().emit(FORCE_RE_RENDER);
-  }, [language]);
+  // Yes, this is a side-effect in a render function, but it's the most performant way
+  // to avoid a flash of content in the wrong language.
+  setLanguage(language);
+  // In order for it to work in child design systems, which import i18n things from
+  // node_modules, we need to also call this version of the function which comes from
+  // our node_modules
+  setLanguageFromPackage(language);
 
   return <Story {...context} />;
 };
