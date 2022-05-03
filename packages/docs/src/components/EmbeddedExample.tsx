a26a784ba0dc46771373b562ebe17e199a2fe11c
Anna Scheuler Kammeyer <33579665+scheul93@users.noreply.github.com>
2022-05-03 15:49:21 -0600

    [WNMGDS-1596] Styles > Base content migration (#1789)
    * embedded examples and base styles content
    
    * adding prettier to format HTML

---
+++ packages/docs/src/components/EmbeddedExample.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/no-danger */
+import React, { useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import Prism from 'prismjs';
+import { ArrowIcon } from '@cmsgov/design-system';
+import parserHtml from 'prettier/parser-html';
+import prettier from 'prettier';
+
+interface EmbeddedExampleProps {
+  children: React.ReactElement;
+}
+
+/**
+ * Shows a code example as its rendered form as well as the HTML that is rendered for it
+ *
+ * Using ReactDOMServer's renderToString function to extract the HTML from a React child
+ * @see https://reactjs.org/docs/react-dom-server.html#rendertostring
+ *
+ * @todo HTML formatting is not great -- look into making it better
+ */
+const EmbeddedExample = ({ children }: EmbeddedExampleProps) => {
+  const [open] = useState(false);
+  const html = ReactDOMServer.renderToString(children);
+  const prettyHtml = prettier.format(html, { parser: 'html', plugins: [parserHtml] });
+  const highlightedContent = Prism.highlight(prettyHtml, Prism.languages.html);
+
+  return (
+    <section className="c-embedded-example">
+      <div className="ds-u-border--1 ds-u-padding--2">{children}</div>
+      <details open={open} className="c-embedded-example__details">
+        <summary className="ds-u-margin-y--1 ds-c-button ds-c-button--small ds-c-button--transparent ds-u-padding--1 ds-u-text-decoration--none">
+          <ArrowIcon direction="right" /> Code snippet
+        </summary>
+        <pre className="ds-u-margin-bottom--4 ds-u-overflow--auto ds-u-padding--2 language-html">
+          <code
+            dangerouslySetInnerHTML={{ __html: highlightedContent }}
+            className="language-html"
+          />
+        </pre>
+      </details>
+    </section>
+  );
+};
+
+export default EmbeddedExample;
