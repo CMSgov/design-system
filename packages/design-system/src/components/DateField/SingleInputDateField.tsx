746be77e42c6f4c06ee078e4df0c404e409a1b96
zarahzachz <sarah.sachs@gmail.com>
2022-05-18 11:03:53 -0400

    Init react-day-picker library.

---
+++ packages/design-system/src/components/DateField/SingleInputDateField.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { format } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+
+const SingleInputDateField = () => {
+  const [selected, setSelected] = React.useState<Date>();
+
+  let footer = <p>Please pick a day.</p>;
+  if (selected) {
+    footer = <p>You picked {format(selected, 'PP')}.</p>;
+  }
+  return <DayPicker mode="single" selected={selected} onSelect={setSelected} footer={footer} />;
+};
+
+export default SingleInputDateField;
--- packages/design-system/src/components/DateField/index.ts
+++ packages/design-system/src/components/DateField/index.ts
@@ -1,2 +1,3 @@
 export { default as DateField } from './MultiInputDateField';
+export { default as SingleInputDateField } from './SingleInputDateField';
 export { default as DateInput } from './DateInput';
