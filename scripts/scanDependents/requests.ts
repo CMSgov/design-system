bbdcb128cdb7b4c79baa41de346711a4abf1f927
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-05-18 17:07:02 -0700

    Remove commented-out code

--- scripts/scanDependents/requests.ts
+++ scripts/scanDependents/requests.ts
@@ -6,17 +6,6 @@ function authorization(accessToken: string) {
   return 'token ' + accessToken;
 }
 
-// export function requestApiEndpoint(...[action, requestParameters]: Parameters<typeof request>) {
-//   return request(action, {
-//     baseUrl,
-//     headers: {
-//       authorization: authorization(authorizationToken),
-//       accept: 'application/vnd.github.v3.text-match+json',
-//     },
-//     ...requestParameters,
-//   })
-// }
-
 export function getApiRequest(accessToken: string, baseUrl: string) {
   return request.defaults({
     baseUrl,
