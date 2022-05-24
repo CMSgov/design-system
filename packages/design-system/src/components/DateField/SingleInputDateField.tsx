4b5959ba14ff48d7ae18cd3288ee99d124e5b96e
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-05-24 09:56:54 -0700

    Trying to render the button as a sibling of the text field :(
    Unfortunately even when using the TextInput and FormControl directly, we still can't get the button as a direct sibling of the input because the LabelMask wraps it in a div with the label mask elements, so it ends up looking weird. We could bottom-align things, but the whole setup doesn't give me a lot of confidence

--- packages/design-system/src/components/DateField/SingleInputDateField.tsx
+++ packages/design-system/src/components/DateField/SingleInputDateField.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import CalendarIcon from '../Icons/CalendarIcon';
 import classNames from 'classnames';
+import omit from 'lodash/omit';
+import pick from 'lodash/pick';
 import { Button } from '../Button';
 import { DayPicker } from 'react-day-picker';
 import { DATE_MASK, RE_DATE } from '../TextField/LabelMask';
-import { FormControlProps } from '../FormControl/FormControl';
-import { TextField } from '../TextField';
+import { FormControl, FormControlProps, FormControlPropKeys } from '../FormControl/FormControl';
+import { TextInput } from '../TextField';
 import { format } from 'date-fns';
 
 export interface SingleInputDateFieldProps extends Omit<FormControlProps, 'label' | 'render'> {
@@ -31,11 +33,9 @@ export interface SingleInputDateFieldProps extends Omit<FormControlProps, 'label
 
 const SingleInputDateField = (props: SingleInputDateFieldProps) => {
   const [pickerVisible, setPickerVisible] = useState(false);
-
+  const containerProps = pick(props, FormControlPropKeys);
   const {
     className,
-    label,
-    name,
     value,
     onBlur,
     onChange,
@@ -46,8 +46,8 @@ const SingleInputDateField = (props: SingleInputDateFieldProps) => {
     toDate,
     toMonth,
     toYear,
-    ...textFieldProps
-  } = props;
+    ...inputProps
+  } = omit(props, FormControlPropKeys);
   const withPicker = fromDate || fromMonth || fromYear;
 
   // TODO: Validate this and make date null if it's invalid. Don't pass a bizarre date
@@ -57,35 +57,37 @@ const SingleInputDateField = (props: SingleInputDateFieldProps) => {
 
   function handlePickerChange() {}
 
-  // let footer = <p>Please pick a day.</p>;
-  // if (selected) {
-  //   footer = <p>You picked {format(selected, 'PP')}.</p>;
-  // }
-  // return <DayPicker mode="single" selected={date} onSelect={handlePickerChange} />;
   return (
-    <div
+    <FormControl
+      {...containerProps}
       className={classNames(
         'ds-c-single-input-date-field',
         { 'ds-c-single-input-date-field--with-picker': withPicker },
         className
       )}
-    >
-      <TextField
-        {...textFieldProps}
-        className="ds-c-single-input-date-field__field"
-        name={name}
-        value={value}
-        label={label}
-        labelMask={DATE_MASK}
-        onChange={(event) => onChange(event, DATE_MASK(event.currentTarget.value, true))}
-      />
-      {withPicker && (
-        <Button onClick={() => setPickerVisible(!pickerVisible)}>
-          <CalendarIcon ariaHidden={false} />
-        </Button>
+      component="div"
+      labelComponent="label"
+      render={({ id, errorId, setRef }) => (
+        <div className="ds-c-single-input-date-field__field-wrapper">
+          <TextInput
+            {...inputProps}
+            {...{ id, setRef, errorId }}
+            className="ds-c-single-input-date-field__field"
+            value={value}
+            labelMask={DATE_MASK}
+            onChange={(event) => onChange(event, DATE_MASK(event.currentTarget.value, true))}
+          />
+          {withPicker && (
+            <Button onClick={() => setPickerVisible(!pickerVisible)}>
+              <CalendarIcon ariaHidden={false} />
+            </Button>
+          )}
+          {pickerVisible && (
+            <DayPicker mode="single" selected={date} onSelect={handlePickerChange} />
+          )}
+        </div>
       )}
-      {pickerVisible && <DayPicker mode="single" selected={date} onSelect={handlePickerChange} />}
-    </div>
+    />
   );
 };
 
