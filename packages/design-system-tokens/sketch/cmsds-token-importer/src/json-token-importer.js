059bf0311322c95c3822671396ef38aeec492bbc
Forrest Baer <forrest@forrestbaer.com>
2022-05-31 12:34:07 -0500

    adding default sketch plugin for use and updating sketch plugin to organize color tokens by name so they are easier to manage

--- packages/design-system-tokens/sketch/cmsds-token-importer/src/json-token-importer.js
+++ packages/design-system-tokens/sketch/cmsds-token-importer/src/json-token-importer.js
@@ -3,17 +3,19 @@ import dialog from '@skpm/dialog';
 import { readFileSync } from '@skpm/fs';
 
 /*
- * split tokens into groups based on root name
- * and store in tokens submenu, returns array
- * of Sketch Swatches
+ * Split tokens into groups based on root name and store in category,
+ * based on first word before '-'.
+ *
+ * @param colorTokens - An object which contains color name:value pairs
+ * @returns An array of Swatch objects created by the Sketch API
  */
 const makeColorSwatches = (colorTokens) => {
   const swatches = [];
   for (const [key, value] of Object.entries(colorTokens)) {
-    let colorName = key.match(/(^[A-Za-z]+-?[A-Za-z]+?)-?\d+?$/);
+    let colorName = key.match(/(^[A-Za-z]*)-?[A-Za-z]*?-?[A-Za-z\d]*?$/);
     colorName = colorName === null ? (colorName = '') : colorName[1] + '/';
-    const currentSwatch = sketch.Swatch.from({
-      name: `tokens/${colorName}${key}`,
+    let currentSwatch = sketch.Swatch.from({
+      name: `${colorName}/${key}`,
       color: value,
     });
     swatches.push(currentSwatch);
