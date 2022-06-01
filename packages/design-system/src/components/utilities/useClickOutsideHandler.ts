9b35f219ed5f7c850406b364cb7977bf12031f85
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-06-01 10:34:05 -0700

    Add doc comments for the `useClickOutsideHandler`

--- packages/design-system/src/components/utilities/useClickOutsideHandler.ts
+++ packages/design-system/src/components/utilities/useClickOutsideHandler.ts
@@ -2,6 +2,17 @@ import { RefObject, useEffect } from 'react';
 
 type ClickOutsideEvent = MouseEvent | TouchEvent;
 
+/**
+ * Listens for mouse and touch events on the document and calls the provided
+ * handler function as long as the event did not originate in one of the
+ * elements corresponding to the React Refs in the `insideRefs` array. For
+ * example, if you want to know if the user clicked outside of a dialog or
+ * its trigger, you can pass it an array containing the refs to the dialog
+ * and the trigger.
+ *
+ * @param insideRefs - Refs to elements that are considered "inside"
+ * @param handler - called when the event target was outside the "inside" elements
+ */
 export function useClickOutsideHandler(
   insideRefs: RefObject<HTMLElement>[],
   handler: (e: ClickOutsideEvent) => any
