f4dbdd3933d34b802368e986f2f797fb37e25de0
Forrest Baer <forrest.baer@adhocteam.us>
2022-06-16 14:07:12 -0500

    adding configuration options widget for doc site components from themes

---
+++ packages/design-system-tokens/src/themes/index.ts
@@ -0,0 +1,6 @@
+export { default as CoreTheme } from './core'
+export { default as CoreComponentTheme } from './core-components'
+export { default as HealthcareTheme } from './healthcare'
+export { default as HealthcareComponentTheme } from './healthcare-components'
+export { default as MedicareTheme } from './medicare'
+export { default as MedicareComponentTheme } from './medicare-components'
