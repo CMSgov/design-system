25d9ed5985a466963c4cba0f97c7991c04d24213
Ni Chia <ni.chia@adhocteam.us>
2022-06-22 15:36:38 -0400

    import core Accordion and overwrite its default

---
+++ packages/ds-healthcare-gov/src/components/Accordion/index.tsx
@@ -0,0 +1,20 @@
+import { AccordionItem } from '@cmsgov/design-system';
+import { MinusCircleIcon, PlusCircleIcon } from '@cmsgov/design-system';
+
+AccordionItem.defaultProps = {
+  ...AccordionItem.defaultProps,
+  // closeIcon: (
+  //   <MinusCircleIcon
+  //     className="ds-c-accordion__button-icon"
+  //     title={t('accordion.close')}
+  //     ariaHidden={false}
+  //   />
+  // ),
+  // openIcon: (
+  //   <PlusCircleIcon
+  //     className="ds-c-accordion__button-icon"
+  //     title={t('accordion.open')}
+  //     ariaHidden={false}
+  //   />
+  // ),
+};
