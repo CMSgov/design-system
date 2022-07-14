09e1b4f35cc88f253cfc4b23ffec7e73f5b857ae
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-07-14 09:59:11 -0700

    [NO-TICKET] Fix scan scripts after node 16 upgrade (#1955)
    Fix scan scripts after node 16 upgrade

--- scripts/scanDependents/index.ts
+++ scripts/scanDependents/index.ts
@@ -26,18 +26,18 @@ const argv = yargs
 
 dotenv.config();
 
-const accessToken = argv.token ?? process.env.GHE_ACCESS_TOKEN;
-if (!accessToken) {
-  console.error(
-    chalk.red(
-      'A GitHub Enterprise personal access token is required. Please provide this as an argument or set a GHE_ACCESS_TOKEN environment variable using an .env file or by exporting that variable within your current shell.'
-    )
-  );
-  yargs.showHelp();
-  process.exit(1);
-}
-
 async function main() {
+  const accessToken = argv.token ?? process.env.GHE_ACCESS_TOKEN;
+  if (!accessToken) {
+    console.error(
+      chalk.red(
+        'A GitHub Enterprise personal access token is required. Please provide this as an argument or set a GHE_ACCESS_TOKEN environment variable using an .env file or by exporting that variable within your current shell.'
+      )
+    );
+    yargs.showHelp();
+    process.exit(1);
+  }
+
   try {
     if (yargs.argv.react) {
       await printReactVersions(accessToken, designSystemPackageNames);
