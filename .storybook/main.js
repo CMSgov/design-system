eed8e23f8402e7b26cb009a7835b65a7f18bc144
Forrest Baer <forrest@forrestbaer.com>
2022-07-15 13:51:33 -0500

    storybook config changes to only include appropriate stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
+const env = process.env.STORYBOOK_DS ?? 'core';
+
+let storyList = ['../packages/design-system/src/**/*.stories.@(js|jsx|ts|tsx|mdx)'];
+if (env === 'healthcare' || env === 'all')
+  storyList.push('../packages/ds-healthcare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)');
+if (env === 'medicare' || env === 'all')
+  storyList.push('../packages/ds-medicare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)');
+
 module.exports = {
   features: {
     postcss: false,
   },
-  stories: [
-    '../packages/design-system/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-    '../packages/ds-healthcare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-    '../packages/ds-medicare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-  ],
+  stories: storyList,
   staticDirs: ['../packages/design-system/src'],
   addons: [
     '@storybook/addon-links',
