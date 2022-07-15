0848232395a46fc3ccb7df51be47543c3a78dcc0
Forrest Baer <forrest@forrestbaer.com>
2022-07-15 17:08:37 -0500

    updates to place data-theme on main html element, removing unwrapper from docs, a few small old sass references fixed and importing correct storybook assets now per system

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,29 @@
 const env = process.env.STORYBOOK_DS ?? 'core';
 
+let statics = ['../packages/design-system/src'];
 let storyList = ['../packages/design-system/src/**/*.stories.@(js|jsx|ts|tsx|mdx)'];
-if (env === 'healthcare' || env === 'all')
+
+if (env === 'healthcare') {
   storyList.push('../packages/ds-healthcare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)');
-if (env === 'medicare' || env === 'all')
+  // statics.push('../packages/ds-healthcare-gov/src')
+}
+if (env === 'medicare') {
   storyList.push('../packages/ds-medicare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)');
+  // statics.push('../packages/ds-medicare-gov/src')
+}
+// gatsby has it's own way of handling static assets
+// but requires all stories available for inclusion as iframes
+if (env === 'gatsby') {
+  storyList.push('../packages/ds-healthcare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)');
+  storyList.push('../packages/ds-medicare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)');
+}
 
 module.exports = {
   features: {
     postcss: false,
   },
   stories: storyList,
-  staticDirs: ['../packages/design-system/src'],
+  staticDirs: statics,
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
