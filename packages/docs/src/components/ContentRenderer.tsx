9f4a3c2bbf0dcc2112640e77b7bf69744ce766e6
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-07-27 15:10:37 -0700

    Fix rebase error

--- packages/docs/src/components/ContentRenderer.tsx
+++ packages/docs/src/components/ContentRenderer.tsx
@@ -5,6 +5,7 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { MDXProvider } from '@mdx-js/react';
 import { toKebabCase, toLowerCaseOneWord } from '../helpers/casingUtils';
 
+import ButtonMigrationTable from './ButtonMigrationTable';
 import ButtonVariationsTable from './ButtonVariationsTable';
 import EmbeddedExample from './EmbeddedExample';
 import StorybookExample from './StorybookExample';
@@ -92,6 +93,7 @@ const customComponents = (theme) => ({
   ResponsiveExample: (props) => <ResponsiveExample theme={theme} {...props} />,
   ComponentThemeOptions: (props) => <ComponentThemeOptions theme={theme} {...props} />,
   ThemeContent: (props) => <ThemeContent theme={theme} {...props} />,
+  ButtonMigrationTable: (props) => <ButtonMigrationTable theme={theme} {...props} />,
   ButtonVariationsTable: (props) => <ButtonVariationsTable theme={theme} {...props} />,
 });
 
