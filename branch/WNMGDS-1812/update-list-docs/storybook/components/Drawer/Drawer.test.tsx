fdf872720c5c436979750e56de84f1ec4d74200b
zarahzachz <sarah.sachs@gmail.com>
2022-07-29 13:52:34 -0400

    Updates

---
+++ branch/WNMGDS-1812/update-list-docs/storybook/components/Drawer/Drawer.test.tsx
@@ -0,0 +1,66 @@
+import Drawer from './Drawer';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const defaultProps = {
+  footerBody: (
+    <div>
+      <p>Some footer content</p>
+    </div>
+  ),
+  footerTitle: 'Footer title',
+  onCloseClick: () => {},
+  heading: 'Drawer title',
+};
+
+function renderDrawer(overwriteProps = {}) {
+  const props = Object.assign({}, defaultProps, overwriteProps);
+  return render(
+    <Drawer {...props}>
+      <p>content</p>
+    </Drawer>
+  );
+}
+
+describe('Drawer', () => {
+  it('renders a dialog', () => {
+    renderDrawer();
+    expect(screen.getByRole('dialog')).toMatchSnapshot();
+  });
+
+  describe('onCloseClick', () => {
+    it('calls onCloseClick on close button click', () => {
+      const onCloseClick = jest.fn();
+      renderDrawer({ onCloseClick });
+      userEvent.click(screen.getByText('Close'));
+
+      expect(onCloseClick).toHaveBeenCalled();
+    });
+
+    it('should handle `esc` with focus trap enabled', () => {
+      const onCloseClick = jest.fn();
+      renderDrawer({ onCloseClick, hasFocusTrap: true });
+      userEvent.keyboard('{Escape}');
+
+      expect(onCloseClick).toHaveBeenCalled();
+    });
+
+    it('removes event listener on unmount', () => {
+      const onCloseClick = jest.fn();
+      const { unmount } = renderDrawer({ onCloseClick, hasFocusTrap: true });
+      unmount();
+      userEvent.keyboard('{Escape}');
+
+      expect(onCloseClick).not.toHaveBeenCalled();
+    });
+
+    it('should not call onCloseClick for other key presses', () => {
+      const onCloseClick = jest.fn();
+      renderDrawer({ onCloseClick, hasFocusTrap: true });
+      userEvent.keyboard('a');
+
+      expect(onCloseClick).not.toHaveBeenCalled();
+    });
+  });
+});
---
+++ branch/WNMGDS-1812/update-list-docs/storybook/components/utilities/visibility.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default {
+  title: 'Utilities/Visibility',
+  parameters: {
+    docs: {
+      source: {
+        type: 'dynamic',
+      },
+    },
+  },
+};
+
+// an example for responsive visibility -- referenced in doc site
+export const visibility = () => (
+  <div>
+    {['sm', 'md', 'lg', 'xl'].map((breakpoint) => (
+      <div className="ds-u-padding--1 ds-u-border--1" key={breakpoint}>
+        <div className={`ds-u-${breakpoint}-visibility--hidden ds-u-color--muted`}>
+          Hidden on {breakpoint} screens and larger
+        </div>
+        <div className={`ds-u-visibility--hidden ds-u-${breakpoint}-visibility--visible`}>
+          Visible on {breakpoint} screens and larger
+        </div>
+      </div>
+    ))}
+  </div>
+);
