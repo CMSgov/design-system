6e14ff46a1f993d6d96dcb8854506e077fca5812
Forrest Baer <forrest@forrestbaer.com>
2022-08-02 13:02:16 -0500

    updated other configurations, changed node-glob for globby which automatically uses gitignore, file restructuring and comments

--- scripts/migration/src/lib/migrate-helpers.mjs
+++ scripts/migration/src/lib/migrate-helpers.mjs
@@ -1,43 +1,96 @@
-import glob from 'glob';
 import chalk from 'chalk';
+import { globby } from 'globby';
+import inquirer from 'inquirer';
 import path from 'path';
 import { readdir, readFile, writeFile } from 'node:fs/promises';
-import inquirer from 'inquirer';
 
-export const error = (message) => {
-  console.log(`${chalk.red('!!')} Error: ${message}`)
-  process.exit(1)
-}
-
-export const getGlob = (pattern, config) => {
+// confirm prompt for starting operations, display warning and y/n dialog
+export const confirmStart = () => {
   return new Promise((resolve, reject) => {
-    glob(pattern, config, (err, matches) => {
-      if (err) reject(err)
-      resolve(matches)
-    })
+    inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'yesno',
+        prefix: `${chalk.red('!!')} Please be sure you are running this in a git repo. ${chalk.red('!!\n!!')} Filesystem changes will occur.                     ${chalk.red('!!\n\n')}${chalk.green('?')}`,
+        message: 'Begin search and replace?',
+        default: true,
+      }])
+      .then(choice => { resolve(choice.yesno) })
+      .catch(err => { reject(err) })
   })
 }
 
+// return array of all globby pattern matches in one level
 export const doPatternSearch = async (config) => {
   let res = []
   for (const p of config.patterns) {
-    res.push(...await getGlob(p, config.globConfig))
+    res.push(...await getGlob(p, config.globbyConfig).catch(err => error(err)))
   }
   return res
 }
 
+// error and quit
+export const error = (message) => {
+  console.log(`${chalk.red('!!')} Error: ${message}\n`)
+  process.exit(1)
+}
+
+// read file contents of all matched files, return filename and data, default matches (0)
 export const getAllFileContents = (fileList) => {
   const readPromises = fileList.map(async file => {
     return readFile(file, 'utf8')
       .then(data => {
         return { file: file, data: data, matches: 0 }
-      })
+      }).catch(err => error(err))
   })
   return Promise.all(readPromises)
 }
 
+// return list of files from path
+export const getConfigFileList = (path) => {
+  return new Promise((resolve, reject) => {
+    readdir(path)
+      .then(files => {
+        resolve(files)
+      })
+      .catch(err => reject(err))
+  })
+}
+
+// returns globby promise based on search pattern and configuration
+// https://github.com/sindresorhus/globby
+export const getGlob = (pattern, config) => {
+  return new Promise((resolve, reject) => {
+    globby(pattern, config)
+      .then(matches => resolve(matches))
+      .catch(err => reject(err))
+  })
+}
+
+// display list of configuration files retreived by readConfigFile
+export const inquireForFile = (folder, options) => {
+  return new Promise((resolve, reject) => {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'file',
+        choices: options,
+        pageSize: 4,
+        prefix: `\nChoose a migration configuration to run from ${folder}\n`,
+      }])
+      .then(choice => {
+        const configPath = `${folder}/${choice['file']}`
+        resolve(readConfigFile(configPath))
+      })
+      .catch(err => { reject(err) })
+  })
+}
+
+// for each expression (string.replace(from, to)) iterate over each file and make
+// changes where a match was found. Records number of matches per file. 
+// @TODO: could probably be sped up but it already seems pretty fast.
 export const modifyFileContents = async (content, expr) => {
-  // filter out items with less than 3 newlines, likely compressed files
+  // filter out files with less than 3 newlines, likely compressed/minified files
   content.filter(f => {
     return (f.data.match(/[\n\r]/g) || []).length > 3
   })
@@ -52,10 +105,10 @@ export const modifyFileContents = async (content, expr) => {
     return f
   })
 
+  // promise for writing file to the FS
   const writePromises = content.map(async content => {
     if (content.matches > 0) {
       return writeFile(content.file, content.data, 'utf8')
-        // add a percentage bar complete
         .then(() => {
           return { file: content.file, matches: content.matches }
         })
@@ -65,80 +118,38 @@ export const modifyFileContents = async (content, expr) => {
     }
   })
 
+  // write out list of files that changed and how many changes were made
   return await Promise.all(writePromises)
     .then((results) => {
       results.forEach(result => {
         if (result.matches > 0) {
           // gives a shorter name
           const shortname = path.relative(process.cwd(), result.file)
-          console.log(`${chalk.green('++')} ${shortname}${chalk.gray(',')} ${chalk.whiteBright(result.matches)} changes`)
+          console.log(`${chalk.yellow('>>')} ${shortname}${chalk.gray(',')} ${chalk.whiteBright(result.matches)} changes`)
         }
       })
     })
 }
 
+// read configuration file and return array with data and config filename
 export const readConfigFile = (file) => {
   return new Promise((resolve, reject) => {
     readFile(file, 'utf8')
       .then(data => {
-        resolve(JSON.parse(data))
+        resolve([JSON.parse(data), file])
       })
       .catch(err => reject(err))
-  })
-}
-
-export const getConfigFileList = (path) => {
-  return new Promise((resolve, reject) => {
-    readdir(path)
-      .then(files => {
-        resolve(files)
-      })
-      .catch(err => reject(err))
-  })
-}
-
-export const confirmStart = () => {
-  return new Promise((resolve, reject) => {
-    inquirer.prompt([
-      {
-        type: 'confirm',
-        name: 'yesno',
-        prefix: `${chalk.red('!!')} Please be sure you are running this in a git repo. ${chalk.red('!!\n!!')} Filesystem changes will occur.                     ${chalk.red('!!\n\n')}${chalk.green('?')}`,
-        message: 'Begin search and replace?',
-        default: true,
-      }])
-      .then((choice) => { resolve(choice.yesno) })
-      .catch((err) => { reject(err) })
-  })
-}
-
-export const inquireForFile = (folder, options) => {
-  return new Promise((resolve, reject) => {
-    inquirer.prompt([
-      {
-        type: 'list',
-        name: 'file',
-        choices: options,
-        pageSize: 4,
-        prefix: `\nChoose a migration configuration to run from ${folder}\n`,
-      }])
-      .then((choice) => {
-        const configPath = `${folder}/${choice['file']}`
-        resolve(readConfigFile(configPath))
-      })
-      .catch((err) => { reject(err) })
   })
 }
 
 export default {
   confirmStart,
   doPatternSearch,
-  getConfigFileList,
+  error,
   getAllFileContents,
+  getConfigFileList,
   getGlob,
   inquireForFile,
+  modifyFileContents,
   readConfigFile,
 }
-
-// find . -path "*/.*" -prune -o -name node_modules -prune -o -type f \( -iname \*.jsx -o -iname \*.tsx -o -iname \*.ts -o -iname \*.js \) -print
-
--- scripts/migration/src/main.js
+++ scripts/migration/src/main.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 import chalk from 'chalk';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
 import {
   confirmStart,
   doPatternSearch,
@@ -13,54 +11,75 @@ import {
   modifyFileContents,
   readConfigFile,
 } from './lib/migrate-helpers.mjs';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs';
 
 (async () => {
-  // get args
+  // get command line args
   const argv = yargs(hideBin(process.argv))
-    .scriptName('migration-helper')
+    .scriptName('cmsds-migrate')
     .usage(
       '$0 --file migration-config-file.json --cwd "../root/directory --ignore "**/ignored/**", "**/another_ignored/**"'
     )
-    .option('file', {
-      type: 'string',
-      description: 'Migration configuration file.',
-    })
-    .option('cwd', {
-      type: 'string',
-      description: 'Working directory to scan.',
-    })
-    .option('ignore', {
-      type: 'array',
-      description: 'Glob patterns to ignore during search.',
+    .options({
+      config: {
+        type: 'string',
+        description: 'Migration configuration file.',
+      },
+      cwd: {
+        type: 'string',
+        description: 'Root directory for scan. Defaults to CWD.',
+        default: process.cwd(),
+      },
+      ignore: {
+        type: 'array',
+        description: 'Other glob patterns to ignore during search. .gitignore included by default.',
+        default: [],
+      },
+      force: {
+        boolean: true,
+        description: 'Skip interactive prompts, use caution!',
+        default: false,
+      },
     })
     .help().argv;
 
+  // get list of configuration files
   const CONFIG_FOLDER = './configs';
-  const configList = await getConfigFileList(CONFIG_FOLDER);
+  const configList = await getConfigFileList(CONFIG_FOLDER).catch((err) => error(err));
 
-  const configData = !argv.file
-    ? await inquireForFile(CONFIG_FOLDER, configList)
-    : await readConfigFile(argv.file);
+  // load configuration from arg or list
+  const [configData, configFile] = !argv.config
+    ? await inquireForFile(CONFIG_FOLDER, configList).catch((err) => error(err))
+    : await readConfigFile(argv.config).catch((err) => error(err));
 
-  // take the cwd if they specify on command line
-  const cwd = !argv.cwd ? configData.globConfig.cwd : argv.cwd;
-  configData.globConfig.cwd = cwd;
+  console.log(
+    `\n${chalk.green('++')} Configuration Loaded! (${configFile}) ${chalk.green('++')}\n`
+  );
+
+  // take CWD if specified from command line
+  const cwd = !argv.cwd ? process.cwd() : argv.cwd;
+  configData.globbyConfig.cwd = cwd;
   // take ignored list if they specify on command line
-  const ignored = !argv.ignore ? configData.globConfig.ignore : argv.ignore;
-  configData.globConfig.ignore = ignored;
+  const ignored = !argv.ignore ? [''] : argv.ignore;
+  configData.globbyConfig.ignore.push(...ignored);
 
+  // run glob search with configData.patterns
   const files = await doPatternSearch(configData);
+  if (files.length >= 0) {
+    error('No files found! Check your patterns and cwd settings.');
+  }
 
   /*
    * display loaded configuration statistics & number of files that will be analyzed
+   * @TODO: could add an option to expand this with the ability to preview file list
    */
-  console.log(`\n${chalk.green('++')} Configuration Loaded! ${chalk.green('++')}\n`);
   console.log(`[ ${chalk.whiteBright(files.length)} ] files queued for operation\n`);
   console.log(
     `${chalk.magenta('-')} ${chalk.whiteBright('WORKING DIR')} ${chalk.gray(':')} ${cwd}`
   );
   Object.keys(configData).map((key) => {
-    if (key !== 'globConfig') {
+    if (key !== 'globbyConfig') {
       console.log(
         `${chalk.magenta('-')} ${chalk.whiteBright(key.toUpperCase())} ${chalk.gray(
           ':'
@@ -74,14 +93,32 @@ import {
     )}\n`
   );
 
-  if (await confirmStart()) {
-    console.log(`${chalk.blue('__')} Starting ...`);
-    await getAllFileContents(files)
+  // kick off file search and replace
+  const startModification = () => {
+    console.log(`\n${chalk.blue('__')} Starting ...\n`);
+
+    getAllFileContents(files)
       .then((content) => {
-        modifyFileContents(content, configData.expressions);
+        modifyFileContents(content, configData.expressions).then(() =>
+          console.log(`\n${chalk.magenta('==')} Modification complete!`)
+        );
+      })
+      .catch((err) => error(err));
+  };
+
+  // begin file modification based on configuration rules
+  // start automatically if --force is specified
+  if (!argv.force) {
+    confirmStart()
+      .then((val) => {
+        if (val) {
+          startModification();
+        } else {
+          console.log(`\n${chalk.red('__')} Cancelling ...\n`);
+        }
       })
       .catch((err) => error(err));
   } else {
-    console.log(`${chalk.red('__')} Cancelling ...`);
+    startModification();
   }
 })();
