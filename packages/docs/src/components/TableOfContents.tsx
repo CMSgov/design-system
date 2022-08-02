0b9b94df6455f0e38399ed7cfb48edeaeb2f93fe
zarahzachz <sarah.sachs@gmail.com>
2022-08-02 16:59:35 -0400

    Add feedback link to ToC.

--- packages/docs/src/components/TableOfContents.tsx
+++ packages/docs/src/components/TableOfContents.tsx
@@ -14,6 +14,10 @@ export interface TableOfContentsProps {
    * additional className string to append to the list
    */
   className?: string;
+  /**
+   * page title
+   */
+  title?: string;
 }
 
 export const TableOfContentsList = ({ items, level, className = '' }: TableOfContentsProps) => {
@@ -23,7 +27,7 @@ export const TableOfContentsList = ({ items, level, className = '' }: TableOfCon
       : 'c-table-of-contents__list-item';
 
   return (
-    <ul className={`c-table-of-contents__list ds-u-padding-right--0 ${className}`}>
+    <ul role="list" className={`c-table-of-contents__list ds-u-padding-right--0 ${className}`}>
       {items.map((item) => (
         <li key={item.title} className={itemClasses}>
           <a href={item.url}>{item.title}</a>
@@ -37,7 +41,7 @@ export const TableOfContentsList = ({ items, level, className = '' }: TableOfCon
 /**
  * The Desktop version of the table of contents
  */
-const TableOfContents = ({ items }: TableOfContentsProps) => {
+const TableOfContents = ({ items, title }: TableOfContentsProps) => {
   const level = 1;
   return items.length ? (
     <div className="c-table-of-contents">
@@ -45,6 +49,15 @@ const TableOfContents = ({ items }: TableOfContentsProps) => {
         On this page{' '}
       </h2>
       <TableOfContentsList items={items} level={level} />
+
+      <h2 className="c-table-of-contents__heading ds-u-margin-y--0 ds-u-md-margin-top--6 ds-u-font-size--base">
+        Have ideas?{' '}
+      </h2>
+      <ul role="list" className="ds-c-list--bare ds-u-md-margin-y--2">
+        <li>
+          <a href="/not-in-sidebar/feedback">Propose a change</a>
+        </li>
+      </ul>
     </div>
   ) : null;
 };
--- packages/docs/src/components/TableOfContentsMobile.tsx
+++ packages/docs/src/components/TableOfContentsMobile.tsx
@@ -5,11 +5,19 @@ import { TableOfContentsProps, TableOfContentsList } from './TableOfContents';
 /**
  * The mobile version of the table of contents
  */
-const TableOfContentsMobile = ({ items }: TableOfContentsProps) => {
+const TableOfContentsMobile = ({ items, title }: TableOfContentsProps) => {
   return items.length ? (
     <Accordion className="c-table-of-contents-mobile">
       <AccordionItem heading="On this page">
         <TableOfContentsList items={items} level={1} className="c-table-of-contents-mobile__list" />
+        <h2 className="c-table-of-contents__heading ds-u-margin-y--0 ds-u-margin-top--3 ds-u-font-size--base">
+          Have ideas?{' '}
+        </h2>
+        <ul role="list" className="ds-c-list--bare ds-u-md-margin-y--2">
+          <li>
+            <a href="/not-in-sidebar/feedback">Propose a change</a>
+          </li>
+        </ul>
       </AccordionItem>
     </Accordion>
   ) : null;
