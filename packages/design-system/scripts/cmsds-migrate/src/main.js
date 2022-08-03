a75daf94a02104a11e05da645ea680b3df5b837d
Forrest Baer <forrest@forrestbaer.com>
2022-08-03 13:55:45 -0500

    small updates

--- packages/design-system/scripts/cmsds-migrate/src/main.js
+++ packages/design-system/scripts/cmsds-migrate/src/main.js
@@ -98,7 +98,7 @@ import yargs from 'yargs';
     getAllFileContents(files, configData.globbyConfig.cwd)
       .then((content) => {
         modifyFileContents(content, configData.expressions).then(() => {
-          console.log(`${chalk.magenta('==')} Modification complete!`);
+          console.log(`${chalk.magenta('==')} Modification complete!\n`);
         });
       })
       .catch((err) => error('getAllFileContents: ' + err));
