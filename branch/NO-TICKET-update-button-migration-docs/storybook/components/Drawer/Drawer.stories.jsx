26ba9c0ac7be2e89c16075eabb275085305df8f0
Scott Weber <scott.weber@adhocteam.us>
2022-08-03 15:51:35 -0400

    Updates

---
+++ branch/NO-TICKET-update-button-migration-docs/storybook/components/Drawer/Drawer.stories.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Title, Subtitle, Description, ArgsTable, PRIMARY_STORY } from '@storybook/addon-docs';
+import { useArgs } from '@storybook/client-api';
+
+import Drawer from './Drawer';
+import { Button } from '../Button';
+
+export default {
+  title: 'Components/Drawer',
+  component: Drawer,
+  argTypes: {
+    closeButtonText: {
+      control: { type: 'text' },
+    },
+    hasFocusTrap: {
+      control: { type: 'boolean' },
+    },
+  },
+  args: {
+    footerTitle: 'Footer Title',
+    footerBody: <p className="ds-text ds-u-margin--0">Footer content</p>,
+    heading: 'Drawer Heading',
+  },
+  // The Drawer was overlapping the docs page, so customizing the docs page to remove the examples
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <ArgsTable story={PRIMARY_STORY} />
+        </>
+      ),
+    },
+  },
+};
+
+const drawerContent = (
+  <>
+    <strong>An Explanation</strong>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+      qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+      officia deserunt mollit anim id est laborum.
+    </p>
+  </>
+);
+
+const Template = ({ data, ...args }) => <Drawer {...args}>{drawerContent}</Drawer>;
+
+export const DrawerDefault = Template.bind({});
+export const DrawerWithStickyPositioning = Template.bind({});
+DrawerWithStickyPositioning.args = {
+  isFooterSticky: true,
+  isHeaderSticky: true,
+};
+
+export const DrawerToggleWithDrawer = () => {
+  const [{ isDrawerVisible, ...args }, setIsDrawerVisible] = useArgs();
+
+  return (
+    <>
+      {isDrawerVisible && (
+        <Drawer
+          {...args}
+          onCloseClick={() => setIsDrawerVisible({ isDrawerVisible: false })}
+          footerTitle="Footer Title"
+          footerBody={<p className="ds-text ds-u-margin--0">Footer content</p>}
+          heading="Drawer Heading"
+        >
+          {drawerContent}
+        </Drawer>
+      )}
+      <Button
+        className="ds-c-drawer__toggle"
+        variation="ghost"
+        onClick={() => setIsDrawerVisible({ isDrawerVisible: true })}
+      >
+        Drawer Toggle
+      </Button>
+    </>
+  );
+};
---
+++ branch/NO-TICKET-update-button-migration-docs/storybook/components/NativeDialog/NativeDialog.tsx
@@ -0,0 +1,58 @@
+import React, { useRef, useEffect, useLayoutEffect, DialogHTMLAttributes } from 'react';
+import dialogPolyfill from './polyfill';
+
+interface NativeDialogProps extends Omit<DialogHTMLAttributes<HTMLElement>, 'children'> {
+  children: React.ReactNode;
+  /**
+   * Function called to close dialog.
+   */
+  exit: (...args: any[]) => any;
+  /**
+   * Determines which native dialog method is called to open the dialog. If the dialog
+   * is opened with `showModal`, focus will be trapped inside the dialog, and it will
+   * need to be closed before the rest of the page can be interacted with. See the
+   * [`showModal` MDN documentation](https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal)
+   * for more details.
+   */
+  showModal?: boolean;
+}
+
+const NativeDialog = ({ children, exit, showModal, ...dialogProps }: NativeDialogProps) => {
+  const dialogRef = useRef(null);
+
+  // Register dialog with the polyfill if necessary
+  useLayoutEffect(() => {
+    // The registerDialog function itself determines if the polyfill needs to be applied
+    dialogPolyfill.registerDialog(dialogRef.current);
+  });
+
+  // Open and close the dialog with the appropriate method on mount and unmount
+  useEffect(() => {
+    const dialogNode = dialogRef.current;
+    showModal ? dialogNode.showModal() : dialogNode.show();
+    return () => {
+      dialogNode.close();
+    };
+  }, [showModal]);
+
+  // Bind and unbind cancel event listeners on mount and unmount
+  useEffect(() => {
+    const dialogNode = dialogRef.current;
+    const handleCancel = (event) => {
+      event.preventDefault();
+      exit();
+    };
+    dialogNode.addEventListener('cancel', handleCancel);
+    return () => {
+      dialogNode.removeEventListener('cancel', handleCancel);
+    };
+  }, [exit]);
+
+  return (
+    <dialog ref={dialogRef} {...dialogProps}>
+      {children}
+    </dialog>
+  );
+};
+
+export default NativeDialog;
