bdf5ed019e532378057a6a96d825731f4e9a8045
Forrest Baer <forrest@forrestbaer.com>
2022-08-04 13:51:43 -0500

    Update packages/design-system/scripts/cmsds-migrate/configs/html-button-migration.mjs
    Co-authored-by: Patrick Wolfert <patrick.wolfert@adhocteam.us>

--- packages/design-system/scripts/cmsds-migrate/configs/html-button-migration.mjs
+++ packages/design-system/scripts/cmsds-migrate/configs/html-button-migration.mjs
@@ -1,6 +1,6 @@
 export default {
   description:
-    'Updates button classes for latest button architecture as of 08/03/2022.\n  - Replaces ds-c-button--primary/success with ds$-c-button--solid\n  - Replaces ds-c-button--transparent with ds-c-button--link\n  - Replaces ds-c-button--inverse with ds-c-button--on-dark\n  - Flags ds-c-button and ds-c-button--hover/active/focus as deprecated',
+    'Updates button classes for latest button architecture as of 08/03/2022.\n  - Replaces ds-c-button--primary/success with ds-c-button--solid\n  - Replaces ds-c-button--transparent with ds-c-button--link\n  - Replaces ds-c-button--inverse with ds-c-button--on-dark\n  - Flags ds-c-button and ds-c-button--hover/active/focus as deprecated',
   patterns: ['**/*.{html,htm}'],
   expressions: [
     {
