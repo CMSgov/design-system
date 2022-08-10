838d029d7d24bbeee29c0e20f167ebcc36ccd427
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-08-10 15:11:10 -0700

    [WNMGDS-1836] Fix auto-scrolling to code snippets on load (#2031)
    * Fix a bug where page scrolled to code iframe on initial load
    
    * Better comments

--- packages/docs/src/components/StorybookExampleFooter.tsx
+++ packages/docs/src/components/StorybookExampleFooter.tsx
@@ -53,7 +53,7 @@ const StorybookExampleFooter = ({ theme, storyId }: StorybookExampleFooterProps)
       errorLoadingHtmlCode();
     }
 
-    // Find the 'Show code' button and click it
+    // Find the 'Show code' button and click it so we can access the code
     const showCodeButton = body.querySelector(codeButtonSelector);
     if (!(showCodeButton && (showCodeButton as HTMLButtonElement).click)) {
       console.error(
@@ -63,6 +63,10 @@ const StorybookExampleFooter = ({ theme, storyId }: StorybookExampleFooterProps)
       errorLoadingReactCode();
       return;
     }
+    // Hide the iframe first so that clicking the button doesn't scroll us to it. We can't
+    // do this when we first render the iframe or it will never load.
+    iframeRef.current.hidden = true;
+    iframeRef.current.style.display = 'none';
     (showCodeButton as HTMLButtonElement).click();
 
     // Read the code out of the resulting code block after waiting for it to be generated
@@ -93,6 +97,7 @@ const StorybookExampleFooter = ({ theme, storyId }: StorybookExampleFooterProps)
       ref={iframeRef}
       loading="lazy"
       onLoad={onIframeLoad}
+      tabIndex={-1}
     />
   );
 
