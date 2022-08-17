92db4e3bbd3420a5204a433891ed676120120498
Forrest Baer <forrest@forrestbaer.com>
2022-08-17 08:17:44 -0500

    updating the medicare readme with new doc site links and removing medicare doc site assets

--- packages/ds-medicare-gov/docs/src/pages/components/SimpleFooter/SimpleFooter.example.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { SimpleFooter } from '@design-system';
-
-ReactDOM.render(<SimpleFooter />, document.getElementById('js-example'));
