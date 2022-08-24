f992805a5612686dd6bb5433c8f4986b6d18210c
Forrest Baer <forrest@forrestbaer.com>
2022-08-24 15:05:05 -0500

    [WNMGDS-1817, WNMGDS-1818, WNMGDS-1861] remove old doc site content, update a11y tests (#2053)
    * removing old doc site content
    
    * updating the medicare readme with new doc site links and removing medicare doc site assets
    
    * updating the healthcare readme with new doc site links and removing outdated doc site content from package
    
    * updating main readme to remove reference for docs site
    
    * updating build scripts, removing doc site generation
    
    * adding updated yarn.lock
    
    * adding bundles to eslintignore so it doesnt take forever
    
    * resetting some things
    
    * adding url-join dependency back to new docs site from old
    
    * big update getting a11y tests to function correctly
    
    * making core default for yarn test
    
    * removing old a11y tests
    
    * updating readme
    
    * removing browser-sync, no longer needed
    
    * adding docs/public to eslintignore, found several other doc site references in build scripts to remove
    
    * lint ignores

--- packages/design-system-scripts/jest/a11y.global-setup.js
+++ packages/design-system-scripts/jest/a11y.global-setup.js
@@ -1,27 +1,15 @@
 require('regenerator-runtime/runtime');
 const StaticServer = require('static-server');
 const chalk = require('chalk');
-const childProcess = require('child_process');
 const log = require('fancy-log');
 
 const APP_PORT = 3001;
 
-function buildApp() {
-  log(chalk.green('\nBuilding docs site in production mode...\n'));
-  // Build files in production while ignoring rootPath
-  childProcess.execSync(
-    process.env.BUILD_COMMAND || 'yarn build-docs --skipLatest --ignoreRootPath',
-    {
-      stdio: ['ignore', 'ignore', process.stderr],
-    }
-  );
-  log(chalk.green('done ✓'));
-}
-
 async function startServer() {
-  log(chalk.green('\nStarting local server hosting production build...'));
+  console.log('\n');
+  log(chalk.green('Starting local web server in ./storybook-static/ ...'));
   const server = new StaticServer({
-    rootPath: process.env.BUILD_PATH,
+    rootPath: './storybook-static/',
     port: APP_PORT,
   });
 
@@ -30,16 +18,9 @@ async function startServer() {
   });
 
   global.__SERVER__ = server;
-  log(chalk.green('done ✓'));
+  log(chalk.green(`Local web server running on port ${APP_PORT} ✓`));
 }
 
 module.exports = async function () {
-  if (process.env.SKIP_BUILD === 'true') {
-    log('\n');
-    log(chalk.yellow('Skipping build and using existing build instead ⚠️'));
-  } else {
-    buildApp();
-  }
   await startServer();
-  log('\n\n');
 };
--- packages/ds-medicare-gov/docs/src/pages/components/Card/Card.example.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Card } from '@design-system';
-
-ReactDOM.render(<Card />, document.getElementById('js-example'));
