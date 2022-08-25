b328ea86fc406efc51565685b51a7f3fa9b9a6ad
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-08-25 10:50:12 -0700

    Whoops, can't remove the stdio config entirely

--- scripts/deployDemo.ts
+++ scripts/deployDemo.ts
@@ -5,17 +5,32 @@ import path from 'path';
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
 // Build the demo site
-spawnSync('yarn', ['--cwd', path.join('packages', 'docs'), 'clean']);
-spawnSync('yarn', ['build-storybook:gatsby']);
+// Need to use spawnSync instead of execSync for 'yarn' to be found consistently. When using
+// spawn, the command is executed in the same environment as this process, which means we're
+// guaranteed to have access to yarn and node in the PATH. The exec function, on the other
+// hand, will execute it in whatever it determines shoudl be the default shell.
+spawnSync('yarn', ['--cwd', path.join('packages', 'docs'), 'clean'], { stdio: 'inherit' });
+spawnSync('yarn', ['build-storybook:gatsby'], { stdio: 'inherit' });
 spawnSync('yarn', ['build:docs'], {
   env: {
     ...process.env,
     PATH_PREFIX: `/design-system/branch/${branch}`,
     PREFIX_PATHS: 'true',
+    stdio: 'inherit',
   },
 });
 
 // Deploy the demo site to a directory on GitHub Pages
-execSync(`yarn gh-pages -d '${path.join('packages', 'docs', 'public')}' --dest "branch/${branch}"`);
+spawnSync(
+  `yarn`,
+  [
+    'gh-pages',
+    '-d',
+    `'${path.join('packages', 'docs', 'public')}'`,
+    '--dest',
+    '"branch/${branch}"',
+  ],
+  { stdio: 'inherit' }
+);
 
 console.log(`Deployed demo doc site to https://cmsgov.github.io/design-system/branch/${branch}`);
