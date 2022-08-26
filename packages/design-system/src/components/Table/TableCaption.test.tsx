1879edf6daa7538bebaa2909e5f29f3fa372431b
Forrest Baer <forrest@forrestbaer.com>
2022-08-26 13:55:29 -0500

    updating tests for all table components

--- packages/design-system/src/components/Table/TableCaption.test.tsx
+++ packages/design-system/src/components/Table/TableCaption.test.tsx
@@ -1,35 +1,29 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+
 import TableCaption from './TableCaption';
-import { mount } from 'enzyme';
 
 const defaultCaptionChildren = 'Foo';
 
-function render(customProps = {}) {
+const makeTableCaption = (customProps = {}) => {
   const props = Object.assign({}, customProps);
   const children = <TableCaption {...props}>{defaultCaptionChildren}</TableCaption>;
 
-  return {
-    props: props,
-    wrapper: mount(<table>{children}</table>),
-  };
-}
+  render(<table>{children}</table>);
+};
 
 describe('TableCaption', function () {
-  it('renders a table caption', () => {
-    const { wrapper } = render();
-    const caption = wrapper.find('caption');
-
-    expect(caption).toHaveLength(1);
-    expect(caption.hasClass('ds-c-table__caption')).toBe(true);
-    expect(caption.text()).toBe(defaultCaptionChildren);
+  it('renders a table caption with default ds caption class', () => {
+    makeTableCaption();
+    const caption = screen.getByText(defaultCaptionChildren);
+    expect(caption).toBeInTheDocument();
+    expect(caption).toHaveClass('ds-c-table__caption');
   });
 
   it('applies additional classNames to caption', () => {
-    const { wrapper } = render({ className: 'foo-caption' });
-    const caption = wrapper.find('caption');
-
-    expect(caption.hasClass('foo-caption')).toBe(true);
-
-    expect(wrapper).toMatchSnapshot();
+    makeTableCaption({ className: 'foo-caption' });
+    const caption = screen.getByText(defaultCaptionChildren);
+    expect(caption).toHaveClass('foo-caption');
+    expect(caption).toMatchSnapshot();
   });
 });
