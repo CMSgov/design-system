04e92ca6c1ba2a6144a5df6867254c16d91c9df6
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-08-29 10:41:14 -0700

    Remove old bash script and add some dependencies back

--- scripts/deployDemo.ts
+++ scripts/deployDemo.ts
@@ -5,10 +5,6 @@ import { join } from 'path';
 const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
 // Build the demo site
-// Need to use spawnSync instead of execSync for 'yarn' to be found consistently. When using
-// spawn, the command is executed in the same environment as this process, which means we're
-// guaranteed to have access to yarn and node in the PATH. The exec function, on the other
-// hand, will execute it in whatever it determines shoudl be the default shell.
 execSync(`yarn --cwd ${join('packages', 'docs')} clean`, { stdio: 'inherit' });
 execSync('yarn build-storybook:gatsby', { stdio: 'inherit' });
 execSync('yarn build:docs', {
