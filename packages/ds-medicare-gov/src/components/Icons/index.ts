579387843f1f7f6311d3bb98c57a0b65a79e3f84
Anna Kammeyer <scheul93@gmail.com>
2022-08-30 14:51:13 -0600

    mgov info circle icon update

--- packages/ds-medicare-gov/src/components/Icons/index.ts
+++ packages/ds-medicare-gov/src/components/Icons/index.ts
@@ -4,6 +4,7 @@ export { default as CheckShieldIcon } from './CheckShieldIcon';
 export { default as Close } from './CloseSymbol';
 export { default as DrugsIcon } from './DrugsIcon';
 export { default as Hamburger } from './HamburgerSymbol';
+export { default as InfoCircleOutlineIcon } from './InfoCircleOutlineIcon';
 export { default as No } from './No';
 export { default as PharmacyIcon } from './PharmacyIcon';
 export { default as PiggyBankIcon } from './PiggyBankIcon';
