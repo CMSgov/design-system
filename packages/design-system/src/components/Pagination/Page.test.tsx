6e905196b0b80b8352dbf472c11302e7bd08d8ec
Anna Kammeyer <scheul93@gmail.com>
2022-08-31 09:15:39 -0600

    updated Page snapshot test element

--- packages/design-system/src/components/Pagination/Page.test.tsx
+++ packages/design-system/src/components/Pagination/Page.test.tsx
@@ -6,12 +6,11 @@ describe('Page', () => {
   const onPageChange = jest.fn();
 
   it('should render interactive el if not current', () => {
-    const { container } = render(
-      <Page index={1} isActive={false} onPageChange={onPageChange} href="/#1" />
-    );
+    render(<Page index={1} isActive={false} onPageChange={onPageChange} href="/#1" />);
+    const pageEl = screen.queryByRole('listitem');
     const buttonEl = screen.getByRole('link');
     expect(buttonEl).toBeDefined();
-    expect(container).toMatchSnapshot();
+    expect(pageEl).toMatchSnapshot();
   });
 
   it('should render static el if current', () => {
