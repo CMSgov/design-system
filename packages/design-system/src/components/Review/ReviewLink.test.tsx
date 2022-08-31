60b8b6cd8c24eee37e7a85baddc38845dc1d26ca
zarahzachz <sarah.sachs@gmail.com>
2022-08-31 11:20:18 -0400

    Update Review unit tests.

---
+++ packages/design-system/src/components/Review/ReviewLink.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReviewLink from './ReviewLink';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const noop = () => {};
+const text = 'link text';
+const defaultProps = {
+  onClick: noop,
+  href: 'my-href',
+};
+
+function renderLink(customProps?, children = text) {
+  const props = { ...defaultProps, ...customProps };
+  return render(<ReviewLink {...props}>{children}</ReviewLink>);
+}
+
+describe('ReviewLink', function () {
+  it('renders link', () => {
+    renderLink();
+
+    const els = screen.getAllByRole('link');
+    expect(els.length).toBe(1);
+
+    const el = els[0];
+    expect(el.textContent).toBe(text);
+  });
+
+  it('props.onClick is called with correct parameters', () => {
+    const onClick = jest.fn();
+    renderLink({ onClick });
+
+    const els = screen.getAllByRole('link');
+    expect(els.length).toBe(1);
+
+    const el = els[0];
+    expect(el.textContent).toBe(text);
+
+    userEvent.click(el);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
