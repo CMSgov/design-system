91d8d087c83a1e3371e66d9dc931aebe39d74d45
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-09-02 15:50:05 -0700

    Increase code coverage at the expense of more snapshots
    I considered not using snapshots, but any meaningful queries I could make against these two other cases would expose implementation details and be more work to maintain.

--- packages/design-system/src/components/TextField/TextField.test.tsx
+++ packages/design-system/src/components/TextField/TextField.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TextField, unmaskValue } from './TextField';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { DATE_MASK } from './useLabelMask';
 
 const defaultProps = {
   label: 'Foo',
@@ -14,6 +15,14 @@ function renderTextField(customProps = {}) {
 describe('TextField', function () {
   it('renders', () => {
     expect(renderTextField().asFragment()).toMatchSnapshot();
+  });
+
+  it('renders with a label mask', () => {
+    expect(renderTextField({ labelMask: DATE_MASK }).asFragment()).toMatchSnapshot();
+  });
+
+  it('renders with a mask', () => {
+    expect(renderTextField({ mask: 'currency' }).asFragment()).toMatchSnapshot();
   });
 
   it('exports unmaskValue method', () => {
