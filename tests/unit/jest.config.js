2656b8756fb99f2f7863a2e7a1dd564d8f15361c
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-09-07 10:24:22 -0700

    Hook up `yarn test:unit` to new config/command
    I made the unit test config not wrapped in a function anymore and removed the module resolution override stuff. I think it might be running faster on average because of that change

--- tests/unit/jest.config.js
+++ tests/unit/jest.config.js
@@ -1,12 +1,10 @@
-const path = require('path');
-
-module.exports = (rootDir, core) => ({
-  rootDir,
+module.exports = {
+  rootDir: '../../packages',
   testEnvironment: 'jsdom',
   testURL: 'http://localhost',
   setupFiles: [require.resolve('react-app-polyfill/stable')],
   // TODO: Find more robust solution for resolving `setupTests.js` considering `rootDir` can change
-  setupFilesAfterEnv: core ? [`<rootDir>/../setupTests.js`] : [`<rootDir>/setupTests.js`],
+  setupFilesAfterEnv: [`<rootDir>/../setupTests.js`],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testPathIgnorePatterns: [
     'dist/',
@@ -18,17 +16,4 @@ module.exports = (rootDir, core) => ({
   ],
   coveragePathIgnorePatterns: ['/node_modules/', 'design-system-scripts'],
   transformIgnorePatterns: ['node_modules(?!/@cmsgov)'],
-  // Add moduleNameMapper for core CMSDS to resolve imports from @cmsgov/design-system to packages/design-system
-  moduleNameMapper: core
-    ? {
-        '^@cmsgov/design-system/(.*)$': `<rootDir>/${path.relative(
-          rootDir,
-          'packages/design-system/$1'
-        )}`,
-        '^@cmsgov/design-system$': `<rootDir>/${path.relative(
-          rootDir,
-          'packages/design-system/src/components/index'
-        )}`,
-      }
-    : {},
-});
+};
