d7fd7797e0e1770235c62f159b58c119c3eb8770
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-09-15 12:31:49 -0700

    Replace logic for skipping stories

--- tests/browser/example.spec.ts
+++ tests/browser/example.spec.ts
@@ -2,8 +2,15 @@ import { test, expect } from '@playwright/test';
 import { stories } from '../../storybook-static/stories.json';
 
 const themes = ['core', 'healthcare', 'medicare'];
+const storySkipList = [
+  'components-idle-timeout--default',
+  'components-skip-nav--default-skip-nav',
+  'components-skip-nav--skip-nav-example',
+];
 
 Object.values(stories).forEach((story) => {
+  if (storySkipList.includes(story.id)) return;
+
   test.describe(`${story.title}/${story.name}`, () => {
     const storyUrl = `http://localhost:6006/iframe.html?viewMode=story&id=${story.id}`;
 
