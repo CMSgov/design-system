6f7b38a4cecf810df9079c0374dea5f9d052169f
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-09-16 15:11:43 -0700

    Move HelpDrawer's analytics code into a separate function to reduce noise

---
+++ packages/design-system/src/components/HelpDrawer/useHelpDrawerAnalytics.ts
@@ -0,0 +1,54 @@
+import { EventCategory, sendLinkEvent, useAnalyticsContent } from '../analytics';
+import { HelpDrawerProps } from './HelpDrawer';
+import { useCallback } from 'react';
+import { helpDrawerSendsAnalytics } from '../flags';
+
+export default function useHelpDrawerAnalytics({
+  analytics,
+  analyticsLabelOverride,
+}: HelpDrawerProps) {
+  const sendHelpDrawerEvent = useCallback(
+    (
+      content: string | undefined,
+      eventAttributes: { event_name: string; ga_eventAction: string }
+    ) => {
+      if (!helpDrawerSendsAnalytics() || analytics === false) {
+        return;
+      }
+
+      const eventHeadingText = analyticsLabelOverride ?? content;
+
+      if (!eventHeadingText) {
+        console.error('No content found for Dialog analytics event');
+        return;
+      }
+
+      sendLinkEvent({
+        event_type: EventCategory.UI_INTERACTION,
+        ga_eventCategory: EventCategory.UI_COMPONENTS,
+        ga_eventLabel: eventHeadingText,
+        heading: eventHeadingText,
+        ...eventAttributes,
+      });
+    },
+    [analytics, analyticsLabelOverride]
+  );
+
+  const [headingRef] = useAnalyticsContent({
+    componentName: 'Dialog',
+    onMount: (content: string | undefined) => {
+      sendHelpDrawerEvent(content, {
+        event_name: 'help_drawer_opened',
+        ga_eventAction: 'opened help drawer',
+      });
+    },
+    onUnmount: (content: string | undefined) => {
+      sendHelpDrawerEvent(content, {
+        event_name: 'help_drawer_closed',
+        ga_eventAction: 'closed help drawer',
+      });
+    },
+  });
+
+  return headingRef;
+}
