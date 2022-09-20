53e6864e39786c360e7c0ff07da268487f02d886
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-09-20 11:18:11 -0700

    [WNMGDS-1914] Change a11y tests to use Playwright (#2149)
    * Convert Selenium + Jest a11y tests to Playwright
    
    * The URL isn't necessary to print anymore because Playwright gives us proper context
    
    The Playwright `expect` function accepts a custom error message as the second argument, which allows us to print all our violations within the context of the error instead of printing it as it happens and it being divorced from the failure messages that would have been printed later with Jest
    
    * Test all themes! But then disable it :(
    
    The medicare tests found issues with the following stories:
    
    Components/ChoiceList/Inverse Option › with medicare theme
    Components/MultiInputDateField/Inverted Multi Input Date Field › with medicare theme
    Components/Dropdown/Inverse Option › with medicare theme
    Components/Month Picker/Inverse Month Picker › with medicare theme
    Components/Month Picker/Inverse Selected Month Picker › with medicare theme
    Components/Month Picker/Inverse Disabled Month Picker › with medicare theme
    
    So I'm going to disable them until we've decided what to do about those weird inverse states that are probably not used in production
    
    * Remove old selenium dependencies
    
    * Actually, healthcare is fine, so enable that
    
    * Git-ignore test output folder
    
    * Remove unused axe tag arrays

--- tests/a11y/jest.global-teardown.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk');
-const log = require('fancy-log');
-
-module.exports = async function () {
-  log(chalk.green('Shutting down local web server...'));
-  global.__SERVER__.stop();
-  log(chalk.green('Web server shutdown complete ✓'));
-};
