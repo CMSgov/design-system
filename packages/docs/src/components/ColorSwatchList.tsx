bc5d851b99a5829e7abdab545dae03d2d5fdb5cb
zarahzachz <sarah.sachs@gmail.com>
2022-09-22 16:55:09 -0400

    Rollback key name on swatch.

--- packages/docs/src/components/ColorSwatchList.tsx
+++ packages/docs/src/components/ColorSwatchList.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
 import classNames from 'classnames';
-import uniqueId from 'lodash/uniqueId';
 
 interface ColorSwatchListProps {
   /**
@@ -66,7 +65,7 @@ const ColorSwatchList = ({ backgroundClass, colorNames, preface, theme }: ColorS
   return (
     <div className="c-swatch-list ds-u-border--1 ds-u-padding--2">
       {colorList.map(({ name, hex }, index) => (
-        <article className="ds-u-margin-bottom--1 c-swatch" key={uniqueId(`${name}-`)}>
+        <article className="ds-u-margin-bottom--1 c-swatch" key={`${name}-${hex}`}>
           <div
             className={`c-swatch__preview ds-u-margin-right--1 ds-u-radius--circle ds-u-fill--${name}`}
             ref={(el) => (refList.current[index] = el)}
