34685830ab36fbc1513e709e8282d027e3a66d2a
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-09-23 13:15:38 -0700

    Finished styling

--- packages/docs/src/components/Content/MaturityChecklist/MaturityChecklistItem.tsx
+++ packages/docs/src/components/Content/MaturityChecklist/MaturityChecklistItem.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Status from './Status';
+import classNames from 'classnames';
 
 export type CheckStatus = boolean | null;
 
@@ -11,25 +13,18 @@ interface MaturityChecklistItemProps {
 /**
  *
  */
-const MaturityChecklistItem = ({ status, title, children }: MaturityChecklistItemProps) => {
-  let check;
-  if (status === true) {
-    check = '[x]';
-  } else if (status === null) {
-    check = 'N/A';
-  } else {
-    check = '[ ]';
-  }
-
-  return (
-    <li className="c-maturity-checklist-item">
-      <div className="c-maturity-checklist-item__description">
-        <h4 className="c-maturity-checklist-item__title">{title}</h4>
-        <div>{children}</div>
-      </div>
-      <div className="c-maturity-checklist-item__status">{check}</div>
-    </li>
-  );
-};
+const MaturityChecklistItem = ({ status, title, children }: MaturityChecklistItemProps) => (
+  <li
+    className={classNames('c-maturity-checklist-item', status === null && 'ds-u-color--gray-light')}
+  >
+    <div className="c-maturity-checklist-item__description">
+      <h4 className="c-maturity-checklist-item__title">{title}</h4>
+      <div>{children}</div>
+    </div>
+    <div className="c-maturity-checklist-item__status">
+      <Status status={status} />
+    </div>
+  </li>
+);
 
 export default MaturityChecklistItem;
