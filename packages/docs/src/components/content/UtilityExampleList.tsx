3b27ed22ea6f448bffde46564234856b60e044bf
Scott Weber <scott.weber@adhocteam.us>
2022-09-30 11:26:46 -0400

    Hack at tsx

--- packages/docs/src/components/content/UtilityExampleList.tsx
+++ packages/docs/src/components/content/UtilityExampleList.tsx
@@ -21,13 +21,6 @@ interface UtilityExampleListProps {
   theme: string;
 }
 
-// converts an rgb string 'rgb(15,24,128)' to a hex value '#0819A9'
-// pulled from tokens conversion
-export const rgbToHex = (r: number, g: number, b: number) => {
-  const hex = ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  return `#${hex}`;
-};
-
 /**
  * displays a list of color swatches with a sample of the color, the SCSS variable name & the hex value
  * @param utilityNames {String[]} a list of color names - should be same as SCSS token
@@ -36,45 +29,16 @@ export const rgbToHex = (r: number, g: number, b: number) => {
  * displays a list of color swatches with a sample of the color, the SCSS variable name & the hex value
  * @param utilityValues {String[]} a list of color names - should be same as SCSS token
  */
-const UtilityExampleList = ({
-  utilityNames,
-  utilityValues,
-  preface,
-  theme,
-}: UtilityExampleListProps) => {
+const UtilityExampleList = ({ utilityNames, utilityValues, preface }: UtilityExampleListProps) => {
   const refList = useRef([]);
-  const initialColors = utilityNames.map((color) => ({ name: color, hex: '' }));
-  const [colorList, setColorList] = useState(initialColors);
-
-  useEffect(() => {
-    // after swatch has been rendered once, pull rgb color from element styles & convert to hex
-    const updatedColorList = colorList.map((colorItem, index) => {
-      const styles = getComputedStyle(refList.current[index]);
-      const rgbColor = styles?.backgroundColor;
-      if (!rgbColor) return colorItem;
-
-      const rgb = rgbColor.match(/([0-9]+)/g);
-
-      if (rgb.length > 3 && parseInt(rgb[3]) === 0) {
-        // Color is transparent
-        return colorItem;
-      }
-
-      return {
-        ...colorItem,
-        hex: rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2])),
-      };
-    });
-
-    setColorList(updatedColorList);
-  }, [
-    // If the theme changes, we need to recalculate our hex values
-    theme,
-  ]);
+  const initialNames = utilityNames.map((color) => ({ name: color }));
+  const initialValues = utilityValues.map((pxvalue) => ({ name: pxvalue }));
+  const [exampleList] = useState(initialNames);
+  const [exampleList2] = useState(initialValues);
 
   return (
     <div className="c-utility-example__list ds-u-border--1 ds-u-padding--2">
-      {colorList.map(({ name, hex }, index) => (
+      {exampleList.map(({ name }, index) => (
         <article className="c-utility-example__row" key={`${name}`}>
           <div className="c-utility-example__container">
             <div
@@ -86,7 +50,13 @@ const UtilityExampleList = ({
             .{preface}
             {name}
           </code>
-          <code>{utilityValues}</code>
+          {exampleList2.map(({ name }, index) => (
+            <div key={`${name}`}>
+              <div ref={(el) => (refList.current[index] = el)}>
+                <code>{name}</code>
+              </div>
+            </div>
+          ))}
         </article>
       ))}
     </div>
