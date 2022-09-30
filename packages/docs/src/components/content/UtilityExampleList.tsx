a614549fae3e271df6c1c74f429bbef4d0a0ca5d
Scott Weber <scott.weber@adhocteam.us>
2022-09-30 18:47:03 -0400

    update utility list control

--- packages/docs/src/components/content/UtilityExampleList.tsx
+++ packages/docs/src/components/content/UtilityExampleList.tsx
@@ -19,6 +19,8 @@ interface UtilityExampleListProps {
    * Name of currently selected theme
    */
   theme: string;
+
+  exampleClass: string;
 }
 
 /**
@@ -29,38 +31,26 @@ interface UtilityExampleListProps {
  * displays a list of color swatches with a sample of the color, the SCSS variable name & the hex value
  * @param utilityValues {String[]} a list of color names - should be same as SCSS token
  */
-const UtilityExampleList = ({ utilityNames, utilityValues, preface }: UtilityExampleListProps) => {
-  const refList = useRef([]);
-  const initialNames = utilityNames.map((color) => ({ name: color }));
-  const initialValues = utilityValues.map((pxvalue) => ({ name: pxvalue }));
-  const [exampleList] = useState(initialNames);
-  const [exampleList2] = useState(initialValues);
-
-  return (
-    <div className="c-utility-example__list ds-u-border--1 ds-u-padding--2">
-      {exampleList.map(({ name }, index) => (
-        <article className="c-utility-example__row" key={`${name}`}>
-          <div className="c-utility-example__container">
-            <div
-              className={`${preface}${name} c-utility-example__item`}
-              ref={(el) => (refList.current[index] = el)}
-            ></div>
-          </div>
-          <code>
-            .{preface}
-            {name}
-          </code>
-          {exampleList2.map(({ name }, index) => (
-            <div key={`${name}`}>
-              <div ref={(el) => (refList.current[index] = el)}>
-                <code>{name}</code>
-              </div>
-            </div>
-          ))}
-        </article>
-      ))}
-    </div>
-  );
-};
+const UtilityExampleList = ({
+  utilityNames,
+  utilityValues,
+  preface,
+  exampleClass,
+}: UtilityExampleListProps) => (
+  <div className={`${exampleClass} c-utility-example__list ds-u-border--1 ds-u-padding--2`}>
+    {utilityNames.map((name, index) => (
+      <article className="c-utility-example__row" key={`${name}`}>
+        <div className="c-utility-example__container">
+          <div className={`${preface}${name} c-utility-example__item`}></div>
+        </div>
+        <code>
+          .{preface}
+          {name}
+        </code>
+        <code>{utilityValues[index]}</code>
+      </article>
+    ))}
+  </div>
+);
 
 export default UtilityExampleList;
