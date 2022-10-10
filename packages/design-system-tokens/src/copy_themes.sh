00fa32188e50a89c71e5f85ef7dc513fba609336
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-10-10 13:03:43 -0700

    Update tokens package to support moving the CSS vars to ds packages

--- packages/design-system-tokens/src/copy_themes.sh
+++ packages/design-system-tokens/src/copy_themes.sh
@@ -7,22 +7,19 @@ copyThemes()
   cd dist
   
   # move files used in distributed pacakges
-  for file in core*[!.map].scss; do
-    cp -v "$file" "../../design-system/src/styles/settings/variables/_${file}"
+  for file in core*.css; do
+    cp -v "$file" "../../design-system/src/styles/settings/variables/${file}"
   done
-  for file in healthcare*[!.map].scss; do
-    cp -v "$file" "../../ds-healthcare-gov/src/styles/settings/_${file}"
+  for file in healthcare*.css; do
+    cp -v "$file" "../../ds-healthcare-gov/src/styles/settings/variables/${file}"
   done
-  for file in medicare*[!.map].scss; do
-    cp -v "$file" "../../ds-medicare-gov/src/styles/settings/variables/_${file}"
+  for file in medicare*.css; do
+    cp -v "$file" "../../ds-medicare-gov/src/styles/settings/variables/${file}"
   done
 
   #move files used in internal tooling
-  for file in *.map.scss; do
-    cp -v "$file" "../../docs/src/styles/theme-variables/_${file}"
-  done
   for file in *.css; do
-    cp -v "$file" "../../docs/src/styles/theme-variables/_${file}"
+    cp -v "$file" "../../docs/src/styles/theme-variables/${file}"
   done
 }
 
