56d92247436374020d1cc2163957ac9d920eaa9f
Forrest Baer <forrest@forrestbaer.com>
2022-10-14 10:35:25 -0500

    updates to linting config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -17,7 +17,9 @@ module.exports = {
   reportNeedlessDisables: true,
   reportInvalidScopeDisables: true,
   rules: {
-    'order/order': ['custom-properties', 'declarations'],
+    'order/order': ['custom-properties', 'declarations', 'rules'],
+    'no-invalid-position-at-import-rule': null,
+    'no-descending-specificity': null,
     'declaration-no-important': [
       true,
       {
@@ -25,11 +27,22 @@ module.exports = {
       },
     ],
     'at-rule-no-unknown': null,
+    'function-no-unknown': [
+      true,
+      {
+        ignoreFunctions: ['percentage', 'calc', 'math.div'],
+      },
+    ],
     'selector-pseudo-class-case': 'lower',
     'order/properties-alphabetical-order': true,
     'no-empty-first-line': true,
     linebreaks: 'unix',
-    ignoreAtRules: ['function', 'if'],
+    'at-rule-allowed-list': [
+      ['function', 'if', 'use', 'media', 'font-face', 'import', 'extend'],
+      {
+        severity: 'warning',
+      },
+    ],
     'function-max-empty-lines': 0,
     'scss/dollar-variable-no-missing-interpolation': true,
     'scss/selector-no-redundant-nesting-selector': true,
