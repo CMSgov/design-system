668515c624588a4387cd4ccb07bfecb604e6b70a
Forrest Baer <forrest@forrestbaer.com>
2022-10-20 15:13:51 -0500

    removes Navigationmenu component from mgov

--- packages/ds-medicare-gov/src/components/NavigationMenu/NavigationMenu.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, cleanup } from '@testing-library/react';
-import NavigationMenu from './NavigationMenu';
-
-afterEach(cleanup);
-
-it('renders without crashing', () => {
-  const { container } = render(<NavigationMenu />);
-  expect(container).toBeInTheDocument();
-});
