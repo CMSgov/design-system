035f2742fc12bfbf19046a324b141728c207b114
Scott Weber <scott.weber@adhocteam.us>
2022-10-27 12:42:11 -0400

    Update packages/design-system/src/components/List/List.stories.jsx
    Co-authored-by: Patrick Wolfert <patrick.wolfert@adhocteam.us>

--- packages/design-system/src/components/List/List.stories.jsx
+++ packages/design-system/src/components/List/List.stories.jsx
@@ -30,18 +30,9 @@ const listMarkup = (type) => {
 
 export const AllLists = () => (
   <>
-    <ul className="ds-c-list" aria-labelledby="unordered-list-id">
-      <li>unordered list item 1</li>
-      <li>unordered list item 2</li>
-    </ul>
-    <ol className="ds-c-list" aria-labelledby="ordered-list-id">
-      <li>ordered list item 1</li>
-      <li>ordered list item 2</li>
-    </ol>
-    <ul className="ds-c-list ds-c-list--bare" aria-labelledby="unstyled-list-id">
-      <li>unstyled list item 1</li>
-      <li>unstyled list item 2</li>
-    </ul>
+    {listMarkup("unordered")}
+    {listMarkup("ordered")}
+    {listMarkup("unstyled")}
   </>
 );
 
