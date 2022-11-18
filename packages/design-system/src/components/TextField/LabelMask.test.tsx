1c9855c6017f2aac2d61062d384bf40aeefd07bb
Forrest Baer <forrest@forrestbaer.com>
2022-11-18 13:07:32 -0600

    LabelMask tests updated

--- packages/design-system/src/components/TextField/LabelMask.test.tsx
+++ packages/design-system/src/components/TextField/LabelMask.test.tsx
@@ -1,8 +1,63 @@
 import { useState } from 'react';
 import LabelMask from './LabelMask';
-import { DATE_MASK, MaskFunction } from './useLabelMask';
+import {
+  DATE_MASK,
+  PHONE_MASK,
+  ZIP_MASK,
+  SSN_MASK,
+  CURRENCY_MASK,
+  MaskFunction,
+} from './useLabelMask';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
+const allMasks = [
+  {
+    name: 'Date',
+    mask: DATE_MASK,
+    default: 'MM/DD/YYYY',
+    defaultData: '04072022',
+    defaultResult: '04/07/2022',
+    partialData: '041',
+    partialResult: '04/01/YYYY',
+  },
+  {
+    name: 'Social Security Number',
+    mask: SSN_MASK,
+    default: 'XXX-XX-XXXX',
+    defaultData: '123456789',
+    defaultResult: '123-45-6789',
+    partialData: '1234',
+    partialResult: '123-4X-XXXX',
+  },
+  {
+    name: 'Phone Number',
+    mask: PHONE_MASK,
+    default: 'XXX-XXX-XXXX',
+    defaultData: '1234567890',
+    defaultResult: '123-456-7890',
+    partialData: '1234',
+    partialResult: '123-4XX-XXXX',
+  },
+  {
+    name: 'US Zip Code',
+    mask: ZIP_MASK,
+    default: 'XXXXX',
+    defaultData: '12345',
+    defaultResult: '12345',
+    partialData: '123',
+    partialResult: '123XX',
+  },
+  {
+    name: 'US Currency',
+    mask: CURRENCY_MASK,
+    default: '$0.00',
+    defaultData: '12345.67',
+    defaultResult: '$12,345.67',
+    partialData: '123',
+    partialResult: '$123.00',
+  },
+];
 
 const TestLabelMask = (props: { mask: MaskFunction }) => {
   const { mask } = props;
@@ -14,67 +69,59 @@ const TestLabelMask = (props: { mask: MaskFunction }) => {
   );
 };
 
-describe('DATE_MASK Label mask', function () {
-  it('renders default date mask, MM/DD/YYYY, when no input value set', () => {
-    const { container, asFragment } = render(<TestLabelMask mask={DATE_MASK} />);
-    const mask = container.querySelector('.ds-c-label-mask');
-    const input = container.querySelector('input');
-
-    expect(input.value).toBe('');
-    expect(mask.textContent).toContain('MM/DD/YYYY');
-
-    expect(asFragment()).toMatchSnapshot();
-  });
-  describe('updates label mask to reflect', () => {
-    it('complete input value set', () => {
-      const { container } = render(<TestLabelMask mask={DATE_MASK} />);
-      const data = '12345678';
-      const maskText = '12/34/5678';
-
+allMasks.forEach((currentMask) => {
+  describe(`${currentMask.name} Label mask`, function () {
+    it(`renders default mask, ${currentMask.default}., when no input value set`, () => {
+      const { container, asFragment } = render(<TestLabelMask mask={currentMask.mask} />);
       const mask = container.querySelector('.ds-c-label-mask');
       const input = container.querySelector('input');
 
-      userEvent.type(input, data);
+      expect(input.value).toBe('');
+      expect(mask.textContent).toContain(currentMask.default);
 
-      expect(input).toHaveValue(data);
-      expect(mask.textContent).toContain(maskText);
+      expect(asFragment()).toMatchSnapshot();
     });
-    it('partial input value set', () => {
-      const { container } = render(<TestLabelMask mask={DATE_MASK} />);
-      const data = '1234';
+    describe('updates label mask to reflect', () => {
+      it('complete input value set', () => {
+        const { container } = render(<TestLabelMask mask={currentMask.mask} />);
+        const data = currentMask.defaultData;
+        const maskText = currentMask.defaultResult;
 
-      const mask = container.querySelector('.ds-c-label-mask');
-      const input = container.querySelector('input');
+        const mask = container.querySelector('.ds-c-label-mask');
+        const input = container.querySelector('input');
 
-      userEvent.type(input, data);
+        userEvent.type(input, data);
 
-      expect(input).toHaveValue(data);
-      expect(mask.textContent).toContain('12/34/YYYY');
+        expect(input).toHaveValue(data);
+        expect(mask.textContent).toContain(maskText);
+      });
+      it('partial input value set', () => {
+        const { container } = render(<TestLabelMask mask={currentMask.mask} />);
+        const data = currentMask.partialData;
+
+        const mask = container.querySelector('.ds-c-label-mask');
+        const input = container.querySelector('input');
+
+        userEvent.type(input, data);
+
+        expect(input).toHaveValue(data);
+        expect(mask.textContent).toContain(currentMask.partialResult);
+      });
     });
-    it('padded values when incomplete input value set', () => {
-      const { container } = render(<TestLabelMask mask={DATE_MASK} />);
-      const data = '1';
-      const maskText = '01/DD/YYYY';
+    // currency mask doesn't currenty reflect formatting in input, only label
+    if (currentMask.mask !== CURRENCY_MASK) {
+      it('formats input value onBlur', () => {
+        const { container } = render(<TestLabelMask mask={currentMask.mask} />);
+        const data = currentMask.defaultData;
+        const formattedData = currentMask.defaultResult;
 
-      const mask = container.querySelector('.ds-c-label-mask');
-      const input = container.querySelector('input');
+        const input = container.querySelector('input');
 
-      userEvent.type(input, data);
+        userEvent.type(input, data);
+        userEvent.tab();
 
-      expect(input).toHaveValue(data);
-      expect(mask.textContent).toContain(maskText);
-    });
-  });
-  it('formats input value onBlur', () => {
-    const { container } = render(<TestLabelMask mask={DATE_MASK} />);
-    const data = '12345678';
-    const formattedData = '12/34/5678';
-
-    const input = container.querySelector('input');
-
-    userEvent.type(input, data);
-    userEvent.tab();
-
-    expect(input).toHaveValue(formattedData);
+        expect(input).toHaveValue(formattedData);
+      });
+    }
   });
 });
