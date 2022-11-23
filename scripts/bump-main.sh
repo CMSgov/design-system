dbdf746dfa95d16cd9472c79a1c25b6fe92f43ae
Forrest Baer <forrest@forrestbaer.com>
2022-11-23 09:03:15 -0600

    pr title

--- scripts/bump-main.sh
+++ scripts/bump-main.sh
@@ -33,6 +33,6 @@ yarn lerna version --no-push --exact --exact ${EXTRA_OPTS[@]}
 git push --set-upstream origin $TEMP_BRANCH
 
 echo "+ Creating pull request via ${CYAN}gh${NC} to merge these updates into ${GREEN}main${NC}..."
-gh pr create --base main --title "[RELEASE] Version bump" --body "Please review version updates and compare against latest release branch" --reviewer "zarahzachz" --reviewer "pwolfert" --reviewer "forrestbaer"
+gh pr create --base main --title "[RELEASE] $(date '+m/%d') Version bump main" --body "Please review version updates and compare against latest release branch" --reviewer "zarahzachz" --reviewer "pwolfert" --reviewer "forrestbaer"
 
 git checkout main
