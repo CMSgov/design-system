f80b9bf924c82c7f2bf50b025e197400f463a28b
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2022-12-09 15:20:20 -0800

    Deleting my local `gatsby-transformer-react-docgen-typescript` plugin
    Before pursuing the idea of the transformer plugin, I was thinking the simpler way would be to write a static json file and then just import it in our Gatsby component. It would require one more pre-requisite step to building the doc site, but it wouldn't require a ton of extra code. The other issue is that it'll be a really big file, but actually it's no different from the giant result of the static graphql query done inside the PropTable component right now.
    
    I was doing some exploration into what other design system doc sites do, and Shopify's Polaris seems to compile all the types into a big json file [and then load it from there](https://github.com/Shopify/polaris/blob/ccdcea22ede61858c47a5f4f5f4fde1727f19177/polaris.shopify.com/pages/components/%5Bcomponent%5D.tsx#L118) into whatever website framework they use. So I'm not crazy.

--- packages/docs/plugins/gatsby-transformer-react-docgen-typescript/gatsby-node.js
+++
@@ -1,5 +0,0 @@
-const { onCreateNode, shouldOnCreateNode } = require(`./on-node-create`);
-
-exports.unstable_shouldOnCreateNode = shouldOnCreateNode;
-exports.onCreateNode = onCreateNode;
-exports.setFieldsOnGraphQLNodeType = require(`./extend-node-type`).default;
