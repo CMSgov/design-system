837b0dba31dd48ae13949177250bf1aa4de7a3dc
Forrest Baer <forrest@forrestbaer.com>
2022-12-21 11:05:51 -0600

    adding build storybook script to copy static assets for use in gatsby

---
+++ scripts/build-storybook.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No color
+
+set -e
+
+OUT="$1"
+
+if [ "$OUT" = "docs" ]; then
+  npx build-storybook -o packages/docs/static/storybook && \
+  echo "${GREEN}info${NC}: Moving static assets for use with gatsby."
+  mkdir -p packages/docs/static/fonts && \
+  mkdir -p packages/docs/static/images && \
+  cp -R packages/docs/static/storybook/fonts/* packages/docs/static/fonts/ && \
+  cp -R packages/docs/static/storybook/images/* packages/docs/static/images/ && \
+  rm -rf packages/docs/static/storybook/fonts && \
+  rm -rf packages/docs/static/storybook/images
+else
+  npx build-storybook -o storybook-static
+fi
