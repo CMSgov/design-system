799508e359bf8e8950c65c90b93d6b05e6385ea8
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-01-06 16:43:26 -0800

    Playing around with pattern backgrounds for swatches with transparency

--- packages/docs/src/components/content/ColorTokenSwatches.tsx
+++ packages/docs/src/components/content/ColorTokenSwatches.tsx
@@ -17,22 +17,42 @@ const swatches: Record<string, SwatchColor[]> = Object.keys(colorTokens).reduce(
  *
  */
 const ColorTokenSwatches = () =>
-  Object.entries(swatches).map(([swatchName, swatchColors]) => (
+  Object.entries(swatches).map(([swatchName, swatchColors], i) => (
     <React.Fragment key={swatchName}>
       <h2 className="ds-u-text-transform--capitalize">{swatchName}</h2>
       <dl>
-        {swatchColors.map(({ name, value }) => (
+        {swatchColors.map(({ name, value }, j) => (
           <div key={`${name}-${value}`}>
             <dt style={{ display: 'inline-block' }}>
               <svg
                 viewBox="0 0 200 32"
                 style={{ width: '200px', height: '32px', verticalAlign: 'middle' }}
               >
-                <rect width="100%" height="100%" fill={value} />
+                <pattern
+                  id={`pattern-checkers-${i}-${j}`}
+                  x="0"
+                  y="0"
+                  width="16"
+                  height="16"
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect fill="#777" x="0" y="0" width="8" height="8"></rect>
+                  <rect fill="#ccc" x="0" y="8" width="8" height="8"></rect>
+                  <rect fill="#ccc" x="8" y="0" width="8" height="8"></rect>
+                  <rect fill="#777" x="8" y="8" width="8" height="8"></rect>
+                </pattern>
+                <rect
+                  x="0"
+                  y="0"
+                  width="100%"
+                  height="100%"
+                  fill={`url(#pattern-checkers-${i}-${j})`}
+                ></rect>
+                <rect x="0" y="0" width="100%" height="100%" fill={value} />
               </svg>
-              <code className="c-swatch__name">{name}</code>
+              <code className="c-swatch__name ds-u-margin-left--1">{name}</code>
             </dt>
-            <dd style={{ display: 'inline-block' }} className="margin-left--1">
+            <dd style={{ display: 'inline-block' }} className="ds-u-margin-left--1">
               <code className="c-swatch__label js-swatch-hex ds-u-fill--transparent ds-u-color--gray">
                 {value}
               </code>
