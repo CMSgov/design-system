921294c210405474d2f4d4582d0db2c99677c2d2
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-01-10 12:07:50 -0800

    Don't use definition lists because they aren't well supported by screen readers

--- packages/docs/src/components/content/ColorRamps.tsx
+++ packages/docs/src/components/content/ColorRamps.tsx
@@ -34,7 +34,7 @@ const ColorRamps = () => (
           <h2 className="ds-u-text-transform--capitalize" id={`hello-${i}`}>
             {swatchName}
           </h2>
-          <dl className="c-color-ramp">
+          <div className="c-color-ramp">
             <svg
               className="c-color-ramp__transparency-pattern"
               viewBox="0 0 1024 1337"
@@ -43,20 +43,28 @@ const ColorRamps = () => (
               <defs>{renderTransparencyPattern(patternId)}</defs>
               <rect x="0" y="0" width="100%" height="100%" fill={`url(#${patternId})`}></rect>
             </svg>
-            {swatchColors.map(({ name, value }) => (
-              <div className="c-color-ramp__item" key={`${name}-${value}`}>
-                <svg viewBox="0 0 1337 32">
-                  <rect x="0" y="-5%" width="100%" height="110%" fill={value} />
-                </svg>
-                <dt className="ds-u-display--inline-block">
-                  <code>{name}</code>
-                </dt>
-                <dd className="ds-u-display--inline-block ds-u-margin-left--1">
-                  <code>{value}</code>
-                </dd>
-              </div>
-            ))}
-          </dl>
+            {swatchColors.map(({ name, value }) => {
+              const nameId = `color-name-${name}`;
+              const valueId = `color-value-${value}`;
+              return (
+                <div className="c-color-ramp__item" key={`${name}-${value}`}>
+                  <svg viewBox="0 0 1337 32" aria-labelledby={`${nameId} ${valueId}`}>
+                    <rect x="0" y="-5%" width="100%" height="110%" fill={value} />
+                  </svg>
+                  <code
+                    className="ds-u-display--inline-block"
+                    id={nameId}
+                    aria-describedby={valueId}
+                  >
+                    {name}
+                  </code>
+                  <code className="ds-u-display--inline-block ds-u-margin-left--1" id={valueId}>
+                    {value}
+                  </code>
+                </div>
+              );
+            })}
+          </div>
         </div>
       );
     })}
