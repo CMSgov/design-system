a65efdf71b3811077fb9d6623a48023c656ed147
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-01-19 13:49:21 -0800

    Add a story and some docs

--- packages/design-system/src/components/utilities/typographyHeadings.stories.tsx
+++ packages/design-system/src/components/utilities/typographyHeadings.stories.tsx
@@ -33,6 +33,17 @@ AllHeadingsOnDark.parameters = {
   baseInverse: true,
 };
 
+export const ContentClass = () => (
+  <div className="ds-content">
+    <h1>Heading level 1</h1>
+    <h2>Heading level 2</h2>
+    <h3>Heading level 3</h3>
+    <h4>Heading level 4</h4>
+    <h5>Heading level 5</h5>
+    <h6>Heading level 6</h6>
+  </div>
+);
+
 export const Heading5xl = () => {
   return (
     <>
