6cac118a4e1b177485aecd210e49d5031b7378cc
Scott Weber <scott.weber@adhocteam.us>
2023-01-19 09:27:27 -0500

    add story for colors

---
+++ packages/design-system/src/components/Color/themeColors.stories.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import uniqueId from 'lodash/uniqueId';
+
+export default {
+  title: 'Foundations/Theme Colors',
+  parameters: {
+    docs: {
+      source: {
+        type: 'dynamic',
+      },
+    },
+  },
+};
+
+export const PrimaryColors = () => (
+  <section className="ds-l-container">
+    <div className="c-color-ramp__item">
+      <svg aria-labelledby="color-name-granite-50 color-value-#f2f2f2">
+        <rect x="0" y="-5%" width="100%" height="110%" fill="#f2f2f2"></rect>
+      </svg>
+      <code
+        className="ds-u-display--block"
+        id="color-name-granite-50"
+        aria-describedby="color-value-#f2f2f2"
+      >
+        50
+      </code>
+      <code className="ds-u-display--block ds-u-margin-left--1" id="color-value-#f2f2f2">
+        #f2f2f2
+      </code>
+    </div>
+  </section>
+);
+
+export const SecondaryColors = () => (
+  <section className="ds-l-container">
+    <div className="c-color-ramp__item">
+      <svg aria-labelledby="color-name-granite-50 color-value-#f2f2f2">
+        <rect x="0" y="-5%" width="100%" height="110%" fill="#f2f2f2"></rect>
+      </svg>
+      <code
+        className="ds-u-display--block"
+        id="color-name-granite-50"
+        aria-describedby="color-value-#f2f2f2"
+      >
+        50
+      </code>
+      <code className="ds-u-display--block ds-u-margin-left--1" id="color-value-#f2f2f2">
+        #f2f2f2
+      </code>
+    </div>
+  </section>
+);
+
+export const BackgroundColors = () => (
+  <section className="ds-l-container">
+    <div className="c-color-ramp__item">
+      <svg aria-labelledby="color-name-granite-50 color-value-#f2f2f2">
+        <rect x="0" y="-5%" width="100%" height="110%" fill="#f2f2f2"></rect>
+      </svg>
+      <code
+        className="ds-u-display--block"
+        id="color-name-granite-50"
+        aria-describedby="color-value-#f2f2f2"
+      >
+        50
+      </code>
+      <code className="ds-u-display--block ds-u-margin-left--1" id="color-value-#f2f2f2">
+        #f2f2f2
+      </code>
+    </div>
+  </section>
+);
+
+export const StatusColors = () => (
+  <section className="ds-l-container">
+    <div className="c-color-ramp__item">
+      <svg aria-labelledby="color-name-granite-50 color-value-#f2f2f2">
+        <rect x="0" y="-5%" width="100%" height="110%" fill="#f2f2f2"></rect>
+      </svg>
+      <code
+        className="ds-u-display--block"
+        id="color-name-granite-50"
+        aria-describedby="color-value-#f2f2f2"
+      >
+        50
+      </code>
+      <code className="ds-u-display--block ds-u-margin-left--1" id="color-value-#f2f2f2">
+        #f2f2f2
+      </code>
+    </div>
+  </section>
+);
+
+export const FocusColors = () => (
+  <section className="ds-l-container">
+    <div className="c-color-ramp__item">
+      <svg aria-labelledby="color-name-granite-50 color-value-#f2f2f2">
+        <rect x="0" y="-5%" width="100%" height="110%" fill="#f2f2f2"></rect>
+      </svg>
+      <code
+        className="ds-u-display--block"
+        id="color-name-granite-50"
+        aria-describedby="color-value-#f2f2f2"
+      >
+        50
+      </code>
+      <code className="ds-u-display--block ds-u-margin-left--1" id="color-value-#f2f2f2">
+        #f2f2f2
+      </code>
+    </div>
+  </section>
+);
