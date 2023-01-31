a8eba4e56d9daac7323b3bf9a3ade361260d4645
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-01-31 14:45:56 -0800

    Fix half-pixel greyish border on swatches

--- packages/docs/src/components/content/ColorSwatch.tsx
+++ packages/docs/src/components/content/ColorSwatch.tsx
@@ -5,8 +5,8 @@ interface ColorSwatchProps extends React.ComponentPropsWithRef<'svg'> {
   title?: string;
 }
 
-const ColorSwatch = ({ colorTokenName, title, ...attrs }: ColorSwatchProps) => (
-  <svg fill={`var(--color-${colorTokenName})`} stroke="rgb(0 0 0 / 20%)" className="c-color-swatch">
+const ColorSwatch = ({ colorTokenName, title }: ColorSwatchProps) => (
+  <svg fill={`var(--color-${colorTokenName})`} className="c-color-swatch">
     {title && <title>{title}</title>}
     <rect x="0" y="-5%" width="100%" height="100%" />
   </svg>
