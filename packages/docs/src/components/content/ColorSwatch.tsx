63e1aae315b7e71316ee8773754326689ca3fb99
Scott Weber <scott.weber@adhocteam.us>
2023-02-01 15:59:26 -0500

    Update color swatches for components

--- packages/docs/src/components/content/ColorSwatch.tsx
+++ packages/docs/src/components/content/ColorSwatch.tsx
@@ -6,9 +6,9 @@ interface ColorSwatchProps extends React.ComponentPropsWithRef<'svg'> {
 }
 
 const ColorSwatch = ({ colorTokenName, title, ...attrs }: ColorSwatchProps) => (
-  <svg fill={`var(--color-${colorTokenName})`} stroke="rgb(0 0 0 / 20%)" className="c-color-swatch">
+  <svg fill={`var(--color-${colorTokenName})`} className="c-color-swatch">
     {title && <title>{title}</title>}
-    <rect x="0" y="-5%" width="100%" height="100%" />
+    <rect x="0" y="0" width="100%" height="100%" />
   </svg>
 );
 
