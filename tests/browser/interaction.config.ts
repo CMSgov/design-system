ff3b3f30bed2bbf98372a337c81aa71115d725f7
Forrest Baer <forrest@forrestbaer.com>
2023-02-02 12:08:22 -0600

    only setting up new snapshot output format for interaction tests until next pr

--- tests/browser/interaction.config.ts
+++ tests/browser/interaction.config.ts
@@ -5,6 +5,7 @@
 
 import { default as pwc } from './playwright.config';
 
+pwc['snapshotPathTemplate'] = 'snapshots/{arg}--{projectName}{ext}';
 pwc.testDir = '../../packages/';
 pwc.testMatch = /.*test\.interaction\.ts/;
 
