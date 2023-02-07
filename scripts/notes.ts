4e95286124a114d613c3452d6fa89f2027b23065
Forrest Baer <forrest@forrestbaer.com>
2023-02-07 10:07:06 -0600

    tweaking and error handling

--- scripts/notes.ts
+++ scripts/notes.ts
@@ -13,14 +13,15 @@ if (process.argv.length == 2) {
 
 /**
  * Accepts: A github pr number
- * Returns: Array of labels applied to the PR
+ * Returns: Array of labels applied to the PR or null if none.
  */
-const getLabels = (pr: string) => {
+const getGithubLabels = (pr: string) => {
   const l = JSON.parse(execSync(`gh pr view ${pr} --json=labels`).toString());
   const labels = l.labels.map((label: typeof l.labels) => {
     return label['name'];
   });
-  return labels;
+  if (labels.length) return labels;
+  return null;
 };
 
 /**
@@ -29,24 +30,30 @@ const getLabels = (pr: string) => {
 const initialFilter = /^.\s(\w*)\s\[([\w-]*)\]\s(.*)\s\(#(\d*)\)$/;
 
 /**
- * Get the difference between one branch and another
+ * Get the difference between one branch and another as a list of
+ * commits with pr #'s and description. --cherry is used to perform a
+ * diff check to rule out commits which just have different hashes.
  */
 const logData = execSync(`git log --pretty=oneline --cherry HEAD...${target}`).toString();
-
 const filteredLogArray = logData.split('\n').filter((line: string) => line.match(initialFilter));
 
 /**
- * Generate an array of objects for each PR from the filtered git log
+ * Generate an array of objects for each PR/commit from the filtered
+ * git log generated above. This data can be sorted into draft notes.
  */
 const sortedLogData = filteredLogArray.map((line: string) => {
-  const m = line.match(initialFilter);
-  if (m) {
+  const matched = line.match(initialFilter);
+
+  if (matched) {
+    const labels = getGithubLabels(matched[4]);
+    const ticket = matched[2].toLowerCase().includes('no') ? null : matched[2];
+
     return {
-      hash: m[1],
-      ticket: m[2],
-      description: m[3],
-      ghpr: m[4],
-      labels: getLabels(m[4]),
+      hash: matched[1],
+      ticket: ticket,
+      description: matched[3],
+      ghpr: matched[4],
+      labels: labels,
     };
   } else {
     console.log(
