84a54db800a1941ff356c039d3693376ab6c3c89
Forrest Baer <forrest@forrestbaer.com>
2023-02-13 14:10:31 -0600

    adding tests and snapshots for tooltip, mask, datefield picker and autocomplete

---
+++ packages/design-system/src/components/Tooltip/Tooltip.test.interaction.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+import themes from '../../../../../themes.json';
+
+const sbID = 'components-tooltip--interactive-content';
+const storyUrl = `http://localhost:6006/iframe.html?viewMode=story&id=${sbID}`;
+
+Object.keys(themes).forEach((theme) => {
+  if (themes[theme].incomplete) return;
+
+  test(`Tooltip interaction: ${theme}`, async ({ page }) => {
+    await page.goto(`${storyUrl}&globals=theme:${theme}`);
+    await page.getByRole('button').click();
+    await expect(page).toHaveScreenshot(`tooltip--open-interactive--${theme}.png`, {
+      fullPage: true,
+    });
+  });
+});
