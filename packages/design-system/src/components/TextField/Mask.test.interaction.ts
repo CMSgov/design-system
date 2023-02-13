5a9909121080eb65c86a2f6180ee513f128c470d
Forrest Baer <forrest@forrestbaer.com>
2023-02-13 15:21:30 -0600

    [WNMGDS-2166] Adding more interaction tests (#2383)
    adding tests and snapshots for tooltip, mask, datefield picker and autocomplete

---
+++ packages/design-system/src/components/TextField/Mask.test.interaction.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import themes from '../../../../../themes.json';
+
+const sbID = 'components-text-field--label-masked-ssn';
+const storyUrl = `http://localhost:6006/iframe.html?viewMode=story&id=${sbID}`;
+
+Object.keys(themes).forEach((theme) => {
+  if (themes[theme].incomplete) return;
+
+  test(`Mask partial text entry: ${theme}`, async ({ page }) => {
+    await page.goto(`${storyUrl}&globals=theme:${theme}`);
+    const elem = page.getByRole('textbox');
+    await elem.type('22', { timeout: 200 });
+    await expect(page).toHaveScreenshot(`mask--text-entry--${theme}.png`, { fullPage: true });
+  });
+
+  test(`Mask partial text entry and exit: ${theme}`, async ({ page }) => {
+    await page.goto(`${storyUrl}&globals=theme:${theme}`);
+    const elem = page.getByRole('textbox');
+    await elem.type('221', { timeout: 200 });
+    await elem.press('Tab');
+    await expect(page).toHaveScreenshot(`mask--text-entry-and-exit--${theme}.png`, {
+      fullPage: true,
+    });
+  });
+});
