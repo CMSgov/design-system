12505b1c4b908ff5a757e5a0d3495177a94fcad1
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-03-02 09:30:40 -0800

    Reuse the heading constant

--- packages/design-system/src/components/utilities/typographyHeadings.stories.tsx
+++ packages/design-system/src/components/utilities/typographyHeadings.stories.tsx
@@ -35,17 +35,17 @@ AllHeadingsOnDark.parameters = {
 
 export const ContentClass = () => (
   <div className="ds-content">
-    <h1>Example heading level 1</h1>
+    <h1>{heading} level 1</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
       labore et dolore magna aliqua.
     </p>
-    <h2>Example heading level 2</h2>
+    <h2>{heading} level 2</h2>
     <p>
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
       commodo consequat.
     </p>
-    <h3>Example heading level 3</h3>
+    <h3>{heading} level 3</h3>
     <p>
       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
@@ -58,7 +58,7 @@ export const ContentClass = () => (
     <p>
       Arcu risus quis varius quam quisque. Suspendisse ultrices gravida dictum fusce ut placerat.
     </p>
-    <h4>Example heading level 4</h4>
+    <h4>{heading} level 4</h4>
     <p>
       Ullamcorper velit sed ullamcorper morbi tincidunt ornare. Sed vulputate mi sit amet mauris
       commodo. Blandit massa enim nec dui nunc. Vestibulum lorem sed risus ultricies tristique.
@@ -67,13 +67,13 @@ export const ContentClass = () => (
       <li>An orderly list of items</li>
       <li>So ordered</li>
     </ol>
-    <h5>Example heading level 5</h5>
+    <h5>{heading} level 5</h5>
     <p>
       Non diam phasellus vestibulum lorem sed risus ultricies tristique. Elementum eu facilisis sed
       odio. Tempor orci dapibus ultrices in iaculis nunc sed augue. Arcu non sodales neque sodales
       ut etiam sit amet.
     </p>
-    <h6>Example heading level 6</h6>
+    <h6>{heading} level 6</h6>
     <p>
       Hac habitasse platea dictumst quisque sagittis purus sit amet. Ultrices neque ornare aenean
       euismod. Dignissim sodales ut eu sem integer vitae.
