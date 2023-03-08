287cc7d72f8b1bd33361c66ee2ba92769b0ad04b
Forrest Baer <forrest@forrestbaer.com>
2023-03-08 13:59:54 -0600

    updated interaction tests for autocomplete and mask as firefox stopped working for these interaction tests for some reason, also bumping playwright a minor version.

--- packages/design-system/src/components/Autocomplete/Autocomplete.test.interaction.ts
+++ packages/design-system/src/components/Autocomplete/Autocomplete.test.interaction.ts
@@ -9,7 +9,7 @@ Object.keys(themes).forEach((theme) => {
 
   test(`Autocomplete select  hover: ${theme}`, async ({ page }) => {
     await page.goto(`${storyUrl}&globals=theme:${theme}`);
-    const elem = page.getByRole('listbox');
+    const elem = page.getByRole('combobox');
     await elem.type('c');
     await elem.press('ArrowDown');
     await expect(page).toHaveScreenshot(`autocomplete--type--${theme}.png`, { fullPage: true });
