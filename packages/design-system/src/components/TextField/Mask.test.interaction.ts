f073f3a19f940bfdb21b5240bf00d842a290885f
Forrest Baer <forrest@forrestbaer.com>
2023-03-13 14:20:50 -0500

    [NO-TICKET] Mask interaction test update (#2421)
    adds slight delay to type to ensure three characters get typed

--- packages/design-system/src/components/TextField/Mask.test.interaction.ts
+++ packages/design-system/src/components/TextField/Mask.test.interaction.ts
@@ -17,7 +17,7 @@ Object.keys(themes).forEach((theme) => {
   test(`Mask partial text entry and exit: ${theme}`, async ({ page }) => {
     await page.goto(`${storyUrl}&globals=theme:${theme}`);
     const elem = page.locator('input.ds-c-field');
-    await elem.type('221');
+    await elem.type('221', { delay: 220 });
     await elem.press('Tab');
     await expect(page).toHaveScreenshot(`mask--text-entry-and-exit--${theme}.png`, {
       fullPage: true,
