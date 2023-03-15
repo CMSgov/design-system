edf9757e325d64e09a1d31aaf6867e0d2fdc7644
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-03-15 16:00:05 -0700

    Remove imports of the deleted modules

--- packages/ds-medicare-gov/src/components/Icons/index.ts
+++ packages/ds-medicare-gov/src/components/Icons/index.ts
@@ -1,11 +1,6 @@
-export { default as Caret } from './Caret';
-export { default as Checkmark } from './Checkmark';
 export { default as CheckShieldIcon } from './CheckShieldIcon';
-export { default as Close } from './CloseSymbol';
 export { default as DrugsIcon } from './DrugsIcon';
-export { default as Hamburger } from './HamburgerSymbol';
 export { default as InfoCircleOutlineIcon } from './InfoCircleOutlineIcon';
-export { default as No } from './No';
 export { default as PharmacyIcon } from './PharmacyIcon';
 export { default as PiggyBankIcon } from './PiggyBankIcon';
 export { default as RoundedStarIcon } from './RoundedStarIcon';
