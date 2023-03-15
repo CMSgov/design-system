680afc6779337bb0306c93dc1cac55cf4814d429
Scott Weber <scott.weber@adhocteam.us>
2023-03-14 20:01:42 -0400

    [WNMGDS-2245] Remove primary alt color (#2416)
    * remove references in utility styles
    
    * update storybook stories
    
    * update themes
    
    * update storybook styles
    
    * Update theme files
    
    * updating snapshots
    
    * Update packages/design-system/src/components/utilities/margin.stories.tsx
    
    Co-authored-by: Patrick Wolfert <patrick.wolfert@adhocteam.us>
    
    * Update packages/design-system/src/components/utilities/padding.stories.tsx
    
    Co-authored-by: Patrick Wolfert <patrick.wolfert@adhocteam.us>
    
    * Remove last unrelated ref image change
    
    * Update the refs with accurate colors
    
    In order to get our tests to pick up on this change, I had to check these out from `main` and then run the tests again. Then the tests created the updated reference images and I was able to check them in
    
    * Use `info` equivalent of old color so there are no story changes at all
    
    * Undo the mask image change
    
    Checked it out from main this time. Just trying to get it out of the PR diff
    
    ---------
    
    Co-authored-by: Forrest Baer <forrest@forrestbaer.com>
    Co-authored-by: Patrick Wolfert <patrick.wolfert@adhocteam.us>

--- packages/design-system/src/components/utilities/padding.stories.tsx
+++ packages/design-system/src/components/utilities/padding.stories.tsx
@@ -12,7 +12,7 @@ export default {
 
 // an example for responsive padding -- referenced in doc site
 export const padding = () => (
-  <div className="ds-u-fill--primary-alt" style={{ maxWidth: 'fit-content' }}>
+  <div className="ds-u-fill--info" style={{ maxWidth: 'fit-content' }}>
     <div className="ds-u-padding--1 ds-u-sm-padding--2 ds-u-md-padding--3 ds-u-lg-padding--4 ds-u-xl-padding--5">
       <span className="ds-u-fill--white">Responsive padding</span>
     </div>
