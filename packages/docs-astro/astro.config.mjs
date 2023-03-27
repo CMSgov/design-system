b43e2783472b94f7f6425eee54346a3ed53106dd
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-03-27 15:04:05 -0700

    Trying to use some of our existing docs features in astro

--- packages/docs-astro/astro.config.mjs
+++ packages/docs-astro/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), react()],
 });
