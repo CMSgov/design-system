5ba7854adc39346af0fba67d9e5a272c3f6fb97e
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-03-28 17:54:09 -0700

    Temporarily restrict number of stories to avoid hitting snapshot quota

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,13 @@
 const stories = [
-  '../packages/design-system/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-  '../packages/ds-healthcare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-  '../packages/ds-medicare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  // Temporarily restrict the number of stories that are tested so I can try everything
+  // out without hitting our monthly snapshots quota
+  '../packages/design-system/src/components/Accordion/*.stories.@(js|jsx|ts|tsx|mdx)',
+  '../packages/design-system/src/components/Alert/*.stories.@(js|jsx|ts|tsx|mdx)',
+  '../packages/ds-healthcare-gov/src/components/Footer/*.stories.@(js|jsx|ts|tsx|mdx)',
+  '../packages/ds-medicare-gov/src/components/SimpleFooter/*.stories.@(js|jsx|ts|tsx|mdx)',
+  // '../packages/design-system/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  // '../packages/ds-healthcare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+  // '../packages/ds-medicare-gov/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
 ];
 module.exports = {
   features: {
