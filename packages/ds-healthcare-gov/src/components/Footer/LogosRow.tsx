5ea3eac8a2e6286a47b3545d90f268218ed2d977
Sarah Sachs <sarahsachs@fastmail.com>
2023-04-11 10:20:44 -0400

    Initial redesign of hgov footer.

--- packages/ds-healthcare-gov/src/components/Footer/LogosRow.tsx
+++ packages/ds-healthcare-gov/src/components/Footer/LogosRow.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from './Logo';
-import { HHSLogo, UsaLogo, WhiteHouseLogo } from '@cmsgov/design-system';
+import { Logo as HCgovLogo } from '../Logo';
 
 interface LogosRowProps {
   t: (string) => string;
@@ -11,26 +11,16 @@ interface LogosRowProps {
  */
 const LogosRow = function (props: LogosRowProps) {
   return (
-    <div className="ds-l-container ds-u-margin-top--4">
-      <div className="ds-l-form-row">
-        <div className="ds-l-col ds-l-col--3 ds-l-sm-col--2 ds-l-md-col--auto">
-          <Logo href="http://www.hhs.gov/" width="67px">
-            <HHSLogo className="hc-c-logo--hhs-logo" ariaHidden={false} />
+    <div className="ds-l-container">
+      <div className="hc-c-footer__logo-row">
+        <div>
+          <Logo href="https://www.hhs.gov/">
+            <HCgovLogo />
           </Logo>
         </div>
-        <div className="ds-l-col ds-l-col--9 ds-l-sm-col--10 ds-l-md-col--auto">
-          <p
-            className="ds-u-font-size--small ds-u-color--muted ds-u-measure--base ds-u-margin--0"
-            dangerouslySetInnerHTML={{ __html: props.t('footer.address') }}
-          />
-        </div>
-        <div className="ds-l-col ds-l-col--12 ds-l-lg-col--auto ds-u-lg-margin-top--0 ds-u-margin-top--2 ds-u-margin-left--auto">
-          <Logo className="ds-u-margin-right--2" href="http://www.whitehouse.gov/" width="76px">
-            <WhiteHouseLogo className="hc-c-logo--wh-logo" ariaHidden={false} />
-          </Logo>
-          <Logo href="http://www.usa.gov/" width="162px">
-            <UsaLogo className="hc-c-logo--usa-logo" ariaHidden={false} />
-          </Logo>
+        <div className="hc-c-footer__address">
+          <p dangerouslySetInnerHTML={{ __html: props.t('footer.address-top') }} />
+          <p dangerouslySetInnerHTML={{ __html: props.t('footer.address-bottom') }} />
         </div>
       </div>
     </div>
