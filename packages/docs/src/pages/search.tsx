94b50aa7866c28b218157c696151f7f3fa34c594
Forrest Baer <forrest@forrestbaer.com>
2023-05-15 10:57:51 -0500

    consolidating search page into single file

--- packages/docs/src/pages/search.tsx
+++ packages/docs/src/pages/search.tsx
@@ -1,40 +1,74 @@
 import React from 'react';
-import { graphql } from 'gatsby';
-
+import { useEffect, useState } from 'react';
+import { TextField } from '@cmsgov/design-system';
+import { useFlexSearch } from 'react-use-flexsearch';
+import { useStaticQuery, graphql } from 'gatsby';
+import { MdxQuery, SearchDataStore, SearchQuery } from '../helpers/graphQLTypes';
 import Layout from '../components/layout/Layout';
-import { MdxQuery } from '../helpers/graphQLTypes';
 import useTheme from '../helpers/useTheme';
-import ContentRenderer from '../components/content/ContentRenderer';
 
-const SearchPage = ({ data, location }: MdxQuery) => {
-  const { slug } = data.mdx;
+const SearchPage = ({ location }: MdxQuery) => {
+  const [query, setQuery] = useState('');
   const theme = useTheme();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    setQuery(params.get('query'));
+  }, []);
+
+  const searchData: SearchQuery = useStaticQuery(graphql`
+    {
+      localSearchPages {
+        index
+        store
+      }
+    }
+  `);
+
+  const { index, store } = searchData.localSearchPages;
+  const results = useFlexSearch(query, index, store);
 
   return (
     <Layout
-      frontmatter={data.mdx.frontmatter}
+      frontmatter={{
+        title: 'Search this site',
+      }}
       location={location}
-      slug={slug}
+      slug="Search"
       theme={theme}
-      tableOfContentsData={data.mdx.tableOfContents?.items}
     >
-      <ContentRenderer data={data.mdx.body} theme={theme} />
+      <div id="search-results">
+        <div className="ds-u-margin-bottom--4">
+          <TextField
+            label="Enter your search terms below:"
+            name="search-field"
+            onChange={(evt) => {
+              setQuery(evt.target.value);
+            }}
+            value={query}
+          />
+        </div>
+        <div>
+          {results.length > 0 && (
+            <div>
+              &quot;{query}&quot; returned <strong>{results.length}</strong> results.
+            </div>
+          )}
+          {query && results.length === 0 && (
+            <div>Search for &quot;{query}&quot; did not return any results.</div>
+          )}
+          <ul>
+            {results.map((result: SearchDataStore) => (
+              <li key={result.id}>
+                <a href={location.origin + '/' + result.path + location.search}>{result.title}</a>
+                <p>{result.excerpt}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 };
-
-export const query = graphql`
-  query SearchPageQuery {
-    mdx(frontmatter: { title: { eq: "Search" } }) {
-      id
-      body
-      slug
-      tableOfContents(maxDepth: 3)
-      frontmatter {
-        title
-      }
-    }
-  }
-`;
 
 export default SearchPage;
