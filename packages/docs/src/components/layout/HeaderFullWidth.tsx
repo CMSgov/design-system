0a82d7480ae2c3bdfa50bf539a4659f5da3510b6
Forrest Baer <forrest@forrestbaer.com>
2023-05-15 11:25:29 -0500

    padding issue with header

--- packages/docs/src/components/layout/HeaderFullWidth.tsx
+++ packages/docs/src/components/layout/HeaderFullWidth.tsx
@@ -4,8 +4,8 @@ import SearchForm from '../content/SearchForm';
 
 const HeaderFullWidth = () => {
   return (
-    <div className="ds-l-row ds-u-margin--0 ds-u-padding--1 ds-u-border-bottom--2">
-      <div className="ds-u-display--flex ds-l-col--6 ds-u-align-items--center">
+    <div className="ds-l-row ds-u-margin--0 ds-u-padding--2 ds-u-padding-left--0 ds-u-border-bottom--2">
+      <div className="ds-u-display--flex ds-l-col--6 ds-u-align-items--center ds-u-padding-left--0">
         <CMSLogoIcon />
         <h1 className="ds-u-padding-left--1 ds-text-heading--2xl ds-u-margin--0">Design System</h1>
       </div>
