b0495edb7d93d93a36317d07226bbc63bab98c84
Forrest Baer <forrest@forrestbaer.com>
2023-05-16 09:52:21 -0500

    adding title as a link, removing header on mobile

--- packages/docs/src/components/layout/HeaderFullWidth.tsx
+++ packages/docs/src/components/layout/HeaderFullWidth.tsx
@@ -4,11 +4,18 @@ import SearchForm from '../content/SearchForm';
 
 const HeaderFullWidth = () => {
   return (
-    <div className="ds-l-row ds-u-margin--0 ds-u-padding--2 ds-u-padding-left--0 ds-u-border-bottom--2">
-      <div className="ds-u-display--flex ds-l-col--6 ds-u-align-items--center ds-u-padding-left--0">
+    <div
+      id="header-full-width"
+      className="ds-l-row ds-u-margin--0 ds-u-padding--2 ds-u-padding-left--0 ds-u-border-bottom--2 ds-u-display--none ds-u-md-display--flex"
+    >
+      <a
+        className="ds-u-display--flex ds-l-col--6 ds-u-align-items--center ds-u-padding-left--0"
+        href="/"
+        title="Return to the homepage"
+      >
         <CMSLogoIcon />
         <h1 className="ds-u-padding-left--1 ds-text-heading--2xl ds-u-margin--0">Design System</h1>
-      </div>
+      </a>
       <SearchForm className="ds-u-display--flex ds-l-col--6 ds-u-align-items--center ds-u-justify-content--end" />
     </div>
   );
