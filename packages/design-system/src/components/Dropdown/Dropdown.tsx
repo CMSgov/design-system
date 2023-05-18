ee1f8bf2febba640d7736a3ab593e1437e6de77a
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-05-17 22:25:50 -0700

    [WNMGDS-2317] Re-implement `Dropdown` as combobox using `Downshift` (#2498)
    * Some basic functionality with lots of decisions yet to be made
    
    * A little more progress and cleanup
    
    * The `getItemProps` function handles setting `aria-selected` already
    
    * Some basic interaction styling for the dropdown menu
    
    * Add support for size prop
    
    * This logic for parsing optgroups and options works but might not be robust enough
    
    * Rendering opt groups mostly works, but you can still keyboard highlight the groups
    
    I think maybe in order to get this to work correctly, I need to exclude the groups from the `items` that I pass the downshift hook
    
    * Mostly working except that the highlightedIndex doesn't get set to selected item index after selection
    
    * Add some comments
    
    * Pass any extra props on options and optgroups to the list items
    
    and move a bunch of functions into a utils module
    
    * Move non-prod prop validation code into separate module
    
    * Still unsure why `highlightedIndex` resets itself after selection
    
    * Some fancier styling
    
    * Cleanup from pairing session
    
    * Basic styles to differentiate groups
    
    * Fix bug in `findElementsOfType` that caused plain option children to error
    
    When testing out a story where the children were just `option` elements without groups, it would throw an error when one of the option's `children` was a string. The cases in the function are hopefully way clearer now, and TypeScript is much happier and is able to check that everything is what it should be at each step without having to type cast!
    
    * Saving the `highlightedIndex` after selection works!
    
    Had to memoize the options since we generate them each time when we parse children
    
    * Go back to including the opt groups in the downshift items array
    
    to get a smoother interaction with pointer devices. As a pointer hovers over the group headings, the hover styles stay on the last hovered item rather than the hover style disappearing when I hover over the group heading. The highlightedIndex doesn't get updated as the pointer passes over the group headings, because they don't exist as items in Downshift. It looks like it "sticks", which feels clunky. They both exhibit the same behavior when using the keyboard, which is to skip the group, so keyboard interaction is not a factor in this decision.
    
    * Clean up types and fix ts error
    
    * Fixing up tests and snapshots and patching holes that they find
    
    * Fix remaining unit tests
    
    * More unit tests about children and optgroups
    
    * Add story for array-based option groups
    
    * Update visual regression tests, add interaction tests, and update some snapshots
    
    There are some snapshot updates that I have not included in this commit because they actually found regressions that I need to fix with the medicare styles
    
    * Fix medicare dropdown icon regression
    
    * Fix regressions and update snapshots
    
    * Revert Autocomplete changes in favor of changes on `main`
    
    * Fix references to `select` in props docs
    
    * Remove this type annotation in doc site version switcher because it's wrong now
    
    * Fix storybook links
    
    ---------
    
    Co-authored-by: Forrest Baer <forrest@forrestbaer.com>

--- packages/design-system/src/components/Dropdown/Dropdown.tsx
+++ packages/design-system/src/components/Dropdown/Dropdown.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useMemo, useRef } from 'react';
 import classNames from 'classnames';
 import useAutofocus from '../utilities/useAutoFocus';
 import { FormFieldProps, FormLabel, useFormLabel } from '../FormLabel';
+import { useSelect, UseSelectStateChangeOptions } from 'downshift';
+import { isOptGroupArray, parseChildren, validateProps } from './utils';
+import { uniqueId } from 'lodash';
 
-export type DropdownDefaultValue = number | string;
-export interface DropdownOptions {
-  label: React.ReactNode;
-  value: number | string;
-}
 export type DropdownSize = 'small' | 'medium';
 export type DropdownValue = number | string;
+
+export interface DropdownChangeObject extends UseSelectStateChangeOptions<any> {
+  target: { value: string };
+  currentTarget: { value: string };
+}
+
+export interface DropdownOption extends React.HTMLAttributes<'option'> {
+  label: string;
+  value: DropdownValue;
+}
+export interface DropdownOptGroup extends React.HTMLAttributes<'optgroup'> {
+  label: string;
+  options: DropdownOption[];
+}
+interface InternalItem extends React.HTMLAttributes<'option' | 'optgroup'> {
+  label: string;
+  value?: number | string;
+  isOptGroup?: boolean;
+}
+const itemToString = (item: InternalItem) => item.label;
 
 export interface BaseDropdownProps extends Omit<FormFieldProps, 'id'> {
   /**
@@ -17,33 +35,29 @@ export interface BaseDropdownProps extends Omit<FormFieldProps, 'id'> {
    */
   ariaLabel?: string;
   /**
-   * Used to define custom dropdown options (i.e. option groups). When using the `children` prop, `options` should be an empty list.
-   */
-  children?: React.ReactNode;
-  /**
    * Sets the initial selected state. Use this for an uncontrolled component;
    * otherwise, use the `value` property.
    */
-  defaultValue?: DropdownDefaultValue;
+  defaultValue?: DropdownValue;
   /**
    * Disables the entire field.
    */
   disabled?: boolean;
   /**
-   * Additional classes to be added to the select element
+   * Additional classes to be added to the dropdown button element
    */
   fieldClassName?: string;
   /**
-   * Sets the focus on the select during the first mount
+   * Sets the focus on the button during the first mount
    */
   autoFocus?: boolean;
   /**
-   * A unique ID to be used for the `select` element. If one isn't provided, a unique ID will be generated.  /**
+   * A unique ID to be used for the `button` element. If one isn't provided, a unique ID will be generated.  /**
    * Additional hint text to display
    */
   id?: string;
   /**
-   * Access a reference to the `select` element
+   * Access a reference to the `button` element
    */
   inputRef?: (...args: any[]) => any;
   /**
@@ -54,12 +68,8 @@ export interface BaseDropdownProps extends Omit<FormFieldProps, 'id'> {
    * The field's `name` attribute
    */
   name: string;
-  /**
-   * The list of options to be rendered. Provide an empty list if using custom options via the `children` prop.
-   */
-  options: DropdownOptions[];
   onBlur?: (...args: any[]) => any;
-  onChange?: (...args: any[]) => any;
+  onChange?: (change: DropdownChangeObject) => any;
   /**
    * Text showing the requirement ("Required", "Optional", etc.). See [Required and Optional Fields]({{root}}/guidelines/forms/#required-and-optional-fields).
    */
@@ -75,68 +85,184 @@ export interface BaseDropdownProps extends Omit<FormFieldProps, 'id'> {
   value?: DropdownValue;
 }
 
+type OptionsOrChildren =
+  | {
+      children?: undefined;
+      /**
+       * The list of options to be rendered. Each item must have a `label` and `value`.
+       */
+      options: DropdownOption[] | DropdownOptGroup[];
+    }
+  | {
+      /**
+       * Used to define custom dropdown options (i.e. option groups). Alternative to `options` prop.
+       */
+      children: React.ReactNode;
+      options?: undefined;
+    };
+
 export type DropdownProps = BaseDropdownProps &
-  Omit<React.ComponentPropsWithRef<'select'>, keyof BaseDropdownProps>;
+  OptionsOrChildren &
+  Omit<React.ComponentPropsWithRef<'button'>, keyof BaseDropdownProps>;
 
 export const Dropdown: React.FC<DropdownProps> = (props: DropdownProps) => {
-  if (process.env.NODE_ENV !== 'production') {
-    // 'ariaLabel' is provided with a `label` prop that is not an empty string
-    if (props.ariaLabel && (typeof props.label !== 'string' || props.label.length > 0)) {
-      console.warn(
-        `Cannot use 'ariaLabel' and 'label' React properties together in the <Dropdown> component. If the 'label' prop is used, it should be written for all users so that an 'ariaLabel' is not needed. The 'ariaLabel' prop is intended to be used only when the input is missing an input label (i.e when an empty string is provided for the 'label' prop)`
-      );
+  validateProps(props);
+
+  const id = useRef(props.id ?? uniqueId('dropdown__button--')).current;
+  const labelId = useRef(props.labelId ?? uniqueId('dropdown__label--')).current;
+  const menuId = useRef(uniqueId('dropdown__menu--')).current;
+
+  // Draw out certain props that we don't want to pass through as attributes
+  const {
+    ariaLabel,
+    autoFocus,
+    children,
+    fieldClassName,
+    onChange,
+    options,
+    size,
+    defaultValue,
+    value,
+    ...extraProps
+  } = props;
+
+  // Turn our options or optgroups into a flat array of selectable items that
+  // we can pass to the Downshift `useSelect` hook. Even though the group
+  // headings are not selectable, Downshift wants to know about them. I've
+  // tried excluding them from the list we give to Downshift, but then the
+  // highlighted index sticks on the last hovered selectable item when hovering
+  // over a group heading, and it doesn't look very good.
+  const optionsOrOptGroups = options ?? parseChildren(children);
+  const items: InternalItem[] = useMemo(
+    () =>
+      !isOptGroupArray(optionsOrOptGroups)
+        ? optionsOrOptGroups
+        : optionsOrOptGroups.reduce((internalItems, optGroup) => {
+            internalItems.push({
+              label: optGroup.label,
+              isOptGroup: true,
+            });
+            internalItems.push(...optGroup.options);
+            return internalItems;
+          }, [] as InternalItem[]),
+    [options, children]
+  );
+
+  let controlledSelectedItem;
+  let defaultSelectedItem;
+  if (value !== undefined) {
+    // Controlled component
+    controlledSelectedItem = items.find((item) => value === item.value);
+    if (!controlledSelectedItem) {
+      throw new Error(`Could not find option matching value: ${value}`);
     }
-    // An empty string `label` is provided without a corresponding `ariaLabel` prop
-    if (!props.ariaLabel && typeof props.label === 'string' && props.label.length === 0) {
-      console.warn(
-        `Please provide an 'ariaLabel' when using the <Dropdown> component without a 'label' prop.`
-      );
-    }
-    if (props.children && props.options.length > 0) {
-      console.warn(
-        `Cannot use 'options' and 'children' React properties at the same time in the <Select> component. Please use 'children' for custom options and 'options' for general cases`
-      );
+  } else {
+    defaultSelectedItem =
+      defaultValue !== undefined
+        ? items.find((item) => defaultValue === item.value)
+        : items.filter((item) => !item.isOptGroup)[0];
+    if (!defaultSelectedItem) {
+      throw new Error('Dropdown component could not determine a default selected option');
     }
   }
 
-  // Select specific props
-  const { ariaLabel, children, fieldClassName, options, size, ...selectProps } = props;
-
-  const optionElements =
-    children ??
-    options.map((option) => (
-      <option key={option.value} value={option.value}>
-        {option.label}
-      </option>
-    ));
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getMenuProps,
+    getItemProps,
+    highlightedIndex,
+  } = useSelect({
+    defaultSelectedItem,
+    selectedItem: controlledSelectedItem,
+    toggleButtonId: id,
+    labelId,
+    menuId,
+    items,
+    itemToString,
+    onSelectedItemChange:
+      onChange &&
+      ((changes: UseSelectStateChangeOptions<any>) => {
+        // Try to support the old API that passed an event object
+        const target = { value: changes.selectedItem.value };
+        onChange({
+          ...changes,
+          target,
+          currentTarget: target,
+        });
+      }),
+  });
 
   const { labelProps, fieldProps, wrapperProps, bottomError } = useFormLabel({
-    ...selectProps,
+    ...extraProps,
+    id,
+    labelId,
+    className: classNames(
+      'ds-c-dropdown',
+      isOpen && 'ds-c-dropdown--open',
+      size && `ds-c-field--${size}`
+    ),
     labelComponent: 'label',
     wrapperIsFieldset: false,
   });
 
-  const selectClassNames = classNames(
-    'ds-c-field',
-    {
-      'ds-c-field--error': props.errorMessage,
-      'ds-c-field--inverse': props.inversed,
-    },
-    size && `ds-c-field--${size}`,
-    fieldClassName
-  );
-
-  const ref = useAutofocus<HTMLSelectElement>(props.autoFocus);
-
-  // we don't want to pass this down to the select element
+  // We don't want to pass these down to the button
   delete fieldProps.errorMessage;
+  delete fieldProps.errorId;
+  delete fieldProps.inversed;
+
+  const buttonProps = getToggleButtonProps({
+    ...fieldProps,
+    ref: useAutofocus<HTMLButtonElement>(props.autoFocus),
+    className: classNames(
+      'ds-c-dropdown__button',
+      'ds-c-field',
+      props.errorMessage && 'ds-c-field--error',
+      props.inversed && 'ds-c-field--inverse',
+      fieldClassName
+    ),
+    'aria-label': ariaLabel,
+  });
+
+  const menuProps = getMenuProps({
+    className: classNames(
+      'ds-c-dropdown__menu',
+      isOptGroupArray(optionsOrOptGroups) && 'ds-c-dropdown__menu--grouped'
+    ),
+  });
+
+  const menuContent = items.map((item, index) => {
+    const { value, label, isOptGroup, className, ...extraAttrs } = item;
+    return (
+      <li
+        key={value ?? label}
+        className={classNames(
+          className,
+          isOptGroup ? 'ds-c-dropdown__item-group' : 'ds-c-dropdown__item',
+          highlightedIndex === index && 'ds-c-dropdown__item--highlighted',
+          selectedItem === item && 'ds-c-dropdown__item--selected'
+        )}
+        {...extraAttrs}
+        {...getItemProps({
+          item,
+          index,
+          disabled: isOptGroup,
+          role: isOptGroup ? 'group' : undefined,
+        })}
+      >
+        {item.label}
+      </li>
+    );
+  });
 
   return (
     <div {...wrapperProps}>
       <FormLabel {...labelProps} fieldId={fieldProps.id} />
-      <select aria-label={ariaLabel} ref={ref} className={selectClassNames} {...fieldProps}>
-        {optionElements}
-      </select>
+      <button {...buttonProps}>{selectedItem.label}</button>
+      <div className="ds-c-dropdown__menu-container" hidden={!isOpen}>
+        <ul {...menuProps}>{menuContent}</ul>
+      </div>
       {bottomError}
     </div>
   );
