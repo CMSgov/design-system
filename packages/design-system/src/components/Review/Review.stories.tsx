90a4cfe3ec3055443b7418ca67913f8d3a8ae83a
Forrest Baer <forrest@forrestbaer.com>
2023-06-22 12:25:02 -0500

    review, skipnav, spinner updated to sb7 format

---
+++ packages/design-system/src/components/Review/Review.stories.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Review from './Review';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Review> = {
+  title: 'Components/Review',
+  component: Review as any,
+  args: {
+    headingLevel: '3',
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Review>;
+
+export const Default: Story = {
+  args: {
+    children: 'This is an example of a default Review component.',
+  },
+};
+
+export const SingleReview: Story = {
+  args: {
+    heading: 'A single Review component',
+    children: 'This is an example of a single Review component.',
+    editHref: '#',
+  },
+};
+
+export const MultipleReviews: Story = {
+  args: {
+    heading: 'Multiple Review components',
+    children: 'Multiple Review components can be combined together one after another.',
+    editHref: '#',
+  },
+  decorators: [
+    (Story) => (
+      <div>
+        {Story()}
+        {Story()}
+      </div>
+    ),
+  ],
+};
+
+export const CustomActions: Story = {
+  args: {
+    heading: 'A Review component with custom actions',
+    children: 'You can add custom action links as needed.',
+    editContent: (
+      <div>
+        <a href="#">Edit</a>
+        <span>|</span>
+        <a href="#">Remove</a>
+      </div>
+    ),
+  },
+};
---
+++ packages/design-system/src/components/SkipNav/SkipNav.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import SkipNav from './SkipNav';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof SkipNav> = {
+  title: 'Components/Skip Nav',
+  component: SkipNav,
+  args: {
+    href: '#main',
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof SkipNav>;
+
+export const Default: Story = {};
+
+export const SkipNavExample: Story = {
+  decorators: [
+    (Story) => (
+      <div>
+        {Story()}
+        <nav className="ds-u-padding--3">
+          <h1 className="ds-u-margin--0">Navigation</h1>
+          <p className="ds-u-margin--0">Clicking on the Skip Nav will skip over this section.</p>
+          <ul>
+            <li>
+              <a href="#">Navigation link 1</a>
+            </li>
+            <li>
+              <a href="#">Navigation link 2</a>
+            </li>
+          </ul>
+        </nav>
+        <main id="main" className="ds-u-padding--3 ds-u-fill--gray-lightest" tabIndex={-1}>
+          <h1 className="ds-u-margin--0">Main content</h1>
+          <p className="ds-u-margin--0">Clicking on the Skip Nav will focus this element.</p>
+        </main>
+      </div>
+    ),
+  ],
+};
