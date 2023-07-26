ea68c98526177171612859550d1e745c263fb2c6
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-07-26 09:23:29 -0700

    [WNMGDS-2446] Fix missing role="option" on dropdown items (#2592)
    * Fix dropdown item roles being clobbered by a wrong assumption
    
    I assumed that if we passed `undefined` for the value of that role property to the `getItemProps`, the default value would come through, but it wasn't.
    
    Axe DevTools flags the missing role and throws 17 errors when you have a dropdown open with these missing roles. When this is fixed, those errors go away.
    
    * Update unit test snapshots

--- packages/design-system/src/components/Dropdown/Dropdown.tsx
+++ packages/design-system/src/components/Dropdown/Dropdown.tsx
@@ -268,7 +268,7 @@ export const Dropdown: React.FC<DropdownProps> = (props: DropdownProps) => {
           item,
           index,
           disabled: isOptGroup,
-          role: isOptGroup ? 'group' : undefined,
+          role: isOptGroup ? 'group' : 'option',
         })}
       >
         {selectedItem === item && (
