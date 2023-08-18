de8bf471a38620a3acc07ceeca06881d445bd938
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-08-18 11:20:11 -0700

    Change name of check icon to match other variable name

--- packages/design-system/src/components/Dropdown/Dropdown.tsx
+++ packages/design-system/src/components/Dropdown/Dropdown.tsx
@@ -253,7 +253,7 @@ export const Dropdown: React.FC<DropdownProps> = (props: DropdownProps) => {
   const caretIcon = (
     <path d="M212.7 148.7c6.2-6.2 16.4-6.2 22.6 0l160 160c6.2 6.2 6.2 16.4 0 22.6s-16.4 6.2-22.6 0L224 182.6 75.3 331.3c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l160-160z" />
   );
-  const checkIcon = (
+  const selectedIcon = (
     <path d="M443.3 100.7c6.2 6.2 6.2 16.4 0 22.6l-272 272c-6.2 6.2-16.4 6.2-22.6 0l-144-144c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L160 361.4l260.7-260.7c6.2-6.2 16.4-6.2 22.6 0z" />
   );
 
@@ -285,7 +285,7 @@ export const Dropdown: React.FC<DropdownProps> = (props: DropdownProps) => {
               className="ds-u-font-size--sm"
               id={selectedIconId}
             >
-              {checkIcon}
+              {selectedIcon}
             </SvgIcon>
           </span>
         )}
