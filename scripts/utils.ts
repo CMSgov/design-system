659ae6e51e85c8fe03d46c385b5b6677eed344ba
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-09-18 10:12:49 -0700

    [NO-TICKET] Avoid tag errors in the release notes script (#2677)
    * Verify that gh is installed first
    
    * Use a more reliable way of fetching the latest tag
    
    We tend to get errors because of missing object data when using the old command. Some more info here: https://gist.github.com/rponte/fdc0724dd984088606b0?permalink_comment_id=4464812
    
    * Refactor notes to use shared code and more reliable tag fetching

---
+++ scripts/utils.ts
@@ -0,0 +1,33 @@
+import c from 'chalk';
+import { execSync, spawnSync } from 'node:child_process';
+
+/**
+ * Execute a shell command and wait for the response. Note that this does not
+ * work for interactive commands. For that, use shI.
+ */
+export function sh(command: string, hideOutput?: boolean): string {
+  const options = hideOutput ? { stdio: 'pipe' as const } : {};
+  return execSync(command, options).toString().trim();
+}
+
+/**
+ * Uses node's `spawnSync` method to spawn an interactive process, which means
+ * you must pass all args as an array instead of including them in the command
+ * string.
+ */
+export function shI(command: string, args: string[]) {
+  spawnSync(command, args, { stdio: 'inherit' });
+}
+
+export function verifyGhInstalled() {
+  try {
+    sh('gh --version');
+  } catch (error) {
+    console.log(
+      `Please check to make sure you have the ${c.green(
+        'gh'
+      )} tool installed (https://cli.github.com)`
+    );
+    process.exit(1);
+  }
+}
