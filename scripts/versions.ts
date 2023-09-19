32f9c559ca1fb7d941bdcefdfb67f208bd724b05
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-09-19 16:26:49 -0700

    Only cull betas when they are older than the current major version

--- scripts/versions.ts
+++ scripts/versions.ts
@@ -8,17 +8,22 @@ const versionsFileName = path.join(root, 'versions.json');
 function readJson(filename: string): { [key: string]: any } {
   return JSON.parse(readFileSync(filename, { encoding: 'utf8' }));
 }
+
 function writeJson(filename: string, json: { [key: string]: any }) {
   writeFileSync(filename, JSON.stringify(json, null, 2));
+}
+
+function getPackageVersion(packageName: string): string {
+  const packageFileName = path.join(root, 'packages', packageName, 'package.json');
+  const packageData = readJson(packageFileName);
+  return packageData.version;
 }
 
 export function appendVersions() {
   const versions = readJson(versionsFileName);
 
   for (const theme of Object.values(themes)) {
-    const packageFileName = path.join(root, 'packages', theme.packageName, 'package.json');
-    const packageData = readJson(packageFileName);
-    const currentVersion = packageData.version;
+    const currentVersion = getPackageVersion(theme.packageName);
     versions[theme.packageName].unshift(currentVersion);
   }
 
@@ -28,9 +33,16 @@ export function appendVersions() {
 export function cullBetaVersions() {
   const versions = readJson(versionsFileName);
 
-  for (const packageName of Object.keys(versions)) {
-    versions[packageName] = versions[packageName].filter(
-      (version: string) => !version.includes('beta')
+  for (const theme of Object.values(themes)) {
+    const currentVersion = getPackageVersion(theme.packageName);
+
+    const isOldBeta = (version: string) =>
+      version.includes('beta') &&
+      // We only care about the first digit in the version string (the major version)
+      parseInt(version) < parseInt(currentVersion);
+
+    versions[theme.packageName] = versions[theme.packageName].filter(
+      (version: string) => !isOldBeta(version)
     );
   }
 
