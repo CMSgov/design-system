989c5563b41ba6223a4aec6cfa783597607ccfe8
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-10-04 13:00:13 -0700

    I can't get this globbing to work anyway. See all previous related commit messages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
-const usePreact = Boolean(process.env.PREACT && JSON.parse(process.env.PREACT));
-
 const extensionGlob = '*.stories.@(js|jsx|ts|tsx|mdx)';
-const directoryGlob = usePreact ? '**' : '!(web-components)/**';
 
 const config: StorybookConfig = {
   addons: [
@@ -20,17 +17,10 @@ const config: StorybookConfig = {
     <link rel="stylesheet" type="text/css" title="themeCss" href="core-theme.css" />
   `,
   stories: [
-    `../packages/design-system/src/${directoryGlob}/${extensionGlob}`,
-    `../packages/ds-healthcare-gov/src/${directoryGlob}/${extensionGlob}`,
-    `../packages/ds-medicare-gov/src/${directoryGlob}/${extensionGlob}`,
+    `../packages/design-system/src/**/${extensionGlob}`,
+    `../packages/ds-healthcare-gov/src/**/${extensionGlob}`,
+    `../packages/ds-medicare-gov/src/**/${extensionGlob}`,
     `../packages/docs/content/**/${extensionGlob}`,
-    // ...(usePreact
-    //   ? []
-    //   : [
-    //       `!../packages/design-system/src/components/web-components/*`,
-    //       `!../packages/ds-healthcare-gov/src/components/web-components/*`,
-    //       `!../packages/ds-medicare-gov/src/components/web-components/*`,
-    //     ]),
   ],
   staticDirs: ['./static/'],
   framework: {
