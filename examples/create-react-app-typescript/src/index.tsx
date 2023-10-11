07ecee84e431087b5704bd2e4b348943ec141412
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-10-11 15:23:14 -0700

    Update the TS example so it's reporting errors
    And I'm not sure what the react root error is about, but I'm silencing it for now because it's noise.

--- examples/create-react-app-typescript/src/index.tsx
+++ examples/create-react-app-typescript/src/index.tsx
@@ -3,11 +3,13 @@
 import './styles/index.scss';
 import App from './components/App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ) as any
 );
