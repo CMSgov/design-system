f0c29bd0b798fb0a82ea110127214beed33b6285
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-10-23 11:25:02 -0700

    Fix the dropdown interaction tests by adding some wait time 🤷

--- packages/design-system/src/components/Dropdown/Dropdown.test.interaction.ts
+++ packages/design-system/src/components/Dropdown/Dropdown.test.interaction.ts
@@ -10,6 +10,8 @@ Object.keys(themes).forEach((theme) => {
   test(`Dropdown open: ${theme}`, async ({ page }) => {
     await page.goto(storyUrl(theme, 'components-dropdown--default'));
     await page.getByRole('button').click();
+    // Cannot figure out an alternative to this wait time
+    await page.waitForTimeout(200);
     await page.getByRole('listbox').waitFor();
     await page.keyboard.press('ArrowDown');
     await expect(page).toHaveScreenshot(`dropdown--open--${theme}.png`, { fullPage: true });
@@ -18,6 +20,8 @@ Object.keys(themes).forEach((theme) => {
   test(`Dropdown open with option groups: ${theme}`, async ({ page }) => {
     await page.goto(storyUrl(theme, 'components-dropdown--option-groups'));
     await page.getByRole('button').click();
+    // Cannot figure out an alternative to this wait time
+    await page.waitForTimeout(200);
     await page.getByRole('listbox').waitFor();
     await page.keyboard.press('ArrowDown');
     await expect(page).toHaveScreenshot(`dropdown-optgroups--open--${theme}.png`, {
