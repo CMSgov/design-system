8a5ea3e0081854138462eb3397c3fe9ab6a500ef
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-10-26 17:01:16 -0700

    Move line height and font weight settings to the theme level
    Medicare already modifies the weights, and these seem pretty specific to the fonts being used. Line height values are already semantic. We want semantic on the theme level and non-semantic at the system (crayon) level.

--- packages/design-system-tokens/src/tokens/font.ts
+++ packages/design-system-tokens/src/tokens/font.ts
@@ -8,10 +8,6 @@ const font = to<FontTokens>()({
   'family-rubik': "Rubik, Seravek, 'Gill Sans Nova', Ubuntu, Calibri, 'DejaVu Sans', source-sans-pro, sans-serif",
   'family-montserrat': "Montserrat, Avenir, Corbel, 'URW Gothic', source-sans-pro, sans-serif",
   'family-bitter': "Bitter, Superclarendon, 'Bookman Old Style', 'URW Bookman', 'URW Bookman L', 'Georgia Pro', Georgia, serif",
-  'line-height-reset': 1,
-  'line-height-heading': 1.3,
-  'line-height-base': 1.5,
-  'line-height-lead': 1.7,
   'size-base': '1rem',
   'size-sm': '.875rem',
   'size-md': '1rem',
@@ -21,10 +17,6 @@ const font = to<FontTokens>()({
   'size-3xl': '2.25rem',
   'size-4xl': '3rem',
   'size-5xl': '3.75rem',
-  'weight-light': 300,
-  'weight-normal': 400,
-  'weight-semibold': 600,
-  'weight-bold': 700,
 });
 
 export default font;
