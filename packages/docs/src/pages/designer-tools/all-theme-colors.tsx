1fa3d04262b88beb4149f8ad70f98fc97e68138d
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2023-11-08 13:38:32 -0800

    Ask search engines not to index the designer tools
    They're not really for consumers of the design system, and the pages themselves don't meet color constrast requirements

--- packages/docs/src/pages/designer-tools/all-theme-colors.tsx
+++ packages/docs/src/pages/designer-tools/all-theme-colors.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../../components/layout/Layout';
+import { Helmet } from 'react-helmet';
 import { MdxQuery } from '../../helpers/graphQLTypes';
 import AllThemesColorTable from '../../components/designer-tools/AllThemesColorTable';
 
@@ -15,7 +16,12 @@ const DesignerToolThemeColorsPage = ({ location }: MdxQuery) => {
       theme={theme}
       fullWidth
     >
-      <AllThemesColorTable />
+      <>
+        <Helmet>
+          <meta name="robots" content="noindex, nofollow" />
+        </Helmet>
+        <AllThemesColorTable />
+      </>
     </Layout>
   );
 };
