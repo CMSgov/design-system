1454db8282d95e339bc15c852ee1bc497f90b504
Sarah Sachs <zarahzachz@users.noreply.github.com>
2023-12-18 16:53:43 -0500

    implement basic layout for redesign - sidenav, page header, footer.

--- packages/docs/src/components/layout/DocSiteFooter.tsx
+++ packages/docs/src/components/layout/DocSiteFooter.tsx
@@ -1,60 +1,49 @@
 import React from 'react';
-import { useState } from 'react';
-import { HHSLogo, PrivacySettingsDialog } from '@cmsgov/design-system';
 
 const DocSiteFooter = () => {
-  const [showDialog, setShowDialog] = useState(false);
-  const openDialog = () => setShowDialog(true);
-  const closeDialog = () => setShowDialog(false);
-
   return (
-    <footer className="c-footer ds-u-padding-x--3 ds-u-sm-padding-x--6">
-      <section className="c-footer__container">
-        <div className="c-footer__links">
-          <a
-            className="c-footer__link"
-            href="http://www.medicare.gov/about-us/plain-writing/plain-writing.html"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Plain Writing
-          </a>
-          <button className="c-footer__link" onClick={openDialog}>
-            Privacy settings
-          </button>
-          {showDialog && (
-            <PrivacySettingsDialog
-              onExit={closeDialog}
-              domain="CMS.gov"
-              privacyPolicyUrl="https://www.cms.gov/privacy"
-              thirdPartyPoliciesUrl="https://www.cms.gov/privacy#h57sjsyz3r1jqc1oje1naete3bvoi6g"
-            />
-          )}
-          <a
-            className="c-footer__link"
-            href="https://cms.gov/privacy/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Privacy Policy
-          </a>
-          <a
-            className="c-footer__link"
-            href="https://designsystem.digital.gov/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            U.S. Web Design System
-          </a>
-        </div>
-        <div className="ds-u-padding-top--2 ds-u-display--flex ds-u-align-items--center">
-          <HHSLogo />
-          <p className="ds-u-margin--0 ds-u-measure--wide c-footer__text">
+    <footer className="c-footer">
+      <div className="ds-l-container">
+        <ul role="list" className="c-footer__links">
+          <li className="c-footer__link">
+            <a href="/contact">Contact us</a>
+          </li>
+          <li className="c-footer__link">
+            <a href="">What&apos;s new</a>
+          </li>
+          <li className="c-footer__link">
+            <a
+              href="https://github.com/CMSgov/design-system/releases"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Release notes
+            </a>
+          </li>
+          <li className="c-footer__link">
+            <a
+              href="http://www.medicare.gov/about-us/plain-writing/plain-writing.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Plain writing
+            </a>
+          </li>
+          <li className="c-footer__link">
+            <a href="https://cms.gov/privacy/" target="_blank" rel="noreferrer">
+              Privacy policy
+            </a>
+          </li>
+        </ul>
+
+        <div className="c-footer__info">
+          {/* need new design.cms.gov logo */}
+          <p>
             A federal government website managed by the Centers for Medicare &amp; Medicaid Services
             7500 Security Boulevard, Baltimore, MD 21124
           </p>
         </div>
-      </section>
+      </div>
     </footer>
   );
 };
