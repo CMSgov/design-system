f729660143252951d652cc440cbdc8eeaa8dc242
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-01-05 15:13:31 -0800

    [NO-TICKET] Update the `create-react-app-typescript` example project (#2868)
    Update the `create-react-app-typescript` example project
    
    Co-authored-by: Sarah <zarahzachz@users.noreply.github.com>

--- examples/create-react-app-typescript/src/components/Examples/MultiInputDateFieldExample.tsx
+++ examples/create-react-app-typescript/src/components/Examples/MultiInputDateFieldExample.tsx
@@ -1,11 +1,11 @@
-import { DateField } from '@cmsgov/design-system';
+import { MultiInputDateField } from '@cmsgov/design-system';
 import React from 'react';
 
-function DateFieldExample() {
+function MultiInputDateFieldExample() {
   return (
     <div>
-      <h2>DateField Example</h2>
-      <DateField
+      <h2>MultiInputDateField Example</h2>
+      <MultiInputDateField
         label="Date of birth"
         errorMessage="Please enter a year in the past"
         monthDefaultValue="10"
@@ -17,4 +17,4 @@ function DateFieldExample() {
   );
 }
 
-export default DateFieldExample;
+export default MultiInputDateFieldExample;
