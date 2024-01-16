1c8b68511299ecca4172ddac2798a0c64c24e838
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-01-16 11:42:42 -0800

    Add some doc comments to the config

--- packages/design-system/src/components/config.ts
+++ packages/design-system/src/components/config.ts
@@ -1,14 +1,47 @@
+import { AnalyticsFunction, sendLinkEvent } from './analytics';
+
 export enum ErrorPlacement {
   Top = 'top',
   Bottom = 'bottom',
 }
 
 export interface Config {
+  /**
+   * Sets the default error placement for form-field components, which is the placement
+   * of the error message relative to the field input. Accepts string values of 'top' or
+   * 'bottom'.
+   */
   errorPlacementDefault: ErrorPlacement;
+  /**
+   * Changing this setting allows applications to override the default `onAnalyticsEvent`
+   * handler function for all analytics-enabled components. To override it for a single
+   * component instance, use the `onAnalyticsEvent` prop instead.
+   */
+  defaultAnalyticsFunction: AnalyticsFunction;
+  /**
+   * Controls whether alert components send analytics data by default. To override this
+   * setting for an individual alert instance, use the `analytics` prop.
+   */
   alertSendsAnalytics: boolean;
+  /**
+   * Controls whether button components send analytics data by default. To override this
+   * setting for an individual button instance, use the `analytics` prop.
+   */
   buttonSendsAnalytics: boolean;
+  /**
+   * Controls whether dialog components send analytics data by default. To override this
+   * setting for an individual dialog instance, use the `analytics` prop.
+   */
   dialogSendsAnalytics: boolean;
+  /**
+   * Controls whether help drawer components send analytics data by default. To override this
+   * setting for an individual help drawer instance, use the `analytics` prop.
+   */
   helpDrawerSendsAnalytics: boolean;
+  /**
+   * Controls whether the header component send analytics data by default. Alternatively,
+   * this can be set on the header instance itself using the `analytics` prop.
+   */
   headerSendsAnalytics: boolean;
 }
 
@@ -16,6 +49,7 @@ export type PartialConfig = Partial<Config>;
 
 export const DEFAULTS: Config = Object.freeze({
   errorPlacementDefault: ErrorPlacement.Top,
+  defaultAnalyticsFunction: sendLinkEvent,
   alertSendsAnalytics: false,
   buttonSendsAnalytics: false,
   dialogSendsAnalytics: false,
