9b6c904e82744877516b08447a3ee35cf2b08969
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-01-17 15:21:38 -0800

    Leverage TS a bit more in the finite state machine

--- packages/design-system/src/components/Dialog/useBodyScrollPrevention.ts
+++ packages/design-system/src/components/Dialog/useBodyScrollPrevention.ts
@@ -3,42 +3,44 @@ import { Reducer, useReducer, useLayoutEffect } from 'react';
 const CLASS_NAME = 'ds--dialog-open';
 const PROPERTY_NAME = '--body_top--dialog-open';
 
-type FiniteStates =
-  | {
-      name: 'open';
-      bodyScrollY: number;
-    }
-  | {
-      name: 'closed';
-    };
+interface OpenState {
+  name: 'open';
+  bodyScrollY: number;
+}
 
-type FiniteStateTransitions = 'open' | 'close';
+interface ClosedState {
+  name: 'closed';
+}
 
-const reducer: Reducer<FiniteStates, FiniteStateTransitions> = (state, transition) => {
-  switch (state.name) {
-    case 'closed':
-      if (transition === 'open') {
-        // https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
-        const y = window.scrollY ?? 0;
-        document.body.classList.add(CLASS_NAME);
-        document.body.style.setProperty(PROPERTY_NAME, `-${y}px`);
-        document.documentElement.style.setProperty('scroll-behavior', 'auto');
-        return {
-          name: 'open',
-          bodyScrollY: y,
-        };
-      }
-      break;
-    case 'open':
-      if (transition === 'close') {
-        document.body.classList.remove(CLASS_NAME);
-        window.scrollTo({ top: state.bodyScrollY, behavior: 'auto' });
-        document.documentElement.style.removeProperty('scroll-behavior');
-        return {
-          name: 'closed',
-        };
-      }
-      break;
+const OPEN = (_state: ClosedState): OpenState => {
+  // https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
+  const y = window.scrollY ?? 0;
+  document.body.classList.add(CLASS_NAME);
+  document.body.style.setProperty(PROPERTY_NAME, `-${y}px`);
+  document.documentElement.style.setProperty('scroll-behavior', 'auto');
+  return {
+    name: 'open',
+    bodyScrollY: y,
+  };
+};
+
+const CLOSE = (state: OpenState): ClosedState => {
+  document.body.classList.remove(CLASS_NAME);
+  window.scrollTo({ top: state.bodyScrollY, behavior: 'auto' });
+  document.documentElement.style.removeProperty('scroll-behavior');
+  return {
+    name: 'closed',
+  };
+};
+
+type FiniteState = OpenState | ClosedState;
+type FiniteStateTransition = typeof OPEN | typeof CLOSE;
+
+const reducer: Reducer<FiniteState, FiniteStateTransition> = (state, transition) => {
+  if (state.name === 'open' && transition === CLOSE) {
+    return transition(state);
+  } else if (state.name === 'closed' && transition === OPEN) {
+    return transition(state);
   }
   return state;
 };
@@ -51,6 +53,6 @@ export function useBodyScrollPrevention(isOpen: boolean) {
   // Needs to use useLayoutEffect because we need to grab the window scroll position
   // before the dialog renders and messes it up.
   useLayoutEffect(() => {
-    dispatch(isOpen ? 'open' : 'close');
+    dispatch(isOpen ? OPEN : CLOSE);
   }, [isOpen]);
 }
