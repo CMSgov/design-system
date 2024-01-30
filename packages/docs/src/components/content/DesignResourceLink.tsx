288504419784eaf8fce04fe8f96135d598a96218
Sarah Sachs <zarahzachz@users.noreply.github.com>
2024-01-30 11:37:42 -0500

    add fallback for themes without sketch design links (cmsgov).

--- packages/docs/src/components/content/DesignResourceLink.tsx
+++ packages/docs/src/components/content/DesignResourceLink.tsx
@@ -17,15 +17,19 @@ const DesignResourceLink = ({ theme, frontmatter }: DesignResourceLinkProps) => 
 
   return (
     <p>
-      <a href={makeSketchUrl(sketchId, theme)} className="ds-u-display--flex">
-        <img
-          alt=""
-          src={withPrefix('/images/sketch-icon.png')}
-          className="ds-u-display--inline ds-u-margin-right--1"
-          style={{ height: '1.25em' }}
-        />
-        Sketch symbol
-      </a>
+      {sketchId ? (
+        <a href={makeSketchUrl(sketchId, theme)} className="ds-u-display--flex">
+          <img
+            alt=""
+            src={withPrefix('/images/sketch-icon.png')}
+            className="ds-u-display--inline ds-u-margin-right--1"
+            style={{ height: '1.25em' }}
+          />
+          Sketch symbol
+        </a>
+      ) : (
+        'This theme does not have a Sketch symbol.'
+      )}
     </p>
   );
 };
