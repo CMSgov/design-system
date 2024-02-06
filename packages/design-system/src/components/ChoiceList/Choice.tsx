CMSgov/design-system

commit 19e40863244d6a27fd7815151f86a5e2237824d8

[NO-TICKET] Make Choice a function component and clean up logging (#2893)
* Clean out a really old debug log

* Fix CloseButton internal className getting overwritten

* Convert Choice to a functional component so it can use new label hooks

A side-effect that I'm okay with is that now all the checkbox labels have generated ids

* Didn't need this, and it was causing error logs to be printed

* Silence the caniuse-lite warnings by updating it

For most of our commands, it was printing the following warning:

```
Browserslist: caniuse-lite is outdated. Please run:
  npx update-browserslist-db@latest
  Why you should do it regularly: https://github.com/browserslist/update-db#readme
```

Updated it so it'd stop.

* Update Choice storybook docs snapshots

* Don't actually want to advertize `errorPlacement` as an option for this component

It's meaningless.

* Revert caniuse-lite update in case it's affecting Jenkins

This reverts commit 4211c5be1a83e559cb13cef185f608f566409547.

* Revert "Revert caniuse-lite update in case it's affecting Jenkins"

This reverts commit 6e1fd523109512b269994fba9769af2942bb6922.

* Update comment

packages/design-system/src/components/ChoiceList/Choice.tsx
@@ -1,16 +1,24 @@
+import React, { useEffect, useState } from 'react';
 import EvEmitter from 'ev-emitter';
-import { Label } from '../Label';
-import React from 'react';
 import classNames from 'classnames';
-import uniqueId from 'lodash/uniqueId';
-import mergeIds from '../utilities/mergeIds';
-import { InlineError } from '../InlineError';
+import useId from '../utilities/useId';
+import { Label } from '../Label';
+import { UseInlineErrorProps, useInlineError } from '../InlineError/useInlineError';
+import { UseLabelPropsProps, useLabelProps } from '../Label/useLabelProps';
+import { UseHintProps, useHint } from '../Hint/useHint';
+import cleanFieldProps from '../utilities/cleanFieldProps';
+import describeField from '../utilities/describeField';
 
 export type ChoiceSize = 'small';
 export type ChoiceType = 'checkbox' | 'radio';
 export type ChoiceValue = number | string;
 
-export interface ChoiceProps {
+export interface BaseChoiceProps {
+  /**
+   * Sets the initial `checked` state. Use this for an uncontrolled component;
+   * otherwise, use the `checked` property.
+   */
+  defaultChecked?: boolean;
   /**
    * Sets the input's `checked` state. Use this in combination with `onChange`
    * for a controlled component; otherwise, set `defaultChecked`.
@@ -27,6 +35,10 @@ export interface ChoiceProps {
    */
   uncheckedChildren?: React.ReactNode;
   /**
+   * Disables the entire field.
+   */
+  disabled?: boolean;
+  /**
    * Additional classes to be added to the root `div` element.
    */
   className?: string;
@@ -35,41 +47,14 @@ export interface ChoiceProps {
    */
   inputClassName?: string;
   /**
-   * Label text or HTML.
-   */
-  label?: React.ReactNode;
-  /**
-   * Additional classes to be added to the `FormLabel`.
-   */
-  labelClassName?: string;
-  /**
-   * Sets the initial `checked` state. Use this for an uncontrolled component;
-   * otherwise, use the `checked` property.
-   */
-  defaultChecked?: boolean;
-  disabled?: boolean;
-  errorMessage?: React.ReactNode;
-  /**
-   * Additional classes to be added to the error message
-   */
-  errorMessageClassName?: string;
-  /**
    * Access a reference to the `input` element
    */
   inputRef?: (...args: any[]) => any;
-  /**
-   * Additional hint text to display below the choice's label
-   */
-  hint?: React.ReactNode;
   /**
    * A unique ID to be used for the input field, as well as the label's
    * `for` attribute. A unique ID will be generated if one isn't provided.
    */
   id?: string;
-  /**
-   * Text showing the requirement ("Required", "Optional", etc.). See [Required and Optional Fields](https://design.cms.gov/patterns/Forms/forms/#required-and-optional-fields).
-   */
-  requirementLabel?: React.ReactNode;
   /**
    * Applies the "inverse" UI theme
    */
@@ -91,19 +76,12 @@ export interface ChoiceProps {
   value: ChoiceValue;
 }
 
-type OmitProps =
-  | 'size'
-  | 'type'
-  | 'value'
-  | 'label'
-  | 'checked'
-  | 'defaultChecked'
-  | 'onBlur'
-  | 'onChange'
-  | 'name'
-  | 'id'
-  | 'className'
-  | 'disabled';
+export type ChoiceProps = BaseChoiceProps &
+  Omit<React.ComponentPropsWithRef<'input'>, keyof BaseChoiceProps> &
+  Omit<
+    UseLabelPropsProps & UseHintProps & UseInlineErrorProps,
+    'id' | 'inversed' | 'errorPlacement'
+  >;
 
 /** Used to emit events to all Choice components */
 const dsChoiceEmitter = new EvEmitter();
@@ -118,172 +96,95 @@ const dsChoiceEmitter = new EvEmitter();
  * [checkbox](https://design.cms.gov/components/checkbox/) and
  * [radio](https://design.cms.gov/components/radio/) documentation pages.
  */
-export class Choice extends React.PureComponent<
-  Omit<React.ComponentPropsWithRef<'input'>, OmitProps> & ChoiceProps,
-  any
-> {
-  constructor(props: ChoiceProps) {
-    super(props);
-    this.input = null;
-    this.handleChange = this.handleChange.bind(this);
-    this.handleUncheck = this.handleUncheck.bind(this);
-    this.id = this.props.id ?? uniqueId('choice--');
-    this.hintId = this.props.hint || this.props.requirementLabel ? `${this.id}__hint` : undefined;
-    this.errorId = this.props.errorMessage ? `${this.id}__error` : undefined;
+export const Choice = (props: ChoiceProps) => {
+  const initialCheckedState = props.checked ?? props.defaultChecked;
+  const [internalCheckedState, setChecked] = useState(initialCheckedState);
+  const isControlled = props.checked !== undefined;
+  const checked = isControlled ? props.checked : internalCheckedState;
+  const radioCheckedEventName = `${props.name}-radio-checked`;
 
-    if (typeof this.props.checked === 'undefined') {
-      this.isControlled = false;
-      // Since this isn't a controlled component, we need a way
-      // to track when the value has changed. This can then be used
-      // to identify when to toggle the visibility of (un)checkedChildren
-      this.state = { checked: this.props.defaultChecked };
-    } else {
-      this.isControlled = true;
-    }
-  }
+  const id = useId('choice--', props.id);
 
-  componentDidMount(): void {
-    // Event emitters are only relevant for uncontrolled radio buttons
-    if (!this.isControlled && this.props.type === 'radio') {
-      this.uncheckEventName = `${this.props.name}-uncheck`;
-      dsChoiceEmitter.on(this.uncheckEventName, this.handleUncheck);
-    }
-  }
+  const { errorId, topError, bottomError } = useInlineError({ ...props, id });
+  const { hintId, hintElement } = useHint({ ...props, id });
+  const labelProps = useLabelProps({ ...props, id });
 
-  componentWillUnmount(): void {
-    // Unbind event emitters are only relevant for uncontrolled radio buttons
-    if (!this.isControlled && this.props.type === 'radio') {
-      dsChoiceEmitter.off(this.uncheckEventName, this.handleUncheck);
-    }
-  }
-
-  input: any;
-  id: string;
-  hintId?: string;
-  errorId?: string;
-  isControlled: boolean;
-  uncheckEventName: string;
-
-  checked(): boolean {
-    if (this.isControlled) {
-      return this.props.checked;
+  // Subscribe to changes from other radio buttons in the same group
+  useEffect(() => {
+    // This logic only applies to uncontrolled radio groups
+    if (props.type !== 'radio' || isControlled) {
+      return;
     }
 
-    return this.state.checked;
-  }
+    const handleRadioChecked = (checkedId: string) => {
+      // A radio button in this group was just checked. If it wasn't this one, uncheck this one
+      if (checkedId !== id) {
+        setChecked(false);
+      }
+    };
 
-  /**
-   * A radio button doesn't receive an onChange event when it is unchecked,
-   * so we fire an "uncheck" event when any radio option is selected. This
-   * allows us to check each radio options' checked state.
-   * @param {String} checkedId - ID of the checked radio option
-   */
-  handleUncheck(checkedId: string): void {
-    if (checkedId !== this.id && this.input.checked !== this.state.checked) {
-      this.setState({ checked: this.input.checked });
-    }
-  }
+    dsChoiceEmitter.on(radioCheckedEventName, handleRadioChecked);
+    return () => {
+      dsChoiceEmitter.off(radioCheckedEventName, handleRadioChecked);
+    };
+  }, [setChecked]);
 
-  handleChange(evt: React.ChangeEvent<HTMLInputElement>): void {
-    if (this.props.onChange) {
-      this.props.onChange(evt);
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    if (props.onChange) {
+      props.onChange(event);
     }
 
-    if (!this.isControlled) {
-      this.setState({ checked: evt.target.checked });
+    if (!isControlled) {
+      setChecked(event.target.checked);
 
-      if (this.props.type === 'radio' && evt.target.checked) {
-        // Emit the uncheck event so other radio options update their state
-        dsChoiceEmitter.emitEvent(this.uncheckEventName, [this.id]);
+      if (props.type === 'radio' && event.target.checked) {
+        // Emit an event so other radio options can uncheck themselves
+        dsChoiceEmitter.emitEvent(radioCheckedEventName, [id]);
       }
     }
   }
 
-  render() {
-    const {
-      'aria-live': ariaLive,
-      'aria-relevant': ariaRelevant,
-      'aria-atomic': ariaAtomic,
-      checkedChildren,
-      className,
-      disabled,
-      errorMessage,
-      errorMessageClassName,
-      hint,
-      inversed,
-      inputClassName,
-      label,
-      labelClassName,
-      requirementLabel,
-      size,
-      uncheckedChildren,
-      inputRef,
-      ...inputProps
-    } = this.props;
+  const {
+    'aria-live': ariaLive,
+    'aria-relevant': ariaRelevant,
+    'aria-atomic': ariaAtomic,
+    className,
+    inversed,
+    inputClassName,
+    inputRef,
+    size,
+    checkedChildren,
+    uncheckedChildren,
+    ...inputProps
+  } = props;
 
-    const inputClasses = classNames(inputClassName, 'ds-c-choice', {
-      'ds-c-choice--inverse': inversed,
-      'ds-c-choice--small': size === 'small',
-    });
-
-    let errorElement;
-    if (errorMessage) {
-      errorElement = (
-        <InlineError id={this.errorId} inversed={inversed} className={errorMessageClassName}>
-          {errorMessage}
-        </InlineError>
-      );
-    }
-
-    // Remove props we have our own implementations for
-    if (inputProps.id) delete inputProps.id;
-    if (inputProps.onChange) delete inputProps.onChange;
-
-    // All the fields that have multiple inputs need to pass down the root
-    // aria-describedby attribute (where the root hint and error messages are
-    // linked) down to the individual inputs
-    inputProps['aria-describedby'] =
-      mergeIds(this.errorId, this.hintId, inputProps['aria-describedby']) || undefined;
-
-    return (
-      <div
-        className={className}
-        aria-live={ariaLive ?? (checkedChildren ? 'polite' : null)}
-        aria-relevant={ariaRelevant ?? (checkedChildren ? 'additions text' : null)}
-        aria-atomic={ariaAtomic ?? (checkedChildren ? 'false' : null)}
-      >
-        <div className="ds-c-choice-wrapper">
-          <input
-            className={inputClasses}
-            id={this.id}
-            onChange={this.handleChange}
-            disabled={disabled}
-            ref={(ref) => {
-              this.input = ref;
-              if (inputRef) {
-                inputRef(ref);
-              }
-            }}
-            {...inputProps}
-          />
-          <Label
-            className={labelClassName}
-            fieldId={this.id}
-            hintId={this.hintId}
-            {...{
-              errorMessage: errorElement,
-              hint,
-              inversed,
-              requirementLabel,
-            }}
-          >
-            {label}
-          </Label>
-        </div>
-        {this.checked() ? checkedChildren : uncheckedChildren}
+  return (
+    <div
+      className={className}
+      aria-live={ariaLive ?? (checkedChildren ? 'polite' : null)}
+      aria-relevant={ariaRelevant ?? (checkedChildren ? 'additions text' : null)}
+      aria-atomic={ariaAtomic ?? (checkedChildren ? 'false' : null)}
+    >
+      <div className="ds-c-choice-wrapper">
+        <input
+          {...cleanFieldProps(inputProps)}
+          id={id}
+          className={classNames(inputClassName, 'ds-c-choice', {
+            'ds-c-choice--inverse': inversed,
+            'ds-c-choice--small': size === 'small',
+          })}
+          onChange={handleChange}
+          ref={inputRef}
+          aria-describedby={describeField({ ...props, errorId, hintId })}
+        />
+        <Label {...labelProps} fieldId={id} />
+        {hintElement}
+        {topError}
+        {bottomError}
       </div>
-    );
-  }
-}
+      {checked ? checkedChildren : uncheckedChildren}
+    </div>
+  );
+};
 
 export default Choice;

packages/design-system/src/components/ChoiceList/Choice.test.tsx
@@ -199,6 +199,62 @@ describe('Choice', () => {
     });
   });
 
+  describe('radio groups', () => {
+    it('uncontrolled radios uncheck when sibling checked', () => {
+      const commonProps = {
+        name: 'foo',
+        type: 'radio' as const,
+      };
+
+      render(
+        <>
+          <Choice {...commonProps} label="A" value="a" id="a" defaultChecked />
+          <Choice {...commonProps} label="B" value="b" id="b" />
+          <Choice {...commonProps} label="C" value="c" id="c" />
+        </>
+      );
+
+      const getRadio = (label: string) => screen.getByLabelText(label) as HTMLInputElement;
+
+      expect(getRadio('A').checked).toBe(true);
+      expect(getRadio('B').checked).toBe(false);
+      expect(getRadio('C').checked).toBe(false);
+
+      userEvent.click(getRadio('B'));
+
+      expect(getRadio('A').checked).toBe(false);
+      expect(getRadio('B').checked).toBe(true);
+      expect(getRadio('C').checked).toBe(false);
+    });
+
+    it('controlled radios do not uncheck when sibling checked', () => {
+      const commonProps = {
+        name: 'foo',
+        type: 'radio' as const,
+      };
+
+      render(
+        <>
+          <Choice {...commonProps} label="A" value="a" id="a" checked />
+          <Choice {...commonProps} label="B" value="b" id="b" checked={false} />
+          <Choice {...commonProps} label="C" value="c" id="c" checked={false} />
+        </>
+      );
+
+      const getRadio = (label: string) => screen.getByLabelText(label) as HTMLInputElement;
+
+      expect(getRadio('A').checked).toBe(true);
+      expect(getRadio('B').checked).toBe(false);
+      expect(getRadio('C').checked).toBe(false);
+
+      userEvent.click(getRadio('B'));
+
+      expect(getRadio('A').checked).toBe(true);
+      expect(getRadio('B').checked).toBe(false);
+      expect(getRadio('C').checked).toBe(false);
+    });
+  });
+
   describe('nested content', () => {
     const props = {
       checkedChildren: (

packages/design-system/src/components/ChoiceList/__snapshots__/Choice.test.tsx.snap
@@ -16,6 +16,7 @@ exports[`Choice accepts a node as innerHTML 1`] = `
       <label
         class="ds-c-label"
         for="static-id"
+        id="static-id__label"
       >
         <p>
           <strong>
@@ -46,6 +47,7 @@ exports[`Choice applies errorMessage to label 1`] = `
       <label
         class="ds-c-label"
         for="static-id"
+        id="static-id__label"
       >
         George Washington
       </label>
@@ -94,6 +96,7 @@ exports[`Choice has a hint and requirementLabel 1`] = `
       <label
         class="ds-c-label"
         for="static-id"
+        id="static-id__label"
       >
         George Washington
       </label>
@@ -124,6 +127,7 @@ exports[`Choice is a checkbox 1`] = `
       <label
         class="ds-c-label"
         for="static-id"
+        id="static-id__label"
       >
         George Washington
       </label>
@@ -148,6 +152,7 @@ exports[`Choice is a radio 1`] = `
       <label
         class="ds-c-label"
         for="static-id"
+        id="static-id__label"
       >
         George Washington
       </label>
@@ -176,6 +181,7 @@ exports[`Choice nested content renders \`checkedChildren\` when checked 1`] = `
       <label
         class="ds-c-label"
         for="static-id"
+        id="static-id__label"
       >
         George Washington
       </label>
@@ -210,6 +216,7 @@ exports[`Choice nested content renders \`uncheckedChildren\` when not checked 1`
       <label
         class="ds-c-label"
         for="static-id"
+        id="static-id__label"
       >
         George Washington
       </label>

packages/design-system/src/components/ChoiceList/__snapshots__/ChoiceList.test.tsx.snap
@@ -56,6 +56,7 @@ exports[`ChoiceList Radio buttons and Checkboxes is a radio button group 1`] = `
         <label
           class="ds-c-label"
           for="static-id__choice--0"
+          id="static-id__choice--0__label"
         >
           Choice 1
         </label>
@@ -75,6 +76,7 @@ exports[`ChoiceList Radio buttons and Checkboxes is a radio button group 1`] = `
         <label
           class="ds-c-label"
           for="static-id__choice--1"
+          id="static-id__choice--1__label"
         >
           Choice 2
         </label>

packages/design-system/src/components/CloseButton/CloseButton.tsx
@@ -31,10 +31,10 @@ export const CloseButton = ({ className, id: idProp, ...buttonAttributes }: Clos
   const id = useId('close-button--', idProp);
   return (
     <button
-      className={classNames('ds-c-close-button', className)}
       type="button"
-      id={id}
       {...buttonAttributes}
+      className={classNames('ds-c-close-button', className)}
+      id={id}
     >
       <CloseIconThin ariaHidden={false} id={`${id}__icon`} />
     </button>

packages/design-system/src/components/MonthPicker/MonthPicker.tsx
@@ -99,6 +99,7 @@ export const MonthPicker = (props: MonthPickerProps) => {
     if (props.onChange) {
       props.onChange(event);
     }
+
     if (!isControlled) {
       const month = parseInt(event.target.value);
       const newSelectedMonths = selectedMonths.slice();

packages/design-system/src/components/MonthPicker/__snapshots__/MonthPicker.test.tsx.snap
@@ -85,6 +85,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--1"
+                id="month-picker--1__choice--1__label"
               >
                 Jan
               </label>
@@ -109,6 +110,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--2"
+                id="month-picker--1__choice--2__label"
               >
                 Feb
               </label>
@@ -133,6 +135,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--3"
+                id="month-picker--1__choice--3__label"
               >
                 Mar
               </label>
@@ -157,6 +160,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--4"
+                id="month-picker--1__choice--4__label"
               >
                 Apr
               </label>
@@ -181,6 +185,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--5"
+                id="month-picker--1__choice--5__label"
               >
                 May
               </label>
@@ -205,6 +210,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--6"
+                id="month-picker--1__choice--6__label"
               >
                 Jun
               </label>
@@ -229,6 +235,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--7"
+                id="month-picker--1__choice--7__label"
               >
                 Jul
               </label>
@@ -253,6 +260,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--8"
+                id="month-picker--1__choice--8__label"
               >
                 Aug
               </label>
@@ -277,6 +285,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--9"
+                id="month-picker--1__choice--9__label"
               >
                 Sep
               </label>
@@ -301,6 +310,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--10"
+                id="month-picker--1__choice--10__label"
               >
                 Oct
               </label>
@@ -325,6 +335,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--11"
+                id="month-picker--1__choice--11__label"
               >
                 Nov
               </label>
@@ -349,6 +360,7 @@ exports[`MonthPicker renders a snapshot 1`] = `
               <label
                 class="ds-c-label"
                 for="month-picker--1__choice--12"
+                id="month-picker--1__choice--12__label"
               >
                 Dec
               </label>

packages/design-system/src/components/PrivacySettingsDialog/__snapshots__/PrivacySettingsTable.test.jsx.snap
@@ -96,6 +96,7 @@ Object {
                     <label
                       class="ds-c-label"
                       for="choice-list--2__choice--0"
+                      id="choice-list--2__choice--0__label"
                     >
                       Allow
                     </label>
@@ -115,6 +116,7 @@ Object {
                     <label
                       class="ds-c-label"
                       for="choice-list--2__choice--1"
+                      id="choice-list--2__choice--1__label"
                     >
                       Don't allow
                     </label>
@@ -176,6 +178,7 @@ Object {
                     <label
                       class="ds-c-label"
                       for="choice-list--3__choice--0"
+                      id="choice-list--3__choice--0__label"
                     >
                       Allow
                     </label>
@@ -195,6 +198,7 @@ Object {
                     <label
                       class="ds-c-label"
                       for="choice-list--3__choice--1"
+                      id="choice-list--3__choice--1__label"
                     >
                       Don't allow
                     </label>
@@ -256,6 +260,7 @@ Object {
                     <label
                       class="ds-c-label"
                       for="choice-list--4__choice--0"
+                      id="choice-list--4__choice--0__label"
                     >
                       Allow
                     </label>
@@ -275,6 +280,7 @@ Object {
                     <label
                       class="ds-c-label"
                       for="choice-list--4__choice--1"
+                      id="choice-list--4__choice--1__label"
                     >
                       Don't allow
                     </label>
@@ -379,6 +385,7 @@ Object {
                   <label
                     class="ds-c-label"
                     for="choice-list--2__choice--0"
+                    id="choice-list--2__choice--0__label"
                   >
                     Allow
                   </label>
@@ -398,6 +405,7 @@ Object {
                   <label
                     class="ds-c-label"
                     for="choice-list--2__choice--1"
+                    id="choice-list--2__choice--1__label"
                   >
                     Don't allow
                   </label>
@@ -459,6 +467,7 @@ Object {
                   <label
                     class="ds-c-label"
                     for="choice-list--3__choice--0"
+                    id="choice-list--3__choice--0__label"
                   >
                     Allow
                   </label>
@@ -478,6 +487,7 @@ Object {
                   <label
                     class="ds-c-label"
                     for="choice-list--3__choice--1"
+                    id="choice-list--3__choice--1__label"
                   >
                     Don't allow
                   </label>
@@ -539,6 +549,7 @@ Object {
                   <label
                     class="ds-c-label"
                     for="choice-list--4__choice--0"
+                    id="choice-list--4__choice--0__label"
                   >
                     Allow
                   </label>
@@ -558,6 +569,7 @@ Object {
                   <label
                     class="ds-c-label"
                     for="choice-list--4__choice--1"
+                    id="choice-list--4__choice--1__label"
                   >
                     Don't allow
                   </label>

commit b35a56cc01411b55b75db9e122c1e8236da651b5

[NO-TICKET] Fix the visual regression in #2893 (#2921)
Fix the healthcare DS giving choice errors a redundant css class that causes a shift

This is the reason we separated the hooks so we could mix and match which ones we use

packages/design-system/src/components/ChoiceList/Choice.tsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import EvEmitter from 'ev-emitter';
 import classNames from 'classnames';
 import useId from '../utilities/useId';
+import { InlineError } from '../InlineError';
 import { Label } from '../Label';
-import { UseInlineErrorProps, useInlineError } from '../InlineError/useInlineError';
+import { UseInlineErrorProps } from '../InlineError/useInlineError';
 import { UseLabelPropsProps, useLabelProps } from '../Label/useLabelProps';
 import { UseHintProps, useHint } from '../Hint/useHint';
 import cleanFieldProps from '../utilities/cleanFieldProps';
@@ -105,9 +106,19 @@ export const Choice = (props: ChoiceProps) => {
 
   const id = useId('choice--', props.id);
 
-  const { errorId, topError, bottomError } = useInlineError({ ...props, id });
   const { hintId, hintElement } = useHint({ ...props, id });
   const labelProps = useLabelProps({ ...props, id });
+
+  let errorId;
+  let errorElement;
+  if (props.errorMessage) {
+    errorId = props.errorId ?? `${props.id}__error`;
+    errorElement = (
+      <InlineError id={errorId} inversed={props.inversed} className={props.errorMessageClassName}>
+        {props.errorMessage}
+      </InlineError>
+    );
+  }
 
   // Subscribe to changes from other radio buttons in the same group
   useEffect(() => {
@@ -179,8 +190,7 @@ export const Choice = (props: ChoiceProps) => {
         />
         <Label {...labelProps} fieldId={id} />
         {hintElement}
-        {topError}
-        {bottomError}
+        {errorElement}
       </div>
       {checked ? checkedChildren : uncheckedChildren}
     </div>