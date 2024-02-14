430d374ecb704bc32b8f712acdf2d7429731af64
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-02-13 16:37:12 -0800

    Web components page

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+// import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react()/*, preact()*/],
 });
