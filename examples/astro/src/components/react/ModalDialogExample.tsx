9f3b0cf7fb36a6bffd81e4cbf6d0861dd8d5fa39
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-02-15 13:42:34 -0800

    Update dialog example to use new API

--- examples/astro/src/components/react/ModalDialogExample.tsx
+++ examples/astro/src/components/react/ModalDialogExample.tsx
@@ -9,28 +9,26 @@ function ModalDialogExample() {
       <Button onClick={() => setShowExampleModal(true)} size="big" variation="solid">
         Click to show modal
       </Button>
-
-      {showExampleModal && (
-        <Dialog
-          onExit={() => setShowExampleModal(false)}
-          heading="Dialog heading"
-          actions={[
-            <button className="ds-c-button ds-c-button--solid ds-u-margin-right--1" key="primary">
-              Dialog action
-            </button>,
-            <button
-              className="ds-c-button ds-c-button--transparent"
-              key="cancel"
-              onClick={() => setShowExampleModal(false)}
-            >
-              Cancel
-            </button>,
-          ]}
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan diam vitae metus
-          lacinia, eget tempor purus placerat.
-        </Dialog>
-      )}
+      <Dialog
+        onExit={() => setShowExampleModal(false)}
+        heading="Dialog heading"
+        actions={[
+          <button className="ds-c-button ds-c-button--solid ds-u-margin-right--1" key="primary">
+            Dialog action
+          </button>,
+          <button
+            className="ds-c-button ds-c-button--transparent"
+            key="cancel"
+            onClick={() => setShowExampleModal(false)}
+          >
+            Cancel
+          </button>,
+        ]}
+        isOpen={showExampleModal}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan diam vitae metus
+        lacinia, eget tempor purus placerat.
+      </Dialog>
     </>
   );
 }
