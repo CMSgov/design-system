a2a065e494589d9ecf39d40b6fe51af8d96162ab
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-02-15 13:51:03 -0800

    Make the Pagination example functional

--- examples/astro/src/components/react/PaginationExample.tsx
+++ examples/astro/src/components/react/PaginationExample.tsx
@@ -1,17 +1,20 @@
 import { Pagination } from '@cmsgov/design-system';
-import React from 'react';
+import React, { useState } from 'react';
 
 function PaginationExample() {
+  const [page, setPage] = useState(5);
+  const totalPages = 10;
   return (
     <>
       <h2>Pagination Example</h2>
       <Pagination
-        currentPage={5}
-        totalPages={10}
-        renderHref={(page) => `#/results/${page}`}
-        onPageChange={(evt, page) => {
+        currentPage={page}
+        totalPages={totalPages}
+        renderHref={(page: number) => `#/results/${page}`}
+        onPageChange={(evt: any, newPage: number) => {
           evt.preventDefault();
-          console.log(`Page ${page}`);
+          setPage(newPage);
+          console.log(`Page ${newPage}`);
         }}
       />
     </>
