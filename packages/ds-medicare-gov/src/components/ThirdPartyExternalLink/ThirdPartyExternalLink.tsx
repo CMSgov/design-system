394688b44e870445e0f3e2475dc6385cd0241f42
Sarah Sachs <zarahzachz@users.noreply.github.com>
2024-02-26 16:46:24 -0500

    clean up third party external link being used in child themes

--- packages/ds-medicare-gov/src/components/ThirdPartyExternalLink/ThirdPartyExternalLink.tsx
+++ packages/ds-medicare-gov/src/components/ThirdPartyExternalLink/ThirdPartyExternalLink.tsx
@@ -1,7 +1,6 @@
 import { ThirdPartyExternalLink } from '@cmsgov/design-system';
 
 ThirdPartyExternalLink.defaultProps = {
-  ...ThirdPartyExternalLink.defaultProps,
   learnMoreUrl:
     'https://www.cms.gov/About-CMS/Agency-Information/Aboutwebsite/PolicyforLinkingtoOutsideWebsites',
 };
