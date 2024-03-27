15c171d8af2f37455c011fd7ba935e729d8e88bd
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-03-27 15:53:56 -0700

    Add enough to fix the unit tests
    Now that I see that it's only the unit tests that need this, I'd like to figure out a way to keep the shim code from being included in our production code.

--- packages/design-system/src/components/NativeDialog/shim.ts
+++ packages/design-system/src/components/NativeDialog/shim.ts
@@ -1,6 +1,6 @@
 /**
  * This polyfill originally came from https://github.com/GoogleChrome/dialog-polyfill/,
- * but we've stripped it down to only what we need to not crash SSR environments that
+ * but we've stripped it down to only what we need to not crash node environments that
  * do not yet support the dialog element.
  */
 
@@ -8,22 +8,46 @@
  * Add shims for dialog functions and properties to the provided element object.
  */
 function addDialogProperties(dialog: any) {
+  function handleKey(event) {
+    if (event.keyCode === 27) {
+      event.preventDefault();
+      event.stopPropagation();
+      dialog.close();
+    }
+  }
+
+  function setOpen(open: boolean) {
+    if (open) {
+      dialog.hasAttribute('open') || dialog.setAttribute('open', '');
+      document.addEventListener('keydown', handleKey);
+    } else {
+      dialog.removeAttribute('open');
+      document.removeEventListener('keydown', handleKey);
+    }
+  }
+
+  dialog.show = () => setOpen(true);
+  dialog.showModal = () => setOpen(true);
+  dialog.close = () => {
+    setOpen(false);
+    const cancelEvent = new CustomEvent('close', {
+      bubbles: false,
+      cancelable: true,
+    });
+    dialog.dispatchEvent(cancelEvent);
+  };
+
   // Set a11y role. Browsers that support dialog implicitly know this already.
   if (!dialog.hasAttribute('role')) {
     dialog.setAttribute('role', 'dialog');
   }
-
-  const noop = () => {};
-  dialog.show = noop;
-  dialog.showModal = noop;
-  dialog.close = noop;
 
   if (!('returnValue' in dialog)) {
     dialog.returnValue = '';
   }
 
   Object.defineProperty(dialog, 'open', {
-    set: noop,
+    set: setOpen,
     get: dialog.hasAttribute.bind(dialog, 'open'),
   });
 }
