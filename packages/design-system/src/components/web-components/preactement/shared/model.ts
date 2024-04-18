7a1ad76d9750bfb90886611d2981f3d49c960539
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-04-18 09:40:43 -0700

    Probably better to throw an error if the tag name is invalid
    That's something that can easily be fixed in the very-important code for defining custom elements. Right now it just fails silently and renames it, which is weird.

--- packages/design-system/src/components/web-components/preactement/shared/model.ts
+++ packages/design-system/src/components/web-components/preactement/shared/model.ts
@@ -29,6 +29,7 @@ enum ErrorTypes {
   Promise = 'Error: Promises cannot be used for SSR',
   Missing = 'Error: Cannot find component in provided function',
   Json = 'Error: Invalid JSON string passed to component',
+  Tag = 'Error: Invalid tag name for custom element. Must include a `-`',
 }
 
 /* -----------------------------------
