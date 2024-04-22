f5c4d5959788f3c638d13b8019cdf0001663ecd2
Sarah Sachs <zarahzachz@users.noreply.github.com>
2024-04-22 16:11:18 -0400

    add role attr so aria-invalid works

--- packages/design-system/src/components/ChoiceList/ChoiceList.tsx
+++ packages/design-system/src/components/ChoiceList/ChoiceList.tsx
@@ -143,6 +143,7 @@ export const ChoiceList: React.FC<ChoiceListProps> = (props: ChoiceListProps) =>
       aria-invalid={invalid}
       aria-describedby={describeField({ ...props, hintId, errorId })}
       className={classNames('ds-c-fieldset', props.className)}
+      role={props.type === 'radio' ? 'radiogroup' : 'listbox'}
     >
       <Label component="legend" {...labelProps} />
       {hintElement}
