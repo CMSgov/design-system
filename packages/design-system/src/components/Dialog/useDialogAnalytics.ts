b59fb3beea1a4219af78ad70dc2750ba61dc66c1
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-04-24 09:06:18 -0700

    Fix `Dialog` firing open and close events at wrong time

--- packages/design-system/src/components/Dialog/useDialogAnalytics.ts
+++ packages/design-system/src/components/Dialog/useDialogAnalytics.ts
@@ -1,11 +1,19 @@
 import { DialogProps } from './Dialog';
-import { EventCategory, EventType, useAnalyticsContent, eventExtensionText } from '../analytics';
+import {
+  EventCategory,
+  EventType,
+  eventExtensionText,
+  getAnalyticsContentFromRefs,
+} from '../analytics';
 import { config } from '../config';
+import { useEffect, useRef } from 'react';
+import usePrevious from '../utilities/usePrevious';
 
 export function useDialogAnalytics({
   analytics,
   analyticsLabelOverride,
   onAnalyticsEvent = config().defaultAnalyticsFunction,
+  isOpen,
 }: DialogProps) {
   function sendDialogEvent(
     content: string | undefined,
@@ -32,20 +40,26 @@ export function useDialogAnalytics({
     });
   }
 
-  const [headingRef] = useAnalyticsContent({
-    onMount: (content: string | undefined) => {
+  // We need to send modal_impression when it's open once and only once
+  // And we need to send modal_closed only when it was open and then closed
+  const headingRef = useRef();
+  const prevIsOpen = usePrevious(isOpen);
+  useEffect(() => {
+    const content = getAnalyticsContentFromRefs([headingRef]);
+    if (isOpen && isOpen !== prevIsOpen) {
+      // It either started open or was closed and now is open
       sendDialogEvent(content, {
         event_name: 'modal_impression',
         event_action: 'modal impression',
       });
-    },
-    onUnmount: (content: string | undefined) => {
+    } else if (!isOpen && prevIsOpen === true) {
+      // Only trigger if it was previously open
       sendDialogEvent(content, {
         event_name: 'modal_closed',
         event_action: 'closed modal',
       });
-    },
-  });
+    }
+  }, [isOpen, prevIsOpen]);
 
   return headingRef;
 }
