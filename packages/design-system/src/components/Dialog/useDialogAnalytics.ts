30b57d8a5fec93d2ad252b396c7ce91affe52256
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-04-24 10:28:37 -0700

    Separate that logic into a hook so it can be shared

--- packages/design-system/src/components/Dialog/useDialogAnalytics.ts
+++ packages/design-system/src/components/Dialog/useDialogAnalytics.ts
@@ -1,13 +1,7 @@
 import { DialogProps } from './Dialog';
-import {
-  EventCategory,
-  EventType,
-  eventExtensionText,
-  getAnalyticsContentFromRefs,
-} from '../analytics';
+import { EventCategory, EventType, eventExtensionText } from '../analytics';
 import { config } from '../config';
-import { useEffect, useRef } from 'react';
-import usePrevious from '../utilities/usePrevious';
+import { useNativeDialogAnalytics } from '../NativeDialog/useNativeDialogAnalytics';
 
 export function useDialogAnalytics({
   analytics,
@@ -40,26 +34,23 @@ export function useDialogAnalytics({
     });
   }
 
-  // We need to send modal_impression when it's open once and only once
-  // And we need to send modal_closed only when it was open and then closed
-  const headingRef = useRef();
-  const prevIsOpen = usePrevious(isOpen);
-  useEffect(() => {
-    const content = getAnalyticsContentFromRefs([headingRef]);
-    if (isOpen && isOpen !== prevIsOpen) {
-      // It either started open or was closed and now is open
+  // We need to send modal_impression when it's open once and only once.
+  // We need to send modal_closed only when it was open and then closed.
+  const headingRef = useNativeDialogAnalytics({
+    isOpen,
+    onOpen: (content?: string) => {
       sendDialogEvent(content, {
         event_name: 'modal_impression',
         event_action: 'modal impression',
       });
-    } else if (!isOpen && prevIsOpen === true) {
-      // Only trigger if it was previously open
+    },
+    onClose: (content?: string) => {
       sendDialogEvent(content, {
         event_name: 'modal_closed',
         event_action: 'closed modal',
       });
-    }
-  }, [isOpen, prevIsOpen]);
+    },
+  });
 
   return headingRef;
 }
