CMSgov/design-system

commit 7fee5f741b90b5599875d141a443b379fad763d1

[WNMGDS-2737] Add ability to spread props on drawer (#3074)
* add ability to spread props on drawer.

* rename some props, create new test for arbitrary values.

packages/design-system/src/components/Drawer/Drawer.tsx
@@ -69,18 +69,40 @@ export interface DrawerProps {
  * [refer to its full documentation page](https://design.cms.gov/components/drawer/).
  */
 export const Drawer = (props: DrawerProps) => {
-  const headingRef = useRef(null);
-  const headingId = useId('drawer--', props.headingId);
+  const {
+    ariaLabel,
+    backdropClickExits,
+    children,
+    className,
+    closeButtonText,
+    closeButtonVariation,
+    footerBody,
+    footerTitle,
+    hasFocusTrap,
+    heading,
+    headingId: userHeadingId,
+    headingLevel,
+    headingRef: userHeadingRef,
+    isFooterSticky,
+    isHeaderSticky,
+    isOpen,
+    onCloseClick,
+    ...otherProps
+  } = props;
 
-  const Heading = `h${props.headingLevel}` as const;
+  const headingRef = useRef(null);
+  const headingId = useId('drawer--', userHeadingId);
+
+  const Heading = `h${headingLevel}` as const;
 
   return (
     <NativeDialog
-      className={classNames(props.className, 'ds-c-drawer')}
-      exit={props.onCloseClick}
-      showModal={props.hasFocusTrap}
-      backdropClickExits={props.backdropClickExits}
-      isOpen={props.isOpen}
+      className={classNames(className, 'ds-c-drawer')}
+      exit={onCloseClick}
+      showModal={true}
+      backdropClickExits={backdropClickExits}
+      isOpen={isOpen}
+      {...otherProps}
     >
       <div className="ds-c-drawer__window" tabIndex={-1} aria-labelledby={headingId}>
         <div className="ds-c-drawer__header">
@@ -89,35 +111,35 @@ export const Drawer = (props: DrawerProps) => {
             className="ds-c-drawer__header-heading"
             ref={(el) => {
               headingRef.current = el;
-              if (props.headingRef) {
-                props.headingRef.current = el;
+              if (userHeadingRef) {
+                userHeadingRef.current = el;
               }
             }}
           >
-            {props.heading}
+            {heading}
           </Heading>
           <Button
-            aria-label={props.ariaLabel ?? t('drawer.ariaLabel')}
+            aria-label={ariaLabel ?? t('drawer.ariaLabel')}
             className="ds-c-drawer__close-button"
             size="small"
-            onClick={props.onCloseClick}
-            variation={props.closeButtonVariation}
+            onClick={onCloseClick}
+            variation={closeButtonVariation}
           >
-            {props.closeButtonText ?? t('drawer.closeButtonText')}
+            {closeButtonText ?? t('drawer.closeButtonText')}
           </Button>
         </div>
         <div
           className={classNames('ds-c-drawer__body', {
-            'ds-c-drawer--is-sticky': props.isHeaderSticky || props.isFooterSticky,
+            'ds-c-drawer--is-sticky': isHeaderSticky || isFooterSticky,
           })}
           tabIndex={0}
         >
-          {props.children}
+          {children}
         </div>
-        {(props.footerTitle || props.footerBody) && (
+        {(footerTitle || footerBody) && (
           <div className="ds-c-drawer__footer">
-            <h4 className="ds-c-drawer__footer-title">{props.footerTitle}</h4>
-            <div className="ds-c-drawer__footer-body">{props.footerBody}</div>
+            <h4 className="ds-c-drawer__footer-title">{footerTitle}</h4>
+            <div className="ds-c-drawer__footer-body">{footerBody}</div>
           </div>
         )}
       </div>

packages/design-system/src/components/Drawer/DrawerManager.stories.tsx
@@ -77,7 +77,6 @@ const SingleDrawerWithToggle = (...args) => {
   return (
     <>
       <Drawer
-        {...args}
         onCloseClick={closeDrawer}
         footerTitle="Footer Title"
         footerBody={<p className="ds-text-body--md ds-u-margin--0">Footer content</p>}

packages/design-system/src/components/Drawer/DrawerManager.tsx
@@ -58,7 +58,7 @@ export const DrawerContext = createContext(null);
  *   isDrawerOpen: boolean; // whether it's open
  *   openDrawer: () => any; // function that opens it
  *   closeDrawer: () => any; // function that closes it
- *   toggleDrawer: () => any; // funcntion that toggles it
+ *   toggleDrawer: () => any; // function that toggles it
  * }
  * ```
  *

packages/design-system/src/components/HelpDrawer/HelpDrawer.test.tsx
@@ -46,6 +46,17 @@ describe('HelpDrawer', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
+  it('passes arbitrary attributes to `<dialog>` element', () => {
+    const screen = render(
+      <HelpDrawer {...defaultProps} data-testid="foo">
+        <p>content</p>
+      </HelpDrawer>
+    );
+
+    const dialogEl = screen.queryByRole('dialog');
+    expect(dialogEl).toHaveAttribute('data-testid');
+  });
+
   describe('Analytics event tracking', () => {
     let tealiumMock;
 

commit 7beb5252e07aa9f5c1c7dfb0e4b47320640dbdb1

Fix bug causing drawers to always trap focus
It was introduced in https://github.com/CMSgov/design-system/commit/7fee5f741b90b5599875d141a443b379fad763d1#diff-9ef68d956bbe4985a98993aa40ecbbc2f643eb437d5cbdd94cd5529587a51ac6R102 and hasn't been released yet

packages/design-system/src/components/Drawer/Drawer.tsx
@@ -99,7 +99,7 @@ export const Drawer = (props: DrawerProps) => {
     <NativeDialog
       className={classNames(className, 'ds-c-drawer')}
       exit={onCloseClick}
-      showModal={true}
+      showModal={hasFocusTrap}
       backdropClickExits={backdropClickExits}
       isOpen={isOpen}
       {...otherProps}