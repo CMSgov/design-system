7983f50c89b215b30f00dc91053a1a0798b38809
Kaipo Kent <kaipo@adhocteam.us>
2024-06-14 12:26:17 -0600

    Update snapshots for ThirdPartyExternalLink

--- packages/design-system/src/components/ThirdPartyExternalLink/ThirdPartyExternalLink.test.tsx
+++ packages/design-system/src/components/ThirdPartyExternalLink/ThirdPartyExternalLink.test.tsx
@@ -36,7 +36,7 @@ describe('ThirdPartyExternalLink', () => {
   });
 
   describe('ThirdPartyExternalLink dialog', () => {
-    it('renders external link dialog', () => {
+    it('renders external link dialog without learnMoreUrl link', () => {
       renderThirdPartyExternalLink();
 
       userEvent.click(getLink());
