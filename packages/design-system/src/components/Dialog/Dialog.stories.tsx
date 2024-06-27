b0d0a59e033cb738fd48d9d6b76498490c48a9be
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-06-27 13:28:01 -0700

    Reproduce it in Storybook for real
    Just having it in another tab didn't actually de-render it because it was being rendered in the root story component function even if it wasn't visible.

--- packages/design-system/src/components/Dialog/Dialog.stories.tsx
+++ packages/design-system/src/components/Dialog/Dialog.stories.tsx
@@ -4,7 +4,6 @@ import { Dialog } from './Dialog';
 import { Button } from '@cmsgov/design-system';
 import { action } from '@storybook/addon-actions';
 import { useDialog } from './useDialog';
-import { TabPanel, Tabs } from '../Tabs';
 
 const meta: Meta<typeof Dialog> = {
   title: 'Components/Dialog',
@@ -71,89 +70,94 @@ export const DialogExample: Story = {
   },
 };
 
+const DialogParent = ({ derender }) => {
+  const [dialogOpen, updateOpen] = useState(false);
+  const showModal = () => updateOpen(true);
+  const hideModal = (...params) => {
+    action('onExit')(...params);
+    updateOpen(false);
+  };
+
+  return (
+    <>
+      <Dialog
+        heading="Hey"
+        onExit={hideModal}
+        isOpen={dialogOpen}
+        actions={
+          <Button variation="solid" onClick={derender}>
+            De-render the modal
+          </Button>
+        }
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan diam vitae metus
+        lacinia, eget tempor purus placerat.
+      </Dialog>
+      <Button onClick={showModal} size="big" variation="solid">
+        Click to show modal
+      </Button>
+    </>
+  );
+};
+
 export const PreventScrollExample: Story = {
-  render: function Component(args) {
-    const [selectedTabId, setSelectedTabId] = useState('hello');
-    const [dialogOpen, updateOpen] = useState(false);
-    const showModal = () => updateOpen(true);
-    const hideModal = (...params) => {
-      action('onExit')(...params);
-      updateOpen(false);
-    };
+  render: function Component() {
+    const [shouldRenderDialogParent, setShouldRendeDialogParent] = useState(true);
 
     return (
-      <Tabs selectedId={selectedTabId}>
-        <TabPanel id="hello" tab="hello">
-          <div className="ds-u-measure--base">
-            <h1>The United States Constitution</h1>
-            <p>
-              We the People of the United States, in Order to form a more perfect Union, establish
-              Justice, insure domestic Tranquility, provide for the common defence, promote the
-              general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity,
-              do ordain and establish this Constitution for the United States of America.
-            </p>
-            <h2>Article I</h2>
-            <h3>Section 1: Congress</h3>
-            <p>
-              All legislative Powers herein granted shall be vested in a Congress of the United
-              States, which shall consist of a Senate and House of Representatives.
-            </p>
+      <div className="ds-u-measure--base">
+        <h1>The United States Constitution</h1>
+        <p>
+          We the People of the United States, in Order to form a more perfect Union, establish
+          Justice, insure domestic Tranquility, provide for the common defence, promote the general
+          Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and
+          establish this Constitution for the United States of America.
+        </p>
+        <h2>Article I</h2>
+        <h3>Section 1: Congress</h3>
+        <p>
+          All legislative Powers herein granted shall be vested in a Congress of the United States,
+          which shall consist of a Senate and House of Representatives.
+        </p>
 
-            <Dialog
-              {...args}
-              onExit={hideModal}
-              isOpen={dialogOpen}
-              actions={
-                <Button variation="solid" onClick={() => setSelectedTabId('world')}>
-                  De-render the modal
-                </Button>
-              }
-            />
-            <Button onClick={showModal} size="big" variation="solid">
-              Click to show modal
-            </Button>
+        {shouldRenderDialogParent && (
+          <DialogParent derender={() => setShouldRendeDialogParent(false)} />
+        )}
 
-            <h3>Section 2: The House of Representatives</h3>
-            <p>
-              The House of Representatives shall be composed of Members chosen every second Year by
-              the People of the several States, and the Electors in each State shall have the
-              Qualifications requisite for Electors of the most numerous Branch of the State
-              Legislature.
-            </p>
-            <p>
-              No Person shall be a Representative who shall not have attained to the Age of twenty
-              five Years, and been seven Years a Citizen of the United States, and who shall not,
-              when elected, be an Inhabitant of that State in which he shall be chosen.
-            </p>
-            <p>
-              Representatives and direct Taxes shall be apportioned among the several States which
-              may be included within this Union, according to their respective Numbers, which shall
-              be determined by adding to the whole Number of free Persons, including those bound to
-              Service for a Term of Years, and excluding Indians not taxed, three fifths of all
-              other Persons. The actual Enumeration shall be made within three Years after the first
-              Meeting of the Congress of the United States, and within every subsequent Term of ten
-              Years, in such Manner as they shall by Law direct.The number of Representatives shall
-              not exceed one for every thirty Thousand, but each State shall have at Least one
-              Representative; and until such enumeration shall be made, the State of New Hampshire
-              shall be entitled to chuse three, Massachusetts eight, Rhode-Island and Providence
-              Plantations one, Connecticut five, New-York six, New Jersey four, Pennsylvania eight,
-              Delaware one, Maryland six, Virginia ten, North Carolina five, South Carolina five,
-              and Georgia three.
-            </p>
-            <p>
-              When vacancies happen in the Representation from any State, the Executive Authority
-              thereof shall issue Writs of Election to fill such Vacancies.
-            </p>
-            <p>
-              The House of Representatives shall chuse their Speaker and other Officers;and shall
-              have the sole Power of Impeachment.
-            </p>
-          </div>
-        </TabPanel>
-        <TabPanel id="world" tab="world">
-          World
-        </TabPanel>
-      </Tabs>
+        <h3>Section 2: The House of Representatives</h3>
+        <p>
+          The House of Representatives shall be composed of Members chosen every second Year by the
+          People of the several States, and the Electors in each State shall have the Qualifications
+          requisite for Electors of the most numerous Branch of the State Legislature.
+        </p>
+        <p>
+          No Person shall be a Representative who shall not have attained to the Age of twenty five
+          Years, and been seven Years a Citizen of the United States, and who shall not, when
+          elected, be an Inhabitant of that State in which he shall be chosen.
+        </p>
+        <p>
+          Representatives and direct Taxes shall be apportioned among the several States which may
+          be included within this Union, according to their respective Numbers, which shall be
+          determined by adding to the whole Number of free Persons, including those bound to Service
+          for a Term of Years, and excluding Indians not taxed, three fifths of all other Persons.
+          The actual Enumeration shall be made within three Years after the first Meeting of the
+          Congress of the United States, and within every subsequent Term of ten Years, in such
+          Manner as they shall by Law direct.The number of Representatives shall not exceed one for
+          every thirty Thousand, but each State shall have at Least one Representative; and until
+          such enumeration shall be made, the State of New Hampshire shall be entitled to chuse
+          three, Massachusetts eight, Rhode-Island and Providence Plantations one, Connecticut five,
+          New-York six, New Jersey four, Pennsylvania eight, Delaware one, Maryland six, Virginia
+          ten, North Carolina five, South Carolina five, and Georgia three.
+        </p>
+        <p>
+          When vacancies happen in the Representation from any State, the Executive Authority
+          thereof shall issue Writs of Election to fill such Vacancies.
+        </p>
+        <p>
+          The House of Representatives shall chuse their Speaker and other Officers;and shall have
+          the sole Power of Impeachment.
+        </p>
+      </div>
     );
   },
 };
