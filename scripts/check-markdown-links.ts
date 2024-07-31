b4a040d88ebb34da7a5aefb3453b8b4ae2596eb2
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-07-31 14:44:55 -0400

    Main functionality complete. Could use some cleaning up.

--- scripts/check-markdown-links.ts
+++ scripts/check-markdown-links.ts
@@ -1,19 +1,14 @@
 import { sh } from './utils';
 import path from 'node:path';
 import fs from 'node:fs';
-
-interface ConfirmUrls {
+import fetch from 'node-fetch';
+interface FilesAndLinks {
   [key: string]: Array<string>;
 }
 
-// Some constants:
+// Construct our find command:
 const appDir = path.resolve(__dirname, '..');
 const fileExtensions = ['.md', '.mdx'];
-const searchExpression = /[http]*[s]*:\/\/[\S]*?[^\)\]\(\"' \n]*/g;
-let foundFiles: string | undefined;
-const urlsToCheck: ConfirmUrls = {};
-
-// Construct our command:
 const commandBase = `find ${appDir} `;
 const commandArgs = fileExtensions
   .map((ext) => {
@@ -23,12 +18,15 @@ const commandArgs = fileExtensions
 
 const findCommand = commandBase.concat(commandArgs);
 
-// Determine if the url we want to query is ok:
+// Prevents us trying to call localhost urls.
 function isValidUrl(url: string): boolean {
   let urlHolder: URL;
   try {
     urlHolder = new URL(url);
-  } catch (_) {
+  } catch (error) {
+    console.log(
+      `${url} could not be turned into a proper URL. Maybe explicitly exclude this type of thing?`
+    );
     return false;
   }
   if (urlHolder.hostname.includes('localhost')) {
@@ -43,40 +41,68 @@ function findLinks(filePath: string, regex: RegExp): void {
     const fileContent = fs.readFileSync(filePath, 'utf8');
     const matches: RegExpMatchArray | null = fileContent.match(regex);
     if (matches) {
-      (urlsToCheck as ConfirmUrls)[filePath] = matches.filter((match) => {
+      (urlsToCheck as FilesAndLinks)[filePath] = matches.filter((match) => {
         if (isValidUrl(match)) {
           return match;
         }
       });
     }
   } catch (error) {
-    console.log(error);
+    console.log(
+      `An error occurred when parsing this file: ${filePath}. \nHere's the error:\n ${error}`
+    );
   }
 }
 
+// Output file is a text file:
+const outputFileName = `${appDir}/brokenLinkReport.txt`;
+
+// Use node-fetch to query urls and return a status code:
+function curl(file: string, url: string): void {
+  const spliceFrom = file.split('/').indexOf('design-system');
+  const filePath: string = file.split('/').splice(spliceFrom).join('/');
+  fetch(url).then((res) => {
+    if (res.status !== 200) {
+      const content = `Filepath: ${filePath}\n\tURL: ${url} Code: ${res.status}\n`;
+      fs.writeFile(outputFileName, content, { flag: 'a+' }, (err) => {
+        if (err) {
+          console.error(`Could not write line to file.\nContent: ${content}. Error: ${err}`);
+        }
+      });
+    }
+  });
+}
+
 // Run our command and plop string output into a variable:
+let foundFiles: string | undefined;
 try {
   foundFiles = sh(findCommand);
 } catch (error) {
   console.log('Your find command failed with the following error:', error);
 }
 
+const urlsToCheck: FilesAndLinks = {};
+
 if (!foundFiles) {
   console.log(`No files found with the extensions ${fileExtensions.join(', ')} in ${appDir}.`);
+  process.exit(0);
+} else {
+  const allFiles = foundFiles.split('\n');
+  const searchExpression = /[http]*[s]*:\/\/[\S]*?[^\)\]\(\"' \n]*/g;
+  allFiles.forEach((file) => findLinks(file, searchExpression));
 }
 
-const allFiles = foundFiles?.split('\n');
+// Create our file/blow away existing content:
+fs.writeFile(outputFileName, '', (err) => {
+  if (err) {
+    console.error(`File not cleaned: ${err}`);
+  } else {
+    console.log('Cleaned file. Preparing to write.');
+  }
+});
 
-allFiles?.forEach((file) => findLinks(file, searchExpression));
-
-// Write function to check the link with curl
-function curl(url: string): void {
-  const resCode = sh(`curl -o - -I -s ${url}`);
-  console.log(resCode);
+for (const [file, links] of Object.entries(urlsToCheck)) {
+  links.forEach((link) => {
+    curl(file, link);
+  });
 }
-
-for (const [page, links] of Object.entries(urlsToCheck)) {
-  console.log(page);
-  links.forEach((link) => curl(link));
-}
-// Write non-200 status urls to file
