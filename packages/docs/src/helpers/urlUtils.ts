db2854e308b04584d07da9e04da1e56154fa98f2
Kim Niedermaier <kimniedermaier+cmsds@navapbc.com>
2024-08-05 15:38:36 -0700

    simplify query search params utility function

--- packages/docs/src/helpers/urlUtils.ts
+++ packages/docs/src/helpers/urlUtils.ts
@@ -40,20 +40,10 @@ export function makePageUrl(fileRelativePath, location: LocationInterface) {
  * @param {string} value - Query string key to search for
  * @returns Value of key if found, null if not
  */
-export function getQueryParamValue(value: string) {
-  let query = {};
+export function getQueryParamValue(value: string): string | null {
   if (typeof window !== 'undefined') {
-    location.search
-      .substr(1)
-      .split('&')
-      .map((item) => {
-        query[item.split('=')[0]] = item.split('=')[1];
-      });
-    if (query[value]) {
-      return query[value];
-    } else {
-      return null;
-    }
+    const searchParams = new URLSearchParams(location.search);
+    return searchParams.get(value);
   } else {
     return null;
   }
