6226bd87c7ace26ddb9e0079bd7242060a4431a0
tamara-corbalt <tamara@corbalt.com>
2024-08-06 16:22:29 -0400

    Filled out storybook argTypes for ds-date-field

--- packages/design-system/src/components/web-components/ds-date-field/ds-date-field.stories.tsx
+++ packages/design-system/src/components/web-components/ds-date-field/ds-date-field.stories.tsx
@@ -19,9 +19,81 @@ const meta: Meta = {
     name: 'ds-date-field',
   },
   argTypes: {
-    'error-message': { control: 'text' },
-    hint: { control: 'text' },
-    'requirement-label': { control: 'text' },
+    name: {
+      description: "The `input` field's `name` attribute.",
+      control: 'text',
+    },
+    'root-id': {
+      description:
+        "A unique ID for this element. A unique ID will be generated if one isn't provided.",
+      control: 'text',
+    },
+    inversed: {
+      description: 'Set to `true` to apply the "inverse" color scheme.',
+      control: 'boolean',
+    },
+    value: {
+      description:
+        "The `input` field's `value` attribute. Typically not used for uncontrolled components (web components).",
+      control: 'text',
+    },
+    'default-value': {
+      description: `Sets the initial value for the component. This value will be used until the user changes the input, at which point the new value will be handled by the component's internal state.
+                    The \`onChange\` event will still be triggered with the updated value, but the component will manage the state internally.`,
+      control: 'text',
+    },
+    'default-month': {
+      description:
+        'The initial month to show in the calendar picker. If not provided, defaults to the month of the currently selected date.',
+      control: 'date',
+    },
+    'from-date': {
+      description:
+        'Earliest day to start month navigation in the calendar picker. (This does not restrict dates entered manually.)',
+      control: 'date',
+    },
+    'from-month': {
+      description:
+        'Earliest month to start month navigation in the calendar picker. (This does not restrict dates entered manually.)',
+      control: 'date',
+    },
+    'from-year': {
+      description:
+        'Earliest year to start month navigation in the calendar picker. (This does not restrict dates entered manually.)',
+      control: 'number',
+    },
+    'to-date': {
+      description:
+        'Latest day to end month navigation in the calendar picker. (This does not restrict dates entered manually.)',
+      control: 'date',
+    },
+    'to-month': {
+      description:
+        'Latest month to end month navigation in the calendar picker. (This does not restrict dates entered manually.)',
+      control: 'date',
+    },
+    'to-year': {
+      description:
+        'Latest year to end month navigation in the calendar picker. (This does not restrict dates entered manually.)',
+      control: 'number',
+    },
+    hint: {
+      description: 'Hint text or HTML.',
+      control: 'text',
+    },
+    label: {
+      description: 'Label text or HTML.',
+      control: 'text',
+    },
+    'requirement-label': {
+      description:
+        'Text showing the requirement (e.g., "Optional", or "Required"). In most cases, this should be used to indicate which fields are optional. See the [form guidelines](https://design.cms.gov/patterns/Forms/forms/) for more info.',
+      control: 'text',
+    },
+    'error-message': {
+      description: 'Enable the error state by providing an error message.',
+      control: 'text',
+    },
   },
 };
 export default meta;
