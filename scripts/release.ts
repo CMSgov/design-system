7eb00e6313e213d9491f69da929db856257e3481
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-08-19 13:17:06 -0400

    lets try putting these here

--- scripts/release.ts
+++ scripts/release.ts
@@ -104,6 +104,15 @@ async function bumpVersions() {
   );
   // Unstage the design-system-tokens package.json
   sh('git checkout ./packages/design-system-tokens/package.json');
+  // Verify that there are actually changes staged
+  if (!sh('git status -s')) {
+    console.log(c.yellow('No version changes made. Exiting...'));
+    process.exit(1);
+  }
+  // Only stage changes to package files
+  sh('git add -u **/package.json');
+  // And discard all other changes
+  sh('git checkout -- .');
   console.log(c.green('Bumped package versions. Bumping dependencies next...'));
 
   getPackages().forEach((pack) => {
@@ -121,16 +130,6 @@ async function bumpVersions() {
       c.green(`Bumped ${packageName} to @cmsgov/design-system@${newDesignSystemVersion()}.`)
     );
   });
-
-  // Verify that there are actually changes staged
-  if (!sh('git status -s')) {
-    console.log(c.yellow('No version changes made. Exiting...'));
-    process.exit(1);
-  }
-  // Only stage changes to package files
-  sh('git add -u **/package.json');
-  // And discard all other changes
-  sh('git checkout -- .');
 
   // Update versions.json
   const currentVersionsByPackage = updateVersions();
