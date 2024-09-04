5bbd2581681832b1af992c267cba25aef9793361
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-09-04 14:24:22 -0400

    make conditional more readable

--- scripts/bump-child-deps-utils.ts
+++ scripts/bump-child-deps-utils.ts
@@ -36,7 +36,7 @@ const getPackageNamesAndLocations = (): Array<{
   const packageJson: any = JSON.parse(sh('npm ls -ws @cmsgov/design-system --json'));
   const deps = packageJson['dependencies'];
   // We don't want to add the core design system as a dependency to itself, so we filter it out.
-  const packageNames = Object.keys(deps).filter((dep) => !(dep === '@cmsgov/design-system'));
+  const packageNames = Object.keys(deps).filter((dep) => dep !== '@cmsgov/design-system');
   return packageNames.map((name: any) => {
     /*
      * The package location is stored relative to the root directory when returned by `npm ls`,
