aa1605dbe9a6e0bf2c144891cb2c0b521c3abce5
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-09-24 13:59:54 -0400

    Building out some tests

---
+++ packages/design-system/src/components/web-components/ds-tooltip/ds-tooltip.test.tsx
@@ -0,0 +1,150 @@
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import './ds-tooltip';
+
+jest.mock('@popperjs/core');
+
+function renderTooltip(props = {}) {
+  return render(<ds-tooltip {...props}>I&apos;m a tooltip!</ds-tooltip>);
+}
+
+describe('ds-tooltip', function () {
+  it('renders a tooltip', () => {
+    const { asFragment } = renderTooltip({ title: "I'm a title!", id: 'static-id' });
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('displays the icon', () => {
+    const { container } = renderTooltip({
+      title: "I'm a title!",
+      id: 'static-id',
+      includeIcon: 'true',
+    });
+    const tooltipIcon = container.querySelector('.ds-c-tooltip-icon');
+    expect(tooltipIcon).toBeInTheDocument();
+  });
+});
+
+// describe('ds-text-field', function () {
+//   it('renders text-field', () => {
+//     const { asFragment } = renderTextField({ id: 'static-id' });
+//     expect(asFragment()).toMatchSnapshot();
+//   });
+
+//   it('renders disabled input', () => {
+//     renderTextField({ disabled: 'true' });
+//     const input = screen.getByRole('textbox');
+//     expect(input).toHaveAttribute('disabled');
+//   });
+
+//   it('does not render disabled when passed false', () => {
+//     renderTextField({ disabled: 'false' });
+//     const input = screen.getByRole('textbox');
+//     expect(input).not.toHaveAttribute('disabled');
+//   });
+
+//   it('applies additional classes to the root element', () => {
+//     const { container } = renderTextField({ 'class-name': 'foobar' });
+//     const root = container.querySelector('.ds-u-clearfix');
+//     expect(root).toHaveClass('foobar');
+//   });
+
+//   it('applies additional classes to the input', () => {
+//     renderTextField({ 'field-class-name': 'foobar' });
+//     const input = screen.getByRole('textbox');
+//     expect(input).toHaveClass('foobar');
+//   });
+
+//   it('applies requirement-label to hint text', () => {
+//     const { container } = renderTextField({ 'requirement-label': 'Optional' });
+//     const input = screen.getByRole('textbox');
+//     const hintId = input.getAttribute('aria-describedby');
+//     const hint = container.querySelector(`#${hintId}`);
+//     expect(hint).toContainHTML('Optional');
+//   });
+
+//   it('renders an error message', () => {
+//     const { container } = renderTextField({ 'error-message': 'Uh-oh!' });
+//     const input = screen.getByRole('textbox');
+//     const errorId = input.getAttribute('aria-describedby');
+//     const error = container.querySelector(`#${errorId}`);
+//     expect(error).toContainHTML('Uh-oh!');
+//   });
+
+//   it('applies inverse class', () => {
+//     const { container } = renderTextField({
+//       inversed: 'true',
+//       hint: 'Hello',
+//       'error-message': 'Ahh!!',
+//     });
+//     const inversedLabel = container.querySelector('.ds-c-label--inverse');
+//     const inversedHint = container.querySelector('.ds-c-hint--inverse');
+//     const inversedError = container.querySelector('.ds-c-inline-error--inverse');
+//     expect(inversedLabel).toBeInTheDocument();
+//     expect(inversedHint).toBeInTheDocument();
+//     expect(inversedError).toBeInTheDocument();
+//   });
+
+//   it('applies size classes', () => {
+//     renderTextField({ size: 'small' });
+//     const input = screen.getByRole('textbox');
+//     expect(input.classList.contains('ds-c-field--small')).toBe(true);
+//   });
+
+//   it('supports multiline text fields', () => {
+//     renderTextField({ multiline: 'true', rows: '3' });
+//     const textArea = screen.getByRole('textbox');
+//     expect(textArea.tagName).toEqual('TEXTAREA');
+//     expect(textArea).toHaveAttribute('rows', '3');
+//   });
+
+//   it('fires a custom ds-change event', () => {
+//     renderTextField();
+
+//     const textFieldRoot = document.querySelector('ds-text-field');
+//     const mockHandler = jest.fn();
+//     textFieldRoot.addEventListener('ds-change', mockHandler);
+
+//     const input = screen.getByRole('textbox');
+//     userEvent.click(input);
+//     userEvent.keyboard('a');
+
+//     expect(mockHandler).toHaveBeenCalledTimes(1);
+//     textFieldRoot.removeEventListener('ds-change', mockHandler);
+//   });
+
+//   it('fires a custom ds-blur event', async () => {
+//     renderTextField();
+
+//     const textFieldRoot = document.querySelector('ds-text-field');
+//     const onBlur = jest.fn();
+//     const onChange = jest.fn();
+//     textFieldRoot.addEventListener('ds-blur', onBlur);
+//     textFieldRoot.addEventListener('ds-change', onChange);
+
+//     const input = screen.getByRole('textbox');
+//     userEvent.click(input);
+//     userEvent.tab();
+
+//     const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+//     await act(async () => {
+//       await sleep(40);
+//     });
+
+//     expect(onBlur).toHaveBeenCalledTimes(1);
+//     expect(onChange).not.toHaveBeenCalled();
+//     textFieldRoot.removeEventListener('ds-blur', onBlur);
+//     textFieldRoot.removeEventListener('ds-change', onChange);
+//   });
+
+//   it('formats a phone number on blur', () => {
+//     renderTextField({ 'label-mask': 'phone' });
+
+//     const input = screen.getByRole('textbox') as HTMLInputElement;
+//     userEvent.click(input);
+//     userEvent.type(input, '1234567890');
+//     userEvent.tab();
+
+//     expect(input.value).toEqual('123-456-7890');
+//   });
+// });
