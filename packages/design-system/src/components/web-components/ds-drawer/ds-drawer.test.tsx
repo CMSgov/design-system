574ec516cce4fbe02a6bea49e8f303e141525e70
Tamara deMent (Corbalt) <tamara@corbalt.com>
2024-10-07 14:50:37 -0400

    [WMNGDS-2918] Adds ds-drawer web component. (#3256)
    * Initial commit; scaffolding for ds-drawer component
    
    * Adds storybook configuration for ds-drawer.
    
    * Removed export from drawerContent to prevent Storybook from creating an unintended extra story.
    
    * Adds documentation for the footer-body slot.
    
    * Adds unit tests to verify drawer rendering, and isOpen and footer-body prop.
    
    * Adds tests to ensure ds-clse-click events are triggered correctly.
    
    * Adds an entrypoint for ds-drawer module and cleans up formatting in ds-drawer.tsx
    
    * Adds snapshots for ds-drawer.
    
    * Track the drawerOpen state so the open and close events work on multiple clicks
    
    * Adds better spacing in ds-drawer.test.tsx for better readability.
    
    * 1) Simplifies rendering logic for footer-body in storybook template, 2) adds tests to validate footer-body rendering behavior with and without a slot, and when an attribute is provided.
    
    * Adds close-button-aria-label to ds-drawer interface.
    
    * Updates the new closeButtonArialLabel prop to be optional in the react Drawer component.
    
    * Updates storybook documentation on close-button-text attribute to specify string input
    
    * Updates storybook doc snaps
    
    * Adds more explicit storybook documentation for the close-button-variation attribute.
    
    * Extends the renderDrawer testing utility with a rerenderDrawer method to support updating attributes in existing component instances.
    
    * Removes unnecessary ariaLabel in ds-drawer preact wrapper.
    
    * Updated sanps for ds-drawer
    
    * Removes backdrop-click-exits attribute from the ds-drawer interface.
    
    * Removes stray reference in ds-drawer preact wrapper.
    
    ---------
    
    Co-authored-by: jack-ryan-nava-pbc <jackryan@navapbc.com>

---
+++ packages/design-system/src/components/web-components/ds-drawer/ds-drawer.test.tsx
@@ -0,0 +1,185 @@
+import { render, screen, cleanup } from '@testing-library/preact';
+import userEvent from '@testing-library/user-event';
+import './ds-drawer';
+
+const children = (
+  <>
+    <strong>An Explanation</strong>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+  </>
+);
+
+function renderDrawer(args, children) {
+  const result = render(
+    <ds-drawer {...args}>
+      {children}
+      <div slot="footer-body">
+        <p className="ds-text-body--md ds-u-margin--0">Default slotted footer content</p>
+      </div>
+    </ds-drawer>
+  );
+
+  return {
+    ...result,
+    rerenderDrawer(newArgs, newChildren) {
+      return result.rerender(
+        <ds-drawer {...newArgs}>
+          {newChildren}
+          <div slot="footer-body">
+            <p className="ds-text-body--md ds-u-margin--0">Default slotted footer content</p>
+          </div>
+        </ds-drawer>
+      );
+    },
+  };
+}
+
+function renderDrawerWithoutSlottedFooter(args, children) {
+  return render(<ds-drawer {...(args as any)}>{children}</ds-drawer>);
+}
+const mockCloseHandler = jest.fn();
+
+describe('Drawer', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    cleanup();
+  });
+
+  it('should render a dialog', () => {
+    renderDrawer(
+      {
+        'is-open': 'true',
+        heading: 'Test Drawer Heading',
+        'close-button-aria-label': 'Close Help Drawer',
+      },
+      children
+    );
+    const drawer = document.querySelector('ds-drawer');
+    expect(drawer).toMatchSnapshot();
+
+    const dialogElement = screen.getByRole('dialog', { name: /test drawer heading/i });
+    expect(dialogElement).toBeInTheDocument();
+
+    const closeButton = screen.getByLabelText('Close Help Drawer');
+    expect(closeButton).toBeInTheDocument();
+
+    const headingElement = screen.getByText('Test Drawer Heading');
+    expect(headingElement).toBeInTheDocument();
+
+    const explanationText = screen.getByText('An Explanation');
+    expect(explanationText).toBeInTheDocument();
+
+    const paragraphText = screen.getByText(
+      /Lorem ipsum dolor sit amet, consectetur adipiscing elit/i
+    );
+    expect(paragraphText).toBeInTheDocument();
+  });
+
+  it("is closed until 'is-open' is set to true", () => {
+    const { rerenderDrawer } = renderDrawer({ 'is-open': 'false' }, children);
+    expect(screen.queryByRole('dialog')).toBe(null);
+    rerenderDrawer({ 'is-open': 'true' }, children);
+    expect((screen.getByRole('dialog') as HTMLDialogElement).open).toBe(true);
+  });
+
+  it('renders footer-body when footer-body attribute is provided', () => {
+    renderDrawerWithoutSlottedFooter(
+      {
+        'is-open': 'true',
+        heading: 'Test Drawer Heading',
+        'footer-body': 'Footer Attribute Content',
+      },
+      children
+    );
+
+    const renderedFooterBodyElement = screen.getByText('Footer Attribute Content');
+    expect(renderedFooterBodyElement).toBeInTheDocument();
+  });
+
+  it('renders slotted footer-body when slot content is provided and attribute is not', () => {
+    renderDrawer(
+      {
+        'is-open': 'true',
+        heading: 'Test Drawer Heading',
+      },
+      children
+    );
+
+    const renderedFooterBodyElement = screen.getByText('Default slotted footer content');
+    expect(renderedFooterBodyElement).toBeInTheDocument();
+  });
+
+  it('prioritizes slotted footer-body over the attribute when both are provided', () => {
+    renderDrawer(
+      {
+        'is-open': 'true',
+        heading: 'Test Drawer Heading',
+        'footer-body': 'Footer-body content from attribute',
+      },
+      children
+    );
+
+    const renderedFooterBodyElement = screen.getByText('Default slotted footer content');
+    expect(renderedFooterBodyElement).toBeInTheDocument();
+  });
+
+  it('should call the `ds-close-click` handler when the close button is clicked', async () => {
+    renderDrawer(
+      {
+        'is-open': 'true',
+        heading: 'Test Drawer Heading',
+        'close-button-aria-label': 'Close help drawer',
+      },
+      children
+    );
+    const drawer = document.querySelector('ds-drawer');
+
+    drawer.addEventListener('ds-close-click', mockCloseHandler);
+
+    const closeButton = await screen.findByRole('button', { name: /close help drawer/i });
+    userEvent.click(closeButton);
+
+    expect(mockCloseHandler).toHaveBeenCalledTimes(1);
+    drawer.removeEventListener('ds-close-click', mockCloseHandler);
+  });
+
+  it('should handle `esc` with focus trap enabled', async () => {
+    renderDrawer({ 'is-open': 'true', 'has-trap-focus': 'true' }, children);
+
+    const drawer = document.querySelector('ds-drawer');
+    drawer.addEventListener('ds-close-click', mockCloseHandler);
+
+    userEvent.keyboard('{Escape}');
+
+    expect(mockCloseHandler).toHaveBeenCalledTimes(1);
+    drawer.removeEventListener('ds-close-click', mockCloseHandler);
+  });
+
+  it('should not call the event handler after unmounting', () => {
+    const { unmount } = renderDrawer({ 'is-open': 'true', 'has-trap-focus': 'true' }, children);
+
+    const drawer = document.querySelector('ds-drawer');
+    drawer.addEventListener('ds-close-click', mockCloseHandler);
+
+    unmount();
+    userEvent.keyboard('{Escape}');
+
+    expect(mockCloseHandler).not.toHaveBeenCalled();
+  });
+
+  it('should not call onCloseClick for other key presses', () => {
+    renderDrawer({ 'is-open': 'true', 'has-trap-focus': 'true' }, children);
+
+    const drawer = document.querySelector('ds-drawer');
+    drawer.addEventListener('ds-close-click', mockCloseHandler);
+
+    userEvent.keyboard('a');
+
+    expect(mockCloseHandler).not.toHaveBeenCalled();
+    drawer.removeEventListener('ds-close-click', mockCloseHandler);
+  });
+});
