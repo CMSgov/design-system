2edd3996c008e758ac6192631e065ea7d9b9149a
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-10-09 09:56:51 -0400

    Initial filter chip code

---
+++ packages/design-system/src/components/web-components/ds-filter-chip/ds-filter-chip.tsx
@@ -0,0 +1,48 @@
+import { define } from '../preactement/define';
+import FilterChip from '../../FilterChip/FilterChip';
+import { FilterChipProps } from '../../FilterChip/FilterChip';
+import { parseBooleanAttr } from '../wrapperUtils';
+
+const attributes = [
+  'aria-clear-label',
+  'class-names',
+  'label',
+  'root-id',
+  'size',
+  'use-alternate-icon',
+] as const;
+
+/* eslint-disable @typescript-eslint/no-namespace */
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'ds-filter-chip': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          [K in (typeof attributes)[number]]?: string;
+        },
+        HTMLElement
+      >;
+    }
+  }
+}
+/* eslint-enable */
+
+interface WrapperProps extends Omit<FilterChipProps, 'useAlternateIcon'> {
+  useAlternateIcon?: string;
+}
+
+const Wrapper = ({ label, useAlternateIcon, ...otherProps }: WrapperProps) => (
+  <FilterChip label={label} useAlternateIcon={parseBooleanAttr(useAlternateIcon)} {...otherProps} />
+);
+
+define('ds-filter-chip', () => Wrapper, {
+  attributes,
+  events: [
+    [
+      'onDelete',
+      (event: MouseEvent | KeyboardEvent) => ({
+        detail: { event },
+      }),
+    ],
+  ],
+} as any);
