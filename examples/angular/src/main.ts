a98c3f26b466fa2ece6eec38717f348674c2986b
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-10-11 14:20:52 -0700

    Configure it to allow web components and add a couple simple ones

--- examples/angular/src/main.ts
+++ examples/angular/src/main.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import '@cmsgov/design-system/web-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
     <div class="ds-content">
+      <ds-skip-nav href="#main"></ds-skip-nav>
+      <ds-usa-banner></ds-usa-banner>
       <main id="main" class="ds-l-container">
         <h1 class="ds-text-heading--4xl ds-u-margin-top--2">Hello from {{ name }}!</h1>
         <a target="_blank" href="https://angular.dev/overview"> Learn more about Angular </a>
