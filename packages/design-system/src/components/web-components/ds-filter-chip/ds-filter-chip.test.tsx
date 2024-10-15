b0b9fdd3fb6977ae9112299e5cdd7c592704e8b0
Jack Ryan <jack.jackryan@gmail.com>
2024-10-15 15:27:52 -0400

    [WNMGDS-2921] Implement Filter Chip Web Component (#3270)
    * Initial filter chip code
    
    * Initial filter chip tests
    
    * Add initial stories
    
    * Fix typo and update snapshot
    
    * Unit tests are passing
    
    * Import tooltip and filter chip
    
    * Add small filter chip example to astro
    
    * Update cdn-webcomponents example
    
    * Hiding controls on multi chip story since they don't seem to work
    
    * Revert "Add small filter chip example to astro"
    
    This reverts commit 4e4446f9fc7f8d0b2bb6e6bfdf83632cf81c997f.
    
    * Change aria-clear-label to clear-label to avoid adding a nonexistent aria label
    
    * Whole lotta snapshot updates
    
    ---------
    
    Co-authored-by: jack-ryan-nava-pbc <jackryan@navapbc.com>

---
+++ packages/design-system/src/components/web-components/ds-filter-chip/ds-filter-chip.test.tsx
@@ -0,0 +1,109 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import './ds-filter-chip';
+
+const defaultAttrs = {
+  label: 'Filter Chip!',
+  'root-id': '12345',
+  'class-name': 'ds-not-a-real-class',
+};
+
+describe('ds-filter-chip', () => {
+  const renderFilterChip = (customProps = {}) => {
+    return render(<ds-filter-chip {...defaultAttrs} {...customProps} />);
+  };
+
+  it('should include children as label', () => {
+    renderFilterChip();
+    const chipEl = screen.getByRole('button');
+    expect(chipEl.querySelector('.ds-c-filter-chip__label').textContent).toEqual('Filter Chip!');
+    expect(chipEl).toMatchSnapshot();
+  });
+
+  it('should use different aria label if provided', () => {
+    renderFilterChip({ 'clear-label': 'Clear' });
+    const chipEl = screen.getByRole('button');
+    expect(chipEl.querySelector('.ds-u-visibility--screen-reader').textContent).toEqual(
+      'Clear Filter Chip! filter .'
+    );
+  });
+
+  it('generates an id when an empty string is provided', () => {
+    renderFilterChip({ 'root-id': '' });
+    const idRegex = /filter-chip--\d+/;
+    expect(screen.getByRole('button').id).toMatch(idRegex);
+  });
+
+  it('generates an id when undefined is provided', () => {
+    renderFilterChip({ 'root-id': undefined });
+    const idRegex = /filter-chip--\d+/;
+    expect(screen.getByRole('button').id).toMatch(idRegex);
+  });
+
+  describe('onDelete', () => {
+    it('should call onDelete function when clicked', () => {
+      const onDelete = jest.fn();
+      renderFilterChip();
+
+      const chipEl = document.querySelector('ds-filter-chip');
+      expect(chipEl).toBeDefined();
+      chipEl.addEventListener('ds-delete', onDelete);
+
+      userEvent.click(screen.getByRole('button'));
+      expect(onDelete).toHaveBeenCalled();
+      chipEl.removeEventListener('ds-delete', onDelete);
+    });
+
+    it('should call onDelete when certain keyboard keys are pressed', () => {
+      const onDelete = jest.fn();
+      renderFilterChip();
+
+      const chipEl = document.querySelector('ds-filter-chip');
+      chipEl.addEventListener('ds-delete', onDelete);
+      screen.getByRole('button').focus();
+
+      userEvent.keyboard('{Enter}');
+      expect(onDelete).toHaveBeenCalledTimes(1);
+      userEvent.keyboard('{Space}');
+      expect(onDelete).toHaveBeenCalledTimes(2);
+      userEvent.keyboard('{Backspace}');
+      expect(onDelete).toHaveBeenCalledTimes(3);
+      userEvent.keyboard('{Delete}');
+      expect(onDelete).toHaveBeenCalledTimes(4);
+
+      chipEl.removeEventListener('ds-delete', onDelete);
+    });
+
+    it('should not call onDelete when most keyboard keys are pressed', () => {
+      const onDelete = jest.fn();
+      renderFilterChip();
+
+      const chipEl = screen.getByRole('button');
+      chipEl.addEventListener('ds-on-delete', onDelete);
+
+      screen.getByRole('button').focus();
+      userEvent.keyboard('{Tab}');
+      expect(onDelete).not.toHaveBeenCalled();
+      userEvent.keyboard('a');
+      expect(onDelete).not.toHaveBeenCalled();
+
+      chipEl.removeEventListener('ds-delete', onDelete);
+    });
+  });
+
+  it('should set big CSS class', () => {
+    renderFilterChip({ size: 'big' });
+    const chipEl = screen.getByRole('button');
+    expect(chipEl.classList).toContain('ds-c-filter-chip__button--big');
+  });
+
+  it('should use alternate icon', () => {
+    renderFilterChip({ 'use-alternate-icon': 'true' });
+    const chipEl = screen.getByRole('button');
+    const iconContainerEl = chipEl.querySelector('.ds-c-filter-chip__clear-icon-container');
+
+    expect(iconContainerEl).toBeDefined();
+    expect(iconContainerEl.classList).toContain('ds-c-filter-chip__clear-icon-alternate-container');
+    expect(iconContainerEl).toMatchSnapshot();
+  });
+});
