368bda63367e1c82ae1ad9bd245781a1a53a4492
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-10-16 14:38:39 -0700

    Strip the example down to just a few components
    These components show the current issue, which is that our custom elements don't have any initial `innerHTML`, which causes various symptoms. For the month picker, we don't get the default checked state or the disabled months. For the alert content, we don't get any of the alert content, but we get the header because it was defined with an attribute.

--- examples/angular/src/main.ts
+++ examples/angular/src/main.ts
@@ -16,10 +16,6 @@ import '@cmsgov/design-system/web-components';
         <div class="ds-u-measure--base ds-u-padding-bottom--4">
           <p>This is an example showing how to use our library of interactive web components.</p>
 
-          <ds-spinner aria-valuetext="aria sets spinner label!"
-            >children don't set spinner label</ds-spinner
-          >
-
           <ds-alert
             variation="success"
             heading="You've loaded the web-components example"
@@ -38,12 +34,6 @@ import '@cmsgov/design-system/web-components';
               >Break things</ds-button
             >
           </ds-alert>
-
-          <ds-choice type="checkbox" label="I agree to the terms and conditions" name="agree">
-            <div slot="checked-children">
-              <ds-alert class="ds-u-margin-top--1">Cool, we hoped you'd check this box.</ds-alert>
-            </div>
-          </ds-choice>
 
           <ds-accordion bordered="true" class="ds-u-margin-top--2">
             <ds-accordion-item heading="First Amendment" default-open="true">
@@ -72,96 +62,6 @@ import '@cmsgov/design-system/web-components';
             <input type="checkbox" value="11" checked disabled />
             <input type="checkbox" value="12" disabled />
           </ds-month-picker>
-
-          <ds-choice-list type="radio" label="Choice list example" name="foo">
-            <ds-choice label="Choice without associated children" value="no children" />
-
-            <ds-choice
-              label="Checked children"
-              hint="Selecting this checkbox will reveal its associated children."
-              value="checked children"
-            >
-              <div slot="checked-children">
-                <div class="ds-c-choice__checkedChild">
-                  <ds-alert heading="🫣 Tag! You're it!">
-                    You can reveal content by applying <code>checked-children</code> to the
-                    <code>slot</code> attribute of an HTML element. Do not forget to include a
-                    <code>div</code> element with the class
-                    <code>ds-c-choice__checkedChild</code> to whatever content you want to show/hide
-                    so it gets side border showing an association with its choice parent.
-                  </ds-alert>
-                </div>
-              </div>
-            </ds-choice>
-            <ds-choice
-              label="Unchecked children"
-              hint="Selecting this checkbox will hide its associated children."
-              value="unchecked children"
-            >
-              <div slot="unchecked-children">
-                <div class="ds-c-choice__checkedChild">
-                  <ds-alert variation="warn" heading="I banish thee!">
-                    You can hide content by applying <code>unchecked-children</code> to the
-                    <code>slot</code> attribute of an HTML element. Do not forget to include a
-                    <code>div</code> element with the class
-                    <code>ds-c-choice__checkedChild</code> to whatever content you want to show/hide
-                    so it gets side border showing an association with its choice parent.
-                  </ds-alert>
-                </div>
-              </div>
-            </ds-choice>
-          </ds-choice-list>
-
-          <h2 class="ds-text-heading--2xl">Adding machine</h2>
-          <form id="calculator">
-            <ds-dropdown id="operation" name="operation" label="Operation">
-              <option value="addition">Addition</option>
-              <option value="subtraction">Subtraction</option>
-            </ds-dropdown>
-            <div
-              class="ds-u-display--flex ds-u-align-items--center ds-u-margin-y--2"
-              style="gap: var(--spacer-2)"
-            >
-              <input
-                type="number"
-                id="a"
-                name="a"
-                value="20"
-                class="ds-c-field ds-c-field--small"
-              />
-              <span id="sign">+</span>
-              <input
-                type="number"
-                id="b"
-                name="b"
-                value="30"
-                class="ds-c-field ds-c-field--small"
-              />
-              =
-              <output name="result" for="a b"></output>
-            </div>
-            <ds-button variation="solid" type="submit">Calculate</ds-button>
-          </form>
-
-          <hr class="ds-u-margin-y--4" />
-
-          <ds-pagination id="pagination" total-pages="10" class="ds-u-margin-y--4"></ds-pagination>
-
-          <h2 class="ds-text-heading--2xl">Controlled text field example</h2>
-          <ds-text-field value="" id="the-text-field" label="Text field example"></ds-text-field>
-
-          <hr class="ds-u-margin-y--4" />
-
-          <h2 class="ds-text-heading--2xl">Autocomplete example</h2>
-          <ds-autocomplete
-            id="the-autocomplete"
-            items='[{"id":"71","name":"Acetaminophen"},{"id":"72","name":"Advil"},{"id":"73","name":"Benadryl"},{"id":"74","name":"Claritin"},{"id":"75","name":"Detrol"},{"id":"76","name":"Excedrin"}]'
-            label="Enter and select a drug to see its cost under each plan."
-            hint="Type a letter to see results, then use ARROW keys to change options, ENTER key to make a selection, ESC to dismiss."
-            clear-search-button="true"
-            class-name="ds-u-padding-bottom--7"
-          >
-          </ds-autocomplete>
         </div>
       </main>
     </div>
