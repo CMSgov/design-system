08e3465fcf6e91182e4ecd856ebefcd3acd05b72
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-10-16 17:09:08 -0700

    Add some web component examples back in

--- examples/angular/src/main.ts
+++ examples/angular/src/main.ts
@@ -16,6 +16,10 @@ import '@cmsgov/design-system/web-components';
         <div class="ds-u-measure--base ds-u-padding-bottom--4">
           <p>This is an example showing how to use our library of interactive web components.</p>
 
+          <ds-spinner aria-valuetext="aria sets spinner label!"
+            >children don't set spinner label</ds-spinner
+          >
+
           <ds-alert
             variation="success"
             heading="You've loaded the web-components example"
@@ -34,6 +38,12 @@ import '@cmsgov/design-system/web-components';
               >Break things</ds-button
             >
           </ds-alert>
+
+          <ds-choice type="checkbox" label="I agree to the terms and conditions" name="agree">
+            <div slot="checked-children">
+              <ds-alert class="ds-u-margin-top--1">Cool, we hoped you'd check this box.</ds-alert>
+            </div>
+          </ds-choice>
 
           <ds-accordion bordered="true" class="ds-u-margin-top--2">
             <ds-accordion-item heading="First Amendment" default-open="true">
@@ -62,6 +72,44 @@ import '@cmsgov/design-system/web-components';
             <input type="checkbox" value="11" checked disabled />
             <input type="checkbox" value="12" disabled />
           </ds-month-picker>
+
+          <ds-choice-list type="radio" label="Choice list example" name="foo">
+            <ds-choice label="Choice without associated children" value="no children" />
+            <ds-choice
+              label="Checked children"
+              hint="Selecting this checkbox will reveal its associated children."
+              value="checked children"
+            >
+              <div slot="checked-children">
+                <div class="ds-c-choice__checkedChild">
+                  <ds-alert heading="🫣 Tag! You're it!">
+                    You can reveal content by applying <code>checked-children</code> to the
+                    <code>slot</code> attribute of an HTML element. Do not forget to include a
+                    <code>div</code> element with the class
+                    <code>ds-c-choice__checkedChild</code> to whatever content you want to show/hide
+                    so it gets side border showing an association with its choice parent.
+                  </ds-alert>
+                </div>
+              </div>
+            </ds-choice>
+            <ds-choice
+              label="Unchecked children"
+              hint="Selecting this checkbox will hide its associated children."
+              value="unchecked children"
+            >
+              <div slot="unchecked-children">
+                <div class="ds-c-choice__checkedChild">
+                  <ds-alert variation="warn" heading="I banish thee!">
+                    You can hide content by applying <code>unchecked-children</code> to the
+                    <code>slot</code> attribute of an HTML element. Do not forget to include a
+                    <code>div</code> element with the class
+                    <code>ds-c-choice__checkedChild</code> to whatever content you want to show/hide
+                    so it gets side border showing an association with its choice parent.
+                  </ds-alert>
+                </div>
+              </div>
+            </ds-choice>
+          </ds-choice-list>
         </div>
       </main>
     </div>
