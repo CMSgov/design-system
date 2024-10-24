e4fa9de08ef521d3956de328682547f06f0d8829
tamara-corbalt <tamara@corbalt.com>
2024-10-24 11:06:16 -0400

    Adds comments explaining the need for the additional parsing.

--- packages/design-system/src/components/web-components/ds-tabs/ds-tabs.tsx
+++ packages/design-system/src/components/web-components/ds-tabs/ds-tabs.tsx
@@ -4,36 +4,44 @@ import { TabPanelProps } from '../../Tabs/TabPanel';
 import { findElementsOfType } from '../../utilities/findElementsOfType';
 import { createElement } from 'react';
 
+/**
+ * Parses custom web components that are `ds-tab-panel` elements and converts them into React components.
+ * This is necessary for the `Tabs` interface, which expects `TabPanel` children to be React components.
+ * The function handles `ds-tab-panel` elements by converting their kebab-cased attributes into camelCased React props.
+ */
+function parseChildren(node) {
+  const elements = findElementsOfType(['ds-tab-panel'], node);
+
+  const mappedElements = elements.map((element) => {
+    const { children, ...attrs } = element.props || {};
+
+    // Rename `root-id` to `id` if it exists.
+    if ('root-id' in attrs) {
+      attrs.id = attrs['root-id'];
+      delete attrs['root-id'];
+    }
+
+    /**
+     * Convert kebab-cased attributes to camelCased props.
+     * E.g., `tab-class-name` becomes `tabClassName`.
+     */
+    const camelCaseAttrs = Object.keys(attrs).reduce((acc, key) => {
+      const camelCaseKey = key.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
+      acc[camelCaseKey] = attrs[key];
+      return acc;
+    }, {} as Partial<TabPanelProps>);
+
+    const reactElem = createElement(TabPanel, camelCaseAttrs as TabPanelProps, children);
+
+    return reactElem;
+  });
+
+  return mappedElements;
+}
+
 const attributes = ['default-selected-id', 'selected-id', 'tablist-class-name', 'tabs-aria-label'];
 
 const Wrapper = ({ tabsAriaLabel, ...props }) => {
-  function parseChildren(node) {
-    const elements = findElementsOfType(['ds-tab-panel'], node);
-
-    const mappedElements = elements.map((element) => {
-      const { children, ...attrs } = element.props || {};
-
-      // Rename `root-id` to `id` if it exists.
-      if ('root-id' in attrs) {
-        attrs.id = attrs['root-id'];
-        delete attrs['root-id'];
-      }
-
-      // Parse kebab-cased attributes into cameCased props.
-      const camelCaseAttrs = Object.keys(attrs).reduce((acc, key) => {
-        const camelCaseKey = key.replace(/-([a-z])/g, (match, letter) => letter.toUpperCase());
-        acc[camelCaseKey] = attrs[key];
-        return acc;
-      }, {} as Partial<TabPanelProps>);
-
-      const reactElem = createElement(TabPanel, camelCaseAttrs as TabPanelProps, children);
-
-      return reactElem;
-    });
-
-    return mappedElements;
-  }
-
   return (
     <Tabs {...props} ariaLabel={tabsAriaLabel}>
       {parseChildren(props.children)}
