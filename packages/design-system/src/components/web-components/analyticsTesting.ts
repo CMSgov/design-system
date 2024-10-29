385b6c51ed3ca44e194edac5a4652a9b622eb15c
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-10-29 14:05:35 -0700

    Do some refactoring so we can reuse these test helper functions in other ways

--- packages/design-system/src/components/web-components/analyticsTesting.ts
+++ packages/design-system/src/components/web-components/analyticsTesting.ts
@@ -1,78 +1,101 @@
 import { UtagContainer } from '../analytics/index';
 import { config } from '../config';
 
+export function mockTealium() {
+  const tealiumMock = jest.fn();
+  (window as any as UtagContainer).utag = {
+    link: tealiumMock,
+  };
+  jest.useFakeTimers();
+  return tealiumMock;
+}
+
+export function unmockTealium() {
+  jest.resetAllMocks();
+  jest.useRealTimers();
+}
+
+export function waitForAnalytics() {
+  jest.advanceTimersToNextTimer();
+  new Promise((resolve) => setTimeout(resolve, 0));
+}
+
+type TestFunction = (params: {
+  tealiumMock: jest.Mock<any, any>;
+  waitForAnalytics: typeof waitForAnalytics;
+}) => Promise<any>;
+
+export function testAnalytics(testName: string, fn: TestFunction) {
+  test(testName, async () => {
+    const tealiumMock = mockTealium();
+    const returnVal = await fn({ tealiumMock, waitForAnalytics });
+    unmockTealium();
+    return returnVal;
+  });
+}
+
+testAnalytics.skip = (testName: string, fn: TestFunction) => {
+  test.skip(testName, () => fn({} as any));
+};
+
 export function testAnalyticsEvents({ tagName, renderComponent, configField, triggerAction }) {
-  function before() {
+  beforeEach(() => {
     config({ [configField]: true });
-    const tealiumMock = jest.fn();
-    (window as any as UtagContainer).utag = {
-      link: tealiumMock,
-    };
-    jest.useFakeTimers();
-    return tealiumMock;
-  }
+  });
 
-  function after() {
+  afterEach(() => {
     config({ [configField]: false });
-    jest.resetAllMocks();
-    jest.useRealTimers();
-  }
+  });
 
-  function waitForAnalytics() {
-    jest.advanceTimersToNextTimer();
-    new Promise((resolve) => setTimeout(resolve, 0));
-  }
-
-  it('sends analytics event', async () => {
-    const tealiumMock = before();
+  testAnalytics('sends analytics event', async ({ tealiumMock, waitForAnalytics }) => {
     renderComponent();
     await triggerAction();
     await waitForAnalytics();
     expect(tealiumMock.mock.calls[0]).toMatchSnapshot();
-    after();
   });
 
-  it('disables analytics event tracking', async () => {
-    const tealiumMock = before();
+  testAnalytics('disables analytics event tracking', async ({ tealiumMock, waitForAnalytics }) => {
     renderComponent({ analytics: 'false' });
     await triggerAction();
     await waitForAnalytics();
     expect(tealiumMock).not.toHaveBeenCalled();
-    after();
   });
 
-  it('setting analytics to true overrides flag value', async () => {
-    const tealiumMock = before();
-    config({ [configField]: false });
-    renderComponent({ analytics: 'true' });
-    await triggerAction();
-    await waitForAnalytics();
-    expect(tealiumMock).toHaveBeenCalled();
-    after();
-  });
+  testAnalytics(
+    'setting analytics to true overrides flag value',
+    async ({ tealiumMock, waitForAnalytics }) => {
+      config({ [configField]: false });
+      renderComponent({ analytics: 'true' });
+      await triggerAction();
+      await waitForAnalytics();
+      expect(tealiumMock).toHaveBeenCalled();
+    }
+  );
 
-  it('overrides analytics event tracking on open', async () => {
-    const tealiumMock = before();
-    renderComponent({ 'analytics-label-override': 'alternate content' });
-    await triggerAction();
-    await waitForAnalytics();
-    expect(tealiumMock.mock.calls[0]).toMatchSnapshot();
-    after();
-  });
+  testAnalytics(
+    'overrides analytics event tracking on open',
+    async ({ tealiumMock, waitForAnalytics }) => {
+      renderComponent({ 'analytics-label-override': 'alternate content' });
+      await triggerAction();
+      await waitForAnalytics();
+      expect(tealiumMock.mock.calls[0]).toMatchSnapshot();
+    }
+  );
 
-  it.skip('allows default analytics function to be ovewridden', async () => {
-    const tealiumMock = before();
-    let analyticsEvent;
-    renderComponent();
-    document.querySelector(tagName).addEventListener('ds-analytics-event', (event: any) => {
-      event.preventDefault();
-      analyticsEvent = event.detail.event;
-    });
-    await triggerAction();
-    await waitForAnalytics();
-    expect(tealiumMock).not.toHaveBeenCalled();
-    expect(analyticsEvent).toBeDefined();
-    expect(analyticsEvent).toMatchSnapshot();
-    after();
-  });
+  testAnalytics.skip(
+    'allows default analytics function to be ovewridden',
+    async ({ tealiumMock, waitForAnalytics }) => {
+      let analyticsEvent;
+      renderComponent();
+      document.querySelector(tagName).addEventListener('ds-analytics-event', (event: any) => {
+        event.preventDefault();
+        analyticsEvent = event.detail.event;
+      });
+      await triggerAction();
+      await waitForAnalytics();
+      expect(tealiumMock).not.toHaveBeenCalled();
+      expect(analyticsEvent).toBeDefined();
+      expect(analyticsEvent).toMatchSnapshot();
+    }
+  );
 }
