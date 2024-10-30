498ce76afaa0968dffab3938a062e40dbd262d3d
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-10-29 20:53:21 -0400

    Updated stories and snapshots

--- packages/design-system/src/components/web-components/ds-modal-dialog/ds-modal-dialog.stories.tsx
+++ packages/design-system/src/components/web-components/ds-modal-dialog/ds-modal-dialog.stories.tsx
@@ -102,88 +102,6 @@ const meta: Meta = {
 };
 export default meta;
 
-const LongContent = ({ args }) => {
-  useEffect(() => {
-    const button = document.querySelector('ds-button');
-    const modal = document.querySelector('ds-modal-dialog');
-    const toggleModal = (event): void => {
-      const targetType = event.target.nodeName ?? '';
-      if (targetType === 'DS-BUTTON') {
-        action('ds-click')(event);
-      } else {
-        action('ds-exit')(event);
-      }
-      const isOpen = modal.getAttribute('is-open');
-      if (isOpen === 'true') {
-        modal.setAttribute('is-open', 'false');
-      } else {
-        modal.setAttribute('is-open', 'true');
-      }
-    };
-    button.addEventListener('ds-click', toggleModal);
-    modal.addEventListener('ds-exit', toggleModal);
-    return () => {
-      button.removeEventListener('ds-click', toggleModal);
-      modal.removeEventListener('ds-exit', toggleModal);
-    };
-  });
-
-  return (
-    <div className="ds-u-measure--base">
-      <h1>The United States Constitution</h1>
-      <p>
-        We the People of the United States, in Order to form a more perfect Union, establish
-        Justice, insure domestic Tranquility, provide for the common defence, promote the general
-        Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and
-        establish this Constitution for the United States of America.
-      </p>
-      <h2>Article I</h2>
-      <h3>Section 1: Congress</h3>
-      <p>
-        All legislative Powers herein granted shall be vested in a Congress of the United States,
-        which shall consist of a Senate and House of Representatives.
-      </p>
-
-      <ds-button>Open Modal</ds-button>
-      <ds-modal-dialog {...args}></ds-modal-dialog>
-
-      <h3>Section 2: The House of Representatives</h3>
-      <p>
-        The House of Representatives shall be composed of Members chosen every second Year by the
-        People of the several States, and the Electors in each State shall have the Qualifications
-        requisite for Electors of the most numerous Branch of the State Legislature.
-      </p>
-      <p>
-        No Person shall be a Representative who shall not have attained to the Age of twenty five
-        Years, and been seven Years a Citizen of the United States, and who shall not, when elected,
-        be an Inhabitant of that State in which he shall be chosen.
-      </p>
-      <p>
-        Representatives and direct Taxes shall be apportioned among the several States which may be
-        included within this Union, according to their respective Numbers, which shall be determined
-        by adding to the whole Number of free Persons, including those bound to Service for a Term
-        of Years, and excluding Indians not taxed, three fifths of all other Persons. The actual
-        Enumeration shall be made within three Years after the first Meeting of the Congress of the
-        United States, and within every subsequent Term of ten Years, in such Manner as they shall
-        by Law direct.The number of Representatives shall not exceed one for every thirty Thousand,
-        but each State shall have at Least one Representative; and until such enumeration shall be
-        made, the State of New Hampshire shall be entitled to chuse three, Massachusetts eight,
-        Rhode-Island and Providence Plantations one, Connecticut five, New-York six, New Jersey
-        four, Pennsylvania eight, Delaware one, Maryland six, Virginia ten, North Carolina five,
-        South Carolina five, and Georgia three.
-      </p>
-      <p>
-        When vacancies happen in the Representation from any State, the Executive Authority thereof
-        shall issue Writs of Election to fill such Vacancies.
-      </p>
-      <p>
-        The House of Representatives shall chuse their Speaker and other Officers;and shall have the
-        sole Power of Impeachment.
-      </p>
-    </div>
-  );
-};
-
 const Template = (args) => {
   useEffect(() => {
     const button = document.querySelector('ds-button');
@@ -251,7 +169,59 @@ export const BackdropClickExits = {
 
 export const PreventScrollExample = {
   render: function Component(args) {
-    return <LongContent args={args} />;
+    return (
+      <div className="ds-u-measure--base">
+        <h1>The United States Constitution</h1>
+        <p>
+          We the People of the United States, in Order to form a more perfect Union, establish
+          Justice, insure domestic Tranquility, provide for the common defence, promote the general
+          Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and
+          establish this Constitution for the United States of America.
+        </p>
+        <h2>Article I</h2>
+        <h3>Section 1: Congress</h3>
+        <p>
+          All legislative Powers herein granted shall be vested in a Congress of the United States,
+          which shall consist of a Senate and House of Representatives.
+        </p>
+
+        <Template {...args} />
+
+        <h3>Section 2: The House of Representatives</h3>
+        <p>
+          The House of Representatives shall be composed of Members chosen every second Year by the
+          People of the several States, and the Electors in each State shall have the Qualifications
+          requisite for Electors of the most numerous Branch of the State Legislature.
+        </p>
+        <p>
+          No Person shall be a Representative who shall not have attained to the Age of twenty five
+          Years, and been seven Years a Citizen of the United States, and who shall not, when
+          elected, be an Inhabitant of that State in which he shall be chosen.
+        </p>
+        <p>
+          Representatives and direct Taxes shall be apportioned among the several States which may
+          be included within this Union, according to their respective Numbers, which shall be
+          determined by adding to the whole Number of free Persons, including those bound to Service
+          for a Term of Years, and excluding Indians not taxed, three fifths of all other Persons.
+          The actual Enumeration shall be made within three Years after the first Meeting of the
+          Congress of the United States, and within every subsequent Term of ten Years, in such
+          Manner as they shall by Law direct.The number of Representatives shall not exceed one for
+          every thirty Thousand, but each State shall have at Least one Representative; and until
+          such enumeration shall be made, the State of New Hampshire shall be entitled to chuse
+          three, Massachusetts eight, Rhode-Island and Providence Plantations one, Connecticut five,
+          New-York six, New Jersey four, Pennsylvania eight, Delaware one, Maryland six, Virginia
+          ten, North Carolina five, South Carolina five, and Georgia three.
+        </p>
+        <p>
+          When vacancies happen in the Representation from any State, the Executive Authority
+          thereof shall issue Writs of Election to fill such Vacancies.
+        </p>
+        <p>
+          The House of Representatives shall chuse their Speaker and other Officers;and shall have
+          the sole Power of Impeachment.
+        </p>
+      </div>
+    );
   },
   args: {
     alert: 'false',
