2313c2b5568a8ae5d1043d025372ca17049866c6
Tamara deMent (Corbalt) <tamara@corbalt.com>
2024-11-04 16:44:07 -0500

    [WMNGDS-3022] Removes `ds-tab-panel` module (#3304)
    * Adds prop validation to ds-tabs parseChildren function.
    
    * Removes ds-tab-panel module
    
    * Removes data prefix from classname attribute.
    
    * Adds a unit test to ensure child elements attrs are parsed correctly
    
    * Adds documentation for the expected attributes for ds-tabs children elements.
    
    * Refines docs
    
    * Update snaps
    
    * Moves an tab attribute to 'required' in docs.
    
    * Extends jsx.IntrinsicElements so typescript recognizes ds-tab-panel elements.
    
    * Fixes typo
    
    * Removing my test attributes from storybook file.
    
    * Move  JSX extension to separate  file to isolate types

---
+++ packages/design-system/src/components/web-components/ds-tabs/ds-tab-panel.types.d.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'ds-tab-panel': JSX.IntrinsicElements['div'] & {
+        id: string;
+        'class-name'?: string;
+        selected?: string;
+        disabled?: string;
+        tab: string;
+        'tab-class-name'?: string;
+        'tab-href'?: string;
+        'tab-id'?: string;
+      };
+    }
+  }
+}
