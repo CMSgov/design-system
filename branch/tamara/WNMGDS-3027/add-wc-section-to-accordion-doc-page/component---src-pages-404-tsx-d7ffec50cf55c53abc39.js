95f55be9fe9c525bc59fcc71604ab929a3111076
tamara-corbalt <tamara@corbalt.com>
2024-11-05 10:46:50 -0500

    Updates

---
+++ branch/tamara/WNMGDS-3027/add-wc-section-to-accordion-doc-page/component---src-pages-404-tsx-d7ffec50cf55c53abc39.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_cmsgov_cms_design_system_docs=self.webpackChunk_cmsgov_cms_design_system_docs||[]).push([[218],{3503:function(t,s,e){e.r(s);var c=e(9478),n=e(2959),o=e(726),a=e(2322);s.default=t=>{let{data:s,location:e}=t;const m=(0,n.Z)();return(0,a.jsx)(c.Z,{frontmatter:s.mdx.frontmatter,location:e,theme:m,children:(0,a.jsx)(o.Z,{data:s.mdx.body,theme:m})})}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-d7ffec50cf55c53abc39.js.map
