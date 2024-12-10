b6bf625c6832722324c3b6f8fcf832345317ea7b
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-12-10 10:30:10 -0800

    [WNMGDS-3057] Update analytics-content-extraction method to support slots (#3347)
    * Re-apply changes to how we find analytics content in our React components
    
    This reverts commit 178c7935d9f5e661ca7c960dbac74e4924cb9df8.
    
    * Actually, get rid of the `trim` and undo the one snapshot change
    
    After doing some more testing, this is actually closer to what the browser does naturally, so it's probably better to match that behavior and not mess with the whitespace

--- packages/design-system/src/components/analytics/getAnalyticsContentFromRefs.ts
+++ packages/design-system/src/components/analytics/getAnalyticsContentFromRefs.ts
@@ -1,7 +1,34 @@
 import { RefObject } from 'react';
 
+/**
+ * Gets text content from an element and even follows `slot` elements across Shadow DOM
+ * barriers to get the full text that the user sees.
+ */
+function getTextContent(nodes: Node[]): string {
+  return nodes
+    .map((node) => {
+      if (node.nodeType === Node.TEXT_NODE) {
+        return node.textContent;
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        if ((node as HTMLElement).tagName === 'SLOT') {
+          // If it's a slot, it won't actually have any content in itself, so we have to
+          // follow the trail to its "assigned nodes" and look there.
+          const assignedNodes = (node as HTMLSlotElement).assignedNodes({ flatten: true });
+          return getTextContent(assignedNodes);
+        } else {
+          return getTextContent(Array.from((node as HTMLElement).childNodes));
+        }
+      } else {
+        return '';
+      }
+    })
+    .join('');
+}
+
 export function getAnalyticsContentFromRefs(refs: RefObject<any>[]): string | undefined {
-  return refs.map((ref) => ref.current?.textContent).find((textContent) => textContent);
+  return refs
+    .map((ref) => (ref.current ? getTextContent(Array.from(ref.current.childNodes)) : undefined))
+    .find((textContent) => textContent);
 }
 
 export default getAnalyticsContentFromRefs;
