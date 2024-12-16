7590964451b802bbcbf986cac9213c7256e3b867
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2024-12-16 13:06:24 -0800

    [NO-TICKET] Change the failure threshold for VRT screenshots (#3353)
    * Add some interaction test helpers for consistency
    
    * Apply the same helper functions to all the other interaction test modules
    
    * Custom matcher things that don't work
    
    I don't want to have to write a bunch of extra "matcher" code. I just want to return the result of the base `toHaveScreenshot` matcher function, but they don't expose it! If I try to run the solution with `custom-matcher.js`, it will yell thusly:
    
    ```
    Error: Package subpath './lib/matchers/toMatchSnapshot.js' is not defined by "exports" in /work/node_modules/@playwright/test/package.json
        at new NodeError (node:internal/errors:399:5)
    ```
    
    If I try to use the one from `expect.ts`, it doesn't like that `baseExpect(page).toHaveScreenshot` returns void. The underlying "matcher" function returns a status object, but when you call `expect(something).theFunction`, it's a void return type.
    
    I think what this all means is that I'm just going to create a helper function instead of extending `expect`. That'll make it a lot clearer in our code that we aren't using the default version
    
    * Revert "Custom matcher things that don't work"
    
    This reverts commit 3b337d7f82f28588202e459ae7963c1755cb7ed3.
    
    * Create an `expectScreenshot` helper and use it everywhere
    
    * Fix unit tests picking up the interaction-test helper module

--- tests/browser/examples.test.ts
+++ tests/browser/examples.test.ts
@@ -1,4 +1,5 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { expectScreenshot } from './expectScreenshot';
 
 const DOMAIN = 'http://localhost:8080';
 const isSmokeTest = Boolean(process.env.SMOKE && JSON.parse(process.env.SMOKE));
@@ -8,7 +9,7 @@ function testCdnExample(exampleName: string) {
     test('matches snapshot', async ({ page }) => {
       // Wait until network idle so we're sure to get the fonts
       await page.goto(`${DOMAIN}/${exampleName}`, { waitUntil: 'networkidle' });
-      await expect(page).toHaveScreenshot(`${exampleName}.png`, { fullPage: true });
+      await expectScreenshot(page, `${exampleName}.png`);
     });
   });
 }
@@ -21,33 +22,33 @@ testCdnExample('cdn-web-components');
 test.describe('preact-app', () => {
   test('matches snapshot', async ({ page }) => {
     await page.goto(`${DOMAIN}/preact-app`);
-    await expect(page).toHaveScreenshot('preact-app.png', { fullPage: true });
+    await expectScreenshot(page, 'preact-app.png');
   });
 });
 
 test.describe('preact-react-app', () => {
   test('matches snapshot', async ({ page }) => {
     await page.goto(`${DOMAIN}/preact-react-app`);
-    await expect(page).toHaveScreenshot('preact-react-app.png', { fullPage: true });
+    await expectScreenshot(page, 'preact-react-app.png');
   });
 });
 
 test.describe('react-app', () => {
   test('matches snapshot', async ({ page }) => {
     await page.goto(`${DOMAIN}/react-app`);
-    await expect(page).toHaveScreenshot('react-app.png', { fullPage: true });
+    await expectScreenshot(page, 'react-app.png');
   });
 });
 
 test.describe('astro', () => {
   test('react page matches snapshot', async ({ page }) => {
     await page.goto(`${DOMAIN}/astro/dist/react`);
-    await expect(page).toHaveScreenshot('astro-react.png', { fullPage: true });
+    await expectScreenshot(page, 'astro-react.png');
   });
 
   test('web-components page matches snapshot', async ({ page }) => {
     await page.goto(`${DOMAIN}/astro/dist/web-components`);
-    await expect(page).toHaveScreenshot('astro-web-components.png', { fullPage: true });
+    await expectScreenshot(page, 'astro-web-components.png');
   });
 });
 
@@ -55,19 +56,17 @@ if (!isSmokeTest) {
   test.describe('astro-themes', () => {
     test('healthcare web-components page matches snapshot', async ({ page }) => {
       await page.goto(`${DOMAIN}/astro-themes/dist/healthcare`);
-      await expect(page).toHaveScreenshot('astro-healthcare-web-components.png', {
-        fullPage: true,
-      });
+      await expectScreenshot(page, 'astro-healthcare-web-components.png');
     });
 
     test('medicare web-components page matches snapshot', async ({ page }) => {
       await page.goto(`${DOMAIN}/astro-themes/dist/medicare`);
-      await expect(page).toHaveScreenshot('astro-medicare-web-components.png', { fullPage: true });
+      await expectScreenshot(page, 'astro-medicare-web-components.png');
     });
 
     test('cmsgov web-components page matches snapshot', async ({ page }) => {
       await page.goto(`${DOMAIN}/astro-themes/dist/cmsgov`);
-      await expect(page).toHaveScreenshot('astro-cmsgov-web-components.png', { fullPage: true });
+      await expectScreenshot(page, 'astro-cmsgov-web-components.png');
     });
   });
 }
