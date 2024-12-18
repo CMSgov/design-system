5a84e88c8d1d71c93a24724c978b05c0334a13fe
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-12-18 16:46:28 -0500

    Add ds-drawer

---
+++ examples/angular/src/app/drawerWrapper/drawer.component.ts
@@ -0,0 +1,28 @@
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  EventEmitter,
+  input,
+  Input,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-drawer',
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './drawer.component.html',
+  styleUrls: ['./drawer.component.css'],
+})
+export class DrawerComponent {
+  @Input() isOpen = 'false';
+  @Output() isOpenChange = new EventEmitter<string>();
+  footerBody = input<string>();
+  footerTitle = input<string>();
+  drawerHeading = input<string>();
+
+  closeDrawer() {
+    this.isOpen = 'false';
+    this.isOpenChange.emit(this.isOpen);
+  }
+}
