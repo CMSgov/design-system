7e79ea35fa3ab861b1ffb9751802a78eeeaa409a
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-12-18 16:46:50 -0500

    Add ds-review

---
+++ examples/angular/src/app/reviewWrapper/review.component.ts
@@ -0,0 +1,14 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, input } from '@angular/core';
+
+@Component({
+  selector: 'app-review',
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './review.component.html',
+  styleUrls: ['./review.component.css'],
+})
+export class ReviewComponent {
+  edit_href = input<string>('#');
+  // Overriding the default in our example component.
+  heading_level = input<string>('2');
+}
