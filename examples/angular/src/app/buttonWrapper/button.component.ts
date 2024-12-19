95aac4902006fff3982fd782298c78b82fe9a2c4
jack-ryan-nava-pbc <jackryan@navapbc.com>
2024-12-19 10:12:50 -0500

    Enabled analytics events on button

--- examples/angular/src/app/buttonWrapper/button.component.ts
+++ examples/angular/src/app/buttonWrapper/button.component.ts
@@ -8,10 +8,15 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, input, Output } from '
   styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
-  buttonVariation = input.required<string>();
+  buttonVariation = input<string>('solid');
+  additionalClasses = input<string>('');
   @Output() buttonClickEvent = new EventEmitter<string>();
 
   handleClick() {
     this.buttonClickEvent.emit('warn');
+  }
+
+  analyticsEvent() {
+    console.log('Button analytics event');
   }
 }
