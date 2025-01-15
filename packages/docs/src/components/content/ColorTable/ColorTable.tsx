c7cba31dc6a2907b78b09b65d1f43a925aaecdaf
Kim Niedermaier <kimniedermaier+cmsds@navapbc.com>
2025-01-15 12:45:03 -0800

    adds `ColorCard` and `ColorTable` components to docs site, ability to render them

---
+++ packages/docs/src/components/content/ColorTable/ColorTable.tsx
@@ -0,0 +1,56 @@
+import { Accordion, AccordionItem } from '@cmsgov/design-system';
+import { ColorCard } from './ColorCard';
+import { FC, PropsWithChildren } from 'react';
+import { clientOnly } from '../../../helpers/clientOnly';
+import { ThemeName, determineColorCategoryUsageByTheme } from '../../../helpers/themeTokens';
+
+interface ColorProps extends PropsWithChildren {
+  /**
+   * color category (e.g. primary, secondary, etc.)
+   */
+  colorCategory: string;
+  /**
+   * Name of currently selected theme
+   */
+  theme: ThemeName;
+}
+
+const ColorTable: FC<ColorProps> = clientOnly(({ colorCategory, theme }) => {
+  const { activeColors, availableColors } = determineColorCategoryUsageByTheme({
+    colorCategory,
+    themeName: theme,
+  });
+
+  return (
+    <>
+      {Boolean(activeColors.length) && (
+        <Accordion bordered className="ds-u-margin-top--2">
+          <AccordionItem heading="Active Colors" defaultOpen={true}>
+            <p className="ds-u-margin-bottom--2">
+              These colors are currently found in components throughout the design system, but don’t
+              represent the entire available palette.
+            </p>
+            {activeColors.map(({ name, attributes }) => (
+              <ColorCard key={name} attributes={attributes} />
+            ))}
+          </AccordionItem>
+        </Accordion>
+      )}
+      {Boolean(availableColors.length) && (
+        <Accordion bordered>
+          <AccordionItem heading="Available Colors" defaultOpen={false}>
+            <p className="ds-u-margin-bottom--2">
+              These colors are available as part of the system palette, but are not currently used
+              in existing components. Using these colors? Let the DS team know.
+            </p>
+            {availableColors.map(({ name, attributes }) => (
+              <ColorCard key={name} attributes={attributes} />
+            ))}
+          </AccordionItem>
+        </Accordion>
+      )}
+    </>
+  );
+});
+
+export default ColorTable;
