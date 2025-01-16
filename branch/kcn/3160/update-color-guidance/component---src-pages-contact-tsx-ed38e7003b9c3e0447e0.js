8bfdde0bf349b13937ec08ebfb3b54400de1eb90
Kim Niedermaier <kimniedermaier+cmsds@navapbc.com>
2025-01-16 15:37:01 -0800

    Updates

---
+++ branch/kcn/3160/update-color-guidance/component---src-pages-contact-tsx-ed38e7003b9c3e0447e0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_cmsgov_cms_design_system_docs=self.webpackChunk_cmsgov_cms_design_system_docs||[]).push([[650],{7187:function(t,s,e){e.r(s);var n=e(8893),a=e(2959),o=e(8364),d=e(2322);s.default=t=>{var s;let{data:e,location:m}=t;const{slug:c}=e.mdx,l=(0,a.Z)();return(0,d.jsx)(n.Z,{frontmatter:e.mdx.frontmatter,location:m,slug:c,theme:l,tableOfContentsData:null===(s=e.mdx.tableOfContents)||void 0===s?void 0:s.items,children:(0,d.jsx)(o.Z,{data:e.mdx.body,theme:l})})}}}]);
+//# sourceMappingURL=component---src-pages-contact-tsx-ed38e7003b9c3e0447e0.js.map
