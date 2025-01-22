cb85a361fd1e1857e27db60c1be8dbf9467c076c
Patrick Wolfert <patrick.wolfert@adhocteam.us>
2025-01-22 15:56:13 -0800

    Missed some places where I needed `--` in the `build-examples.ts` script

--- scripts/build-examples.ts
+++ scripts/build-examples.ts
@@ -4,11 +4,11 @@ const isSmokeTest = Boolean(process.env.SMOKE && JSON.parse(process.env.SMOKE));
 
 shV('npm run apply-example-templates');
 
-shV('npm --prefix ./examples/astro run build --base /astro/dist');
+shV('npm --prefix ./examples/astro run build -- --base /astro/dist');
 shV('npm --prefix ./examples/preact-app run build');
 shV('npm --prefix ./examples/preact-react-app run build');
 shV('npm --prefix ./examples/react-app run build');
 
 if (!isSmokeTest) {
-  shV('npm --prefix ./examples/astro-themes run build --base /astro-themes/dist');
+  shV('npm --prefix ./examples/astro-themes run build -- --base /astro-themes/dist');
 }
