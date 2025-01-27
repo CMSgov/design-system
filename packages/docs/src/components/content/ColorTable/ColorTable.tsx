0ba0518c6fac28f4ff8434e66921369188a6f0b6
Kim Niedermaier <kimniedermaier+cmsds@navapbc.com>
2025-01-27 09:56:23 -0800

    [WNMGDS-3160] Updates docs site color guidance (#3395)
    * adds pagination font family css variable
    
    * adds css for colorCard component
    
    * adds `ColorCard` and `ColorTable` components to docs site, ability to render them
    
    * adds a utility function for capitalizing component names
    
    * takes a first pass at determining color usage by theme based on tokens
    
    * wires up new ColorTable in addition to existing color example list
    
    * refactors color token usage based on multiple categories
    
    * removes `ColorExampleList` in `theme-colors` markdown
    
    * refactors helper functions to determine available colors
    
    * removes `clientOnly` distinction on `ColorTable` component
    
    * moves filter and formatting logic to separate functions
    
    * Include link to confluence page to gather feedback for available but most likely not used colors

---
+++ packages/docs/src/components/content/ColorTable/ColorTable.tsx
@@ -0,0 +1,61 @@
+import { Accordion, AccordionItem } from '@cmsgov/design-system';
+import { FC, PropsWithChildren } from 'react';
+import { ThemeName, determineColorCategoryUsageByTheme } from '../../../helpers/themeTokens';
+import { ColorCard } from './ColorCard';
+
+interface ColorProps extends PropsWithChildren {
+  /**
+   * color categories such as "primary", "secondary, "accent"
+   */
+  colorCategory: string | string[];
+  /**
+   * Name of currently selected theme
+   */
+  theme: ThemeName;
+  /**
+   * Whether to match the color category exactly or not
+   */
+  exactMatch?: boolean;
+}
+
+const ColorTable: FC<ColorProps> = ({ colorCategory, theme, exactMatch }) => {
+  const { activeColors, availableColors } = determineColorCategoryUsageByTheme({
+    colorCategory,
+    themeName: theme,
+    exactMatch,
+  });
+
+  return (
+    <>
+      {Boolean(activeColors.length) && (
+        <Accordion bordered className="ds-u-margin-top--2">
+          <AccordionItem heading="Active Colors" defaultOpen={true}>
+            <p className="ds-u-margin-bottom--2">
+              These colors are currently found in components throughout the design system, but don’t
+              represent the entire available palette.
+            </p>
+            {activeColors.map(({ name, attributes }) => (
+              <ColorCard key={name} attributes={attributes} />
+            ))}
+          </AccordionItem>
+        </Accordion>
+      )}
+      {Boolean(availableColors.length) && (
+        <Accordion bordered>
+          <AccordionItem heading="Available Colors" defaultOpen={false}>
+            <p className="ds-u-margin-bottom--2">
+              These colors are available as part of the system palette, but are not currently used
+              in existing components. Using these colors?{' '}
+              <a href="https://confluence.cms.gov/x/U26MRw">Let the DS team know.</a>
+            </p>
+            {availableColors.map(({ name, attributes }) => (
+              <ColorCard key={name} attributes={attributes} />
+            ))}
+          </AccordionItem>
+        </Accordion>
+      )}
+    </>
+  );
+};
+
+export default ColorTable;
