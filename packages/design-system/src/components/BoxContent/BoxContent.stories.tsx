abffb4695907a7f5d7e5168e4953d76816552d12
jack-ryan-nava-pbc <jackryan@navapbc.com>
2025-01-29 10:49:31 -0500

    Initial styling and component setup

---
+++ packages/design-system/src/components/BoxContent/BoxContent.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BoxContent from './BoxContent';
+
+const meta: Meta = {
+  component: BoxContent,
+  argTypes: {
+    children: { control: 'text' },
+    bordered: { control: 'boolean', defaultValue: true },
+    heading: { control: 'text' },
+    headingLevel: { control: 'text' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof BoxContent>;
+
+const BoxContentTemplate: Story = {
+  render: ({ ...args }) => {
+    return <BoxContent {...args}>{args.children}</BoxContent>;
+  },
+};
+
+export const Default = {
+  ...BoxContentTemplate,
+  args: {
+    heading: 'The Inflation Reduction Act',
+    children:
+      "The Inflation Reduction Act keeps these safvings and lower costs through 2025. If you qualify for savings, you'll find out the lower costs when you shop for plans.",
+    bordered: true,
+  },
+};
---
+++ packages/design-system/src/components/BoxContent/BoxContent.tsx
@@ -0,0 +1,56 @@
+import type * as React from 'react';
+import { FunctionComponent } from 'react';
+import classNames from 'classnames';
+
+export type BoxContentHeadingLevel = '1' | '2' | '3' | '4' | '5' | '6';
+
+interface BoxContentProps {
+  /**
+   * Applies a border to the box content.
+   */
+  bordered?: boolean;
+  /**
+   * Content to be displayed within the Box
+   */
+  children?: React.ReactNode;
+  /**
+   * Additional classes to be added to the component
+   */
+  className?: string;
+  /**
+   * Text for the box content heading
+   */
+  heading?: React.ReactNode;
+  /**
+   * Heading type to override default `<h2>`.
+   */
+  headingLevel?: BoxContentHeadingLevel;
+  /**
+   * Quotation?
+   */
+}
+
+const BoxContent: FunctionComponent<BoxContentProps> = (props: BoxContentProps) => {
+  const { bordered, children, className, heading, headingLevel = '2' } = props;
+
+  const classes = classNames(
+    'ds-c-box-content',
+    bordered && 'ds-c-box-content--bordered',
+    className
+  );
+  let headingElement;
+  if (heading) {
+    const Heading = `h${headingLevel}` as const;
+    headingElement = <Heading className="ds-c-box-content__heading">{heading}</Heading>;
+  }
+  return (
+    <aside className={classes}>
+      <div className="ds-c-box-content__body">
+        {headingElement}
+        <div className={heading ? 'ds-c-box-content__text' : ''}>{children}</div>
+      </div>
+    </aside>
+  );
+};
+
+export default BoxContent;
