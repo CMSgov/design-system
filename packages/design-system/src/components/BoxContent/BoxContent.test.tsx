778c0115fc73b53d50953aad835ed2093ae3e97e
jack-ryan-nava-pbc <jackryan@navapbc.com>
2025-01-31 17:06:05 -0500

    Beginnging tests

---
+++ packages/design-system/src/components/BoxContent/BoxContent.test.tsx
@@ -0,0 +1,8 @@
+import BoxContent from './BoxContent';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+// Test that heading renders properly
+
+// Test that border renders properly
+
+// Test that quote option renders properly
