4b7c2e348f132d30b289f9d36c5c3cee40272e58
jack-ryan-nava-pbc <jackryan@navapbc.com>
2025-02-12 14:55:34 -0500

    Fire analytics event on 404 page load

--- packages/docs/src/pages/404.tsx
+++ packages/docs/src/pages/404.tsx
@@ -4,8 +4,47 @@ import Layout from '../components/layout/Layout';
 import { MdxQuery } from '../helpers/graphQLTypes';
 import useTheme from '../helpers/useTheme';
 import ContentRenderer from '../components/content/ContentRenderer';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    utag: {
+      view: any;
+    };
+  }
+}
 
 const NotFoundPage = ({ data, location }: MdxQuery) => {
+  useEffect(() => {
+    if (window && window.utag) {
+      let env = 'prod';
+
+      switch (location.hostname) {
+        case 'localhost':
+          env = 'dev';
+          break;
+        case 'cmsgov.github.io':
+          env = 'test';
+          break;
+        case 'design.cms.gov':
+          env = 'prod';
+          break;
+        default:
+          env = 'prod';
+      }
+
+      window.utag.view({
+        content_language: 'en',
+        content_type: 'html',
+        logged_in: 'false',
+        page_name: 'Page not found - CMS Design System',
+        page_type: 'true', //If page is a 404 (error page) this is set to true, otherwise it is false
+        site_environment: env, //Used to include or exclude traffic from different testing environments. Ex: test, test0, imp, production
+        site_section: 'Page not found - CMS Design System',
+      });
+    }
+  }, []);
+
   const theme = useTheme();
   return (
     <Layout frontmatter={data.mdx.frontmatter} location={location} theme={theme}>
