4bec9e43ab9bd448cef5fe1642eeb1bcf05f92c1
tamara-corbalt <tamara@corbalt.com>
2025-02-17 11:36:07 -0500

    Moved index.html and index.tsx to playwright directory.

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from '@playwright/experimental-ct-react';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './',
+  testDir: './tests/browser-components',
   /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
   testIgnore: [],
   // testMatch: '**/*.spec.tsx',
