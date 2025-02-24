3adabdb136a286f55ae91a3661ce64d17657b92f
Kim Niedermaier <kimniedermaier+cmsds@navapbc.com>
2025-02-24 10:17:00 -0800

    simplifies jest config preact settings

--- tests/unit/jest.config.js
+++ tests/unit/jest.config.js
@@ -6,7 +6,6 @@ const preactModuleMapper =
   usePreact || useWebComponents
     ? {
         '^react$': 'preact/compat',
-        '^react-dom/test-utils$': 'preact/test-utils',
         '^react-dom$': 'preact/compat',
         '^react/jsx-runtime$': 'preact/jsx-runtime',
       }
