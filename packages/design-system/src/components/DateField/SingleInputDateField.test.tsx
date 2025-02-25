7436703adcef4cf2dadf8c9c7433b70af14abdee
Kim Niedermaier <kimniedermaier+cmsds@navapbc.com>
2025-02-25 13:24:07 -0800

    updates date field unit tests

--- packages/design-system/src/components/DateField/SingleInputDateField.test.tsx
+++ packages/design-system/src/components/DateField/SingleInputDateField.test.tsx
@@ -12,7 +12,10 @@ const defaultProps = {
 };
 
 function renderField(props = {}) {
-  return render(<SingleInputDateField {...defaultProps} {...props} />);
+  return {
+    user: userEvent.setup(),
+    ...render(<SingleInputDateField {...defaultProps} {...props} />),
+  };
 }
 
 function getInput() {
@@ -47,24 +50,24 @@ describe('SingleInputDateField', function () {
     expect(screen.queryByRole('button')).not.toBeInTheDocument();
   });
 
-  it('masks in label', function () {
-    const { container } = renderField({ value: '11-01' });
-    userEvent.click(getInput());
+  it('masks in label', async function () {
+    const { container, user } = renderField({ value: '11-01' });
+    await user.click(getInput());
     expect(container.querySelector('.ds-c-label-mask').textContent).toContain('11/01/YYYY');
   });
 
-  it('calls onChange when input changes', function () {
+  it('calls onChange when input changes', async function () {
     const onChange = jest.fn();
-    renderField({ onChange });
-    userEvent.type(getInput(), '1');
+    const { user } = renderField({ onChange });
+    await user.type(getInput(), '1');
     expect(onChange).toHaveBeenCalledWith('1', '01');
   });
 
-  it('calls onChange when input loses focus', function () {
+  it('calls onChange when input loses focus', async function () {
     const onChange = jest.fn();
-    renderField({ onChange, value: '01-02-2000' });
-    userEvent.click(getInput());
-    userEvent.tab();
+    const { user } = renderField({ onChange, value: '01-02-2000' });
+    await user.click(getInput());
+    await user.tab();
     expect(onChange).toHaveBeenCalledWith('01/02/2000', '01/02/2000');
   });
 
@@ -81,8 +84,8 @@ describe('SingleInputDateField', function () {
       return renderField({ ...defaultPickerProps, ...props });
     }
 
-    it('renders with picker', () => {
-      const { container } = renderPicker();
+    it('renders with picker', async () => {
+      const { container, user } = renderPicker();
 
       const label = container.querySelector('.ds-c-label');
       const hint = container.querySelector('.ds-c-hint');
@@ -104,7 +107,7 @@ describe('SingleInputDateField', function () {
       expect(button.firstElementChild.tagName).toBe('svg');
       expect(button.firstElementChild.classList).toContain('ds-c-icon--calendar');
 
-      userEvent.click(button);
+      await user.click(button);
       expect(screen.getByRole('dialog')).toBeInTheDocument();
     });
 
