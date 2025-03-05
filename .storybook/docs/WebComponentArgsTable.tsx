db97a80e6f2009453d6697de7beea2021b70cb9b
jack-ryan-nava-pbc <jackryan@navapbc.com>
2025-03-05 17:06:08 -0500

    Add new table support for subcomponents

--- .storybook/docs/WebComponentArgsTable.tsx
+++ .storybook/docs/WebComponentArgsTable.tsx
@@ -1,4 +1,5 @@
 import { Markdown, useOf } from '@storybook/blocks';
+import { useState } from 'react';
 
 function optToCodeBlock(opt: undefined | string) {
   const formattedOpt = opt === undefined ? 'undefined' : `"${opt}"`;
@@ -29,13 +30,22 @@ function getTypeLabel(argType: any) {
   }
 }
 
-/**
- * A table documenting a web component's supported attributes
- */
-export const WebComponentArgsTable = ({ of }) => {
-  const resolvedOf = useOf(of || 'story', ['story', 'meta']);
-  const argTypes = resolvedOf.type === 'story' ? resolvedOf.story.argTypes : {};
+// This interface was previously inferred by Typescript when the argTypes variable was assigned,
+// but by extracting the generateTable function out into a separate function we needed to expliclty define
+// that type since it cannot be inferred when the function is defined.
+interface ArgType {
+  table?: {
+    disable?: boolean;
+  };
+  controlsOnly?: boolean;
+  description?: string;
+  control?: {
+    type?: string;
+  };
+  options?: string[];
+}
 
+function generateTable(args: Record<string, ArgType>): React.ReactElement {
   return (
     <table className="docblock-argstable">
       <thead className="docblock-argstable-head">
@@ -52,7 +62,7 @@ export const WebComponentArgsTable = ({ of }) => {
         </tr>
       </thead>
       <tbody className="docblock-argstable-body">
-        {Object.entries(argTypes)
+        {Object.entries(args)
           .filter(([_key, argType]) => !argType.table?.disable && !argType.controlsOnly)
           .map(([key, argType]) => (
             <tr>
@@ -76,4 +86,61 @@ export const WebComponentArgsTable = ({ of }) => {
       </tbody>
     </table>
   );
+}
+
+/**
+ * A table documenting a web component's supported attributes
+ */
+export const WebComponentArgsTable = ({ of }) => {
+  const resolvedOf = useOf(of || 'story', ['story', 'meta']);
+  const argTypes = resolvedOf.type === 'story' ? resolvedOf.story.argTypes : {};
+  const subcomponents = resolvedOf.type === 'story' ? resolvedOf.story.subcomponents : undefined;
+  const title = resolvedOf.type === 'story' ? resolvedOf.story.title.split('/')[1] : '';
+  const [currentTab, setCurrentTab] = useState(title);
+
+  const handleTabClick = (tab) => {
+    setCurrentTab(tab);
+  };
+
+  if (subcomponents) {
+    return (
+      <>
+        <div className="sb-tab-container">
+          <div role="tablist" style={{ whiteSpace: 'normal' }}>
+            <button
+              onClick={() => handleTabClick(title)}
+              type="button"
+              role="tab"
+              className={currentTab === title ? 'sb-tabbutton sb-tabbutton__active' : 'tabbutton'}
+            >
+              {title}
+            </button>
+            {Object.keys(subcomponents).map((key) => {
+              return (
+                <button
+                  onClick={() => handleTabClick(key)}
+                  type="button"
+                  role="tab"
+                  className={currentTab === key ? 'sb-tabbutton sb-tabbutton__active' : 'tabbutton'}
+                >
+                  {key}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          {currentTab === title && <div>{generateTable(argTypes)}</div>}
+          {Object.keys(subcomponents).map((key) => {
+            return (
+              currentTab === key && (
+                <div key={key}>{generateTable(subcomponents[key] as Record<string, ArgType>)}</div>
+              )
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+  return generateTable(argTypes);
 };
