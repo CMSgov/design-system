0d5250899343f28f4d2a06dc22b61e86ad655f26
Jack Ryan <jackryan@navapbc.com>
2025-03-06 15:12:08 -0500

    [NO-TICKET] Add subcomponents table for web components (#3463)
    * Add new table support for subcomponents
    
    * Just use our built in tabs and tab panels
    
    * Fix missing accepted values
    
    * Small changes

--- .storybook/docs/WebComponentArgsTable.tsx
+++ .storybook/docs/WebComponentArgsTable.tsx
@@ -1,4 +1,5 @@
 import { Markdown, useOf } from '@storybook/blocks';
+import { Tabs, TabPanel } from '../../packages/design-system/src/components';
 
 function optToCodeBlock(opt: undefined | string) {
   const formattedOpt = opt === undefined ? 'undefined' : `"${opt}"`;
@@ -6,7 +7,7 @@ function optToCodeBlock(opt: undefined | string) {
 }
 
 function getTypeLabel(argType: any) {
-  const controlType = argType.control?.type;
+  const controlType = argType.control?.type ?? argType.control;
   if (controlType) {
     switch (controlType) {
       case 'text':
@@ -29,13 +30,24 @@ function getTypeLabel(argType: any) {
   }
 }
 
-/**
- * A table documenting a web component's supported attributes
- */
-export const WebComponentArgsTable = ({ of }) => {
-  const resolvedOf = useOf(of || 'story', ['story', 'meta']);
-  const argTypes = resolvedOf.type === 'story' ? resolvedOf.story.argTypes : {};
+// This interface was previously inferred by Typescript when the argTypes variable was assigned,
+// but by extracting the generateTable function out into a separate function we needed to explicitly define
+// that type since it cannot be inferred when the function is defined.
+interface ArgType {
+  table?: {
+    disable?: boolean;
+  };
+  controlsOnly?: boolean;
+  description?: string;
+  control?:
+    | {
+        type?: string;
+      }
+    | string;
+  options?: string[];
+}
 
+function generateTable(args: Record<string, ArgType>): React.ReactElement {
   return (
     <table className="docblock-argstable">
       <thead className="docblock-argstable-head">
@@ -52,7 +64,7 @@ export const WebComponentArgsTable = ({ of }) => {
         </tr>
       </thead>
       <tbody className="docblock-argstable-body">
-        {Object.entries(argTypes)
+        {Object.entries(args)
           .filter(([_key, argType]) => !argType.table?.disable && !argType.controlsOnly)
           .map(([key, argType]) => (
             <tr>
@@ -76,4 +88,32 @@ export const WebComponentArgsTable = ({ of }) => {
       </tbody>
     </table>
   );
+}
+
+/**
+ * A table documenting a web component's supported attributes
+ */
+export const WebComponentArgsTable = ({ of }) => {
+  const resolvedOf = useOf(of || 'story', ['story', 'meta']);
+  const argTypes = resolvedOf.type === 'story' ? resolvedOf.story.argTypes : {};
+  const subcomponents = resolvedOf.type === 'story' ? resolvedOf.story.subcomponents : undefined;
+  const title = resolvedOf.type === 'story' ? resolvedOf.story.title.split('/')[1] : '';
+
+  if (subcomponents) {
+    return (
+      <Tabs>
+        <TabPanel key={`${title}`} id={`${title}_id`} tab={title}>
+          {generateTable(argTypes)}
+        </TabPanel>
+        {Object.keys(subcomponents).map((key) => {
+          return (
+            <TabPanel key={key} id={`${key}`} tab={key}>
+              {generateTable(subcomponents[key] as Record<string, ArgType>)}
+            </TabPanel>
+          );
+        })}
+      </Tabs>
+    );
+  }
+  return generateTable(argTypes);
 };
