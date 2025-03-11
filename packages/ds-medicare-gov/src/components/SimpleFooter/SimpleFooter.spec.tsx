fbf14388d3e17037c322eb1468f1ff59a0d83e8e
Tamara deMent (Corbalt) <tamara@corbalt.com>
2025-03-11 09:57:43 -0400

    [WNMGDS-3138] Make Medicare URLs language-aware in `SimpleFooter` (#3466)
    Adds baseUrl logic based on the language prop & adds unit tests to verify.

--- packages/ds-medicare-gov/src/components/SimpleFooter/SimpleFooter.spec.tsx
+++ packages/ds-medicare-gov/src/components/SimpleFooter/SimpleFooter.spec.tsx
@@ -1,10 +1,60 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import SimpleFooter from './SimpleFooter';
 
-afterEach(cleanup);
+describe('SimpleFooter', () => {
+  it('renders without crashing', () => {
+    render(<SimpleFooter aboutMedicareLabel="About SimpleFooter" />);
 
-it('renders without crashing', () => {
-  render(<SimpleFooter aboutMedicareLabel="About SimpleFooter" />);
+    expect(screen.getByText(/about SimpleFooter/i)).toBeInTheDocument();
+  });
 
-  expect(screen.getByText(/about SimpleFooter/i)).toBeInTheDocument();
+  it('uses Spanish Medicare URLs when language="es"', () => {
+    render(<SimpleFooter language="es" aboutMedicareLabel="About Medicare" />);
+
+    expect(screen.getByText('About Medicare')).toHaveAttribute(
+      'href',
+      'https://es.medicare.gov/about-us'
+    );
+    expect(screen.getByRole('link', { name: /Accessibility/i })).toHaveAttribute(
+      'href',
+      'https://es.medicare.gov/about-us/accessibility-nondiscrimination-notice'
+    );
+    expect(screen.getByRole('link', { name: /Privacy Policy/i })).toHaveAttribute(
+      'href',
+      'https://es.medicare.gov/privacy-policy'
+    );
+    expect(screen.getByRole('link', { name: /Using This Site/i })).toHaveAttribute(
+      'href',
+      'https://es.medicare.gov/about-us/using-this-site'
+    );
+    expect(screen.getByRole('link', { name: /Plain Writing/i })).toHaveAttribute(
+      'href',
+      'https://es.medicare.gov/about-us/plain-writing'
+    );
+  });
+
+  it('uses English Medicare URLs when language="en" (default)', () => {
+    render(<SimpleFooter language="en" aboutMedicareLabel="About Medicare" />);
+
+    expect(screen.getByText('About Medicare')).toHaveAttribute(
+      'href',
+      'https://www.medicare.gov/about-us'
+    );
+    expect(screen.getByRole('link', { name: /Accessibility/i })).toHaveAttribute(
+      'href',
+      'https://www.medicare.gov/about-us/accessibility-nondiscrimination-notice'
+    );
+    expect(screen.getByRole('link', { name: /Privacy Policy/i })).toHaveAttribute(
+      'href',
+      'https://www.medicare.gov/privacy-policy'
+    );
+    expect(screen.getByRole('link', { name: /Using This Site/i })).toHaveAttribute(
+      'href',
+      'https://www.medicare.gov/about-us/using-this-site'
+    );
+    expect(screen.getByRole('link', { name: /Plain Writing/i })).toHaveAttribute(
+      'href',
+      'https://www.medicare.gov/about-us/plain-writing'
+    );
+  });
 });
