b3d844972a2db2a450b29b742984ac6f89109d49
tamara-corbalt <tamara@corbalt.com>
2025-03-17 16:31:41 -0400

    Adds a test to verify aria-hidden applied to month picker labels & updates snapshot

--- packages/design-system/src/components/MonthPicker/MonthPicker.test.tsx
+++ packages/design-system/src/components/MonthPicker/MonthPicker.test.tsx
@@ -98,6 +98,18 @@ describe('MonthPicker', () => {
     });
   });
 
+  it('hides labels from screen readers with aria-hidden', () => {
+    renderMonthPicker();
+
+    const labels = screen.getAllByText((content, element) => {
+      return element?.tagName.toLowerCase() === 'label';
+    });
+
+    labels.forEach((label) => {
+      expect(label).toHaveAttribute('aria-hidden', 'true');
+    });
+  });
+
   it('calls `onChange` and maintains state when checking/unchecking checkboxes', () => {
     const onChange = jest.fn();
     renderMonthPicker({ onChange });
