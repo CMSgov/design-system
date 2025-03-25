679c291b6716a44cde1a7e0bcc3cf7dcb556d2ab
tamara-corbalt <tamara@corbalt.com>
2025-03-25 11:10:53 -0400

    Adds backdropClickExits to argTypes

--- packages/design-system/src/components/web-components/ds-drawer/ds-drawer.stories.tsx
+++ packages/design-system/src/components/web-components/ds-drawer/ds-drawer.stories.tsx
@@ -31,6 +31,11 @@ const meta: Meta = {
     },
   },
   argTypes: {
+    'backdrop-click-exits': {
+      description:
+        'Pass `true` to have the dialog close when its backdrop pseudo-element is clicked. \n\n**Note:** This only works when `hasFocusTrap` is also set to `"true"`, since that causesthe dialog to be opened with `showModal()` which enables the backdrop.',
+      control: 'boolean',
+    },
     'close-button-aria-label': {
       description: 'Gives more context to screen readers on the Drawer close button.',
       control: 'text',
