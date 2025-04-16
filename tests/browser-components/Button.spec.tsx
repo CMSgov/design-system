a7ee6a4b0741e4e87891fc2abc7cd1ad00b72ceb
Kim Niedermaier <kimniedermaier+cmsds@navapbc.com>
2025-04-16 15:26:19 -0700

    Adds more test examples

--- tests/browser-components/Button.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/experimental-ct-react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
-import Button from './Button';
-
-test('renders button', async ({ mount }) => {
-  const component = await mount(<Button />);
-  await expect(component).toContainText('Click Me');
-});
--- tests/browser-components/Button.tsx
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
-
-const Button = () => <button>Cli ck Me</button>;
-export default Button;
