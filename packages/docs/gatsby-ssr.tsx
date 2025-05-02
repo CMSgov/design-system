04ec27deec2a1e55ce3dd9853039fdc201b2783e
jack-ryan-nava-pbc <jackryan@navapbc.com>
2025-05-02 11:51:30 -0400

    Use ref for variable storage and remove unneeded function

--- packages/docs/gatsby-ssr.tsx
+++ packages/docs/gatsby-ssr.tsx
@@ -1,9 +1,5 @@
 const proc = require('process');
 
-const makeDangerousHTML = (html) => {
-  return { __html: html };
-};
-
 const HeadComponentsJs = (content) => (
   <script type="text/javascript" dangerouslySetInnerHTML={content}></script>
 );
@@ -11,14 +7,14 @@ const HeadComponentsJs = (content) => (
 exports.onRenderBody = ({ setPreBodyComponents }) => {
   const env = proc.env.NODE_ENV == 'production' ? 'prod' : 'dev';
 
-  const tealiumSnippet = `
+  const dangerousTealiumSnippet = {
+    __html: `
     (function(a,b,c,d){
         a='https://tealium-tags.cms.gov/cms-design/${env}/utag.js';
         b=document;c='script';d=b.createElement(c);d.src=a;d.type='text/javascript';d.async=true;
         a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
-    })();`;
-
-  const dangerousTealiumSnippet = makeDangerousHTML(tealiumSnippet);
+    })();`,
+  };
 
   setPreBodyComponents(HeadComponentsJs(dangerousTealiumSnippet));
 };
