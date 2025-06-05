52429f0ec0a317bceb8239511d5ef21034a62684
Jack Ryan <jackryan@navapbc.com>
2025-06-05 18:36:52 -0400

    [WNMGDS-3414] Fix Textfield regex patterns (#3632)
    * Escape regexes
    
    * Update tests

--- packages/design-system/src/components/TextField/Mask.test.tsx
+++ packages/design-system/src/components/TextField/Mask.test.tsx
@@ -48,7 +48,7 @@ describe('Mask', function () {
     const input = screen.getByRole('textbox');
     expect(input.classList).toContain('ds-c-field--ssn');
     expect(input).toHaveAttribute('type', 'text');
-    expect(input).toHaveAttribute('pattern', '[0-9-*]*');
+    expect(input).toHaveAttribute('pattern', '[0-9\\-*]*');
     expect(input).toHaveAttribute('inputmode', 'numeric');
   });
 
@@ -63,7 +63,7 @@ describe('Mask', function () {
     const input = screen.getByRole('textbox');
     expect(input.classList).toContain('ds-c-field--currency');
     expect(input).toHaveAttribute('type', 'text');
-    expect(input).toHaveAttribute('pattern', '[0-9.,-]*');
+    expect(input).toHaveAttribute('pattern', '[0-9.,\\-]*');
     expect(input).toHaveAttribute('inputmode', 'numeric');
   });
 
