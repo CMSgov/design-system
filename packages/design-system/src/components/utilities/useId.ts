23c950f4d42433e1dd32192cc669db4372effe9b
tamara-corbalt <tamara@corbalt.com>
2025-07-11 15:50:14 -0400

    Update useId hook

--- packages/design-system/src/components/utilities/useId.ts
+++ packages/design-system/src/components/utilities/useId.ts
@@ -1,12 +1,6 @@
-import uniqueId from 'lodash/uniqueId';
-import { useRef } from 'react';
+import { useId as useReactId } from 'react';
 
-/**
- * Generates a unique id.
- *
- * TODO: Once we're on React 18, we can use the `useId` hook instead of rolling
- * our own with `useRef` and lodash.
- */
 export default function useId(prefix?: string, providedId?: string) {
-  return useRef(providedId ?? uniqueId(prefix)).current;
+  const reactId = useReactId();
+  return providedId ?? `${prefix ?? ''}${reactId}`;
 }
