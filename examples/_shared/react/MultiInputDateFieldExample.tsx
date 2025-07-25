cac2538dc161ab8d63095d0401fdf827d2fe5b83
tamara-corbalt <tamara@corbalt.com>
2025-07-25 14:42:10 -0400

    Initial commit

--- examples/_shared/react/MultiInputDateFieldExample.tsx
+++ examples/_shared/react/MultiInputDateFieldExample.tsx
@@ -1,16 +1,41 @@
 import { MultiInputDateField } from '@cmsgov/design-system';
+import { useState } from 'react';
 
 function MultiInputDateFieldExample() {
+  const [date, setDate] = useState({ day: '', month: '', year: '' });
+  const [touched, setTouched] = useState(false);
+  const [errors, setErrors] = useState({ year: '' });
+
+  const handleBlur = () => {
+    setTouched(true);
+    const yearNum = parseInt(date.year, 10);
+    if (!yearNum || yearNum >= new Date().getFullYear()) {
+      setErrors({ year: 'Please enter a year in the past' });
+    } else {
+      setErrors({ year: '' });
+    }
+  };
+
   return (
     <>
-      <h2>MultiInputDateField Example</h2>
+      <h2>Trigger Label Re-render Example</h2>
       <MultiInputDateField
-        label="Date of birth"
-        errorMessage="Please enter a year in the past"
-        monthDefaultValue="10"
-        dayDefaultValue="31"
-        yearDefaultValue="2050"
-        yearInvalid
+        label={touched ? <strong>Date of birth in strong</strong> : 'Date of birth'}
+        hint="For example: 10/31/1965"
+        monthLabel={touched ? <span className="dynamic-label">After</span> : 'Before'}
+        dayLabel="Day"
+        yearLabel="Year"
+        errorMessage={touched ? errors.year : ''}
+        monthValue={date.month}
+        dayValue={date.day}
+        yearValue={date.year}
+        onChange={(event, dateObject) => {
+          setDate(dateObject);
+        }}
+        onComponentBlur={handleBlur}
+        dayInvalid={!!errors.year}
+        monthInvalid={!!errors.year}
+        yearInvalid={!!errors.year}
       />
     </>
   );
