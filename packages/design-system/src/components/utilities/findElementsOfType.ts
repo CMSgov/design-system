58ae7a7466bba4cff8cc921afbe685d94e3e939f
tamara-corbalt <tamara@corbalt.com>
2025-09-30 14:18:01 -0400

    Add the comments back in

--- packages/design-system/src/components/utilities/findElementsOfType.ts
+++ packages/design-system/src/components/utilities/findElementsOfType.ts
@@ -10,19 +10,22 @@ type AllowedElements = keyof React.JSX.IntrinsicElements | CustomElements;
 
 export function findElementsOfType(types: AllowedElements[], node: ReactNode): ReactElement<any>[] {
   if (!node || !(isValidElement(node) || Array.isArray(node))) {
+    // There's nothing to recurse on, and this is not the droid we're looking for
     return [];
   }
 
   if (isValidElement(node) && types.includes(node.type as AllowedElements)) {
+    // We found it! Return an array because it will be flattened
     return [node];
   }
 
   if (Array.isArray(node)) {
+    // Recurse on each member of the array and flatten the result
     return node.reduce(
       (acc: ReactElement<any>[], child: ReactNode) => [...acc, ...findElementsOfType(types, child)],
       []
     );
   }
-
+  // It's a React element, so recurse on its children (a ReactNode)
   return findElementsOfType(types, (node as ReactElement<any>).props?.children);
 }
