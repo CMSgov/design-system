78c65cf00c2958863eba36feb74d201da0121e32
Derek Houck <derekhouck@navapbc.com>
2025-10-03 15:26:11 -0500

    Add test template

--- packages/design-system/src/components/utilities/convert-icons.test.ts
+++ packages/design-system/src/components/utilities/convert-icons.test.ts
@@ -1,6 +1,6 @@
-import { mkdir, rm } from 'node:fs/promises';
+import { mkdir, readFile, rm } from 'node:fs/promises';
 import path from 'node:path';
-import { convertIcon, convertIcons } from './convert-icons';
+import { convertIcon, convertIcons, customizeTestTemplate } from './convert-icons';
 import { existsSync } from 'node:fs';
 
 const tempDir = path.join(__dirname, 'temp');
@@ -26,13 +26,40 @@ describe('convertIcons', () => {
   });
 
   describe('convertIcon', () => {
-    it('creates a new file in the given path', async () => {
-      const iconName = 'testFile';
-      const webComponentName = 'ds-test-file';
+    const iconName = 'TestIcon';
+    const webComponentName = 'ds-test-icon';
 
+    it('creates a new file in the given path', async () => {
       await convertIcon(iconName, tempDir);
 
       expect(existsSync(`${tempDir}/${webComponentName}.tsx`)).toBe(true);
+    });
+
+    it('creates a test file next to the component file', async () => {
+      const testFileName = 'ds-test-icon.test.tsx';
+
+      await convertIcon(iconName, tempDir);
+
+      const testFile = await readFile(`${tempDir}/${testFileName}`, { encoding: 'utf8' });
+
+      const re = new RegExp(String.raw`describe\('${webComponentName}'`);
+      expect(testFile).toMatch(re);
+    });
+  });
+
+  describe('customizeTestTemplate', () => {
+    it('replaces WEB_COMPONENT_NAME with the web component name', () => {
+      const testTemplate = `
+        This a test template. WEB_COMPONENT_NAME should changed to the web
+        component name, regardless of whether is separated by spaces or if
+        it('WEB_COMPONENT_NAME').is.in.code()
+      `;
+      const webComponentName = 'ds-test-icon';
+
+      const re = new RegExp(webComponentName);
+      const customizedTemplate = customizeTestTemplate(webComponentName, testTemplate);
+      expect(customizedTemplate).toMatch(re);
+      expect(customizedTemplate).not.toMatch(/WEB_COMPONENT_NAME/);
     });
   });
 });
--- packages/design-system/src/components/utilities/convert-icons.ts
+++ packages/design-system/src/components/utilities/convert-icons.ts
@@ -1,5 +1,5 @@
 import { kebabCaseIt } from 'case-it/kebab';
-import { writeFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 import * as Icons from '../Icons';
 
@@ -9,11 +9,47 @@ const iconsPath = path.join(webComponentsPath, 'ds-icons');
 
 const icons = Object.keys(Icons);
 
-export const convertIcon = async (iconName: string, filePath: string = iconsPath) => {
-  const webComponentName = kebabCaseIt(iconName);
-  await writeFile(`${filePath}/ds-${webComponentName}.tsx`, 'Hello World');
+/**
+ * Customizes the test template for a specific web component.
+ *
+ * @param webComponentName The name of the web component being tested.
+ * @param template The test template that will be customized.
+ * @returns The customized test template.
+ */
+export const customizeTestTemplate = (webComponentName: string, template: string) => {
+  return template.replace(/WEB_COMPONENT_NAME/g, webComponentName);
 };
 
+/**
+ * Converts a React icon component into a web component.
+ *
+ * @param iconName The name of the icon being converted.
+ * @param filePath The path of the folder where the web component files should live.
+ */
+export const convertIcon = async (iconName: string, filePath: string = iconsPath) => {
+  const webComponentName = `ds-${kebabCaseIt(iconName)}`;
+
+  const testTemplate = await readFile(`${iconsPath}/templates/test-template.txt`, {
+    encoding: 'utf8',
+  });
+
+  await Promise.all([
+    // the web component
+    writeFile(`${filePath}/${webComponentName}.tsx`, 'Hello World'),
+    // test file
+    writeFile(
+      `${filePath}/${webComponentName}.test.tsx`,
+      customizeTestTemplate(webComponentName, testTemplate)
+    ),
+  ]);
+};
+
+/**
+ * Calls `convertIcon` on a list of icons.
+ *
+ * @param iconNames Names list of icons being converted.
+ * @param filePath The path of the folder where the new files should live.
+ */
 export const convertIcons = async (iconNames: string[] = icons, filePath: string = iconsPath) => {
   await Promise.all(iconNames.map((iconName) => convertIcon(iconName, filePath)));
 };
