9c8f817eabd528980defe3aaf0525c871ea97873
Derek Houck <derekhouck@navapbc.com>
2025-10-08 14:49:16 -0500

    Refactor arrow icon web component

--- packages/design-system/src/components/web-components/ds-icons/ds-arrow-icon.tsx
+++ packages/design-system/src/components/web-components/ds-icons/ds-arrow-icon.tsx
@@ -21,8 +21,8 @@ interface WrapperProps extends Omit<IconCommonProps, 'ariaHidden'> {
   direction?: ArrowIconDirectionType;
 }
 
-const Wrapper = ({ ariaHidden, direction, ...otherProps }: WrapperProps) => (
-  <ArrowIcon ariaHidden={JSON.parse(ariaHidden)} direction={direction} {...otherProps} />
+const Wrapper = ({ ariaHidden, ...otherProps }: WrapperProps) => (
+  <ArrowIcon ariaHidden={JSON.parse(ariaHidden)} {...otherProps} />
 );
 
 define('ds-arrow-icon', () => Wrapper, { attributes: arrowAttributes });
