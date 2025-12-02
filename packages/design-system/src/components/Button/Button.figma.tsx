e6901b7a5126057902e5a6056e67f6539c66e59e
Jack Ryan <jackryan@navapbc.com>
2025-12-02 16:54:05 -0500

    [CMSDS-3700] Button Figma Code Connect Snippet (#3848)
    * Initial addition of props
    
    * The smallest of changes
    
    * Add in design tickets to comments:
    
    * Remove potentially confusing onClick handler
    
    * Remove todo that is unneeded

---
+++ packages/design-system/src/components/Button/Button.figma.tsx
@@ -0,0 +1,36 @@
+import Button from './Button';
+import figma from '@figma/code-connect';
+
+figma.connect(Button, 'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=145%3A8144', {
+  props: {
+    focused: figma.boolean('Focused'),
+    // TODO: Extract disabled into a boolean as well.
+    // Design Ticket: https://jira.cms.gov/browse/CMSDS-3721?atlLinkOrigin=c2xhY2staW50ZWdyYXRpb258aXNzdWU%3D
+    // disabled: figma.enum("State", {
+    //   Disabled: true,
+    // }),
+    onDark: figma.boolean('On Dark'),
+    children: figma.string('Button Text'),
+    variation: figma.enum('Fill', {
+      Outline: undefined,
+      Solid: 'solid',
+      Ghost: 'ghost',
+    }),
+    size: figma.enum('Variation', {
+      Default: undefined,
+      Small: 'small',
+      Big: 'big',
+    }),
+    // TODO: Convert this property to a boolean within Figma.
+    // Design Ticket: https://jira.cms.gov/browse/CMSDS-3722?atlLinkOrigin=c2xhY2staW50ZWdyYXRpb258aXNzdWU%3D
+    // isAlternate: figma.enum('Color', {
+    //   "Main": "main",
+    //   "Alternate": "alternate"
+    // })
+  },
+  example: ({ children, onDark, size, variation }) => (
+    <Button onDark={onDark} size={size} variation={variation}>
+      {children}
+    </Button>
+  ),
+});
