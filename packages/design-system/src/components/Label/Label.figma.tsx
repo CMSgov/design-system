aae1b16c359808bbeb4e11ec895238af68378426
jack-ryan-nava-pbc <jackryan@navapbc.com>
2025-12-09 13:48:37 -0500

    Added necessary props and toggles for common additional components currently listed in the design file.

--- packages/design-system/src/components/Label/Label.figma.tsx
+++ packages/design-system/src/components/Label/Label.figma.tsx
@@ -1,15 +1,30 @@
 import Label from './Label';
+import { Hint } from '../Hint';
+import { InlineError } from '../InlineError';
 import figma from '@figma/code-connect';
 
 figma.connect(Label, 'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=18331%3A6061', {
   props: {
-    // These props were automatically mapped based on your linked code:
     inversed: figma.boolean('Inversed'),
-    // No matching props could be found for these Figma properties:
-    // "hasError": figma.boolean('Has error'),
-    // "hasLink": figma.boolean('Has link'),
-    // "hasHint": figma.boolean('Has hint'),
-    // "medicareCaps": figma.boolean('Medicare Caps')
+    children: figma.string('Heading text'),
+    // FYI: I'm using these props as toggles to add in separate Hint and InlineError
+    // components. This is how we would compose a form label with a hint/inline error
+    // and we want consumers of this code (AI Agents) to have the proper composition available.
+    hasError: figma.boolean('Has error'),
+    hasLink: figma.boolean('Has link'),
+    hasHint: figma.boolean('Has hint'),
   },
-  example: (props) => <Label inversed={props.inversed} />,
+  example: ({ children, hasError, hasHint, inversed }) => {
+    return (
+      <>
+        <Label inversed={inversed}>{children}</Label>
+        {hasHint ? (
+          <Hint inversed={inversed} id="test-id">
+            This is the hint text.
+          </Hint>
+        ) : null}
+        {hasError ? <InlineError inversed={inversed}>This is the error text.</InlineError> : null}
+      </>
+    );
+  },
 });
