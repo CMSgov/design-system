3b613c881f5ea06edcc49cbe5218c795630dd99d
jack-ryan-nava-pbc <jackryan@navapbc.com>
2025-12-11 11:18:16 -0500

    Added error placement prop and notes for design todos

--- packages/design-system/src/components/TextField/TextField.figma.tsx
+++ packages/design-system/src/components/TextField/TextField.figma.tsx
@@ -6,15 +6,19 @@ figma.connect(
   'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=232%3A65622',
   {
     props: {
-      // Changing this state in Figma does not alter the designs?
+      // TODO: Changing this state in Figma does not alter the designs.
       disabled: figma.enum('State', {
         Disabled: true,
       }),
       errorMessage: figma.enum('State', {
         Error: 'This is the error message.',
       }),
+      errorPlacement: figma.boolean('Has error bottom', {
+        true: 'bottom',
+        false: 'top',
+      }),
       // TODO: Add a number value for the "rows" optional prop to increase the
-      // number of lines that the text field occupies.
+      // number of lines that the text field occupies. Design ticket: https://jira.cms.gov/browse/CMSDS-3765
       multiline: figma.boolean('Multiline'),
       value: figma.string('Input Text'),
       inversed: figma.boolean('On dark'),
@@ -24,13 +28,16 @@ figma.connect(
         Large: undefined,
       }),
     },
-    example: ({ disabled, inversed, multiline, size, value }) => (
+    example: ({ disabled, errorMessage, errorPlacement, inversed, multiline, size, value }) => (
       <TextField
         disabled={disabled}
+        errorMessage={errorMessage}
+        errorPlacement={errorPlacement}
         inversed={inversed}
         multiline={multiline}
         size={size}
         name={'Required name of the TextField'}
+        // TODO: Label is required, it should not exist as a boolean toggle in the Figma Component
         label={'Required label of the TextField'}
         value={value}
       />
