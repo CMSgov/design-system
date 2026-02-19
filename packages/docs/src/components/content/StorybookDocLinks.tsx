ceaf18b7da11ed0f8cdfec65cda4495c23879913
Tamara deMent (Corbalt) <tamara@corbalt.com>
2026-02-19 15:52:13 -0500

    [CMSDS-3668] Removes React's legacy `Children` API from `StorybookDocLinks` (#3923)
    Refactor StorybookDocLinks without Children API

--- packages/docs/src/components/content/StorybookDocLinks.tsx
+++ packages/docs/src/components/content/StorybookDocLinks.tsx
@@ -1,15 +1,14 @@
-import { Children } from 'react';
 import type * as React from 'react';
 import { humanizeList } from '../../helpers/humanizeList';
 
 interface StorybookDocLinksProps {
-  children: React.ReactElement<any>;
+  children: React.ReactElement | React.ReactElement[];
   theme: string;
   tech?: 'react' | 'wc';
 }
 
 const StorybookDocLinks = ({ children, tech = 'react' }: StorybookDocLinksProps) => {
-  const links = Children.toArray(children);
+  const links = Array.isArray(children) ? children : [children];
   const formattedLinks = humanizeList(links, { conjunction: 'and' });
 
   return (
