36222cc0de272d3f65647d22a0ea4678b57a48a2
tamara-corbalt <tamara@corbalt.com>
2026-03-17 14:49:08 -0400

    Remove unnecessary afterEach hook

--- packages/ds-medicare-gov/src/components/Icons/icons.test.tsx
+++ packages/ds-medicare-gov/src/components/Icons/icons.test.tsx
@@ -2,35 +2,34 @@ import { render, screen } from '@testing-library/react';
 import { setLanguage } from '@cmsgov/design-system';
 import { AbuseIcon, CostsIcon } from './index';
 
-describe('Icons', () => {
-  afterEach(() => {
-    setLanguage('en');
-  });
+describe('Medicare Icons', () => {
+  describe('translations', () => {
+    it('renders English titles when language is set to en', () => {
+      setLanguage('en');
 
-  it('renders translated titles from core in English', () => {
-    setLanguage('en');
+      render(
+        <>
+          <AbuseIcon ariaHidden={false} />
+          <CostsIcon ariaHidden={false} />
+        </>
+      );
 
-    render(
-      <>
-        <AbuseIcon ariaHidden={false} />
-        <CostsIcon ariaHidden={false} />
-      </>
-    );
+      expect(screen.getByTitle('Abuse')).toBeInTheDocument();
+      expect(screen.getByTitle('Costs')).toBeInTheDocument();
+    });
 
-    expect(screen.getByTitle('Abuse')).toBeInTheDocument();
-    expect(screen.getByTitle('Costs')).toBeInTheDocument();
-  });
+    it('renders Spanish titles when language is set to es', () => {
+      setLanguage('es');
 
-  it('renders translated titles from core in Spanish', () => {
-    setLanguage('es');
+      render(
+        <>
+          <AbuseIcon ariaHidden={false} />
+          <CostsIcon ariaHidden={false} />
+        </>
+      );
 
-    render(
-      <>
-        <AbuseIcon ariaHidden={false} />
-        <CostsIcon ariaHidden={false} />
-      </>
-    );
-    expect(screen.getByTitle('Abuso')).toBeInTheDocument();
-    expect(screen.getByTitle('Costos')).toBeInTheDocument();
+      expect(screen.getByTitle('Abuso')).toBeInTheDocument();
+      expect(screen.getByTitle('Costos')).toBeInTheDocument();
+    });
   });
 });
