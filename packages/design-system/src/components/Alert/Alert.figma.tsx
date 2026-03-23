fce4e022ba4e56fafd6a157148604a2adf3cee7c
jack-ryan-nava-pbc <jackryan@navapbc.com>
2026-03-23 13:27:32 -0400

    Point to correct branch

--- packages/design-system/src/components/Alert/Alert.figma.tsx
+++ packages/design-system/src/components/Alert/Alert.figma.tsx
@@ -1,41 +1,35 @@
 import Alert from './Alert';
 import figma from '@figma/code-connect';
 
-// Original branch
-// "https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=56%3A4727"
-figma.connect(
-  Alert,
-  'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/branch/EY3W2duP5LJfOrHYdEU0ps/Design-System-Library?m=auto&node-id=56-4727&t=NzUAuvg76hM0oNJF-1',
-  {
-    props: {
-      children: figma.boolean('Has body text', {
-        true: figma.string('Alert body text'),
-        false: undefined,
-      }),
-      heading: figma.boolean('Has title', {
-        true: figma.string('Alert Title'),
-        false: undefined,
-      }),
-      // This is goofy because the wording in Figma is reversed from the prop.
-      hideIcon: figma.boolean('Has icon', {
-        true: undefined,
-        false: true,
-      }),
-      variation: figma.enum('Alert Type', {
-        Info: undefined,
-        Success: 'success',
-        Warning: 'warn',
-        Error: 'error',
-      }),
-      weight: figma.boolean('Lightweight', {
-        true: 'lightweight',
-        false: undefined,
-      }),
-    },
-    example: ({ children, heading, hideIcon, variation, weight }) => (
-      <Alert heading={heading} hideIcon={hideIcon} variation={variation} weight={weight}>
-        {children}
-      </Alert>
-    ),
-  }
-);
+figma.connect(Alert, 'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=56%3A4727', {
+  props: {
+    children: figma.boolean('Has body text', {
+      true: figma.string('Alert body text'),
+      false: undefined,
+    }),
+    heading: figma.boolean('Has title', {
+      true: figma.string('Alert Title'),
+      false: undefined,
+    }),
+    // This is goofy because the wording in Figma is reversed from the prop.
+    hideIcon: figma.boolean('Has icon', {
+      true: undefined,
+      false: true,
+    }),
+    variation: figma.enum('Alert Type', {
+      Info: undefined,
+      Success: 'success',
+      Warning: 'warn',
+      Error: 'error',
+    }),
+    weight: figma.boolean('Lightweight', {
+      true: 'lightweight',
+      false: undefined,
+    }),
+  },
+  example: ({ children, heading, hideIcon, variation, weight }) => (
+    <Alert heading={heading} hideIcon={hideIcon} variation={variation} weight={weight}>
+      {children}
+    </Alert>
+  ),
+});
