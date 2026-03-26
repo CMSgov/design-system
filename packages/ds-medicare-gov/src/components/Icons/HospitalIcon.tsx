3bdc89fb2479cbccac29e4bb4b891253ac2cb257
Kamille <25205945+kamiTT@users.noreply.github.com>
2026-03-26 15:28:28 -0400

    update snapshots + added translations

--- packages/ds-medicare-gov/src/components/Icons/HospitalIcon.tsx
+++ packages/ds-medicare-gov/src/components/Icons/HospitalIcon.tsx
@@ -1,10 +1,9 @@
 import type * as React from 'react';
-import { SvgIcon } from '@cmsgov/design-system';
+import { SvgIcon, t } from '@cmsgov/design-system';
 import { IconCommonProps } from '@cmsgov/design-system';
 
 const defaultProps = {
   className: '',
-  title: 'Hospital',
   viewBox: '0 0 32 32',
 };
 
@@ -13,12 +12,12 @@ function HospitalIcon(props: IconCommonProps): React.ReactElement {
 
   return (
     <span className="icon-wrapper">
-      <SvgIcon {...defaultProps} {...props} className={iconCssClasses}>
+      <SvgIcon title={t('icons.hospital')} {...defaultProps} {...props} className={iconCssClasses}>
         <path
           fillRule="evenodd"
           clipRule="evenodd"
-          d="M31.573 1.606C31.573.719 30.855 0 29.97 0H1.604C.718 0 0 .719 0 1.606v28.362c0 .887.718 1.605 1.604 1.605H29.97c.886 0 1.604-.718 1.604-1.605zM7.316 6.16h5.036v6.93h6.869V6.16h5.037v19.252H19.22v-7.701h-6.869v7.7H7.317z"
-          fill="#1e3c70"
+          d="M31.5733 1.60562C31.5733 0.718871 30.8552 0 29.9693 0H1.60408C0.718101 0 0 0.718871 0 1.60562V29.9677C0 30.8545 0.718101 31.5733 1.60408 31.5733H29.9693C30.8552 31.5733 31.5733 30.8545 31.5733 29.9677V1.60562ZM7.31577 6.16065H12.3525V13.0914H19.2208V6.16065H24.2576V25.4127H19.2208V17.7119H12.3525V25.4127H7.31577V6.16065Z"
+          fill="#1E3C70"
         />
       </SvgIcon>
     </span>
