d6ec680282491c36d7436e605b15401c528de981
jack-ryan-nava-pbc <jackryan@navapbc.com>
2026-03-27 11:00:55 -0400

    Making things more obvious

--- packages/design-system/src/components/FilterChip/FilterChip.figma.tsx
+++ packages/design-system/src/components/FilterChip/FilterChip.figma.tsx
@@ -1,11 +1,9 @@
 import FilterChip from './FilterChip';
 import figma from '@figma/code-connect';
 
-// Testing Branch URL: https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/branch/jvOasW4uIxbZWSWoJk5Bup/Design-System-Library?m=dev&node-id=207-42776
-// Main Branch URL: "https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=207%3A42776"
 figma.connect(
   FilterChip,
-  'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/branch/jvOasW4uIxbZWSWoJk5Bup/Design-System-Library?m=dev&node-id=207-42776',
+  'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=207%3A42776',
   {
     props: {
       label: figma.string('Text'),
@@ -13,7 +11,7 @@ figma.connect(
         Default: undefined,
         Big: 'big',
       }),
-      onDelete: () => 'deleted',
+      onDelete: () => 'REPLACE ME WITH A CALLBACK FUNCTION',
     },
     example: ({ label, onDelete, size }) => (
       <FilterChip label={label} onDelete={onDelete} size={size} />
