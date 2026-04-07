dcd71527ec05203421f16cc770cd03e1660d7c72
jack-ryan-nava-pbc <jackryan@navapbc.com>
2026-04-07 16:12:51 -0400

    Swapping branches before publish

--- packages/ds-medicare-gov/src/components/SimpleFooter/SimpleFooter.figma.tsx
+++ packages/ds-medicare-gov/src/components/SimpleFooter/SimpleFooter.figma.tsx
@@ -1,9 +1,11 @@
 import SimpleFooter from './SimpleFooter';
 import figma from '@figma/code-connect';
 
+// Test branch: https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/branch/m5vQUe1j796fuQ64nLwEei/Design-System-Library?m=auto&node-id=392-13684&t=ogIvYBNIgMomfwIp-1
+// https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=392%3A13684
 figma.connect(
   SimpleFooter,
-  'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/?node-id=392%3A13684',
+  'https://www.figma.com/design/OYkYP4pC9jwS7j2qafwmiv/branch/m5vQUe1j796fuQ64nLwEei/Design-System-Library?m=auto&node-id=392-13684&t=ogIvYBNIgMomfwIp-1',
   {
     props: {},
     example: (props) => <SimpleFooter {...props} />,
