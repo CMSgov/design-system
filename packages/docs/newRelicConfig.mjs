997f76a2e97c802b72751339f3221e48b73bd2f0
jack-ryan-nava-pbc <jackryan@navapbc.com>
2026-04-24 17:11:25 -0400

    Simplify

--- packages/docs/newRelicConfig.mjs
+++ packages/docs/newRelicConfig.mjs
@@ -1,27 +1,18 @@
 export default function generateNewRelicPlugin() {
+    // Default to 'off' so we don't initialize New Relic at all on localhost
   const NEW_RELIC_MODE = process.env.NEW_RELIC_MODE || 'off';
 
-  const NEW_RELIC_BASE_CONFIG = {
+  const NEW_RELIC_CONFIG = {
     instrumentationType: 'proAndSPA',
     trustKey: '39033',
-    licenseKey: 'NRJS-d12a0b7909b564e0959',
     beacon: 'gov-bam.nr-data.net',
     errorBeacon: 'gov-bam.nr-data.net',
-  };
-
-  const NEW_RELIC_CONFIG_BY_MODE = {
-    dev: {
-      ...NEW_RELIC_BASE_CONFIG,
-      accountId: process.env.NEW_RELIC_ACCOUNT_ID_DEV,
-      agentID: process.env.NEW_RELIC_AGENT_ID_DEV,
-      applicationID: process.env.NEW_RELIC_AGENT_ID_DEV,
-    },
-    prod: {
-      ...NEW_RELIC_BASE_CONFIG,
-      accountId: process.env.NEW_RELIC_ACCOUNT_ID_PROD || '6704482',
-      agentID: process.env.NEW_RELIC_AGENT_ID_PROD || '1134604697',
-      applicationID: process.env.NEW_RELIC_AGENT_ID_PROD || '1134604697',
-    },
+    // These values change between the Dev and Prod instances of New Relic.
+    licenseKey: process.env.NEW_RELIC_LICENSE_KEY,
+    accountId: process.env.NEW_RELIC_ACCOUNT_ID,
+    agentID: process.env.NEW_RELIC_AGENT_ID,
+    // Application ID and Agent ID appear to be the same on the New Relic side.
+    applicationID: process.env.NEW_RELIC_AGENT_ID,
   };
 
   if (!['off', 'dev', 'prod'].includes(NEW_RELIC_MODE)) {
@@ -36,8 +27,7 @@ export default function generateNewRelicPlugin() {
       : {
           resolve: 'gatsby-plugin-newrelic',
           options: {
-            // @ts-ignore
-            config: NEW_RELIC_CONFIG_BY_MODE[NEW_RELIC_MODE],
+            config: NEW_RELIC_CONFIG,
           },
         };
 
