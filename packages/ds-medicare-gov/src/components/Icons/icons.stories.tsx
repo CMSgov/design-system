0fbfba33503ee5077f61bb634f7a04190efa23c3
Jack Ryan <jackryan@navapbc.com>
2026-08-07 10:06:42 -0400

    [CMSDS-4196] Add Bell Icon (#4182)
    * Add bell icon and all the necessary stuff
    
    * Updated VRTs
    
    * Update VRTs

--- packages/ds-medicare-gov/src/components/Icons/icons.stories.tsx
+++ packages/ds-medicare-gov/src/components/Icons/icons.stories.tsx
@@ -2,6 +2,7 @@ import {
   AboutIcon,
   AbuseIcon,
   AppleIcon,
+  BellIcon,
   BinocularsIcon,
   BirthingFriendlyIcon,
   ChecklistIcon,
@@ -62,6 +63,11 @@ const iconData = [
     defaultTitle: 'Apple',
     component: <AppleIcon />,
     name: 'AppleIcon',
+  },
+  {
+    defaultTitle: 'Bell',
+    component: <BellIcon />,
+    name: 'BellIcon',
   },
   {
     defaultTitle: 'Binoculars',
