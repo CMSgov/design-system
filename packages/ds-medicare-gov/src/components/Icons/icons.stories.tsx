a48e818fec850af957e4809114e40d7880f77f4c
jack-ryan-nava-pbc <jackryan@navapbc.com>
2026-08-10 11:40:24 -0400

    Add initial icon and translations

--- packages/ds-medicare-gov/src/components/Icons/icons.stories.tsx
+++ packages/ds-medicare-gov/src/components/Icons/icons.stories.tsx
@@ -1,6 +1,7 @@
 import {
   AboutIcon,
   AbuseIcon,
+  AlarmIcon,
   AppleIcon,
   BellIcon,
   BinocularsIcon,
@@ -60,6 +61,11 @@ const iconData = [
     defaultTitle: 'Abuse',
     component: <AbuseIcon />,
     name: 'AbuseIcon',
+  },
+  {
+    defaultTitle: 'Alarm',
+    component: <AlarmIcon />,
+    name: 'AlarmIcon',
   },
   {
     defaultTitle: 'Apple',
--- packages/ds-medicare-gov/src/components/Icons/index.ts
+++ packages/ds-medicare-gov/src/components/Icons/index.ts
@@ -1,5 +1,6 @@
 export { default as AboutIcon } from './AboutIcon';
 export { default as AbuseIcon } from './AbuseIcon';
+export { default as AlarmIcon } from './AlarmIcon';
 export { default as AppleIcon } from './AppleIcon';
 export { default as BellIcon } from './BellIcon';
 export { default as BinocularsIcon } from './BinocularsIcon';
